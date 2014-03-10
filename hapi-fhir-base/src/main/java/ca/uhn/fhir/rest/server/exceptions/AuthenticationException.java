daa5ed6ea8a4af5f4779d9c43678c0ba2320ead2
Yogthos <dmitri.sotnikov@uhn.on.ca>
2014-03-10 10:43:33 -0400

    updated authenticate method in ISecurityManager to be void and throw an AuthenticationException, updated RestfulServer to return 401 when SecurityManager is present and authenticate throws the exception

---
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/server/exceptions/AuthenticationException.java
@@ -0,0 +1,12 @@
+package ca.uhn.fhir.rest.server.exceptions;
+
+import javax.servlet.ServletException;
+
+/**
+ * Created by dsotnikov on 3/10/2014.
+ */
+public class AuthenticationException extends ServletException {
+
+    private static final long serialVersionUID = 1L;
+
+}
