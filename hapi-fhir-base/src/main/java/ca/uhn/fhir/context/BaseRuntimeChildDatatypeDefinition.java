df93b808e89e25acc7f1c3d368659b5b080781b5
t106uhn <t106uhn@UHN433292.uhn.ca>
2014-05-21 15:28:12 -0400

    my change

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/context/BaseRuntimeChildDatatypeDefinition.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/context/BaseRuntimeChildDatatypeDefinition.java
@@ -17,6 +17,8 @@ package ca.uhn.fhir.context;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * hello
  * #L%
  */
 
