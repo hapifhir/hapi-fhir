2e89fd7554d146fdf7786577d65fcdec5fd78876
t106uhn <t106uhn@UHN433292.uhn.ca>
2014-09-26 18:38:38 -0400

    fixed compile error with org-apache-commons.StringUtils

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/parser/BaseParser.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/parser/BaseParser.java
@@ -29,7 +29,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
 import ca.uhn.fhir.context.BaseRuntimeDeclaredChildDefinition;
