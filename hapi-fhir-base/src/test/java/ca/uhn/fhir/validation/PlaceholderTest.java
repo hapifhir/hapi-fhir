d2e6fc22539e418beb0a6ef876ff6456f3682c0f
James Agnew <jamesagnew@gmail.com>
2014-10-29 15:48:05 -0400

    Test updates to make Jenkins happy

---
+++ hapi-fhir-base/src/test/java/ca/uhn/fhir/validation/PlaceholderTest.java
@@ -0,0 +1,15 @@
+package ca.uhn.fhir.validation;
+
+import org.junit.Test;
+
+public class PlaceholderTest {
+
+	/**
+	 * This is just here to make Jenkins happy
+	 */
+	@Test
+	public void testPass() {
+		// nothing
+	}
+	
+}
