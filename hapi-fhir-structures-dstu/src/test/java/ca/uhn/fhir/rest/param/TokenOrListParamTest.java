f7209b45efaeca0397652833c86582cf3f4f10e0
b.debeaubien <b.debeaubien@systemsmadesimple.com>
2014-12-30 16:16:20 -0500

    #73 - added matchesToken method to BaseCodingDt and doesCodingListMatch to TokenOrListParam

---
+++ hapi-fhir-structures-dstu/src/test/java/ca/uhn/fhir/rest/param/TokenOrListParamTest.java
@@ -0,0 +1,37 @@
+package ca.uhn.fhir.rest.param;
+
+import static org.junit.Assert.*;
+
+import ca.uhn.fhir.model.dstu.composite.CodingDt;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenOrListParamTest {
+    @Test
+    public void whenParamListHasAnyMatchingCodingsForCodingList_doesCodingListMatch_shouldBeTrue() {
+        TokenOrListParam params = new TokenOrListParam();
+        params.add("http://foo.org", "53");
+        params.add("http://bar.org", "52");
+
+        List<CodingDt> codings = new ArrayList<CodingDt>();
+        codings.add(new CodingDt("http://baz.org", "53"));
+        codings.add(new CodingDt("http://bar.org", "52"));
+
+        assertTrue(params.doesCodingListMatch(codings));
+    }
+
+    @Test
+    public void whenParamListHasNoMatchingCodingsForCodingList_doesCodingListMatch_shouldBeFalse() {
+        TokenOrListParam params = new TokenOrListParam();
+        params.add("http://foo.org", "53");
+        params.add("http://bar.org", "52");
+
+        List<CodingDt> codings = new ArrayList<CodingDt>();
+        codings.add(new CodingDt("http://baz.org", "53"));
+        codings.add(new CodingDt("http://bar.org", "11"));
+
+        assertFalse(params.doesCodingListMatch(codings));
+    }
+}
