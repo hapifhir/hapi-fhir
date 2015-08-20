7645c240f491431cfe220fbebe4e71e480f8c81e
James Agnew <jamesagnew@gmail.com>
2015-08-20 15:27:24 -0400

    Get instance validator working across FHIR versions

--- hapi-fhir-structures-hl7org-dstu2/src/main/java/ca/uhn/fhir/validation/FhirInstanceValidator.java
+++ hapi-fhir-structures-hl7org-dstu2/src/main/java/ca/uhn/fhir/validation/FhirInstanceValidator.java
@@ -23,6 +23,7 @@ import org.xml.sax.InputSource;
 
 import ca.uhn.fhir.context.ConfigurationException;
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.rest.server.EncodingEnum;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 
@@ -32,104 +33,121 @@ import com.google.gson.JsonObject;
 
 public class FhirInstanceValidator extends BaseValidatorBridge implements IValidatorModule {
 
-	static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(FhirInstanceValidator.class);
+  private static FhirContext ourHl7OrgCtx;
 
-	private DocumentBuilderFactory myDocBuilderFactory;
+  static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(FhirInstanceValidator.class);
+  private DocumentBuilderFactory myDocBuilderFactory;
 
-	public FhirInstanceValidator() {
-		myDocBuilderFactory = DocumentBuilderFactory.newInstance();
-		myDocBuilderFactory.setNamespaceAware(true);
-	}
+  public FhirInstanceValidator() {
+    myDocBuilderFactory = DocumentBuilderFactory.newInstance();
+    myDocBuilderFactory.setNamespaceAware(true);
+  }
 
-	private String determineResourceName(Document theDocument) {
-		Element root = null;
+  private String determineResourceName(Document theDocument) {
+    Element root = null;
 
-		NodeList list = theDocument.getChildNodes();
-		for (int i = 0; i < list.getLength(); i++) {
-			if (list.item(i) instanceof Element) {
-				root = (Element) list.item(i);
-				break;
-			}
-		}
-		root = theDocument.getDocumentElement();
-		return root.getLocalName();
-	}
+    NodeList list = theDocument.getChildNodes();
+    for (int i = 0; i < list.getLength(); i++) {
+      if (list.item(i) instanceof Element) {
+        root = (Element) list.item(i);
+        break;
+      }
+    }
+    root = theDocument.getDocumentElement();
+    return root.getLocalName();
+  }
 
-	private StructureDefinition loadProfileOrReturnNull(List<ValidationMessage> theMessages, FhirContext theCtx, String theResourceName) {
-		if (isBlank(theResourceName)) {
-			theMessages.add(new ValidationMessage().setLevel(IssueSeverity.FATAL).setMessage("Could not determine resource type from request. Content appears invalid."));
-			return null;
-		}
+  private FhirContext getHl7OrgDstu2Ctx(FhirContext theCtx) {
+    if (theCtx.getVersion().getVersion() == FhirVersionEnum.DSTU2_HL7ORG) {
+      return theCtx;
+    }
+    FhirContext retVal = ourHl7OrgCtx;
+    if (retVal == null) {
+      retVal = FhirContext.forDstu2Hl7Org();
+      ourHl7OrgCtx = retVal;
+    }
+    return retVal;
+  }
 
-		String profileCpName = "/org/hl7/fhir/instance/model/profile/" + theResourceName.toLowerCase() + ".profile.xml";
-		String profileText;
-		try {
-			profileText = IOUtils.toString(FhirInstanceValidator.class.getResourceAsStream(profileCpName), "UTF-8");
-		} catch (IOException e1) {
-			theMessages.add(new ValidationMessage().setLevel(IssueSeverity.FATAL).setMessage("No profile found for resource type " + theResourceName));
-			return null;
-		}
-		StructureDefinition profile = theCtx.newXmlParser().parseResource(StructureDefinition.class, profileText);
-		return profile;
-	}
+  private StructureDefinition loadProfileOrReturnNull(List<ValidationMessage> theMessages, FhirContext theCtx,
+      String theResourceName) {
+    if (isBlank(theResourceName)) {
+      theMessages.add(new ValidationMessage().setLevel(IssueSeverity.FATAL)
+          .setMessage("Could not determine resource type from request. Content appears invalid."));
+      return null;
+    }
 
-	protected List<ValidationMessage> validate(FhirContext theCtx, String theInput, EncodingEnum theEncoding) {
-		WorkerContext workerContext = new WorkerContext();
-		org.hl7.fhir.instance.validation.InstanceValidator v;
-		try {
-			v = new org.hl7.fhir.instance.validation.InstanceValidator(workerContext);
-		} catch (Exception e) {
-			throw new ConfigurationException(e);
-		}
+    String profileCpName = "/org/hl7/fhir/instance/model/profile/" + theResourceName.toLowerCase() + ".profile.xml";
+    String profileText;
+    try {
+      profileText = IOUtils.toString(FhirInstanceValidator.class.getResourceAsStream(profileCpName), "UTF-8");
+    } catch (IOException e1) {
+      theMessages.add(new ValidationMessage().setLevel(IssueSeverity.FATAL)
+          .setMessage("No profile found for resource type " + theResourceName));
+      return null;
+    }
+    StructureDefinition profile = getHl7OrgDstu2Ctx(theCtx).newXmlParser().parseResource(StructureDefinition.class,
+        profileText);
+    return profile;
+  }
 
-		List<ValidationMessage> messages = new ArrayList<ValidationMessage>();
+  protected List<ValidationMessage> validate(FhirContext theCtx, String theInput, EncodingEnum theEncoding) {
+    WorkerContext workerContext = new WorkerContext();
+    org.hl7.fhir.instance.validation.InstanceValidator v;
+    try {
+      v = new org.hl7.fhir.instance.validation.InstanceValidator(workerContext);
+    } catch (Exception e) {
+      throw new ConfigurationException(e);
+    }
 
-		if (theEncoding == EncodingEnum.XML) {
-			Document document;
-			try {
-				DocumentBuilder builder = myDocBuilderFactory.newDocumentBuilder();
-				InputSource src = new InputSource(new StringReader(theInput));
-				document = builder.parse(src);
-			} catch (Exception e2) {
-				ourLog.error("Failure to parse XML input", e2);
-				ValidationMessage m = new ValidationMessage();
-				m.setLevel(IssueSeverity.FATAL);
-				m.setMessage("Failed to parse input, it does not appear to be valid XML:" + e2.getMessage());
-				return Collections.singletonList(m);
-			}
+    List<ValidationMessage> messages = new ArrayList<ValidationMessage>();
 
-			String resourceName = determineResourceName(document);
-			StructureDefinition profile = loadProfileOrReturnNull(messages, theCtx, resourceName);
-			if (profile != null) {
-				try {
-					v.validate(messages, document, profile);
-				} catch (Exception e) {
-					throw new InternalErrorException("Unexpected failure while validating resource", e);
-				}
-			}
-		} else if (theEncoding == EncodingEnum.JSON) {
-			Gson gson = new GsonBuilder().create();
-			JsonObject json = gson.fromJson(theInput, JsonObject.class);
+    if (theEncoding == EncodingEnum.XML) {
+      Document document;
+      try {
+        DocumentBuilder builder = myDocBuilderFactory.newDocumentBuilder();
+        InputSource src = new InputSource(new StringReader(theInput));
+        document = builder.parse(src);
+      } catch (Exception e2) {
+        ourLog.error("Failure to parse XML input", e2);
+        ValidationMessage m = new ValidationMessage();
+        m.setLevel(IssueSeverity.FATAL);
+        m.setMessage("Failed to parse input, it does not appear to be valid XML:" + e2.getMessage());
+        return Collections.singletonList(m);
+      }
 
-			String resourceName = json.get("resourceType").getAsString();
-			StructureDefinition profile = loadProfileOrReturnNull(messages, theCtx, resourceName);
-			if (profile != null) {
-				try {
-					v.validate(messages, json, profile);
-				} catch (Exception e) {
-					throw new InternalErrorException("Unexpected failure while validating resource", e);
-				}
-			}
-		} else {
-			throw new IllegalArgumentException("Unknown encoding: " + theEncoding);
-		}
+      String resourceName = determineResourceName(document);
+      StructureDefinition profile = loadProfileOrReturnNull(messages, theCtx, resourceName);
+      if (profile != null) {
+        try {
+          v.validate(messages, document, profile);
+        } catch (Exception e) {
+          throw new InternalErrorException("Unexpected failure while validating resource", e);
+        }
+      }
+    } else if (theEncoding == EncodingEnum.JSON) {
+      Gson gson = new GsonBuilder().create();
+      JsonObject json = gson.fromJson(theInput, JsonObject.class);
 
-		return messages;
-	}
+      String resourceName = json.get("resourceType").getAsString();
+      StructureDefinition profile = loadProfileOrReturnNull(messages, theCtx, resourceName);
+      if (profile != null) {
+        try {
+          v.validate(messages, json, profile);
+        } catch (Exception e) {
+          throw new InternalErrorException("Unexpected failure while validating resource", e);
+        }
+      }
+    } else {
+      throw new IllegalArgumentException("Unknown encoding: " + theEncoding);
+    }
 
-	@Override
-	protected List<ValidationMessage> validate(IValidationContext<?> theCtx) {
-		return validate(theCtx.getFhirContext(), theCtx.getResourceAsString(), theCtx.getResourceAsStringEncoding());
-	}
+    return messages;
+  }
+
+  @Override
+  protected List<ValidationMessage> validate(IValidationContext<?> theCtx) {
+    return validate(theCtx.getFhirContext(), theCtx.getResourceAsString(), theCtx.getResourceAsStringEncoding());
+  }
 
 }
