e7ad9a96be4b9ddecd659f3248e3a17bd4d35bac
James Agnew <jamesagnew@gmail.com>
2015-09-28 10:40:13 -0400

    Fix build

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/BaseFhirDaoTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/BaseFhirDaoTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
 import ca.uhn.fhir.model.dstu2.resource.Condition;
+import ca.uhn.fhir.model.dstu2.resource.Observation;
+import ca.uhn.fhir.model.primitive.DateTimeDt;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import ca.uhn.fhir.rest.param.ResourceParameter;
@@ -20,6 +23,14 @@ public class BaseFhirDaoTest  extends BaseJpaTest {
 		assertEquals("2011-01-01T11:12:21.0000Z", match.getLastUpdated().getLowerBound().getValueAsString());
 		assertEquals(ReferenceParam.class, match.get("patient").get(0).get(0).getClass());
 		assertEquals("304", ((ReferenceParam)match.get("patient").get(0).get(0)).getIdPart());
+		
+		Observation observation = new Observation();
+		
+		PeriodDt period = new PeriodDt();
+		period.setStart(new DateTimeDt("2011-01-02T11:22:33Z"));
+		period.setEnd(new DateTimeDt("2011-01-02T11:33:33Z"));
+		observation.setEffective(period);
+		
 	}
 	
 }
