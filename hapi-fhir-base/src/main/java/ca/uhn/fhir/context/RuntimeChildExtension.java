072ec29f13957a0bc028117ba46e8f36b27345cc
James Agnew <jamesagnew@gmail.com>
2016-04-11 10:52:27 -0400

    Fix build error

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/context/RuntimeChildExtension.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/context/RuntimeChildExtension.java
@@ -1,16 +1,11 @@
 package ca.uhn.fhir.context;
 
-import static org.hamcrest.Matchers.emptyCollectionOf;
-
 import java.lang.reflect.Field;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 import org.hl7.fhir.instance.model.api.IBase;
-import org.hl7.fhir.instance.model.api.IBaseExtension;
 
-import ca.uhn.fhir.context.BaseRuntimeElementDefinition.ChildTypeEnum;
 import ca.uhn.fhir.model.api.annotation.Child;
 import ca.uhn.fhir.model.api.annotation.Description;
 
