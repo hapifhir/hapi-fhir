fd7f9a9b98403d5ced4125113382fcee34acb4bc
James Agnew <jamesagnew@gmail.com>
2016-05-27 10:27:32 -0400

    Auto close contextx in JPA tests

---
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/z/ZContextCloserDstu2Test.java
@@ -0,0 +1,20 @@
+package ca.uhn.fhir.jpa.z;
+
+import org.junit.Test;
+import org.springframework.test.annotation.DirtiesContext;
+
+import ca.uhn.fhir.jpa.dao.dstu2.BaseJpaDstu2Test;
+
+public class ZContextCloserDstu2Test extends BaseJpaDstu2Test {
+
+	/**
+	 *  this is just here to close the context when this package's test are done
+	 */
+	@Test
+	@DirtiesContext()
+	public void testCloseContext() {
+		// nothing
+	}
+	
+	
+}
