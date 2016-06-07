7123205a837b4e470ec4d56eeb8d19db280a5813
James Agnew <jamesagnew@gmail.com>
2016-06-07 10:04:37 -0700

    Fix build

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/context/RuntimeChildDeclaredExtensionDefinition.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/context/RuntimeChildDeclaredExtensionDefinition.java
@@ -20,7 +20,6 @@ package ca.uhn.fhir.context;
  * #L%
  */
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.hamcrest.Matchers.emptyCollectionOf;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
