7f15520db8467bde7d6457c16859e114997a456b
James Agnew <jamesagnew@gmail.com>
2016-08-21 11:39:44 -0700

    Don't include Location header in server responses for DSTU3 servers

--- hapi-fhir-structures-dstu3/src/test/java/ca/uhn/fhir/rest/server/ReadDstu3Test.java
+++ hapi-fhir-structures-dstu3/src/test/java/ca/uhn/fhir/rest/server/ReadDstu3Test.java
@@ -32,46 +32,29 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.annotation.*;
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.Read;
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.MyPatientWithExtensions;
 import ca.uhn.fhir.util.PortUtil;
 import ca.uhn.fhir.util.TestUtil;
 
-public class CreateDstu3Test {
+public class ReadDstu3Test {
 	private static CloseableHttpClient ourClient;
 
 	private static FhirContext ourCtx = FhirContext.forDstu3();
-	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(CreateDstu3Test.class);
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ReadDstu3Test.class);
 	private static int ourPort;
 	private static Server ourServer;
 
-	/**
-	 * #342
-	 */
-	@Test
-	public void testCreateWithInvalidContent() throws Exception {
-
-		HttpPost httpPost = new HttpPost("http://localhost:" + ourPort + "/Patient");
-		httpPost.setEntity(new StringEntity("FOO", ContentType.parse("application/xml+fhir; charset=utf-8")));
-		HttpResponse status = ourClient.execute(httpPost);
-
-		String responseContent = IOUtils.toString(status.getEntity().getContent(), StandardCharsets.UTF_8);
-		IOUtils.closeQuietly(status.getEntity().getContent());
-
-		ourLog.info("Response was:\n{}", responseContent);
-
-		assertEquals(400, status.getStatusLine().getStatusCode());
-
-		assertThat(responseContent, containsString("<OperationOutcome xmlns=\"http://hl7.org/fhir\"><issue><severity value=\"error\"/><code value=\"processing\"/><diagnostics value=\""));
-		assertThat(responseContent, containsString("Failed to parse request body as XML resource. Error was: com.ctc.wstx.exc.WstxUnexpectedCharException: Unexpected character 'F'"));
-
-	}
 
 	@Test
-	public void testSearch() throws Exception {
+	public void testRead() throws Exception {
 
-		HttpGet httpGet = new HttpGet("http://localhost:" + ourPort + "/Patient?_format=xml&_pretty=true");
+		HttpGet httpGet = new HttpGet("http://localhost:" + ourPort + "/Patient/2?_format=xml&_pretty=true");
 		HttpResponse status = ourClient.execute(httpGet);
 
 		String responseContent = IOUtils.toString(status.getEntity().getContent(), StandardCharsets.UTF_8);
@@ -80,11 +63,13 @@ public class CreateDstu3Test {
 		ourLog.info("Response was:\n{}", responseContent);
 
 		assertEquals(200, status.getStatusLine().getStatusCode());
+		assertEquals("http://localhost:" + ourPort + "/Patient/2/_history/2", status.getFirstHeader(Constants.HEADER_CONTENT_LOCATION).getValue());
+		assertEquals(null, status.getFirstHeader(Constants.HEADER_LOCATION));
 
 		//@formatter:off
 		assertThat(responseContent, stringContainsInOrder(
 			"<Patient xmlns=\"http://hl7.org/fhir\">", 
-				"<id value=\"0\"/>", 
+				"<id value=\"2\"/>", 
 				"<meta>", 
 					"<profile value=\"http://example.com/StructureDefinition/patient_with_extensions\"/>", 
 				"</meta>", 
@@ -93,9 +78,8 @@ public class CreateDstu3Test {
 				"</modifierExtension>", 
 			"</Patient>"));
 		//@formatter:on
-
-		assertThat(responseContent, not(containsString("http://hl7.org/fhir/")));
 	}
+
 
 	@AfterClass
 	public static void afterClassClearContext() throws Exception {
@@ -128,40 +112,22 @@ public class CreateDstu3Test {
 
 	public static class PatientProvider implements IResourceProvider {
 
-		@Create()
-		public MethodOutcome create(@ResourceParam Patient theIdParam) {
-			return new MethodOutcome(new IdType("Patient", "1"), true);
-		}
-
 		@Override
 		public Class<Patient> getResourceType() {
 			return Patient.class;
 		}
 
-		@Read()
+		@Read(version=true)
 		public MyPatientWithExtensions read(@IdParam IdType theIdParam) {
 			MyPatientWithExtensions p0 = new MyPatientWithExtensions();
 			p0.setId(theIdParam);
+			if (theIdParam.hasVersionIdPart() == false) {
+				p0.setIdElement(p0.getIdElement().withVersion("2"));
+			}
 			p0.setDateExt(new DateType("2011-01-01"));
 			return p0;
 		}
 
-		@Search
-		public List<IBaseResource> search() {
-			ArrayList<IBaseResource> retVal = new ArrayList<IBaseResource>();
-
-			MyPatientWithExtensions p0 = new MyPatientWithExtensions();
-			p0.setId(new IdType("Patient/0"));
-			p0.setDateExt(new DateType("2011-01-01"));
-			retVal.add(p0);
-
-			Patient p1 = new Patient();
-			p1.setId(new IdType("Patient/1"));
-			p1.addName().addFamily("The Family");
-			retVal.add(p1);
-
-			return retVal;
-		}
 
 	}
 
