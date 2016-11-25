69871bb8c29cdba70dacaec6405c2cdeea34d255
jamesagnew <jamesagnew@gmail.com>
2016-11-24 19:25:37 -0500

    Improve testing on CorsInterceptor

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/server/interceptor/CorsInterceptor.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/server/interceptor/CorsInterceptor.java
@@ -19,14 +19,50 @@ public class CorsInterceptor extends InterceptorAdapter {
 	private CorsConfiguration myConfig;
 
 	/**
-	 * Constructor
+	 * Constructor which creates an interceptor with default CORS configuration for use in
+	 * a FHIR server. This includes:
+	 * <ul>
+	 * <li>Allowed Origin: *</li>
+	 * <li>Allowed Header: Origin</li>
+	 * <li>Allowed Header: Accept</li>
+	 * <li>Allowed Header: X-Requested-With</li>
+	 * <li>Allowed Header: Content-Type</li>
+	 * <li>Allowed Header: Access-Control-Request-Method</li>
+	 * <li>Allowed Header: Access-Control-Request-Headers</li>
+	 * <li>Exposed Header: Location</li>
+	 * <li>Exposed Header: Content-Location</li>
+	 * </ul>
+	 * Note that this configuration is useful for quickly getting CORS working, but
+	 * in a real production system you probably want to consider whether it is
+	 * appropriate for your situation. In particular, using "Allowed Origin: *"
+	 * isn't always the right thing to do.
 	 */
 	public CorsInterceptor() {
-		this(new CorsConfiguration());
+		this(createDefaultCorsConfig());
+	}
+
+	private static CorsConfiguration createDefaultCorsConfig() {
+		CorsConfiguration retVal = new CorsConfiguration();
+
+		// *********************************************************
+		// Update constructor documentation if you change these:
+		// *********************************************************
+
+		retVal.addAllowedHeader("Origin");
+		retVal.addAllowedHeader("Accept");
+		retVal.addAllowedHeader("X-Requested-With");
+		retVal.addAllowedHeader("Content-Type");
+		retVal.addAllowedHeader("Access-Control-Request-Method");
+		retVal.addAllowedHeader("Access-Control-Request-Headers");
+		retVal.addAllowedOrigin("*");
+		retVal.addExposedHeader("Location");
+		retVal.addExposedHeader("Content-Location");
+
+		return retVal;
 	}
 
 	/**
-	 * Constructor
+	 * Constructor which accepts the given configuration
 	 * 
 	 * @param theConfiguration
 	 *           The CORS configuration
