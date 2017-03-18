b59dd77bfc504b17e6a470d3ad26495a1ad60ad3
James Agnew <jamesagnew@gmail.com>
2017-03-17 21:10:50 -0400

    Fix compile error

--- examples/src/main/java/example/ConverterExamples.java
+++ examples/src/main/java/example/ConverterExamples.java
@@ -1,5 +1,25 @@
 package example;
 
+import org.hl7.fhir.convertors.NullVersionConverterAdvisor;
+import org.hl7.fhir.convertors.VersionConvertorAdvisor;
+import org.hl7.fhir.convertors.VersionConvertor_10_20;
+import org.hl7.fhir.exceptions.FHIRException;
+
 public class ConverterExamples {
 
+	public void c1020() throws FHIRException {
+	//START SNIPPET: 1020
+		// Create a converter
+		VersionConvertorAdvisor advisor = new NullVersionConverterAdvisor();
+		VersionConvertor_10_20 converter = new VersionConvertor_10_20(advisor);
+		
+		// Create an input resource to convert
+		org.hl7.fhir.instance.model.Observation input = new org.hl7.fhir.instance.model.Observation();
+		input.setEncounter(new org.hl7.fhir.instance.model.Reference("Encounter/123"));
+		
+		// Convert the resource
+		org.hl7.fhir.dstu3.model.Observation output = converter.convertObservation(input);
+		String context = output.getContext().getReference();
+	//END SNIPPET: 1020
+	}
 }
