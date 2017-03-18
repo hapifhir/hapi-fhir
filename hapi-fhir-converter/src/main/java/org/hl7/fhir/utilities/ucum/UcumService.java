295e9ec18519b1814e0378c8788dabc4b5da1738
James <jamesagnew@gmail.com>
2017-03-17 21:49:08 -0400

    Updates for 2.3

--- hapi-fhir-converter/src/main/java/org/hl7/fhir/utilities/ucum/UcumService.java
+++ hapi-fhir-converter/src/main/java/org/hl7/fhir/utilities/ucum/UcumService.java
@@ -1,5 +1,25 @@
 package org.hl7.fhir.utilities.ucum;
 
+/*
+ * #%L
+ * HAPI FHIR - Converter
+ * %%
+ * Copyright (C) 2014 - 2017 University Health Network
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 /*******************************************************************************
  * Crown Copyright (c) 2006+, Copyright (c) 2006 - 2014 Kestral Computing & Health Intersections.
  * All rights reserved. This program and the accompanying materials
