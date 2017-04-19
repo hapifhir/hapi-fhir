15424572ef1ddd540d134992dbb5cc88b0f34f85
James <jamesagnew@gmail.com>
2017-04-19 05:18:49 -0400

    Complete R3 merge

--- hapi-fhir-converter/src/main/java/org/hl7/fhir/convertors/VersionConvertorAdvisor.java
+++ hapi-fhir-converter/src/main/java/org/hl7/fhir/convertors/VersionConvertorAdvisor.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.hl7.fhir.convertors;
 
 /*-
@@ -27,32 +26,13 @@ import org.hl7.fhir.dstu3.model.ValueSet;
 import org.hl7.fhir.exceptions.FHIRException;
 
 public interface VersionConvertorAdvisor {
-  boolean ignoreEntry(org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent src);
+	boolean ignoreEntry(org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent src);
 
-  // called ?
-  org.hl7.fhir.instance.model.Resource convert(org.hl7.fhir.dstu3.model.Resource resource) throws FHIRException;
+	// called ?
+	org.hl7.fhir.instance.model.Resource convert(org.hl7.fhir.dstu3.model.Resource resource) throws FHIRException;
 
-  // called when an r2 value set has a codeSystem in it
-  void handleCodeSystem(CodeSystem tgtcs, ValueSet source);
+	// called when an r2 value set has a codeSystem in it
+	void handleCodeSystem(CodeSystem tgtcs, ValueSet source);
 
-  CodeSystem getCodeSystem(ValueSet src);
+	CodeSystem getCodeSystem(ValueSet src);
 }
-=======
-package org.hl7.fhir.convertors;
-
-import org.hl7.fhir.dstu3.model.CodeSystem;
-import org.hl7.fhir.dstu3.model.ValueSet;
-import org.hl7.fhir.exceptions.FHIRException;
-
-public interface VersionConvertorAdvisor {
-  boolean ignoreEntry(org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent src);
-
-  // called ?
-  org.hl7.fhir.instance.model.Resource convert(org.hl7.fhir.dstu3.model.Resource resource) throws FHIRException;
-
-  // called when an r2 value set has a codeSystem in it
-  void handleCodeSystem(CodeSystem tgtcs, ValueSet source);
-
-  CodeSystem getCodeSystem(ValueSet src);
-}
->>>>>>> dstu3_latest_structs
