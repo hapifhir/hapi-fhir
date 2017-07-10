a8e248036e73e54d0a88f4e18948403bb7cd35b2
Sebastien Riviere <sebastien.riviere@agfa.com>
2017-07-10 12:38:33 +0200

    Fix STU3 validation: find correct extension profile, use custom datatype profile, allow to disable terminology checks

--- hapi-fhir-structures-dstu3/src/main/java/org/hl7/fhir/dstu3/elementmodel/Property.java
+++ hapi-fhir-structures-dstu3/src/main/java/org/hl7/fhir/dstu3/elementmodel/Property.java
@@ -2,6 +2,7 @@ package org.hl7.fhir.dstu3.elementmodel;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 import org.hl7.fhir.dstu3.conformance.ProfileUtilities;
 import org.hl7.fhir.dstu3.context.IWorkerContext;
@@ -203,7 +204,7 @@ public class Property {
     ElementDefinition ed = definition;
     StructureDefinition sd = structure;
     List<ElementDefinition> children = ProfileUtilities.getChildMap(sd, ed);
-    if (children.isEmpty()) {
+    if (children.isEmpty()  || isElementWithOnlyExtension(ed, children)) {
       // ok, find the right definitions
       String t = null;
       if (ed.getType().size() == 1)
@@ -240,7 +241,13 @@ public class Property {
         }
       }
       if (!"xhtml".equals(t)) {
-        sd = context.fetchResource(StructureDefinition.class, "http://hl7.org/fhir/StructureDefinition/"+t);
+        final String url;
+        if (StringUtils.isNotBlank(ed.getType().get(0).getProfile())) {
+         url = ed.getType().get(0).getProfile();
+        } else {
+          url = "http://hl7.org/fhir/StructureDefinition/" + t;
+        }
+        sd = context.fetchResource(StructureDefinition.class, url);
         if (sd == null)
           throw new DefinitionException("Unable to find type '"+t+"' for name '"+elementName+"' on property "+definition.getPath());
         children = ProfileUtilities.getChildMap(sd, sd.getSnapshot().getElement().get(0));
@@ -340,5 +347,18 @@ public class Property {
     return context;
   }
 
+  private boolean isElementWithOnlyExtension(final ElementDefinition ed, final List<ElementDefinition> children) {
+    boolean result = false;
+    if (!ed.getType().isEmpty()) {
+      result = true;
+      for (final ElementDefinition ele : children) {
+        if (!ele.getPath().contains("extension")) {
+          result = false;
+          break;
+        }
+      }
+    }
+    return result;
+  }
 
 }
