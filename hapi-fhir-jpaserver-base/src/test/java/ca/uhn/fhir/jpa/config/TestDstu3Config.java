fa050c466559f2376c5efd18929893552251211e
James <jamesagnew@gmail.com>
2017-07-21 08:12:22 -0400

    Restore random connection count to tests

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/config/TestDstu3Config.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/config/TestDstu3Config.java
@@ -43,8 +43,7 @@ public class TestDstu3Config extends BaseJavaConfigDstu3 {
 		 * and catch any potential deadlocks caused by database connection
 		 * starvation
 		 */
-		int maxThreads = (int)(Math.random() * 6) + 2;
-		maxThreads = 2;
+		int maxThreads = (int)(Math.random() * 6) + 1;
 		retVal.setMaxTotal(maxThreads);
 
 		DataSource dataSource = ProxyDataSourceBuilder
