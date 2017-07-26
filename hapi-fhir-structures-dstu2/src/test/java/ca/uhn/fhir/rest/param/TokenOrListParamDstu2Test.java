6aa58cf887e9cfea22e64fd1353be85a8928ff22
James Agnew <jamesagnew@gmail.com>
2017-07-26 15:30:24 -0400

    Move tests in anticipation of HAPI 3

--- hapi-fhir-structures-dstu2/src/test/java/ca/uhn/fhir/rest/param/TokenOrListParamDstu2Test.java
+++ hapi-fhir-structures-dstu2/src/test/java/ca/uhn/fhir/rest/param/TokenOrListParamDstu2Test.java
@@ -1,19 +1,18 @@
 package ca.uhn.fhir.rest.param;
 
-import static org.junit.Assert.*;
-
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.dstu.composite.CodingDt;
-import ca.uhn.fhir.rest.method.QualifiedParamList;
-import ca.uhn.fhir.util.TestUtil;
-
-import org.junit.AfterClass;
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TokenOrListParamTest {
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import ca.uhn.fhir.model.dstu2.composite.CodingDt;
+import ca.uhn.fhir.util.TestUtil;
+
+public class TokenOrListParamDstu2Test {
 	@Test
 	public void testWhenParamListHasAnyMatchingCodingsForCodingList_doesCodingListMatch_shouldBeTrue() {
 		TokenOrListParam params = new TokenOrListParam();
@@ -53,22 +52,6 @@ public class TokenOrListParamTest {
 		assertFalse(params.doesCodingListMatch(codings));
 	}
 
-	/**
-	 * See #192
-	 */
-	@Test
-	public void testParseExcaped() {
-		TokenOrListParam params = new TokenOrListParam();
-		params.setValuesAsQueryTokens(ourCtx, null, QualifiedParamList.singleton("system|code-include-but-not-end-with-comma\\,suffix"));
-		
-		assertEquals(1, params.getListAsCodings().size());
-		assertEquals("system", params.getListAsCodings().get(0).getSystemElement().getValue());
-		assertEquals("code-include-but-not-end-with-comma,suffix", params.getListAsCodings().get(0).getCodeElement().getValue());
-	}
-	
-
-	private static FhirContext ourCtx = FhirContext.forDstu1();
-	
 	@AfterClass
 	public static void afterClassClearContext() {
 		TestUtil.clearAllStaticFieldsForUnitTest();
