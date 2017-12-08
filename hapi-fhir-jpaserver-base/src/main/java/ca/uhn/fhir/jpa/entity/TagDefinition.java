e838b0e32dbb4c96d5f1b5b36ee042be622fb0b8
James Agnew <jamesagnew@gmail.com>
2017-12-07 18:03:17 -0600

    Remove unneccesary column

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/entity/TagDefinition.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/entity/TagDefinition.java
@@ -23,18 +23,7 @@ package ca.uhn.fhir.jpa.entity;
 import java.io.Serializable;
 import java.util.Collection;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -78,7 +67,8 @@ public class TagDefinition implements Serializable {
 	@Enumerated(EnumType.ORDINAL)
 	private TagTypeEnum myTagType;
 
-	private Integer myHashCode;
+	@Transient
+	private transient Integer myHashCode;
 
 	public TagDefinition() {
 	}
