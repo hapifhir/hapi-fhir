a8e5413d1f6907e2949bf370954204ace0e9a880
James Agnew <jamesagnew@gmail.com>
2017-12-22 13:47:40 -0500

    Fix two issues found by ErrorProne

--- hapi-fhir-structures-dstu3/src/main/java/org/hl7/fhir/dstu3/conformance/ProfileUtilities.java
+++ hapi-fhir-structures-dstu3/src/main/java/org/hl7/fhir/dstu3/conformance/ProfileUtilities.java
@@ -1833,8 +1833,8 @@ public class ProfileUtilities extends TranslatingUtilities {
     case BUNDLED : return "b";
     case CONTAINED : return "c";
     case REFERENCED: return "r";
+    default: return "?";
     }
-    return "?";
   }
 
 
@@ -2271,7 +2271,7 @@ public class ProfileUtilities extends TranslatingUtilities {
         if (definition != null && definition.hasShort()) {
           if (!c.getPieces().isEmpty()) c.addPiece(gen.new Piece("br"));
           c.addPiece(checkForNoChange(definition.getShortElement(), gen.new Piece(null, gt(definition.getShortElement()), null)));
-        } else if (fallback != null && fallback != null && fallback.hasShort()) {
+        } else if (fallback != null && fallback.hasShort()) {
           if (!c.getPieces().isEmpty()) c.addPiece(gen.new Piece("br"));
           c.addPiece(checkForNoChange(fallback.getShortElement(), gen.new Piece(null, gt(fallback.getShortElement()), null)));
         }
