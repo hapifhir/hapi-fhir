fb327c60cd032c96a0f72683831986c9b271cae3
jamesagnew <jamesagnew@gmail.com>
2018-02-04 15:04:45 -0500

    FIx spring boot issue

--- hapi-fhir-spring-boot/hapi-fhir-spring-boot-autoconfigure/src/main/java/ca/uhn/fhir/spring/boot/autoconfigure/FhirAutoConfiguration.java
+++ hapi-fhir-spring-boot/hapi-fhir-spring-boot-autoconfigure/src/main/java/ca/uhn/fhir/spring/boot/autoconfigure/FhirAutoConfiguration.java
@@ -9,9 +9,9 @@ package ca.uhn.fhir.spring.boot.autoconfigure;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -162,7 +162,7 @@ public class FhirAutoConfiguration {
 	@EnableConfigurationProperties(FhirProperties.class)
 	static class FhirJpaServerConfiguration {
 
-		@Bean()
+		@Bean
 		@ConditionalOnMissingBean
 		public ScheduledExecutorFactoryBean scheduledExecutorService() {
 			ScheduledExecutorFactoryBean b = new ScheduledExecutorFactoryBean();
@@ -170,8 +170,7 @@ public class FhirAutoConfiguration {
 			return b;
 		}
 
-		@Bean
-		@ConditionalOnMissingBean
+		@Bean(name="hapiJpaTaskExecutor")
 		public AsyncTaskExecutor taskScheduler() {
 			ConcurrentTaskScheduler retVal = new ConcurrentTaskScheduler();
 			retVal.setConcurrentExecutor(scheduledExecutorService().getObject());
