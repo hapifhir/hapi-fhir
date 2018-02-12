18e4d85625e0503c1c568937b06eef63c8453e60
James Agnew <jamesagnew@gmail.com>
2018-02-11 19:26:03 -0500

    Add functions to stopwatch

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/util/StopWatch.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/util/StopWatch.java
@@ -2,7 +2,9 @@ package ca.uhn.fhir.jpa.util;
 
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /*
  * #%L
@@ -13,9 +15,9 @@ import java.util.Date;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,9 +39,20 @@ public class StopWatch {
 
 	/**
 	 * Constructor
+	 *
+	 * @param theStart The time to record as the start for this timer
 	 */
-	public StopWatch(Date theNow) {
-		myStarted = theNow.getTime();
+	public StopWatch(Date theStart) {
+		myStarted = theStart.getTime();
+	}
+
+	public long getMillis() {
+		long now = System.currentTimeMillis();
+		return now - myStarted;
+	}
+
+	public long getMillis(Date theNow) {
+		return theNow.getTime() - myStarted;
 	}
 
 	public long getMillisAndRestart() {
@@ -49,19 +62,38 @@ public class StopWatch {
 		return retVal;
 	}
 
-	public long getMillis() {
-		long now = System.currentTimeMillis();
-		long retVal = now - myStarted;
-		return retVal;
-	}
-
-	public long getMillis(Date theNow) {
-		long retVal = theNow.getTime() - myStarted;
-		return retVal;
+	/**
+	 * @param theNumOperations Ok for this to be 0, it will be treated as 1
+	 */
+	public int getMillisPerOperation(int theNumOperations) {
+		return (int) (((double) getMillis()) / Math.max(1.0, theNumOperations));
 	}
 
 	public Date getStartedDate() {
 		return new Date(myStarted);
+	}
+
+	public double getThroughput(int theNumOperations, TimeUnit theUnit) {
+		if (theNumOperations <= 0) {
+			return 0.0f;
+		}
+
+		long millisElapsed = Math.max(1, getMillis());
+		long periodMillis = theUnit.toMillis(1);
+
+		double numerator = theNumOperations;
+		double denominator = ((double)millisElapsed)/((double)periodMillis);
+
+		return numerator / denominator;
+	}
+
+	public String formatThroughput(int theNumOperations, TimeUnit theUnit) {
+		double throughput = getThroughput(theNumOperations, theUnit);
+		return new DecimalFormat("0.0").format(throughput);
+	}
+
+	public void restart() {
+		myStarted = System.currentTimeMillis();
 	}
 
 	/**
@@ -70,6 +102,23 @@ public class StopWatch {
 	@Override
 	public String toString() {
 		return formatMillis(getMillis());
+	}
+
+	/**
+	 * Append a right-aligned and zero-padded numeric value to a `StringBuilder`.
+	 */
+	static private void append(StringBuilder tgt, String pfx, int dgt, long val) {
+		tgt.append(pfx);
+		if (dgt > 1) {
+			int pad = (dgt - 1);
+			for (long xa = val; xa > 9 && pad > 0; xa /= 10) {
+				pad--;
+			}
+			for (int xa = 0; xa < pad; xa++) {
+				tgt.append('0');
+			}
+		}
+		tgt.append(val);
 	}
 
 	static public String formatMillis(long val) {
@@ -90,29 +139,6 @@ public class StopWatch {
 		append(buf, ":", 2, ((val % DateUtils.MILLIS_PER_MINUTE) / DateUtils.MILLIS_PER_SECOND));
 		append(buf, ".", 3, (val % DateUtils.MILLIS_PER_SECOND));
 		return buf.toString();
-	}
-
-	/** Append a right-aligned and zero-padded numeric value to a `StringBuilder`. */
-	static private void append(StringBuilder tgt, String pfx, int dgt, long val) {
-		tgt.append(pfx);
-		if (dgt > 1) {
-			int pad = (dgt - 1);
-			for (long xa = val; xa > 9 && pad > 0; xa /= 10) {
-				pad--;
-			}
-			for (int xa = 0; xa < pad; xa++) {
-				tgt.append('0');
-			}
-		}
-		tgt.append(val);
-	}
-
-	/**
-	 * @param theNumOperations Ok for this to be 0, it will be treated as 1
-	 */
-	public int getMillisPerOperation(int theNumOperations) {
-		int numOperations = Math.min(1, theNumOperations);
-		return (int)(((double) getMillis()) / Math.max(1.0, numOperations));
 	}
 
 }
