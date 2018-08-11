7eb36c3392b3e24e888f0e83b7aa3c43b56699d7
jamesagnew <jamesagnew@gmail.com>
2018-08-11 16:14:53 -0400

    Work on subscription

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/HashMapResourceProvider.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/HashMapResourceProvider.java
@@ -34,6 +34,7 @@ import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.exceptions.ResourceGoneException;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import ca.uhn.fhir.util.ValidateUtil;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
@@ -43,6 +44,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * This class is a simple implementation of the resource provider
@@ -338,8 +341,15 @@ public class HashMapResourceProvider<T extends IBaseResource> implements IResour
 		return id;
 	}
 
+	/**
+	 * @param theConditional This is provided only so that subclasses can implement if they want
+	 */
 	@Update
-	public MethodOutcome update(@ResourceParam T theResource) {
+	public MethodOutcome update(
+		@ResourceParam T theResource,
+		@ConditionalUrlParam String theConditional) {
+
+		ValidateUtil.isTrueOrThrowInvalidRequest(isBlank(theConditional), "This server doesn't support conditional update");
 
 		String idPartAsString = theResource.getIdElement().getIdPart();
 		TreeMap<Long, T> versionToResource = getVersionToResource(idPartAsString);
