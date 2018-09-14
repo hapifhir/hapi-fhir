321e88c811e45c6ed9858514dc48bfb9cbe577ec
jamesagnew <jamesagnew@gmail.com>
2018-09-14 05:25:07 -0400

    License header updates

--- hapi-fhir-jpaserver-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/DriverTypeEnum.java
+++ hapi-fhir-jpaserver-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/DriverTypeEnum.java
@@ -11,11 +11,21 @@ import javax.annotation.Nonnull;
 
 /*-
  * #%L
- * Smile CDR - CDR
+ * HAPI FHIR JPA Server - Migration
  * %%
- * Copyright (C) 2016 - 2018 Simpatico Intelligent Systems Inc
+ * Copyright (C) 2014 - 2018 University Health Network
  * %%
- * All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
 
