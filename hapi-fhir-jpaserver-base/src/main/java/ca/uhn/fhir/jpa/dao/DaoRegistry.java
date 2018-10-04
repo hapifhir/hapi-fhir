9c27e8e6dd3229784b9d79eda8a6d5816d30c7bf
James Agnew <jamesagnew@gmail.com>
2018-10-04 15:12:21 -0400

    Fix an issue with the spring boot build

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/DaoRegistry.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/DaoRegistry.java
@@ -9,9 +9,9 @@ package ca.uhn.fhir.jpa.dao;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,14 +22,12 @@ package ca.uhn.fhir.jpa.dao;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.RuntimeResourceDefinition;
-import ca.uhn.fhir.rest.param.HasAndListParam;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,10 +37,20 @@ public class DaoRegistry implements ApplicationContextAware {
 	@Autowired
 	private FhirContext myCtx;
 	private volatile Map<String, IFhirResourceDao<?>> myResourceNameToResourceDao;
+	private volatile IFhirSystemDao<?, ?> mySystemDao;
 
 	@Override
 	public void setApplicationContext(ApplicationContext theApplicationContext) throws BeansException {
 		myAppCtx = theApplicationContext;
+	}
+
+	public IFhirSystemDao<?, ?> getSystemDao() {
+		IFhirSystemDao<?, ?> retVal = mySystemDao;
+		if (retVal == null) {
+			retVal = myAppCtx.getBean(IFhirSystemDao.class);
+			mySystemDao = retVal;
+		}
+		return retVal;
 	}
 
 	public IFhirResourceDao<?> getResourceDao(String theResourceName) {
