16d77cad090e3a3072f2703d78422c95b37eb117
Ken Stevens <khstevens@gmail.com>
2018-10-30 21:17:03 -0400

    First pass close to being ready to test

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/subscription/matcher/SubscriptionMatcherInMemory.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/subscription/matcher/SubscriptionMatcherInMemory.java
@@ -7,15 +7,22 @@ import ca.uhn.fhir.jpa.dao.index.ResourceIndexedSearchParams;
 import ca.uhn.fhir.jpa.entity.ResourceTable;
 import ca.uhn.fhir.jpa.service.MatchUrlService;
 import ca.uhn.fhir.jpa.subscription.ResourceModifiedMessage;
+import ca.uhn.fhir.model.api.IQueryParameterType;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 @Lazy
 public class SubscriptionMatcherInMemory implements ISubscriptionMatcher {
+	private Logger ourLog = LoggerFactory.getLogger(SubscriptionMatcherInMemory.class);
 
 	@Autowired
 	private FhirContext myContext;
@@ -36,6 +43,14 @@ public class SubscriptionMatcherInMemory implements ISubscriptionMatcher {
 		searchParams.extractFromResource(entity, resource);
 
 		SearchParameterMap searchParameterMap = myMatchUrlService.translateMatchUrl(criteria, resourceDefinition);
+
+		for (Map.Entry<String, List<List<? extends IQueryParameterType>>> entry : searchParameterMap.entrySet()) {
+			for (List<? extends IQueryParameterType> parameters : entry.getValue()) {
+				for (IQueryParameterType param : parameters) {
+					ourLog.debug("Param {}: {}", entry, param);
+				}
+			}
+		}
 
 		// FIXME KHS implement
 
