cefacdc18ce92687b6ed6e685c7602bf380fb7cd
Matti Uusitalo <matti.uusitalo@fi.fujitsu.com>
2018-11-30 09:34:49 +0200

    Returned to original state as the experiment failed

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/dstu3/hapi/validation/FhirInstanceValidator.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/dstu3/hapi/validation/FhirInstanceValidator.java
@@ -637,7 +637,7 @@ public class FhirInstanceValidator extends BaseValidatorBridge implements IValid
 
 		@Override
 		public boolean isNoTerminologyServer() {
-			return true;
+			return myWrap.isNoTerminologyServer();
 		}
 
 		@Override
