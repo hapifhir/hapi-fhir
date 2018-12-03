b2e20b5c31243d98ddea68eeae4f61f7f20c7589
Matti Uusitalo <matti.uusitalo@fi.fujitsu.com>
2018-12-03 13:48:40 +0200

    temporarily set hard coded error when no profile found

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/dstu3/hapi/validation/FhirInstanceValidator.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/dstu3/hapi/validation/FhirInstanceValidator.java
@@ -249,6 +249,7 @@ public class FhirInstanceValidator extends BaseValidatorBridge implements IValid
 		v.setResourceIdRule(IdStatus.OPTIONAL);
 		v.setNoTerminologyChecks(isNoTerminologyChecks());
 		v.setMyEnableWhenEvaluator(enableWhenEvaluatorSupplier.apply(wrappedWorkerContext));
+		v.setErrorForUnknownProfiles(true); // TODO must be configurable instead of hardcoded to be always true
 
 		List<ValidationMessage> messages = new ArrayList<>();
 
