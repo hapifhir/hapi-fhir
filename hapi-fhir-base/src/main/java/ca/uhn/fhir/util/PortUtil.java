3a3b81a6a8d175761a207d686b4872e19de3564a
jamesagnew <jamesagnew@gmail.com>
2018-12-08 17:52:48 -0500

    Preparing things for OpenJDK 11

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/PortUtil.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/PortUtil.java
@@ -9,9 +9,9 @@ package ca.uhn.fhir.util;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,13 @@ package ca.uhn.fhir.util;
  * #L%
  */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.LinkedHashSet;
 
 /**
@@ -28,6 +34,7 @@ import java.util.LinkedHashSet;
  */
 @CoverageIgnore
 public class PortUtil {
+	private static final Logger ourLog = LoggerFactory.getLogger(PortUtil.class);
 	private static LinkedHashSet<Integer> ourPorts = new LinkedHashSet<>();
 
 	/*
@@ -35,26 +42,38 @@ public class PortUtil {
 	 */
 	private PortUtil() {
 		// nothing
-	} 
-	
+	}
+
 	/**
 	 * This is really only used for unit tests but is included in the library so it can be reused across modules. Use with caution.
 	 */
 	public static int findFreePort() {
-		ServerSocket server;
 		try {
 			int port;
 			do {
-				server = new ServerSocket(0);
-				server.setReuseAddress(true);
-				port = server.getLocalPort();
-				server.close();
+				try (ServerSocket server = new ServerSocket(0)) {
+					server.setReuseAddress(true);
+					port = server.getLocalPort();
+				}
 			} while (!ourPorts.add(port));
 
-			Thread.sleep(500);
+			// Make sure that we can't connect to the server, meaning the port is
+			// successfully released
+			for (int i = 0; i < 20; i++) {
+				Socket connector = new Socket();
+				try {
+					connector.connect(new InetSocketAddress(port));
+				} catch (ConnectException e) {
+					break;
+				}
+				ourLog.info("Port {} is still in use - Waiting...", port);
+				Thread.sleep(250);
+			}
+
+			Thread.sleep(1000);
+
 			return port;
 		} catch (Exception e) {
-			//FIXME resource leak
 			throw new Error(e);
 		}
 	}
