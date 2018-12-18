86b3e45a32e1cd208119065d39c5a61f1ed8975e
jamesagnew <jamesagnew@gmail.com>
2018-12-18 14:08:01 -0500

    License header updates

--- hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/module/subscriber/email/EmailDetails.java
+++ hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/module/subscriber/email/EmailDetails.java
@@ -2,7 +2,7 @@ package ca.uhn.fhir.jpa.subscription.module.subscriber.email;
 
 /*-
  * #%L
- * HAPI FHIR JPA Server
+ * HAPI FHIR Subscription Server
  * %%
  * Copyright (C) 2014 - 2018 University Health Network
  * %%
