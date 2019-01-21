690af1c7ff252233d5dd97bfb8f6e49982c70849
jamesagnew <jamesagnew@gmail.com>
2019-01-20 19:43:08 -0500

    Add interceptor example

---
+++ examples/src/main/java/example/interceptor/MyTestInterceptor.java
@@ -0,0 +1,25 @@
+package example.interceptor;
+
+import ca.uhn.fhir.jpa.model.interceptor.api.Hook;
+import ca.uhn.fhir.jpa.model.interceptor.api.Interceptor;
+import ca.uhn.fhir.jpa.model.interceptor.api.Pointcut;
+import ca.uhn.fhir.jpa.subscription.module.CanonicalSubscription;
+import ca.uhn.fhir.jpa.subscription.module.subscriber.ResourceDeliveryMessage;
+
+/**
+ * Interceptor class
+ */
+@Interceptor
+public class MyTestInterceptor {
+
+	@Hook(Pointcut.SUBSCRIPTION_BEFORE_REST_HOOK_DELIVERY)
+	public boolean beforeRestHookDelivery(ResourceDeliveryMessage theDeliveryMessage, CanonicalSubscription theSubscription) {
+
+		String header = "Authorization: Bearer 1234567";
+
+		theSubscription.addHeader(header);
+
+		return true;
+	}
+
+}
