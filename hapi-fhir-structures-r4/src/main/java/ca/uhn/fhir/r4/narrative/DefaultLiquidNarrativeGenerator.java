d621777526140551f1b4701a4b09143120acfbc1
Ken Stevens <khstevens@gmail.com>
2019-02-20 19:13:49 -0500

    time to circle back with James

--- hapi-fhir-structures-r4/src/main/java/ca/uhn/fhir/r4/narrative/DefaultLiquidNarrativeGenerator.java
+++ hapi-fhir-structures-r4/src/main/java/ca/uhn/fhir/r4/narrative/DefaultLiquidNarrativeGenerator.java
@@ -11,7 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DefaultLiquidNarrativeGenerator extends BaseNarrativeGenerator {
 	private static final Logger ourLog = LoggerFactory.getLogger(DefaultLiquidNarrativeGenerator.class);
