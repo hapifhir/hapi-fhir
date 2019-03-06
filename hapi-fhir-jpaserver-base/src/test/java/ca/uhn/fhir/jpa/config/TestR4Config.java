5dccf93506cdda81a97536fe01d9b7f3bd42a4ec
James Agnew <jamesagnew@gmail.com>
2019-03-06 17:15:30 -0500

    Fix broken test

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/config/TestR4Config.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/config/TestR4Config.java
@@ -5,10 +5,12 @@ import ca.uhn.fhir.validation.ResultSeverityEnum;
 import net.ttddyy.dsproxy.listener.SingleQueryCountHolder;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -25,7 +27,7 @@ import static org.junit.Assert.fail;
 public class TestR4Config extends BaseJavaConfigR4 {
 
 	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(TestR4Config.class);
-	private static int ourMaxThreads;
+	public static Integer ourMaxThreads;
 
 	static {
 		/*
@@ -33,7 +35,9 @@ public class TestR4Config extends BaseJavaConfigR4 {
 		 * and catch any potential deadlocks caused by database connection
 		 * starvation
 		 */
-		ourMaxThreads = (int) (Math.random() * 6.0) + 1;
+		if (ourMaxThreads == null) {
+			ourMaxThreads = (int) (Math.random() * 6.0) + 1;
+		}
 	}
 
 	private Exception myLastStackTrace;
@@ -90,7 +94,6 @@ public class TestR4Config extends BaseJavaConfigR4 {
 		retVal.setMaxWaitMillis(10000);
 		retVal.setUsername("");
 		retVal.setPassword("");
-
 		retVal.setMaxTotal(ourMaxThreads);
 
 		DataSource dataSource = ProxyDataSourceBuilder
