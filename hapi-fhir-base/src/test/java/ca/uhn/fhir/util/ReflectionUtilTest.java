b7cb50ea26abfb26b0b4b1a40ed7b3cbab239aae
jvi <jvi@trifork.com>
2019-04-07 15:12:42 +0200

    Added test of reflection util for default methods

--- hapi-fhir-base/src/test/java/ca/uhn/fhir/util/ReflectionUtilTest.java
+++ hapi-fhir-base/src/test/java/ca/uhn/fhir/util/ReflectionUtilTest.java
@@ -1,13 +1,17 @@
 package ca.uhn.fhir.util;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import ca.uhn.fhir.context.ConfigurationException;
+import com.google.common.collect.Lists;
+import com.google.common.truth.Truth;
 import org.junit.Test;
 
-import ca.uhn.fhir.context.ConfigurationException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ReflectionUtilTest {
 
@@ -45,4 +49,22 @@ public class ReflectionUtilTest {
 			assertEquals("Failed to instantiate java.util.List", e.getMessage());
 		}
 	}
+
+	@Test
+	public void testFindDefaultMethod() throws NoSuchMethodException {
+		Collection<Method> methods = ReflectionUtil.getAllMethods(DummyClass.class, true, true);
+		List<Method> method = Lists.newArrayList(DummyClass.class.getMethod("whatEver"));
+		Truth.assertThat(methods).containsAnyIn(method);
+	}
+
+	class DummyClass implements DummyInterfaceWithDefaultMethod {
+
+	}
+
+	interface DummyInterfaceWithDefaultMethod {
+		default void whatEver() {
+
+		}
+	}
+
 }
