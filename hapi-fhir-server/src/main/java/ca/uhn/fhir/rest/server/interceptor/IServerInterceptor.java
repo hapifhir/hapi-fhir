66a669949dcfdbcce0c567a646b7d147d734724d
James Agnew <jamesagnew@gmail.com>
2019-05-29 21:16:33 -0400

    Add docs - Fix #1309

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/IServerInterceptor.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/IServerInterceptor.java
@@ -9,9 +9,9 @@ package ca.uhn.fhir.rest.server.interceptor;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,7 +47,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -59,6 +58,8 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
  * <b>See:</b> See the <a href="http://jamesagnew.github.io/hapi-fhir/doc_rest_server_interceptor.html">server
  * interceptor documentation</a> for more information on how to use this class.
  * </p>
+ * Note that unless otherwise stated, it is possible to throw any subclass of
+ * {@link BaseServerResponseException} from any interceptor method.
  */
 public interface IServerInterceptor {
 
@@ -428,7 +429,7 @@ public interface IServerInterceptor {
 
 			IInterceptorService interceptorService = server.getInterceptorService();
 
-			HookParams params= new HookParams();
+			HookParams params = new HookParams();
 			params.add(RestOperationTypeEnum.class, theOperationType);
 			params.add(this);
 			interceptorService.callHooks(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED, params);
