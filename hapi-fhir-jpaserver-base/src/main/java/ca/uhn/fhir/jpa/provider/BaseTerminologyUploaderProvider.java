1f055be3567935a4934773f2ae634d2e7083883a
James Agnew <jamesagnew@gmail.com>
2019-07-28 14:28:46 -0400

    Updates

---
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/BaseTerminologyUploaderProvider.java
@@ -0,0 +1,10 @@
+package ca.uhn.fhir.jpa.provider;
+
+import ca.uhn.fhir.jpa.model.util.JpaConstants;
+
+public class BaseTerminologyUploaderProvider {
+
+	// FIXME: remove this before 4.0.0
+	public static final String UPLOAD_EXTERNAL_CODE_SYSTEM = JpaConstants.UPLOAD_EXTERNAL_CODE_SYSTEM;
+
+}
