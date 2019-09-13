75ddba7f2d2b15c3f17ebc422dfc6a633f20e5a8
Diederik Muylwyk <diederik.muylwyk@gmail.com>
2019-09-13 11:49:01 -0400

    Add EXTERNAL_COPYRIGHT_NOTICE property for LOINC uploader.

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TerminologyLoaderSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TerminologyLoaderSvcImpl.java
@@ -480,6 +480,11 @@ public class TerminologyLoaderSvcImpl implements IHapiTerminologyLoaderSvc {
 			}
 		}
 
+		// FIXME: DM 2019-09-13 - Manually add EXTERNAL_COPYRIGHT_NOTICE property until Regenstrief adds this to loinc.xml
+		String externalCopyRightNoticeCode = "EXTERNAL_COPYRIGHT_NOTICE";
+		CodeSystem.PropertyType externalCopyRightNoticeType = CodeSystem.PropertyType.STRING;
+		propertyNamesToTypes.put(externalCopyRightNoticeCode, externalCopyRightNoticeType);
+
 		IRecordHandler handler;
 
 		// Part
