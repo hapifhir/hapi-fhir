b51a93a2b507fb6dd57b56f2664ce97f3d3a92d0
James Agnew <jamesagnew@gmail.com>
2019-10-18 15:46:02 -0400

    Intermittent test fix

--- hapi-fhir-base/src/test/java/ca/uhn/fhir/util/FileUtilTest.java
+++ hapi-fhir-base/src/test/java/ca/uhn/fhir/util/FileUtilTest.java
@@ -8,13 +8,13 @@ public class FileUtilTest {
 
 	@Test
 	public void formatFileSize() {
-		assertEquals("0 Bytes", FileUtil.formatFileSize(0));
-		assertEquals("1 Bytes", FileUtil.formatFileSize(1));
-		assertEquals("1.2 kB", FileUtil.formatFileSize(1234));
-		assertEquals("12.1 kB", FileUtil.formatFileSize(12345));
-		assertEquals("11.8 MB", FileUtil.formatFileSize(12345678));
-		assertEquals("103.5 GB", FileUtil.formatFileSize(111111111111L));
-		assertEquals("101.1 TB", FileUtil.formatFileSize(111111111111111L));
-		assertEquals("10105.5 TB", FileUtil.formatFileSize(11111111111111111L));
+		assertEquals("0 Bytes", FileUtil.formatFileSize(0).replace(",", "."));
+		assertEquals("1 Bytes", FileUtil.formatFileSize(1).replace(",", "."));
+		assertEquals("1.2 kB", FileUtil.formatFileSize(1234).replace(",", "."));
+		assertEquals("12.1 kB", FileUtil.formatFileSize(12345).replace(",", "."));
+		assertEquals("11.8 MB", FileUtil.formatFileSize(12345678).replace(",", "."));
+		assertEquals("103.5 GB", FileUtil.formatFileSize(111111111111L).replace(",", "."));
+		assertEquals("101.1 TB", FileUtil.formatFileSize(111111111111111L).replace(",", "."));
+		assertEquals("10105.5 TB", FileUtil.formatFileSize(11111111111111111L).replace(",", "."));
 	}
 }
