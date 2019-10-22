eaa4337a3af250b054225e231ebb4b323efb8586
James Agnew <jamesagnew@gmail.com>
2019-10-22 15:01:42 -0400

    Add some test logging

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/subscription/CountingInterceptor.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/subscription/CountingInterceptor.java
@@ -1,21 +1,32 @@
 package ca.uhn.fhir.jpa.subscription;
 
+import jdk.internal.joptsimple.internal.Strings;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.ChannelInterceptorAdapter;
 
-public class CountingInterceptor extends ChannelInterceptorAdapter {
+import java.util.ArrayList;
+import java.util.List;
 
-	private int mySentCount;
+public class CountingInterceptor implements ChannelInterceptor {
+
+	private List<String> mySent = new ArrayList<>();
 
 	public int getSentCount() {
-		return mySentCount;
+		return mySent.size();
 	}
 
 	@Override
 	public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
 		if (sent) {
-			mySentCount++;
+			mySent.add(message.toString());
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "[" + Strings.join(mySent, "\n") + "]";
 	}
 }
