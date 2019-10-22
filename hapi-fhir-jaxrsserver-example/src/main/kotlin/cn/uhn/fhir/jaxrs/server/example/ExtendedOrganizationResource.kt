a859b210e3726feb5f34cd5059ae27ee75248ce0
jelmer.terwal <jelmer.terwal@nedap.com>
2019-10-22 14:34:07 +0200

    Previously the server would not start when using a CodeableConcept in a List in combination with Kotlin

--- hapi-fhir-jaxrsserver-example/src/main/kotlin/cn/uhn/fhir/jaxrs/server/example/ExtendedOrganizationResource.kt
+++ hapi-fhir-jaxrsserver-example/src/main/kotlin/cn/uhn/fhir/jaxrs/server/example/ExtendedOrganizationResource.kt
@@ -8,7 +8,6 @@ import ca.uhn.fhir.rest.annotation.OptionalParam
 import ca.uhn.fhir.rest.annotation.Search
 import ca.uhn.fhir.rest.api.Constants
 import ca.uhn.fhir.rest.param.StringParam
-import org.hl7.fhir.dstu3.model.Organization
 import javax.ejb.Stateless
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -17,15 +16,15 @@ import javax.ws.rs.core.MediaType
 @Path("Organization")
 @Stateless
 @Produces(MediaType.APPLICATION_JSON, Constants.CT_FHIR_JSON, Constants.CT_FHIR_XML)
-class ExtendedOrganizationResource : AbstractJaxRsResourceProvider<Organization>(FhirContext.forDstu3()) {
-   override fun getResourceType(): Class<Organization>? = Organization::class.java
+class ExtendedOrganizationResource : AbstractJaxRsResourceProvider<ExtendedOrganization>(FhirContext.forDstu3()) {
+   override fun getResourceType(): Class<ExtendedOrganization>? = ExtendedOrganization::class.java
 
    @Search
    fun find(
       @OptionalParam(name = "_id") theId: StringParam?,
       @IncludeParam(allow = ["Patient:general-practitioner"]) includes: Collection<Include>?
-   ): List<Organization> {
-      val organization = Organization().also {
+   ): List<ExtendedOrganization> {
+      val organization = ExtendedOrganization().also {
          it.id = "id"
       }
       return listOf(organization)
