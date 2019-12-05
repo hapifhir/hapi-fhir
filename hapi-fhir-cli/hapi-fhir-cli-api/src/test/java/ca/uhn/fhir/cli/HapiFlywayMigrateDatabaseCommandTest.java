1ce5dca5db218b3f6fb8322868675719da80f8b5
Ken Stevens <khstevens@gmail.com>
2019-12-05 15:32:53 -0500

    intermittent failure?

--- hapi-fhir-cli/hapi-fhir-cli-api/src/test/java/ca/uhn/fhir/cli/HapiFlywayMigrateDatabaseCommandTest.java
+++ hapi-fhir-cli/hapi-fhir-cli-api/src/test/java/ca/uhn/fhir/cli/HapiFlywayMigrateDatabaseCommandTest.java
@@ -37,6 +37,7 @@ public class HapiFlywayMigrateDatabaseCommandTest {
 		System.setProperty("test", "true");
 	}
 
+	// TODO INTERMITTENT This just failed for me on CI with a BadSqlGrammarException
 	@Test
 	public void testMigrateFrom340() throws IOException, SQLException {
 
