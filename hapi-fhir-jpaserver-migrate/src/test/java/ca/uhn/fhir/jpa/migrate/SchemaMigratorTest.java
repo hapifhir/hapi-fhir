0b30033b0a5b94c370d73a65f8fdda0436b4cfce
Ken Stevens <khstevens@gmail.com>
2019-12-09 11:19:18 -0500

    upgrade flyway to latest version automatically repair failed migrations

--- hapi-fhir-jpaserver-migrate/src/test/java/ca/uhn/fhir/jpa/migrate/SchemaMigratorTest.java
+++ hapi-fhir-jpaserver-migrate/src/test/java/ca/uhn/fhir/jpa/migrate/SchemaMigratorTest.java
@@ -4,37 +4,29 @@ import ca.uhn.fhir.context.ConfigurationException;
 import ca.uhn.fhir.jpa.migrate.taskdef.AddTableRawSqlTask;
 import ca.uhn.fhir.jpa.migrate.taskdef.BaseTest;
 import com.google.common.collect.ImmutableList;
+import org.flywaydb.core.api.FlywayException;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 
+import javax.annotation.Nonnull;
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.contains;
+import static org.junit.Assert.*;
 
 public class SchemaMigratorTest extends BaseTest {
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(SchemaMigratorTest.class);
 
 	@Test
 	public void testMigrationRequired() {
-		AddTableRawSqlTask task = new AddTableRawSqlTask("1", "1");
-		task.setTableName("SOMETABLE");
-		task.addSql(DriverTypeEnum.H2_EMBEDDED, "create table SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
-
-		SchemaMigrator schemaMigrator = new SchemaMigrator(SchemaMigrator.HAPI_FHIR_MIGRATION_TABLENAME, getDataSource(), new Properties(), ImmutableList.of(task));
-		schemaMigrator.setDriverType(DriverTypeEnum.H2_EMBEDDED);
+		SchemaMigrator schemaMigrator = createSchemaMigrator("create table SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
 
 		try {
 			schemaMigrator.validate();
 			fail();
 		} catch (ConfigurationException e) {
-			assertEquals("The database schema for " + getUrl() + " is out of date.  Current database schema version is unknown.  Schema version required by application is " + task.getFlywayVersion() + ".  Please run the database migrator.", e.getMessage());
+			assertEquals("The database schema for " + getUrl() + " is out of date.  Current database schema version is unknown.  Schema version required by application is 1.1.  Please run the database migrator.", e.getMessage());
 		}
 
 		schemaMigrator.migrate();
@@ -44,12 +36,22 @@ public class SchemaMigratorTest extends BaseTest {
 
 
 	@Test
-	public void testMigrationRequiredNoFlyway() throws SQLException {
-		AddTableRawSqlTask task = new AddTableRawSqlTask("1", "1");
-		task.setTableName("SOMETABLE");
-		task.addSql(DriverTypeEnum.H2_EMBEDDED, "create table SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
+	public void testRepairFailedMigration() {
+		SchemaMigrator schemaMigrator = createSchemaMigrator("create fable SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
+		try {
+			schemaMigrator.migrate();
+			fail();
+		} catch (FlywayException e) {
+			assertEquals(org.springframework.jdbc.BadSqlGrammarException.class, e.getCause().getCause().getClass());
+		}
+		schemaMigrator = createSchemaMigrator("create table SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
+		schemaMigrator.migrate();
+	}
 
-		SchemaMigrator schemaMigrator = new SchemaMigrator(SchemaMigrator.HAPI_FHIR_MIGRATION_TABLENAME, getDataSource(), new Properties(), ImmutableList.of(task));
+
+	@Test
+	public void testMigrationRequiredNoFlyway() throws SQLException {
+		SchemaMigrator schemaMigrator = createSchemaMigrator("create table SOMETABLE (PID bigint not null, TEXTCOL varchar(255))");
 		schemaMigrator.setDriverType(DriverTypeEnum.H2_EMBEDDED);
 		schemaMigrator.setDontUseFlyway(true);
 
@@ -66,4 +68,13 @@ public class SchemaMigratorTest extends BaseTest {
 
 	}
 
+	@Nonnull
+	private SchemaMigrator createSchemaMigrator(String theSql) {
+		AddTableRawSqlTask task = new AddTableRawSqlTask("1", "1");
+		task.setTableName("SOMETABLE");
+		task.addSql(DriverTypeEnum.H2_EMBEDDED, theSql);
+		SchemaMigrator retval = new SchemaMigrator(SchemaMigrator.HAPI_FHIR_MIGRATION_TABLENAME, getDataSource(), new Properties(), ImmutableList.of(task));
+		retval.setDriverType(DriverTypeEnum.H2_EMBEDDED);
+		return retval;
+	}
 }
