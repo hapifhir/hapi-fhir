a45a720a9bfa76179a6d0f3df68e56a737ebcb87
Ken Stevens <khstevens@gmail.com>
2020-03-25 16:24:46 -0400

    add another failing test

--- hapi-fhir-jpaserver-empi/src/test/java/ca/uhn/fhir/jpa/empi/svc/EmpiCandidateSearchSvcTest.java
+++ hapi-fhir-jpaserver-empi/src/test/java/ca/uhn/fhir/jpa/empi/svc/EmpiCandidateSearchSvcTest.java
@@ -1,8 +1,12 @@
 package ca.uhn.fhir.jpa.empi.svc;
 
 import ca.uhn.fhir.jpa.empi.BaseEmpiR4Test;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Patient;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +16,10 @@ public class EmpiCandidateSearchSvcTest extends BaseEmpiR4Test {
 
 	@Test
 	public void findCandidates() {
+		Patient createdJane = createPatient(buildPatientWithNameAndId("Jane", JANE_ID));
+		Patient newJane = buildPatientWithNameAndId("Jane", JANE_ID);
 
+		List<IBaseResource> result = myEmpiCandidateSearchSvc.findCandidates(newJane);
+		assertEquals(1, result.size());
 	}
 }
