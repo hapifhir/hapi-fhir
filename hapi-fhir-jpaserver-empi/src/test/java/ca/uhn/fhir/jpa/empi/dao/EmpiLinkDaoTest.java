f876021221e89f0ef4c480595776bdf03afdee3e
Ken Stevens <khstevens@gmail.com>
2020-03-25 15:48:46 -0400

    cleaned up tests

--- hapi-fhir-jpaserver-empi/src/test/java/ca/uhn/fhir/jpa/empi/dao/EmpiLinkDaoTest.java
+++ hapi-fhir-jpaserver-empi/src/test/java/ca/uhn/fhir/jpa/empi/dao/EmpiLinkDaoTest.java
@@ -4,8 +4,14 @@ import ca.uhn.fhir.jpa.api.EmpiMatchResultEnum;
 import ca.uhn.fhir.jpa.api.EmpiLinkSourceEnum;
 import ca.uhn.fhir.jpa.empi.BaseEmpiR4Test;
 import ca.uhn.fhir.jpa.empi.entity.EmpiLink;
+import ca.uhn.fhir.jpa.helper.ResourceTableHelper;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Person;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
 
 public class EmpiLinkDaoTest extends BaseEmpiR4Test {
 	@Autowired
@@ -13,12 +19,14 @@ public class EmpiLinkDaoTest extends BaseEmpiR4Test {
 
 	@Test
 	public void testSave() {
+		Person person = createPerson();
+		Patient patient = createPatient();
+
 		EmpiLink empiLink = new EmpiLink();
 		empiLink.setLinkSource(EmpiLinkSourceEnum.MANUAL);
 		empiLink.setMatchResult(EmpiMatchResultEnum.MATCH);
-		// FIXME KHS get this to work with pids
-		empiLink.setPersonPid(myPersonPid);
-		empiLink.setResourcePid(myPatientPid);
+		empiLink.setPersonPid(ResourceTableHelper.getPidOrNull(person));
+		empiLink.setResourcePid(ResourceTableHelper.getPidOrNull(patient));
 		myEmpiLinkDao.save(empiLink);
 	}
 }
---
+++ hapi-fhir-jpaserver-empi/src/test/java/ca/uhn/fhir/jpa/empi/svc/EmpiCandidateSearchSvcTest.java
@@ -0,0 +1,17 @@
+package ca.uhn.fhir.jpa.empi.svc;
+
+import ca.uhn.fhir.jpa.empi.BaseEmpiR4Test;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class EmpiCandidateSearchSvcTest extends BaseEmpiR4Test {
+	@Autowired
+	EmpiCandidateSearchSvc myEmpiCandidateSearchSvc;
+
+	@Test
+	public void findCandidates() {
+
+	}
+}
