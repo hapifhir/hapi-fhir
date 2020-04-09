c1d682ed02e00abce48ccf48c1fd42c7d0d20378
jamesagnew <jamesagnew@gmail.com>
2020-04-09 12:36:33 -0400

    Test fixes

--- hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/interceptor/ResponseSizeCapturingInterceptorTest.java
+++ hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/interceptor/ResponseSizeCapturingInterceptorTest.java
@@ -1,8 +1,10 @@
 package ca.uhn.fhir.rest.server.interceptor;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.test.utilities.server.HashMapResourceProviderRule;
 import ca.uhn.fhir.test.utilities.server.RestfulServerRule;
+import ca.uhn.test.concurrency.PointcutLatch;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Patient;
@@ -28,13 +30,17 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.waitAtMost;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -63,10 +69,17 @@ public class ResponseSizeCapturingInterceptorTest {
 	}
 
 	@Test
-	public void testReadResource() {
+	public void testReadResource() throws InterruptedException {
+		PointcutLatch createLatch = new PointcutLatch(Pointcut.SERVER_PROCESSING_COMPLETED);
+		createLatch.setExpectedCount(1);
+		ourServerRule.getRestfulServer().getInterceptorService().registerAnonymousInterceptor(Pointcut.SERVER_PROCESSING_COMPLETED, createLatch);
+
 		Patient resource = new Patient();
 		resource.setActive(true);
 		IIdType id = ourServerRule.getFhirClient().create().resource(resource).execute().getId().toUnqualifiedVersionless();
+
+		createLatch.awaitExpected();
+		ourServerRule.getRestfulServer().getInterceptorService().unregisterInterceptor(createLatch);
 
 		myInterceptor.registerConsumer(myConsumer);
 
@@ -84,8 +97,8 @@ public class ResponseSizeCapturingInterceptorTest {
 		resource = ourServerRule.getFhirClient().read().resource(Patient.class).withId(id).execute();
 		assertEquals(true, resource.getActive());
 
-		await().until(()->stacks.size() > 0);
-		await().until(()->stacks.stream().collect(Collectors.joining("\n")), not(matchesPattern(Pattern.compile(".*INVOCATION.*INVOCATION.*", Pattern.MULTILINE | Pattern.DOTALL))));
+		verify(myConsumer, timeout(Duration.ofSeconds(10)).times(1)).accept(myResultCaptor.capture());
+		assertEquals(100, myResultCaptor.getValue().getWrittenChars());
 	}
 
 
