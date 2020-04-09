1167807f450a39593bdc6f2a492ece2b42cb4b95
jamesagnew <jamesagnew@gmail.com>
2020-04-09 07:34:31 -0400

    Try to track down intermittent test failure

--- hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/interceptor/ResponseSizeCapturingInterceptorTest.java
+++ hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/interceptor/ResponseSizeCapturingInterceptorTest.java
@@ -3,6 +3,7 @@ package ca.uhn.fhir.rest.server.interceptor;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.test.utilities.server.HashMapResourceProviderRule;
 import ca.uhn.fhir.test.utilities.server.RestfulServerRule;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.After;
@@ -18,9 +19,22 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -56,11 +70,22 @@ public class ResponseSizeCapturingInterceptorTest {
 
 		myInterceptor.registerConsumer(myConsumer);
 
+		List<String> stacks = Collections.synchronizedList(new ArrayList<>());
+		doAnswer(t->{
+			ResponseSizeCapturingInterceptor.Result result =t.getArgument(0, ResponseSizeCapturingInterceptor.Result.class);
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+				stacks.add("INVOCATION\n" + result.getRequestDetails().getCompleteUrl() + "\n" + ExceptionUtils.getStackTrace(e));
+			}
+			return null;
+		}).when(myConsumer).accept(any());
+
 		resource = ourServerRule.getFhirClient().read().resource(Patient.class).withId(id).execute();
 		assertEquals(true, resource.getActive());
 
-		verify(myConsumer, Mockito.timeout(Duration.ofSeconds(10)).times(1)).accept(myResultCaptor.capture());
-		assertEquals(100, myResultCaptor.getValue().getWrittenChars());
+		await().until(()->stacks.size() > 0);
+		await().until(()->stacks.stream().collect(Collectors.joining("\n")), not(matchesPattern(Pattern.compile(".*INVOCATION.*INVOCATION.*", Pattern.MULTILINE | Pattern.DOTALL))));
 	}
 
 
