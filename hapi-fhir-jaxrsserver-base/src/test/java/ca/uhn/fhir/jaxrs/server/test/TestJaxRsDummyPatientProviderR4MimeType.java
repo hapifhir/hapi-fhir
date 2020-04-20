ff53c3517337e499789756993c3add764630e3c5
axdad <zhe.wang@agfa.com>
2020-04-20 14:41:25 +0200

    added unit test for pull request https://github.com/jamesagnew/hapi-fhir/pull/1704/commits/44f9b416eea74bd5e061427cf7d05ac2dddf73f9

--- hapi-fhir-jaxrsserver-base/src/test/java/ca/uhn/fhir/jaxrs/server/test/TestJaxRsDummyPatientProviderR4MimeType.java
+++ hapi-fhir-jaxrsserver-base/src/test/java/ca/uhn/fhir/jaxrs/server/test/TestJaxRsDummyPatientProviderR4MimeType.java
@@ -3,9 +3,10 @@ package ca.uhn.fhir.jaxrs.server.test;
 /**
  * A dummy patient provider exposing no methods
  */
-public class TestJaxRsDummyPatientProviderR4 extends AbstractDummyPatientProvider {
+public class TestJaxRsDummyPatientProviderR4MimeType extends AbstractDummyPatientProvider {
 
 	@Override public String getBaseForServer() {
-		return "https://fhirserver/fhir/r4";
+		return "https://fhirserver/fhir";
 	}
+
 }
