ea817de68ac3799ebedc3b2c03dcc906a2b3b62f
Thomas Papke <web@thopap.de>
2020-04-29 15:01:39 +0200

    Extend support for additional forwared headers x-forwarded-port and (#1788)
    x-forwarded-prefix

---
+++ hapi-fhir-server/src/test/java/ca/uhn/fhir/rest/server/ApacheProxyAddressStrategyTest.java
@@ -0,0 +1,101 @@
+package ca.uhn.fhir.rest.server;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class ApacheProxyAddressStrategyTest {
+
+	@Test
+	public void testWithoutForwarded() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://localhost/imagingstudy/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwardedHostWithoutForwardedProtoHttps() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://my.example.host/imagingstudy/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwardedHostWithoutForwardedProtoHttp() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				false);
+		MockHttpServletRequest request = prepareRequest();
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("http://my.example.host/imagingstudy/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwarded() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		request.addHeader("X-Forwarded-Proto", "https");
+		request.addHeader("X-Forwarded-Prefix", "server-prefix/fhir");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://my.example.host/server-prefix/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwardedWithHostPrefixWithSlash() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+		request.addHeader("host", "localhost");
+
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		request.addHeader("X-Forwarded-Proto", "https");
+		request.addHeader("X-Forwarded-Prefix", "/server-prefix/fhir");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://my.example.host/server-prefix/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwardedWithoutPrefix() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		request.addHeader("X-Forwarded-Proto", "https");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://my.example.host/imagingstudy/fhir", serverBase);
+	}
+
+	@Test
+	public void testWithForwardedHostAndPort() {
+		ApacheProxyAddressStrategy addressStrategy = new ApacheProxyAddressStrategy(
+				true);
+		MockHttpServletRequest request = prepareRequest();
+
+		request.addHeader("X-Forwarded-Host", "my.example.host");
+		request.addHeader("X-Forwarded-Port", "345");
+		String serverBase = addressStrategy.determineServerBase(null, request);
+		assertEquals("https://my.example.host:345/imagingstudy/fhir",
+				serverBase);
+	}
+
+	private MockHttpServletRequest prepareRequest() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setMethod("POST");
+		request.setScheme("https");
+		request.setServerPort(443);
+		request.setServletPath("/fhir");
+		request.setServerName("localhost");
+		request.setRequestURI("/imagingstudy/fhir/imagingstudy?_format=json");
+		request.setContextPath("/imagingstudy");
+		return request;
+	}
+}
