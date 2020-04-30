ffb3fb9eba4446df582e76dd2b8a9725e4e69848
Ken Stevens <khstevens@gmail.com>
2020-04-30 15:40:31 -0400

    check that we validate bad field names (we do)

--- hapi-fhir-server-empi/src/test/java/ca/uhn/fhir/empi/rules/config/EmpiRuleValidatorTest.java
+++ hapi-fhir-server-empi/src/test/java/ca/uhn/fhir/empi/rules/config/EmpiRuleValidatorTest.java
@@ -2,9 +2,14 @@ package ca.uhn.fhir.empi.rules.config;
 
 import ca.uhn.fhir.context.ConfigurationException;
 import ca.uhn.fhir.empi.rules.json.EmpiRulesJson;
+import com.google.common.base.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -22,9 +27,21 @@ public class EmpiRuleValidatorTest {
 			myEmpiRuleValidator.validate(sampleEmpiRulesJson);
 			fail();
 		} catch (ConfigurationException e){
-			assertThat(e.getMessage(), is(equalTo("Enterprise Identifier System (eidSystem) must be a valid URI")));
-
+			assertThat(e.getMessage(), is("Enterprise Identifier System (eidSystem) must be a valid URI"));
 		}
-
    }
+   
+   @Test
+	public void testNonExistentMatchField() throws IOException {
+		EmpiSettingsImpl empiSettings = new EmpiSettingsImpl();
+		DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource("bad-rules.json");
+		String json = IOUtils.toString(resource.getInputStream(), Charsets.UTF_8);
+		try {
+			empiSettings.setScriptText(json);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertThat(e.getMessage(), is("There is no matchField with name foo"));
+		}
+	}
 }
