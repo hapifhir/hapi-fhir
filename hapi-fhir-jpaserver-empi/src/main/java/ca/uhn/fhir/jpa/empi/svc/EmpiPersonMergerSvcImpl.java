49620500c9bd36d5bf73cf08b86a89bc61cc8139
Ken Stevens <khstevens@gmail.com>
2020-05-05 13:17:46 -0400

    field merging works

--- hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/svc/EmpiPersonMergerSvcImpl.java
+++ hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/svc/EmpiPersonMergerSvcImpl.java
@@ -1,13 +1,20 @@
 package ca.uhn.fhir.jpa.empi.svc;
 
 import ca.uhn.fhir.empi.api.IEmpiPersonMergerSvc;
+import ca.uhn.fhir.empi.util.PersonHelper;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmpiPersonMergerSvcImpl implements IEmpiPersonMergerSvc {
+	@Autowired
+	PersonHelper myPersonHelper;
+
 	@Override
 	public IBaseResource mergePersons(IBaseResource thePersonToDelete, IBaseResource thePersonToKeep) {
+		myPersonHelper.mergePersonFields(thePersonToDelete, thePersonToKeep);
 		return thePersonToKeep;
 	}
+
 }
