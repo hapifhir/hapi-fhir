44869856da2fc01da253281bffd46a27753ad544
IanMMarshall <49525404+IanMMarshall@users.noreply.github.com>
2020-05-22 13:50:24 -0400

    Update hapi-fhir-base/src/main/java/ca/uhn/fhir/interceptor/api/Pointcut.java
    Co-authored-by: James Agnew <jamesagnew@gmail.com>

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/interceptor/api/Pointcut.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/interceptor/api/Pointcut.java
@@ -1282,7 +1282,7 @@ public enum Pointcut {
 
 	/**
 	 * <b>Storage Hook:</b>
-	 * Invoked when a resource delete operation is about to fail due to referential integrity checks. Intended for use with ca.uhn.fhir.jpa.interceptor.CascadingDeleteInterceptor.
+	 * Invoked when a resource delete operation is about to fail due to referential integrity checks. Intended for use with {@literal ca.uhn.fhir.jpa.interceptor.CascadingDeleteInterceptor}.
 	 * <p>
 	 * Hooks will have access to the list of resources that have references to the resource being deleted.
 	 * </p>
