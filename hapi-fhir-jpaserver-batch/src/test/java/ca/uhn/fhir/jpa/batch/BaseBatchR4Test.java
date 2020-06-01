aa7d1cbcb7f9454173ebf6df7f53b95d5f34adeb
Tadgh <garygrantgraham@gmail.com>
2020-06-01 11:32:36 -0700

    Wip getting tasks built

--- hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/BaseBatchR4Test.java
+++ hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/BaseBatchR4Test.java
@@ -1,16 +1,9 @@
 package ca.uhn.fhir.jpa.batch;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.batch.config.BatchJobConfig;
 import ca.uhn.fhir.jpa.batch.config.InMemoryJobRepositoryBatchConfig;
 import ca.uhn.fhir.jpa.batch.svc.DummyService;
-import ca.uhn.fhir.jpa.config.TestJpaR4Config;
-import ca.uhn.fhir.jpa.dao.index.IdHelperService;
-import ca.uhn.fhir.jpa.test.BaseJpaR4Test;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Person;
-import org.hl7.fhir.r4.model.Practitioner;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.batch.core.Job;
@@ -23,23 +16,13 @@ import org.springframework.transaction.PlatformTransactionManager;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BatchJobConfig.class, InMemoryJobRepositoryBatchConfig.class, TestJpaR4Config.class})
-abstract public class BaseBatchR4Test extends BaseJpaR4Test {
+@ContextConfiguration(classes = {BatchJobConfig.class, InMemoryJobRepositoryBatchConfig.class})
+abstract public class BaseBatchR4Test {
  	private static final Logger ourLog = getLogger(BaseBatchR4Test.class);
 
  	@Autowired
 	protected PlatformTransactionManager myPlatformTransactionManager;
 
-	@Autowired
-	protected FhirContext myFhirContext;
-	@Autowired
-	protected IFhirResourceDao<Person> myPersonDao;
-	@Autowired
-	protected IFhirResourceDao<Patient> myPatientDao;
-	@Autowired
-	protected IFhirResourceDao<Practitioner> myPractitionerDao;
-	@Autowired
-	protected IdHelperService myIdHelperService;
 	@Autowired
 	protected DummyService myDummyService;
 	@Autowired
