a458e2af4b28383afbfb030940bfb56294d162a0
jamesagnew <jamesagnew@gmail.com>
2020-06-12 14:27:51 -0400

    Add type parameter to resource reindexing operation

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/BaseJpaSystemProviderDstu2Plus.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/BaseJpaSystemProviderDstu2Plus.java
@@ -27,14 +27,23 @@ import org.hl7.fhir.instance.model.api.IBaseParameters;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IPrimitiveType;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 public abstract class BaseJpaSystemProviderDstu2Plus<T, MT> extends BaseJpaSystemProvider<T, MT> {
 
 
 	@Operation(name = MARK_ALL_RESOURCES_FOR_REINDEXING, idempotent = true, returnParameters = {
 		@OperationParam(name = "status")
 	})
-	public IBaseResource markAllResourcesForReindexing() {
-		getResourceReindexingSvc().markAllResourcesForReindexing();
+	public IBaseResource markAllResourcesForReindexing(
+		@OperationParam(name="type", min = 0, max = 1, typeName = "code") IPrimitiveType<String> theType
+	) {
+
+		if (theType != null && isNotBlank(theType.getValueAsString())) {
+			getResourceReindexingSvc().markAllResourcesForReindexing(theType.getValueAsString());
+		} else {
+			getResourceReindexingSvc().markAllResourcesForReindexing();
+		}
 
 		IBaseParameters retVal = ParametersUtil.newInstance(getContext());
 
