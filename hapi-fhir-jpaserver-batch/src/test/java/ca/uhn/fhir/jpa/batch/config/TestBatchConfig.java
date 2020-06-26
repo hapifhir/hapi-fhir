8f6aee64b132853d72cf6a0c4036289fd6dc6fea
Tadgh <garygrantgraham@gmail.com>
2020-06-26 12:22:23 -0700

    Fix batch project to now use an async task launcher

--- hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/config/TestBatchConfig.java
+++ hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/config/TestBatchConfig.java
@@ -5,6 +5,8 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -14,6 +16,17 @@ public class TestBatchConfig {
 	@Bean
 	public PlatformTransactionManager hapiTransactionManager() {
 		return new ResourcelessTransactionManager();
+	}
+
+	@Bean
+	public TaskExecutor jobLaunchingTaskExecutor() {
+		ThreadPoolTaskExecutor asyncTaskExecutor = new ThreadPoolTaskExecutor();
+		asyncTaskExecutor.setCorePoolSize(5);
+		asyncTaskExecutor.setMaxPoolSize(10);
+		asyncTaskExecutor.setQueueCapacity(500);
+		asyncTaskExecutor.setThreadNamePrefix("JobLauncher-");
+		asyncTaskExecutor.initialize();
+		return asyncTaskExecutor;
 	}
 	@Bean
 	public BatchConfigurer batchConfigurer() {
--- hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/svc/BatchSvcTest.java
+++ hapi-fhir-jpaserver-batch/src/test/java/ca/uhn/fhir/jpa/batch/svc/BatchSvcTest.java
@@ -1,18 +1,14 @@
 package ca.uhn.fhir.jpa.batch.svc;
 
 import ca.uhn.fhir.jpa.batch.BaseBatchR4Test;
-import ca.uhn.fhir.jpa.batch.config.BatchJobConfig;
 import org.junit.Test;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class BatchSvcTest extends BaseBatchR4Test {
-	@Autowired
-	private BatchJobConfig myBatchJobConfig;
 
 	@Test
 	public void testApplicationContextLoads() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, InterruptedException {
