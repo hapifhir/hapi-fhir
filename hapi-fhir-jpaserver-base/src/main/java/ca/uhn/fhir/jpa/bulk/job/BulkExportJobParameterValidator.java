5c14a6c217381c29e7f3d51c80d47e767873bc62
James Agnew <jamesagnew@gmail.com>
2020-07-09 22:14:25 -0400

    Revert pointless SQL optimization (#1977)
    * Revert pointless SQL optimization
    
    * Fix tests

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/job/BulkExportJobParameterValidator.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/job/BulkExportJobParameterValidator.java
@@ -28,8 +28,9 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -39,49 +40,54 @@ import java.util.Optional;
 public class BulkExportJobParameterValidator implements JobParametersValidator {
 	@Autowired
 	private IBulkExportJobDao myBulkExportJobDao;
+	@Autowired
+	private PlatformTransactionManager myTransactionManager;
 
 	@Override
-	@Transactional
 	public void validate(JobParameters theJobParameters) throws JobParametersInvalidException {
 		if (theJobParameters == null) {
 			throw new JobParametersInvalidException("This job needs Parameters: [readChunkSize], [jobUUID], [filters], [outputFormat], [resourceTypes]");
 		}
 
-		StringBuilder errorBuilder = new StringBuilder();
-		Long readChunkSize = theJobParameters.getLong("readChunkSize");
-		if (readChunkSize == null || readChunkSize < 1) {
-			 errorBuilder.append("There must be a valid number for readChunkSize, which is at least 1. ");
-		}
-		String jobUUID = theJobParameters.getString("jobUUID");
-		Optional<BulkExportJobEntity> oJob = myBulkExportJobDao.findByJobId(jobUUID);
-		if (!StringUtils.isBlank(jobUUID) && !oJob.isPresent()) {
-			errorBuilder.append("There is no persisted job that exists with UUID: " + jobUUID + ". ");
-		}
-
-
-		boolean hasExistingJob = oJob.isPresent();
-		//Check for to-be-created parameters.
-		if (!hasExistingJob) {
-			String resourceTypes = theJobParameters.getString("resourceTypes");
-			if (StringUtils.isBlank(resourceTypes)) {
-				errorBuilder.append("You must include [resourceTypes] as a Job Parameter");
-			} else {
-				String[] resourceArray = resourceTypes.split(",");
-				Arrays.stream(resourceArray).filter(resourceType -> resourceType.equalsIgnoreCase("Binary"))
-					.findFirst()
-					.ifPresent(resourceType -> {
-						errorBuilder.append("Bulk export of Binary resources is forbidden");
-					});
+		TransactionTemplate txTemplate = new TransactionTemplate(myTransactionManager);
+		String errorMessage = txTemplate.execute(tx -> {
+			StringBuilder errorBuilder = new StringBuilder();
+			Long readChunkSize = theJobParameters.getLong("readChunkSize");
+			if (readChunkSize == null || readChunkSize < 1) {
+				errorBuilder.append("There must be a valid number for readChunkSize, which is at least 1. ");
 			}
-
-			String outputFormat = theJobParameters.getString("outputFormat");
-			if (!StringUtils.isBlank(outputFormat) && !Constants.CT_FHIR_NDJSON.equals(outputFormat)) {
-				errorBuilder.append("The only allowed format for Bulk Export is currently " + Constants.CT_FHIR_NDJSON);
+			String jobUUID = theJobParameters.getString("jobUUID");
+			Optional<BulkExportJobEntity> oJob = myBulkExportJobDao.findByJobId(jobUUID);
+			if (!StringUtils.isBlank(jobUUID) && !oJob.isPresent()) {
+				errorBuilder.append("There is no persisted job that exists with UUID: " + jobUUID + ". ");
 			}
 
 
-		}
-		String errorMessage = errorBuilder.toString();
+			boolean hasExistingJob = oJob.isPresent();
+			//Check for to-be-created parameters.
+			if (!hasExistingJob) {
+				String resourceTypes = theJobParameters.getString("resourceTypes");
+				if (StringUtils.isBlank(resourceTypes)) {
+					errorBuilder.append("You must include [resourceTypes] as a Job Parameter");
+				} else {
+					String[] resourceArray = resourceTypes.split(",");
+					Arrays.stream(resourceArray).filter(resourceType -> resourceType.equalsIgnoreCase("Binary"))
+						.findFirst()
+						.ifPresent(resourceType -> {
+							errorBuilder.append("Bulk export of Binary resources is forbidden");
+						});
+				}
+
+				String outputFormat = theJobParameters.getString("outputFormat");
+				if (!StringUtils.isBlank(outputFormat) && !Constants.CT_FHIR_NDJSON.equals(outputFormat)) {
+					errorBuilder.append("The only allowed format for Bulk Export is currently " + Constants.CT_FHIR_NDJSON);
+				}
+
+
+			}
+			return errorBuilder.toString();
+		});
+
 		if (!StringUtils.isEmpty(errorMessage)) {
 			throw new JobParametersInvalidException(errorMessage);
 		}
