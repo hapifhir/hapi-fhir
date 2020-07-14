d3a34c17342a9ca6f7caa0ab1502ea12f559a7c7
Ken Stevens <khstevens@gmail.com>
2020-07-14 18:49:50 -0400

    review feedback javadoc

--- hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/svc/candidate/CandidateStrategyEnum.java
+++ hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/svc/candidate/CandidateStrategyEnum.java
@@ -1,7 +1,10 @@
 package ca.uhn.fhir.jpa.empi.svc.candidate;
 
 public enum CandidateStrategyEnum {
+	/** Find Person candidates based on matching EID */
 	EID,
+	/** Find Person candidates based on a link already existing for the target resource */
 	LINK,
+	/** Find Person candidates based on other targets that match the incoming target using the EMPI Matching rules */
 	SCORE
 }
