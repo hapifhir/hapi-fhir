f5222f3105ec08dfd84bef887e128516625827cc
jamesagnew <jamesagnew@gmail.com>
2020-07-17 08:27:10 -0400

    License headeer update

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/GraphQLQueryUrl.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/GraphQLQueryUrl.java
@@ -16,7 +16,7 @@ package ca.uhn.fhir.rest.annotation;
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the LBoicense.
+ * limitations under the License.
  * #L%
  */
 
