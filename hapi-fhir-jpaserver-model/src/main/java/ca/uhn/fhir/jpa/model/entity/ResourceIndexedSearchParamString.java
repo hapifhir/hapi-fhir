2134d40f1ccece656e9265eb53ead05cc70f3bdd
James Agnew <jamesagnew@gmail.com>
2020-10-09 07:52:46 -0400

    Account for timezones in lastUpdated search (#2113)
    * Add test
    
    * Fix
    
    * Add changelog
    
    * Test fixes
    
    * Test fix
    
    * Test fixes
    
    * Resolve fixme
    
    * Test fixes
    
    * Remove debug logs
    
    * Try to get CI tests passing again

--- hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamString.java
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamString.java
@@ -261,7 +261,7 @@ public class ResourceIndexedSearchParamString extends BaseResourceIndexedSearchP
 	}
 
 	@Override
-	public boolean matches(IQueryParameterType theParam, boolean theUseOrdinalDatesForDayComparison) {
+	public boolean matches(IQueryParameterType theParam) {
 		if (!(theParam instanceof StringParam)) {
 			return false;
 		}
