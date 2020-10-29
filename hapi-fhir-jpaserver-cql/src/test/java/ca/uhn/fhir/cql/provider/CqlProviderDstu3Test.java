ec72a6126dc80e30a1954b754992702e8005d4c7
Kevin Dougan <kevin.dougan@smilecdr.com>
2020-10-29 15:57:43 -0400

    Unit Tests.

--- hapi-fhir-jpaserver-cql/src/test/java/ca/uhn/fhir/cql/provider/CqlProviderDstu3Test.java
+++ hapi-fhir-jpaserver-cql/src/test/java/ca/uhn/fhir/cql/provider/CqlProviderDstu3Test.java
@@ -1,10 +1,20 @@
 package ca.uhn.fhir.cql.provider;
 
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.cql.BaseCqlDstu3Test;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import ca.uhn.fhir.jpa.api.dao.IFhirSystemDao;
+import ca.uhn.fhir.model.primitive.IdDt;
+import com.google.common.base.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.DateType;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.MeasureReport;
+import org.hl7.fhir.dstu3.model.Parameters;
+import org.hl7.fhir.dstu3.model.StringType;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +25,17 @@ import org.opencds.cqf.dstu3.providers.MeasureOperationsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CqlProviderDstu3Test extends BaseCqlDstu3Test {
 	private static final Logger ourLog = LoggerFactory.getLogger(CqlProviderDstu3Test.class);
@@ -27,6 +46,12 @@ public class CqlProviderDstu3Test extends BaseCqlDstu3Test {
 	DaoRegistry myDaoRegistry;
 	@Autowired
 	IFhirResourceDao<Patient> myPatientDao;
+	@Autowired
+	FhirContext myFhirContext;
+	@Autowired
+	IFhirSystemDao mySystemDao;
+	@Autowired
+	PlatformTransactionManager myPlatformTransactionManager;
 
 	private EvaluationProviderFactory evaluationProviderFactory;
 	MeasureOperationsProvider myProvider;
@@ -53,5 +78,35 @@ public class CqlProviderDstu3Test extends BaseCqlDstu3Test {
 			null, null, null, null);
 		assertNotNull(measureReport);
 		// FIXME KBD assert on stuff in measureReport
+	}
+
+	@Test
+	public void evaluatePatientBundleMeasure() throws IOException {
+		DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource("patient-measure-test-bundle.json");
+		String json = IOUtils.toString(resource.getInputStream(), Charsets.UTF_8);
+		Bundle bundle = (Bundle) myFhirContext.newJsonParser().parseResource(json);
+		System.out.println("Got the JSON");
+		Bundle result = (Bundle) mySystemDao.transaction(null, bundle);
+		assertNotNull(result);
+		List<Bundle.BundleEntryComponent> entries = result.getEntry();
+		assertThat(entries, hasSize(24));
+		assertEquals(entries.get(0).getResponse().getStatus(), "201 Created");
+		assertEquals(entries.get(23).getResponse().getStatus(), "201 Created");
+
+		IdType measureId = new IdType();
+		measureId.setId("Measure/measureId-asf");
+		String patient = "Patient/Patient-6529";
+		String periodStart = "2003-01-01";
+		String periodEnd = "2003-12-31";
+
+		MeasureReport report = myProvider.evaluateMeasure(measureId, periodStart, periodEnd, null, null,
+			patient, null, null, null, null, null, null);
+
+		for (MeasureReport.MeasureReportGroupComponent group : report.getGroup()) {
+			for (MeasureReport.MeasureReportGroupPopulationComponent population : group.getPopulation()) {
+				assertTrue(population.getCount() > 0);
+			}
+		}
 	}
 }
