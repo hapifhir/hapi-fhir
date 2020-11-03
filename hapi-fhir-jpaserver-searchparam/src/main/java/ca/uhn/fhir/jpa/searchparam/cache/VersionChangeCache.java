8e6c50945a621fa064d28e145b1c4a7a85b56bd8
Kevin Dougan <kevin.dougan@smilecdr.com>
2020-11-03 12:27:00 -0500

    Added code to VersionChangeCache class.

--- hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/cache/VersionChangeCache.java
+++ hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/cache/VersionChangeCache.java
@@ -7,4 +7,20 @@ import java.util.Map;
 
 public class VersionChangeCache {
 	private final Map<IIdType, Long> myVersionMap = new HashMap<>();
+
+	public void clear() {
+		myVersionMap.clear();
+	}
+
+	public Long addOrUpdate(IIdType theIIdType, Long theVersion) {
+		return myVersionMap.put(theIIdType, theVersion);
+	}
+
+	public Long get(IIdType theIIdType) {
+		return myVersionMap.get(theIIdType);
+	}
+
+	public Long remove(IIdType theIIdType) {
+		return myVersionMap.remove(theIIdType);
+	}
 }
