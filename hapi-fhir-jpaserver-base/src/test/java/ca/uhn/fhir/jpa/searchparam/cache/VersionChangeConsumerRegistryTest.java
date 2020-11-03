06b4833ad6e5f5f8bfd76191a5ddc350909f32f8
Ken Stevens <khstevens@gmail.com>
2020-11-03 14:43:58 -0500

    added polling test

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/searchparam/cache/VersionChangeConsumerRegistryTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/searchparam/cache/VersionChangeConsumerRegistryTest.java
@@ -1,8 +1,11 @@
 package ca.uhn.fhir.jpa.searchparam.cache;
 
+import ca.uhn.fhir.interceptor.api.HookParams;
 import ca.uhn.fhir.jpa.dao.r4.BaseJpaR4Test;
 import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
 import ca.uhn.fhir.rest.server.messaging.BaseResourceMessage;
+import ca.uhn.test.concurrency.IPointcutLatch;
+import ca.uhn.test.concurrency.PointcutLatch;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.Patient;
@@ -11,25 +14,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VersionChangeConsumerRegistryTest extends BaseJpaR4Test {
 	@Autowired
 	IVersionChangeConsumerRegistry myVersionChangeConsumerRegistry;
 
-	IIdType myResourceId;
-	BaseResourceMessage.OperationTypeEnum myOperationTypeEnum;
+	private final TestCallback myTestCallback = new TestCallback();
 
 	@BeforeEach
 	public void before() {
-		myResourceId = null;
-		myOperationTypeEnum = null;
-
-		IVersionChangeConsumer callback = (theResourceId, theOperationType) -> {
-			myResourceId = theResourceId;
-			myOperationTypeEnum = theOperationType;
-		};
-		myVersionChangeConsumerRegistry.registerResourceVersionChangeConsumer("Patient", SearchParameterMap.newSynchronous(), callback);
+		myTestCallback.clear();
+		myVersionChangeConsumerRegistry.registerResourceVersionChangeConsumer("Patient", SearchParameterMap.newSynchronous(), myTestCallback);
 	}
 
 	@AfterEach
@@ -38,35 +36,90 @@ public class VersionChangeConsumerRegistryTest extends BaseJpaR4Test {
 	}
 
 	@Test
-	public void testRegisterInterceptor() {
+	public void testRegisterInterceptor() throws InterruptedException {
 		Patient patient = new Patient();
 		patient.setActive(true);
-		IIdType patientId = myPatientDao.create(patient).getId();
-		assertEquals(myResourceId, patientId);
-		assertEquals(1L, myResourceId.getVersionIdPartAsLong());
-		assertEquals(myOperationTypeEnum, BaseResourceMessage.OperationTypeEnum.CREATE);
+
+		IIdType patientId = createPatientWithLatch(patient);
+		IIdType resourceId = myTestCallback.getResourceId();
+		BaseResourceMessage.OperationTypeEnum operationTypeEnum = myTestCallback.getOperationTypeEnum();
+		assertEquals(resourceId, patientId);
+		assertEquals(1L, resourceId.getVersionIdPartAsLong());
+		assertEquals(operationTypeEnum, BaseResourceMessage.OperationTypeEnum.CREATE);
 
 		patient.setActive(false);
 		patient.setGender(Enumerations.AdministrativeGender.FEMALE);
 		myPatientDao.update(patient);
-		assertEquals(2L, myResourceId.getVersionIdPartAsLong());
-		assertEquals(myOperationTypeEnum, BaseResourceMessage.OperationTypeEnum.UPDATE);
+		assertEquals(2L, resourceId.getVersionIdPartAsLong());
+		assertEquals(operationTypeEnum, BaseResourceMessage.OperationTypeEnum.UPDATE);
 
 		myPatientDao.delete(patientId);
-		assertEquals(2L, myResourceId.getVersionIdPartAsLong());
-		assertEquals(myOperationTypeEnum, BaseResourceMessage.OperationTypeEnum.DELETE);
+		assertEquals(2L, resourceId.getVersionIdPartAsLong());
+		assertEquals(operationTypeEnum, BaseResourceMessage.OperationTypeEnum.DELETE);
+	}
+
+	private IIdType createPatientWithLatch(Patient thePatient) throws InterruptedException {
+		myTestCallback.setExpectedCount(1);
+		IIdType retval = myPatientDao.create(thePatient).getId();
+		myTestCallback.awaitExpected();
+		return retval;
 	}
 
 	@Test
-	public void testRegisterPolling() {
+	public void testRegisterPolling() throws InterruptedException {
 		Patient patient = new Patient();
 		patient.setActive(true);
-		IIdType patientId = myPatientDao.create(patient).getId();
-		assertEquals(myResourceId, patientId);
-		assertEquals(1L, myResourceId.getVersionIdPartAsLong());
-		assertEquals(myOperationTypeEnum, BaseResourceMessage.OperationTypeEnum.CREATE);
+		IIdType patientId = createPatientWithLatch(patient);
+		IIdType resourceId = myTestCallback.getResourceId();
+		BaseResourceMessage.OperationTypeEnum operationTypeEnum = myTestCallback.getOperationTypeEnum();
 
+		assertEquals(resourceId, patientId);
+		assertEquals(1L, resourceId.getVersionIdPartAsLong());
+		assertEquals(operationTypeEnum, BaseResourceMessage.OperationTypeEnum.CREATE);
+
+		myTestCallback.setExpectedCount(1);
 		myVersionChangeConsumerRegistry.forceRefresh();
-		// FIXME KHS switch to latch and assert latch got called by forced refresh
+		List<HookParams> calledWith = myTestCallback.awaitExpected();
+		IIdType calledWithId  = (IIdType) PointcutLatch.getLatchInvocationParameter(calledWith);
+		assertEquals(patientId, calledWithId);
+	}
+
+	private static class TestCallback implements IVersionChangeConsumer, IPointcutLatch {
+		private final PointcutLatch myLatch = new PointcutLatch("VersionChangeConsumer called");
+
+		private IIdType myResourceId;
+		private BaseResourceMessage.OperationTypeEnum myOperationTypeEnum;
+
+		@Override
+		public void accept(IIdType theResourceId, BaseResourceMessage.OperationTypeEnum theOperationType) {
+			myResourceId = theResourceId;
+			myOperationTypeEnum = theOperationType;
+			myLatch.call(theResourceId);
+		}
+
+		@Override
+		public void clear() {
+			myResourceId = null;
+			myOperationTypeEnum = null;
+			myLatch.clear();
+		}
+
+		@Override
+		public void setExpectedCount(int count) {
+			myLatch.setExpectedCount(count);
+		}
+
+		@Override
+		public List<HookParams> awaitExpected() throws InterruptedException {
+			return myLatch.awaitExpected();
+		}
+
+		public IIdType getResourceId() {
+			return myResourceId;
+		}
+
+		public BaseResourceMessage.OperationTypeEnum getOperationTypeEnum() {
+			return myOperationTypeEnum;
+		}
 	}
 }
