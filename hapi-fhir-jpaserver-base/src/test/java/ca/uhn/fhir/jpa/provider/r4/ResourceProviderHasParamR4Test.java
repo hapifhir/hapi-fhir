3274fbe342e1482dcba8168c0ffffdf17f18c052
Frank Tao <frankjtao@gmail.com>
2020-11-02 22:12:53 -0500

    Fixed composite search for the new SearchBuilder

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderHasParamR4Test.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderHasParamR4Test.java
@@ -2,48 +2,42 @@ package ca.uhn.fhir.jpa.provider.r4;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Device;
 import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Observation.ObservationComponentComponent;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Period;
+import org.hl7.fhir.r4.model.Quantity;
 import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.StringType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import ca.uhn.fhir.jpa.api.config.DaoConfig;
-import ca.uhn.fhir.jpa.dao.data.ISearchDao;
-import ca.uhn.fhir.model.primitive.InstantDt;
 import ca.uhn.fhir.parser.StrictErrorHandler;
 import ca.uhn.fhir.rest.client.interceptor.CapturingInterceptor;
 import ca.uhn.fhir.util.UrlUtil;
 
-@SuppressWarnings("Duplicates")
+
 public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 
-	public static final int LARGE_NUMBER = 77;
 	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ResourceProviderHasParamR4Test.class);
 	private CapturingInterceptor myCapturingInterceptor = new CapturingInterceptor();
-	@Autowired
-	private ISearchDao mySearchEntityDao;
 
 	@Override
 	@AfterEach
@@ -79,17 +73,6 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 	}
 
 
-	private ArrayList<IBaseResource> genResourcesOfType(Bundle theRes, Class<? extends IBaseResource> theClass) {
-		ArrayList<IBaseResource> retVal = new ArrayList<>();
-		for (BundleEntryComponent next : theRes.getEntry()) {
-			if (next.getResource() != null) {
-				if (theClass.isAssignableFrom(next.getResource().getClass())) {
-					retVal.add(next.getResource());
-				}
-			}
-		}
-		return retVal;
-	}
 	@Test
 	public void testHasParameter() throws Exception {
 		
@@ -169,8 +152,8 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 			encounter.getClass_().setCode("IMP").setSystem("urn:system");
 			encounter.getPeriod().setStartElement(new DateTimeType("2020-01-01")).setEndElement(new DateTimeType("2020-09-30"));
 			encounter.setSubject(new Reference(pid0));
-			
-			IIdType encounterId = myEncounterDao.create(encounter, mySrd).getId().toUnqualifiedVersionless();
+						
+			myEncounterDao.create(encounter, mySrd);
 			
 			ourLog.info("Encounter: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(encounter));
 		}
@@ -180,6 +163,7 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 		ourLog.info("uri = " + uri);
 		
 		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
+		System.out.println("ids.size() = " + ids.size());
 		assertThat(ids, contains(pid0.getValue()));
 	}
 	
@@ -227,9 +211,8 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 			encounter.getClass_().setCode("IMP").setSystem("urn:system");
 			encounter.getPeriod().setStartElement(new DateTimeType("2020-01-01")).setEndElement(new DateTimeType("2020-09-30"));
 			encounter.setSubject(new Reference(pid0));
-			
-			IIdType encounterId = myEncounterDao.create(encounter, mySrd).getId().toUnqualifiedVersionless();
-			
+						
+			myEncounterDao.create(encounter, mySrd);
 			ourLog.info("Encounter: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(encounter));
 		}
 		
@@ -242,8 +225,7 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 	}
 	
 	@Test
-	@Disabled
-	public void testHasCompositeParameter() throws Exception {
+	public void testHasCompositeParameterCodeValueQuantity() throws Exception {
 		IIdType pid0;
 		{
 			Patient patient = new Patient();
@@ -263,6 +245,11 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 			obs.getSubject().setReferenceElement(pid0);
 			CodeableConcept cc = obs.getCode();
 			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+			
+			Quantity q = new Quantity();
+			q.setValue(200);
+			obs.setValue(q);
+			
 			myObservationDao.create(obs, mySrd);
 			
 			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
@@ -279,31 +266,223 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 		}
 
 		String uri = ourServerBase + "/Patient?_has:Observation:subject:code-value-quantity=http://" + UrlUtil.escapeUrlParam("loinc.org|2345-7$gt180");
-		
-		System.out.println(uri);
-		
+		ourLog.info("uri = " + uri);
 		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
 		assertThat(ids, contains(pid0.getValue()));
 	}
 
-	
-	
-	
-	private List<String> searchAndReturnUnqualifiedIdValues(String theUri) throws IOException {
-		List<String> ids;
-		HttpGet get = new HttpGet(theUri);
-
-		ourLog.info("About to perform search for: {}", theUri);
-
-		try (CloseableHttpResponse response = ourHttpClient.execute(get)) {
-			String resp = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
-			ourLog.info(resp);
-			Bundle bundle = myFhirCtx.newXmlParser().parseResource(Bundle.class, resp);
-			ids = toUnqualifiedIdValues(bundle);
+	@Test
+	public void testHasCompositeParameterCodeValueDate() throws Exception {
+		IIdType pid0;
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			pid0 = myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
 		}
-		return ids;
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("FOO");
+			obs.getSubject().setReferenceElement(pid0);
+			CodeableConcept cc = obs.getCode();
+			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+						
+			Period period = new Period();
+			period.setStartElement(new DateTimeType("2020-01-01"));
+			period.setEndElement(new DateTimeType("2020-09-30"));
+			obs.setValue(period);
+						
+			myObservationDao.create(obs, mySrd);
+			
+			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
+		}
+		{
+			Device device = new Device();
+			device.addIdentifier().setValue("DEVICEID");
+			IIdType devId = myDeviceDao.create(device, mySrd).getId().toUnqualifiedVersionless();
+
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("NOLINK");
+			obs.setDevice(new Reference(devId));
+			myObservationDao.create(obs, mySrd);
+		}
+
+		String uri = ourServerBase + "/Patient?_has:Observation:subject:code-value-date=http://" + UrlUtil.escapeUrlParam("loinc.org|2345-7$gt2019");
+		ourLog.info("uri = " + uri);
+		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
+		assertThat(ids, contains(pid0.getValue()));
 	}
 
+	@Test
+	public void testHasCompositeParameterCompCodeValueQuantity() throws Exception {
+		IIdType pid0;
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			pid0 = myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("FOO");
+			obs.getSubject().setReferenceElement(pid0);
+			CodeableConcept cc = obs.getCode();
+			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+						
+			Period period = new Period();
+			period.setStartElement(new DateTimeType("2020-01-01"));
+			period.setEndElement(new DateTimeType("2020-09-30"));
+			obs.setValue(period);
+						
+			ObservationComponentComponent comp = obs.addComponent();
+			CodeableConcept compCC = comp.getCode();
+			compCC.addCoding().setCode("2345-8").setSystem("http://loinc.org");
+			
+			Quantity q = new Quantity();
+			q.setValue(200);
+			comp.setValue(q);
+			
+			myObservationDao.create(obs, mySrd);
+			
+			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
+		}
+		{
+			Device device = new Device();
+			device.addIdentifier().setValue("DEVICEID");
+			IIdType devId = myDeviceDao.create(device, mySrd).getId().toUnqualifiedVersionless();
+
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("NOLINK");
+			obs.setDevice(new Reference(devId));
+			myObservationDao.create(obs, mySrd);
+		}
+
+		String uri = ourServerBase + "/Patient?_has:Observation:subject:combo-code-value-quantity=http://" + UrlUtil.escapeUrlParam("loinc.org|2345-8$200");
+		ourLog.info("uri = " + uri);
+		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
+		assertThat(ids, contains(pid0.getValue()));
+	}
+
+	@Test
+	public void testHasCompositeParameterCodeValueStringOr() throws Exception {
+		IIdType pid0;
+		IIdType pid1;
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			pid0 = myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			pid1 = myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("FOO");
+			obs.getSubject().setReferenceElement(pid0);
+			CodeableConcept cc = obs.getCode();
+			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+			
+			obs.setValue(new StringType("100"));
+			
+			myObservationDao.create(obs, mySrd);
+			
+			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
+		}
+		
+		{
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("BAR");
+			obs.getSubject().setReferenceElement(pid1);
+			CodeableConcept cc = obs.getCode();
+			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+			
+			obs.setValue(new StringType("200"));
+			
+			myObservationDao.create(obs, mySrd);
+			
+			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
+		}
+		{
+			Device device = new Device();
+			device.addIdentifier().setValue("DEVICEID");
+			IIdType devId = myDeviceDao.create(device, mySrd).getId().toUnqualifiedVersionless();
+
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("NOLINK");
+			obs.setDevice(new Reference(devId));
+			myObservationDao.create(obs, mySrd);
+		}
+
+		String uri = ourServerBase + "/Patient?_has:Observation:subject:code-value-string=http://" + UrlUtil.escapeUrlParam("loinc.org|2345-7$100,http://loinc.org|2345-7$200");
+		ourLog.info("uri = " + uri);
+		
+		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
+		assertEquals(2, ids.size());
+	}
+	
+	@Test
+	public void testHasCompositeParameterWithNoResult() throws Exception {
+		IIdType pid0;
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			pid0 = myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Patient patient = new Patient();
+			patient.addIdentifier().setSystem("urn:system").setValue("001");
+			patient.addName().setFamily("Tester").addGiven("Joe");
+			myPatientDao.create(patient, mySrd).getId().toUnqualifiedVersionless();
+		}
+		{
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("FOO");
+			obs.getSubject().setReferenceElement(pid0);
+			CodeableConcept cc = obs.getCode();
+			cc.addCoding().setCode("2345-7").setSystem("http://loinc.org");
+			
+			Quantity q = new Quantity();
+			q.setValue(200);
+			obs.setValue(q);
+			
+			myObservationDao.create(obs, mySrd);
+			
+			ourLog.info("Observation: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(obs));
+		}
+		{
+			Device device = new Device();
+			device.addIdentifier().setValue("DEVICEID");
+			IIdType devId = myDeviceDao.create(device, mySrd).getId().toUnqualifiedVersionless();
+
+			Observation obs = new Observation();
+			obs.addIdentifier().setSystem("urn:system").setValue("NOLINK");
+			obs.setDevice(new Reference(devId));
+			myObservationDao.create(obs, mySrd);
+		}
+
+		String uri = ourServerBase + "/Patient?_has:Observation:subject:code-value-quantity=http://" + UrlUtil.escapeUrlParam("loinc.org|2345-7$lt180");
+		ourLog.info("uri = " + uri);
+		List<String> ids = searchAndReturnUnqualifiedVersionlessIdValues(uri);
+		assertEquals(0, ids.size());
+	}
+	
 	private List<String> searchAndReturnUnqualifiedVersionlessIdValues(String uri) throws IOException {
 		List<String> ids;
 		HttpGet get = new HttpGet(uri);
@@ -315,10 +494,6 @@ public class ResourceProviderHasParamR4Test extends BaseResourceProviderR4Test {
 			ids = toUnqualifiedVersionlessIdValues(bundle);
 		}
 		return ids;
-	}
-
-	private String toStr(Date theDate) {
-		return new InstantDt(theDate).getValueAsString();
 	}
 
 
