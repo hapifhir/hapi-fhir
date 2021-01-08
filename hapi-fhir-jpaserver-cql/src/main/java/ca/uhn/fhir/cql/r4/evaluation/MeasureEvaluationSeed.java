99001eead3795aeb36b90f9b0ba99126de193448
JPercival <JPercival@users.noreply.github.com>
2021-01-08 05:37:07 -0700

    Fix bugs, add changes from ruler, fix more discrepancies (#2275)

--- hapi-fhir-jpaserver-cql/src/main/java/ca/uhn/fhir/cql/r4/evaluation/MeasureEvaluationSeed.java
+++ hapi-fhir-jpaserver-cql/src/main/java/ca/uhn/fhir/cql/r4/evaluation/MeasureEvaluationSeed.java
@@ -20,92 +20,93 @@ import java.util.Date;
 import java.util.List;
 
 public class MeasureEvaluationSeed {
-    private Measure measure;
-    private Context context;
-    private Interval measurementPeriod;
-    private LibraryLoader libraryLoader;
-    private LibraryResolutionProvider<org.hl7.fhir.r4.model.Library> libraryResourceProvider;
-    private EvaluationProviderFactory providerFactory;
-    private DataProvider dataProvider;
+	private Measure measure;
+	private Context context;
+	private Interval measurementPeriod;
+	private LibraryLoader libraryLoader;
+	private LibraryResolutionProvider<org.hl7.fhir.r4.model.Library> libraryResourceProvider;
+	private EvaluationProviderFactory providerFactory;
+	private DataProvider dataProvider;
 
-    public MeasureEvaluationSeed(EvaluationProviderFactory providerFactory, LibraryLoader libraryLoader,
-            LibraryResolutionProvider<org.hl7.fhir.r4.model.Library> libraryResourceProvider) {
-        this.providerFactory = providerFactory;
-        this.libraryLoader = libraryLoader;
-        this.libraryResourceProvider = libraryResourceProvider;
-    }
+	public MeasureEvaluationSeed(EvaluationProviderFactory providerFactory, LibraryLoader libraryLoader,
+			LibraryResolutionProvider<org.hl7.fhir.r4.model.Library> libraryResourceProvider) {
+		this.providerFactory = providerFactory;
+		this.libraryLoader = libraryLoader;
+		this.libraryResourceProvider = libraryResourceProvider;
+	}
 
-    public Measure getMeasure() {
-        return this.measure;
-    }
+	public Measure getMeasure() {
+		return this.measure;
+	}
 
-    public Context getContext() {
-        return this.context;
-    }
+	public Context getContext() {
+		return this.context;
+	}
 
-    public Interval getMeasurementPeriod() {
-        return this.measurementPeriod;
-    }
+	public Interval getMeasurementPeriod() {
+		return this.measurementPeriod;
+	}
 
-    public DataProvider getDataProvider() {
-        return this.dataProvider;
-    }
+	public DataProvider getDataProvider() {
+		return this.dataProvider;
+	}
 
-    public void setup(Measure measure, String periodStart, String periodEnd, String productLine, String source,
-            String user, String pass) {
-        this.measure = measure;
+	public void setup(Measure measure, String periodStart, String periodEnd, String productLine, String source,
+			String user, String pass) {
+		this.measure = measure;
 
-        LibraryHelper.loadLibraries(measure, this.libraryLoader, this.libraryResourceProvider);
+		LibraryHelper.loadLibraries(measure, this.libraryLoader, this.libraryResourceProvider);
 
-        // resolve primary library
-        Library library = LibraryHelper.resolvePrimaryLibrary(measure, libraryLoader, this.libraryResourceProvider);
+		// resolve primary library
+		Library library = LibraryHelper.resolvePrimaryLibrary(measure, libraryLoader, this.libraryResourceProvider);
 
-        // resolve execution context
-        context = new Context(library);
-        context.registerLibraryLoader(libraryLoader);
+		// resolve execution context
+		context = new Context(library);
+		context.registerLibraryLoader(libraryLoader);
 
-        List<Triple<String, String, String>> usingDefs = UsingHelper.getUsingUrlAndVersion(library.getUsings());
+		List<Triple<String, String, String>> usingDefs = UsingHelper.getUsingUrlAndVersion(library.getUsings());
 
-        if (usingDefs.size() > 1) {
-            throw new IllegalArgumentException(
-                    "Evaluation of Measure using multiple Models is not supported at this time.");
-        }
+		if (usingDefs.size() > 1) {
+			throw new IllegalArgumentException(
+					"Evaluation of Measure using multiple Models is not supported at this time.");
+		}
 
-        // If there are no Usings, there is probably not any place the Terminology
-        // actually used so I think the assumption that at least one provider exists is
-        // ok.
-        TerminologyProvider terminologyProvider = null;
-        if (usingDefs.size() > 0) {
-            // Creates a terminology provider based on the first using statement. This
-            // assumes the terminology
-            // server matches the FHIR version of the CQL.
-            terminologyProvider = this.providerFactory.createTerminologyProvider(usingDefs.get(0).getLeft(),
-                    usingDefs.get(0).getMiddle(), source, user, pass);
-            context.registerTerminologyProvider(terminologyProvider);
-        }
+		// If there are no Usings, there is probably not any place the Terminology
+		// actually used so I think the assumption that at least one provider exists is
+		// ok.
+		TerminologyProvider terminologyProvider = null;
+		if (usingDefs.size() > 0) {
+			// Creates a terminology provider based on the first using statement. This
+			// assumes the terminology
+			// server matches the FHIR version of the CQL.
+			terminologyProvider = this.providerFactory.createTerminologyProvider(usingDefs.get(0).getLeft(),
+					usingDefs.get(0).getMiddle(), source, user, pass);
+			context.registerTerminologyProvider(terminologyProvider);
+		}
 
-        for (Triple<String, String, String> def : usingDefs) {
-            this.dataProvider = this.providerFactory.createDataProvider(def.getLeft(), def.getMiddle(),
-                    terminologyProvider);
-            context.registerDataProvider(def.getRight(), dataProvider);
-        }
+		for (Triple<String, String, String> def : usingDefs) {
+			this.dataProvider = this.providerFactory.createDataProvider(def.getLeft(), def.getMiddle(),
+					terminologyProvider);
+			context.registerDataProvider(def.getRight(), dataProvider);
+		}
 
-        // resolve the measurement period
-        measurementPeriod = new Interval(DateHelper.resolveRequestDate(periodStart, true), true,
-			  DateHelper.resolveRequestDate(periodEnd, false), true);
+		// resolve the measurement period
+		measurementPeriod = new Interval(DateHelper.resolveRequestDate(periodStart, true), true,
+				DateHelper.resolveRequestDate(periodEnd, false), true);
 
-        context.setParameter(null, "Measurement Period",
-                new Interval(DateTime.fromJavaDate((Date) measurementPeriod.getStart()), true,
-                        DateTime.fromJavaDate((Date) measurementPeriod.getEnd()), true));
+		context.setParameter(null, "Measurement Period",
+				new Interval(DateTime.fromJavaDate((Date) measurementPeriod.getStart()), true,
+						DateTime.fromJavaDate((Date) measurementPeriod.getEnd()), true));
 
-        if (productLine != null) {
-            context.setParameter(null, "Product Line", productLine);
-        }
+		if (productLine != null) {
+			context.setParameter(null, "Product Line", productLine);
+		}
 
-        context.setExpressionCaching(true);
+		context.setExpressionCaching(true);
 
-        DebugMap debugMap = new DebugMap();
-        debugMap.setIsLoggingEnabled(true);
-        context.setDebugMap(debugMap);
-    }
+		// This needs to be made configurable
+		DebugMap debugMap = new DebugMap();
+		debugMap.setIsLoggingEnabled(true);
+		context.setDebugMap(debugMap);
+	}
 }
