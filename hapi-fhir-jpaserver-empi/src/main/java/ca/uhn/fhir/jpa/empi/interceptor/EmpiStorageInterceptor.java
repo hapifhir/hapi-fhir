7e7e771f5ab4a6437cf08230cdc5e99f08001bfc
Tadgh <garygrantgraham@gmail.com>
2021-01-20 14:41:57 -0500

    Update copyright

--- hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/interceptor/EmpiStorageInterceptor.java
+++ hapi-fhir-jpaserver-empi/src/main/java/ca/uhn/fhir/jpa/empi/interceptor/EmpiStorageInterceptor.java
@@ -4,7 +4,7 @@ package ca.uhn.fhir.jpa.empi.interceptor;
  * #%L
  * HAPI FHIR JPA Server - Enterprise Master Patient Index
  * %%
- * Copyright (C) 2014 - 2020 University Health Network
+ * Copyright (C) 2014 - 2021 University Health Network
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
