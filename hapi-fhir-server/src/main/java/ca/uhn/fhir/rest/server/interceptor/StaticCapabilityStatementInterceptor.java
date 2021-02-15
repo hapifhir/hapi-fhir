7ce4607afa65d573455b3fcf552315aec5d546e5
jamesagnew <jamesagnew@gmail.com>
2021-02-15 13:27:26 -0500

    Fix javadocs

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/StaticCapabilityStatementInterceptor.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/StaticCapabilityStatementInterceptor.java
@@ -1,5 +1,25 @@
 package ca.uhn.fhir.rest.server.interceptor;
 
+/*-
+ * #%L
+ * HAPI FHIR - Server Framework
+ * %%
+ * Copyright (C) 2014 - 2021 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
@@ -23,7 +43,7 @@ public class StaticCapabilityStatementInterceptor {
 	/**
 	 * Sets the CapabilityStatement to use
 	 *
-	 * @see #setCapabilityStatementResource(String)  for an alternate way to supply the CapabilityStatement
+	 * @see #setCapabilityStatementResource(String) #setCapabilityStatementResource(String) is an alternate way to supply the CapabilityStatement
 	 */
 	public void setCapabilityStatement(IBaseConformance theCapabilityStatement) {
 		myCapabilityStatement = theCapabilityStatement;
@@ -34,7 +54,7 @@ public class StaticCapabilityStatementInterceptor {
 	 * the CapabiltyStatement, then the given classpath resource will be read and parsed as a FHIR
 	 * CapabilityStatement.
 	 *
-	 * @see #setCapabilityStatement(IBaseConformance) for an alternate way to supply the CapabilityStatement
+	 * @see #setCapabilityStatement(IBaseConformance) #setCapabilityStatement(IBaseConformance) is an alternate way to supply the CapabilityStatement
 	 */
 	public void setCapabilityStatementResource(String theClasspath) {
 		myCapabilityStatementResource = theClasspath;
