85824efa512f2b23a1b1d75407d0d5b3ec1faad9
Frank Tao <38163583+frankjtao@users.noreply.github.com>
2021-03-10 16:58:10 -0500

    Added index to improve the performance (#2385)
    * Added index to improve the performance
    
    * Added index for FORCED_ID
    
    * Added comments for the index of while 'missing' is supported.
    
    * Updated comments
    
    * Moved the migration script to V5_4_0

--- hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamDate.java
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamDate.java
@@ -55,6 +55,7 @@ import java.util.Date;
 	// We previously had an index called IDX_SP_DATE - Dont reuse
 	@Index(name = "IDX_SP_DATE_HASH", columnList = "HASH_IDENTITY,SP_VALUE_LOW,SP_VALUE_HIGH"),
 	@Index(name = "IDX_SP_DATE_HASH_LOW", columnList = "HASH_IDENTITY,SP_VALUE_LOW"),
+	@Index(name = "IDX_SP_DATE_HASH_HIGH", columnList = "HASH_IDENTITY,SP_VALUE_HIGH"),
 	@Index(name = "IDX_SP_DATE_ORD_HASH", columnList = "HASH_IDENTITY,SP_VALUE_LOW_DATE_ORDINAL,SP_VALUE_HIGH_DATE_ORDINAL"),
 	@Index(name = "IDX_SP_DATE_ORD_HASH_LOW", columnList = "HASH_IDENTITY,SP_VALUE_LOW_DATE_ORDINAL"),
 	@Index(name = "IDX_SP_DATE_RESID", columnList = "RES_ID"),
