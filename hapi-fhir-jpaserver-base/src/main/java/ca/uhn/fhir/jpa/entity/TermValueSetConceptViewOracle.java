a1a31c841253ebfa4ce0bde95f2bf440ab2db1ea
Mike G <michel.gleeson@gmail.com>
2021-03-15 22:36:15 -0400

    copy paste from non Oracle version

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/entity/TermValueSetConceptViewOracle.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/entity/TermValueSetConceptViewOracle.java
@@ -24,11 +24,13 @@ import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.Subselect;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
@@ -54,4 +56,87 @@ import java.io.Serializable;
 		"FROM TRM_VALUESET_CONCEPT vsc " +
 		"LEFT OUTER JOIN TRM_VALUESET_C_DESIGNATION vscd ON vsc.PID = vscd.VALUESET_CONCEPT_PID"
 )
-public class TermValueSetConceptViewOracle extends TermValueSetConceptView {}
+public class TermValueSetConceptViewOracle implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="PID", length = 1000 /* length only needed to satisfy JpaEntityTest, it's not used*/)
+	private String id; // still set automatically
+
+	@Column(name = "CONCEPT_PID")
+	private Long myConceptPid;
+
+	@Column(name = "CONCEPT_VALUESET_PID")
+	private Long myConceptValueSetPid;
+
+	@Column(name = "CONCEPT_VALUESET_ORDER")
+	private int myConceptOrder;
+
+	@Column(name = "CONCEPT_SYSTEM_URL", length = TermCodeSystem.MAX_URL_LENGTH)
+	private String myConceptSystemUrl;
+
+	@Column(name = "CONCEPT_CODEVAL", length = TermConcept.MAX_CODE_LENGTH)
+	private String myConceptCode;
+
+	@Column(name = "CONCEPT_DISPLAY", length = TermConcept.MAX_DESC_LENGTH)
+	private String myConceptDisplay;
+
+	@Column(name = "DESIGNATION_PID")
+	private Long myDesignationPid;
+
+	@Column(name = "DESIGNATION_LANG", length = TermConceptDesignation.MAX_LENGTH)
+	private String myDesignationLang;
+
+	@Column(name = "DESIGNATION_USE_SYSTEM", length = TermConceptDesignation.MAX_LENGTH)
+	private String myDesignationUseSystem;
+
+	@Column(name = "DESIGNATION_USE_CODE", length = TermConceptDesignation.MAX_LENGTH)
+	private String myDesignationUseCode;
+
+	@Column(name = "DESIGNATION_USE_DISPLAY", length = TermConceptDesignation.MAX_LENGTH)
+	private String myDesignationUseDisplay;
+
+	@Column(name = "DESIGNATION_VAL", length = TermConceptDesignation.MAX_VAL_LENGTH)
+	private String myDesignationVal;
+
+
+	public Long getConceptPid() {
+		return myConceptPid;
+	}
+
+	public String getConceptSystemUrl() {
+		return myConceptSystemUrl;
+	}
+
+	public String getConceptCode() {
+		return myConceptCode;
+	}
+
+	public String getConceptDisplay() {
+		return myConceptDisplay;
+	}
+
+	public Long getDesignationPid() {
+		return myDesignationPid;
+	}
+
+	public String getDesignationLang() {
+		return myDesignationLang;
+	}
+
+	public String getDesignationUseSystem() {
+		return myDesignationUseSystem;
+	}
+
+	public String getDesignationUseCode() {
+		return myDesignationUseCode;
+	}
+
+	public String getDesignationUseDisplay() {
+		return myDesignationUseDisplay;
+	}
+
+	public String getDesignationVal() {
+		return myDesignationVal;
+	}
+}
