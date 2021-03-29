8fb4a9c75db1a421bb8a7bfc0cdc993c6386f337
jamesagnew <jamesagnew@gmail.com>
2021-03-29 05:36:30 -0400

    Compile fix

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ServerCapabilityStatementProvider.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ServerCapabilityStatementProvider.java
@@ -54,9 +54,9 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 /*
  * #%L
- * HAPI FHIR Structures - DSTU2 (FHIR v1.0.0)
+ * HAPI FHIR - Server Framework
  * %%
- * Copyright (C) 2014 - 2015 University Health Network
+ * Copyright (C) 2014 - 2021 Smile CDR, Inc.
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
