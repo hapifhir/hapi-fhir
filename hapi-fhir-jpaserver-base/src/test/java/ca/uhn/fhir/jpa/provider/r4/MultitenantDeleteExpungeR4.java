a9d0f5d8a718e49e34c3c1b0b9bf9f9654438e05
Ken Stevens <khstevens@gmail.com>
2021-06-01 22:04:31 -0400

    test pointcut gets called

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/MultitenantDeleteExpungeR4.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/MultitenantDeleteExpungeR4.java
@@ -1,5 +1,9 @@
 package ca.uhn.fhir.jpa.provider.r4;
 
+import ca.uhn.fhir.interceptor.api.HookParams;
+import ca.uhn.fhir.interceptor.api.IAnonymousInterceptor;
+import ca.uhn.fhir.interceptor.api.Pointcut;
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.jpa.batch.BatchJobsConfig;
 import ca.uhn.fhir.rest.api.CacheControlDirective;
 import ca.uhn.fhir.rest.server.provider.ProviderConstants;
@@ -9,12 +13,17 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.DecimalType;
 import org.hl7.fhir.r4.model.Parameters;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static ca.uhn.fhir.jpa.model.util.JpaConstants.DEFAULT_PARTITION_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class MultitenantDeleteExpungeR4 extends BaseMultitenantResourceProviderR4Test {
 	private static final Logger ourLog = LoggerFactory.getLogger(MultitenantDeleteExpungeR4.class);
@@ -39,6 +48,9 @@ public class MultitenantDeleteExpungeR4 extends BaseMultitenantResourceProviderR
 		Parameters input = new Parameters();
 		input.addParameter(ProviderConstants.OPERATION_DELETE_EXPUNGE_URL, "/Patient?active=false");
 
+
+		IAnonymousInterceptor interceptor = mock(IAnonymousInterceptor.class);
+		myInterceptorRegistry.registerAnonymousInterceptor(Pointcut.STORAGE_PARTITION_SELECTED, interceptor);
 		// execute
 
 		myTenantClientInterceptor.setTenantId(TENANT_B);
@@ -52,6 +64,13 @@ public class MultitenantDeleteExpungeR4 extends BaseMultitenantResourceProviderR
 		ourLog.info(myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(response));
 		myBatchJobHelper.awaitAllBulkJobCompletions(BatchJobsConfig.DELETE_EXPUNGE_JOB_NAME);
 
+		ArgumentCaptor<HookParams> captor = ArgumentCaptor.forClass(HookParams.class);
+		verify(interceptor, times(3)).invoke(eq(Pointcut.STORAGE_PARTITION_SELECTED), captor.capture());
+
+		RequestPartitionId partitionId = captor.getValue().get(RequestPartitionId.class);
+		assertEquals(TENANT_B_ID, partitionId.getPartitionIds().get(0).intValue());
+		assertEquals(TENANT_B, partitionId.getPartitionNames().get(0));
+
 		DecimalType jobIdPrimitive = (DecimalType) response.getParameter(ProviderConstants.OPERATION_DELETE_EXPUNGE_RESPONSE_JOB_ID);
 		Long jobId = jobIdPrimitive.getValue().longValue();
 
@@ -62,6 +81,7 @@ public class MultitenantDeleteExpungeR4 extends BaseMultitenantResourceProviderR
 		assertEquals(2, getAllPatientsInTenant(TENANT_A).getTotal());
 		assertEquals(1, getAllPatientsInTenant(TENANT_B).getTotal());
 		assertEquals(0, getAllPatientsInTenant(DEFAULT_PARTITION_NAME).getTotal());
+
 	}
 
 	private Bundle getAllPatientsInTenant(String theTenantId) {
