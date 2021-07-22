3fdd7e876f028ba08f50c9561aed27ceb665cae1
James Agnew <jamesagnew@gmail.com>
2021-07-22 08:34:28 -0400

    Fix NPE in IdHelperService (#2819)

---
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/index/IdHelperServiceTest.java
@@ -0,0 +1,35 @@
+package ca.uhn.fhir.jpa.dao.index;
+
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
+import ca.uhn.fhir.jpa.model.config.PartitionSettings;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IdHelperServiceTest {
+
+	@Test
+	public void testReplaceDefault_AllPartitions() {
+
+		IdHelperService svc = new IdHelperService();
+		PartitionSettings partitionSettings = new PartitionSettings();
+		partitionSettings.setDefaultPartitionId(1);
+		svc.setPartitionSettingsForUnitTest(partitionSettings);
+
+		RequestPartitionId outcome = svc.replaceDefault(RequestPartitionId.allPartitions());
+		assertSame(RequestPartitionId.allPartitions(), outcome);
+	}
+
+	@Test
+	public void testReplaceDefault_DefaultPartition() {
+
+		IdHelperService svc = new IdHelperService();
+		PartitionSettings partitionSettings = new PartitionSettings();
+		partitionSettings.setDefaultPartitionId(1);
+		svc.setPartitionSettingsForUnitTest(partitionSettings);
+
+		RequestPartitionId outcome = svc.replaceDefault(RequestPartitionId.defaultPartition());
+		assertEquals(1, outcome.getPartitionIds().get(0));
+	}
+
+}
