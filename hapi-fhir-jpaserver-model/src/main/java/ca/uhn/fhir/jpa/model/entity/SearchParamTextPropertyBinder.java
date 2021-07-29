40a19b7f00f636ff93b5987d64ff5cdfa313c450
Tadgh <garygrantgraham@gmail.com>
2021-07-29 16:16:18 -0400

    whoops missed an add

---
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/SearchParamTextPropertyBinder.java
@@ -0,0 +1,80 @@
+package ca.uhn.fhir.jpa.model.entity;
+
+/*-
+ * #%L
+ * HAPI FHIR JPA Server
+ * %%
+ * Copyright (C) 2014 - 2021 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.mapper.pojo.bridge.PropertyBridge;
+import org.hibernate.search.mapper.pojo.bridge.binding.PropertyBindingContext;
+import org.hibernate.search.mapper.pojo.bridge.mapping.programmatic.PropertyBinder;
+import org.hibernate.search.mapper.pojo.bridge.runtime.PropertyBridgeWriteContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+/**
+ * Allows hibernate search to index
+ *
+ * CodeableConcept.text
+ * Coding.display
+ * Identifier.type.text
+ *
+ */
+public class SearchParamTextPropertyBinder implements PropertyBinder {
+
+
+	public static final String SEARCH_PARAM_TEXT_PREFIX = "text-";
+	private static final Logger ourLog = LoggerFactory.getLogger(SearchParamTextPropertyBinder.class);
+
+	@Override
+	public void bind(PropertyBindingContext thePropertyBindingContext) {
+		thePropertyBindingContext.dependencies().use("myKey").use("myValue");
+		IndexSchemaElement indexSchemaElement = thePropertyBindingContext.indexSchemaElement();
+
+		//In order to support dynamic fields, we have to use field templates. We _must_ define the template at bootstrap time and cannot
+		//create them adhoc. https://docs.jboss.org/hibernate/search/6.0/reference/en-US/html_single/#mapper-orm-bridge-index-field-dsl-dynamic
+		//I _think_ im doing the right thing here by indicating that everything matching this template uses this analyzer.
+		indexSchemaElement.fieldTemplate("propTemplate", f -> f.asString().analyzer("standard"))
+			.matchingPathGlob(SEARCH_PARAM_TEXT_PREFIX +  "*");
+
+
+		thePropertyBindingContext.bridge(new SearchParamTextPropertyBridge());
+	}
+
+	private class SearchParamTextPropertyBridge implements PropertyBridge {
+
+		@Override
+		public void write(DocumentElement theDocument, Object theObject, PropertyBridgeWriteContext thePropertyBridgeWriteContext) {
+			Map<String, String> searchParamTexts = (Map<String, String>) theObject;
+
+			if (searchParamTexts != null) {
+				searchParamTexts.entrySet()
+					.forEach(entry -> {
+						theDocument.addValue(SEARCH_PARAM_TEXT_PREFIX + entry.getKey(), entry.getValue());
+						ourLog.trace("Adding Search Param Text: {}{} -- {}", SEARCH_PARAM_TEXT_PREFIX, entry.getKey(), entry.getValue());
+					});
+				}
+			}
+		}
+}
