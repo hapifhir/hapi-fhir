b1564651e6e435fa484893c86c9e44734dfeefb7
Frank Tao <frankjtao@gmail.com>
2021-08-18 22:17:14 -0400

    Process batch in parallel

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderR4BundleTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderR4BundleTest.java
@@ -1,17 +1,25 @@
 package ca.uhn.fhir.jpa.provider.r4;
 
-import ca.uhn.fhir.jpa.model.util.JpaConstants;
-import ca.uhn.fhir.rest.server.exceptions.NotImplementedOperationException;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import ca.uhn.fhir.jpa.model.util.JpaConstants;
+import ca.uhn.fhir.rest.server.exceptions.NotImplementedOperationException;
 
 public class ResourceProviderR4BundleTest extends BaseResourceProviderR4Test {
 
@@ -58,5 +66,41 @@ public class ResourceProviderR4BundleTest extends BaseResourceProviderR4Test {
 
 	}
 
+	@Test
+	public void testBundleBatch() {
+		List<String> ids = createPatients(50);
+
+		Bundle input = new Bundle();
+		input.setType(BundleType.BATCH);
+
+		for (String id : ids)
+		    input.addEntry().getRequest().setMethod(HTTPVerb.GET).setUrl(id);
+
+		Bundle output = myClient.transaction().withBundle(input).execute();
+
+		//ourLog.info("Bundle: \n" + myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(output));
+		
+		assertEquals(50, output.getEntry().size());
+		List<BundleEntryComponent> bundleEntries = output.getEntry();
+
+		int i=0;
+		for (BundleEntryComponent bundleEntry : bundleEntries) {
+			assertEquals(ids.get(i++),  bundleEntry.getResource().getIdElement().toUnqualifiedVersionless().getValueAsString());
+		}
+
+	}
+
+	private List<String> createPatients(int count) {
+		List<String> ids = new ArrayList<String>();
+		for (int i = 0; i < count; i++) {
+			Patient patient = new Patient();
+			patient.setGender(AdministrativeGender.MALE);
+			patient.addIdentifier().setSystem("urn:foo").setValue("A");
+			patient.addName().setFamily("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".substring(i, i+1));
+			String id = myPatientDao.create(patient).getId().toUnqualifiedVersionless().getValue();
+			ids.add(id);
+		}
+		return ids;
+	}
 
 }
