d7015db6682b451a638e03f30e98e3db68976a26
Ken Stevens <khstevens@gmail.com>
2021-09-19 21:01:47 -0400

    correct accidental Logs change

--- hapi-fhir-storage-api/src/main/java/ca/uhn/fhir/mdm/log/Logs.java
+++ hapi-fhir-storage-api/src/main/java/ca/uhn/fhir/mdm/log/Logs.java
@@ -4,52 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Logs {
-
-	private static final Logger ourSecurityTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.security_troubleshooting");
-	private static final Logger ourHttpTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.http_troubleshooting");
-	private static final Logger ourQueueTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.subscription_troubleshooting");
-	private static final Logger ourLiveBundleTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.livebundle_troubleshooting");
-	private static final Logger ourMongoDbTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.mongodb_troubleshooting");
-	private static final Logger ourChannelImportTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.channel_import_troubleshooting");
-	private static final Logger ourMdmTroubleshootingLog = getMdmTroubleshootingLog();
-	private static final Logger ourRealtimeExportTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.realtime_export_troubleshooting");
-	private static final Logger ourConnectionPoolTroubleshootingLog = LoggerFactory.getLogger("ca.cdr.log.connection_pool_troubleshooting");
-
-	public static Logger getHttpTroubleshootingLog() {
-		return ourHttpTroubleshootingLog;
-	}
-
-	public static Logger getSubscriptionTroubleshootingLog() {
-		return ourQueueTroubleshootingLog;
-	}
-
-	public static Logger getSecurityTroubleshootingLog() {
-		return ourSecurityTroubleshootingLog;
-	}
-
-	public static Logger getLiveBundleTroubleshootingLog() {
-		return ourLiveBundleTroubleshootingLog;
-	}
-
-	public static Logger getMongoDbTroubleshootingLog() {
-		return ourMongoDbTroubleshootingLog;
-	}
+	private static final Logger ourMdmTroubleshootingLog = LoggerFactory.getLogger("ca.uhn.fhir.log.mdm_troubleshooting");
 
 	public static Logger getMdmTroubleshootingLog() {
 		return ourMdmTroubleshootingLog;
 	}
-
-	public static Logger getRealtimeExportTroubleshootingLog() {
-		return ourRealtimeExportTroubleshootingLog;
-	}
-
-	public static Logger getChannelImportTroubleshootingLog() {
-		return ourChannelImportTroubleshootingLog;
-	}
-
-	public static Logger getConnectionPoolTroubleshootingLog() {
-		return ourConnectionPoolTroubleshootingLog;
-	}
-
-
 }
