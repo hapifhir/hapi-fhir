6c503ebf8ea81e99a64a8ea3b2adc9c31019f666
Ben Li-Sauerwine <bsauerwine@gmail.com>
2021-09-22 22:52:46 -0400

    NDJsonParser with one example test.

---
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/parser/NDJsonParser.java
@@ -0,0 +1,111 @@
+package ca.uhn.fhir.parser;
+
+/*
+ * #%L
+ * HAPI FHIR - Core Library
+ * %%
+ * Copyright (C) 2014 - 2021 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import ca.uhn.fhir.context.*;
+import ca.uhn.fhir.rest.api.EncodingEnum;
+import ca.uhn.fhir.util.BundleBuilder;
+import ca.uhn.fhir.util.BundleUtil;
+import org.hl7.fhir.instance.model.api.*;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.List;
+
+
+/**
+ * This class is the FHIR NDJSON parser/encoder. Users should not interact with this class directly, but should use
+ * {@link FhirContext#newNDJsonParser()} to get an instance.
+ */
+public class NDJsonParser extends BaseParser {
+
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(NDJsonParser.class);
+
+        private IParser myJsonParser;
+        private FhirContext myFhirContext;
+
+	/**
+	 * Do not use this constructor, the recommended way to obtain a new instance of the NDJSON parser is to invoke
+	 * {@link FhirContext#newNDJsonParser()}.
+	 *
+	 * @param theParserErrorHandler
+	 */
+	public NDJsonParser(FhirContext theContext, IParserErrorHandler theParserErrorHandler) {
+		super(theContext, theParserErrorHandler);
+                myFhirContext = theContext;
+
+                myJsonParser = theContext.newJsonParser();
+	}
+
+        @Override
+        public IParser setPrettyPrint(boolean thePrettyPrint) {
+                myJsonParser.setPrettyPrint(thePrettyPrint);
+                return this;
+        }
+
+        @Override
+        public EncodingEnum getEncoding() {
+                return EncodingEnum.NDJSON;
+        }
+
+	@Override
+	protected void doEncodeResourceToWriter(IBaseResource theResource, Writer theWriter, EncodeContext theEncodeContext) throws IOException {
+                // We only encode bundles to NDJSON.
+                if (!(IBaseBundle.class.isAssignableFrom(theResource.getClass()))) {
+                        throw new IllegalArgumentException("NDJsonParser can only encode Bundle types.  Received " + theResource.getClass().getName());
+                }
+
+                // Ok, convert the bundle to a list of resources.
+                List<IBaseResource> theBundleResources = BundleUtil.toListOfResources(myFhirContext, (IBaseBundle) theResource);
+
+                // Now we write each one in turn.
+                for (IBaseResource theBundleEntryResource : theBundleResources) {
+                        myJsonParser.encodeResourceToWriter(theBundleEntryResource, theWriter);
+                }
+	}
+
+	@Override
+	public <T extends IBaseResource> T doParseResource(Class<T> theResourceType, Reader theReader) throws DataFormatException {
+                // We can only parse to bundles.
+                if ((theResourceType != null) && (!(IBaseBundle.class.isAssignableFrom(theResourceType)))) {
+                        throw new DataFormatException("NDJsonParser can only parse to Bundle types.  Received " + theResourceType.getName());
+                }
+
+                try {
+                        // Now we go through line-by-line parsing the JSON and then stuffing it into a bundle.
+                        BundleBuilder myBuilder = new BundleBuilder(myFhirContext);
+                        BufferedReader myBufferedReader = new BufferedReader(theReader);
+                        String jsonString = myBufferedReader.readLine();
+                        while (jsonString != null) {
+                                // And add it to a collection in a Bundle.
+                                myBuilder.addCollectionEntry(myJsonParser.parseResource(jsonString));
+                                // Try to read another line.
+                                jsonString = myBufferedReader.readLine();
+                        }
+
+                        return (T) myBuilder.getBundle();
+                } catch (IOException err) {
+                        throw new DataFormatException(err.getMessage());
+                }
+	}
+}
---
+++ hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/parser/NDJsonParserTest.java
@@ -0,0 +1,68 @@
+package ca.uhn.fhir.parser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ca.uhn.fhir.context.FhirContext;
+
+import ca.uhn.fhir.util.BundleBuilder;
+import ca.uhn.fhir.util.TestUtil;
+
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Patient;
+
+public class NDJsonParserTest {
+	private static FhirContext ourCtx;
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(NDJsonParserTest.class);
+
+        private String toNDJson(IBaseResource bundle) throws DataFormatException {
+                IParser p = ourCtx.newNDJsonParser();
+                return p.encodeResourceToString(bundle);
+        }
+
+        private IBaseResource fromNDJson(String ndjson) throws DataFormatException {
+                IParser p = ourCtx.newNDJsonParser();
+                return p.parseResource(ndjson);
+        } 
+
+        private boolean fhirResourcesEqual(IBaseResource expected, IBaseResource actual) {
+                // I would prefer to use, e.g., EqualsBuilder to do this instead.
+                String encoded_expected = ourCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(expected);
+                String encoded_actual = ourCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(actual);
+
+                ourLog.info("Expected: {}", encoded_expected);
+                ourLog.info("Actual  : {}", encoded_actual);
+
+                return encoded_expected.equals(encoded_actual);
+        }
+
+        @Test
+        public void testSinglePatientEncodeDecode() {
+                BundleBuilder myBuilder = new BundleBuilder(ourCtx);
+
+                Patient p = new Patient();
+                p.setId("Patient/P1");
+                myBuilder.addCollectionEntry(p);
+                IBaseResource myBundle = myBuilder.getBundle();
+                IBaseResource responseBundle = fromNDJson(toNDJson(myBundle));
+
+                assertTrue(fhirResourcesEqual(myBundle, responseBundle));
+        }
+
+        @BeforeAll
+        public static void beforeClass() {
+                ourCtx = FhirContext.forR4();
+        }
+
+	@AfterAll
+	public static void afterClassClearContext() {
+		TestUtil.clearAllStaticFieldsForUnitTest();
+	}
+}
