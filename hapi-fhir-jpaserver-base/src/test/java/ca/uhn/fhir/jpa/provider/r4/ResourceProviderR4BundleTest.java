072c030c2d55de1c13a6d7e7cd2114a526189672
Tadgh <garygrantgraham@gmail.com>
2021-09-28 09:55:06 -0400

    Remove modifications, defaults work

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderR4BundleTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderR4BundleTest.java
@@ -118,10 +118,8 @@ public class ResourceProviderR4BundleTest extends BaseResourceProviderR4Test {
 
 	@Test
 	public void testHighConcurrencyWorks() throws IOException, InterruptedException {
-		myDaoConfig.setBundleBatchPoolSize(20);
-		myDaoConfig.setBundleBatchMaxPoolSize(100);
 		List<Bundle> bundles = new ArrayList<>();
-		for (int i =0 ; i < 20; i ++) {
+		for (int i =0 ; i < 10; i ++) {
 			bundles.add(myFhirCtx.newJsonParser().parseResource(Bundle.class, IOUtils.toString(getClass().getResourceAsStream("/r4/identical-tags-batch.json"), Charsets.UTF_8)));
 		}
 
