af39270c835baea07ee2645ac973ded569b61d50
Long Ma <longma@Longs-MBP.hitronhub.home>
2021-10-14 14:36:02 -0600

    added AbstractResourceToFileWriter

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/export/job/ResourceToFileWriter.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/export/job/ResourceToFileWriter.java
@@ -21,68 +21,32 @@ package ca.uhn.fhir.jpa.bulk.export.job;
  */
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
-import ca.uhn.fhir.jpa.api.model.DaoMethodOutcome;
-import ca.uhn.fhir.jpa.batch.log.Logs;
 import ca.uhn.fhir.jpa.bulk.export.svc.BulkExportDaoSvc;
 import ca.uhn.fhir.jpa.entity.BulkExportCollectionFileEntity;
-import ca.uhn.fhir.jpa.partition.SystemRequestDetails;
-import ca.uhn.fhir.parser.IParser;
-import ca.uhn.fhir.rest.api.Constants;
-import ca.uhn.fhir.util.BinaryUtil;
 import org.hl7.fhir.instance.model.api.IBaseBinary;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
-import org.slf4j.Logger;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStreamWriter;
-import java.util.List;
 import java.util.Optional;
 
-public class ResourceToFileWriter implements ItemWriter<List<IBaseResource>> {
-	private static final Logger ourLog = Logs.getBatchTroubleshootingLog();
-
-	@Autowired
-	private FhirContext myFhirContext;
-
-	@Autowired
-	private DaoRegistry myDaoRegistry;
-
+public class ResourceToFileWriter extends AbstractResourceToFileWriter {
 	@Autowired
 	private BulkExportDaoSvc myBulkExportDaoSvc;
 
-	private final ByteArrayOutputStream myOutputStream;
-	private final OutputStreamWriter myWriter;
-	private IParser myParser;
-
-	@Value("#{stepExecutionContext['bulkExportCollectionEntityId']}")
-	private Long myBulkExportCollectionEntityId;
-
-	@Value("#{stepExecutionContext['resourceType']}")
-	private String myResourceType;
-
-	private IFhirResourceDao<IBaseBinary> myBinaryDao;
-
-
-	public ResourceToFileWriter() {
-		myOutputStream = new ByteArrayOutputStream();
-		myWriter = new OutputStreamWriter(myOutputStream, Constants.CHARSET_UTF8);
+	public ResourceToFileWriter(FhirContext theFhirContext, DaoRegistry theDaoRegistry) {
+		super(theFhirContext, theDaoRegistry);
 	}
 
 	@PostConstruct
 	public void start() {
-		myParser = myFhirContext.newJsonParser().setPrettyPrint(false);
 		myBinaryDao = getBinaryDao();
 	}
 
-	private Optional<IIdType> flushToFiles() {
+	@Override
+	protected Optional<IIdType> flushToFiles() {
 		if (myOutputStream.size() > 0) {
 			IIdType createdId = createBinaryFromOutputStream();
 			BulkExportCollectionFileEntity file = new BulkExportCollectionFileEntity();
@@ -98,34 +62,8 @@ public class ResourceToFileWriter implements ItemWriter<List<IBaseResource>> {
 		return Optional.empty();
 	}
 
-	private IIdType createBinaryFromOutputStream() {
-		IBaseBinary binary = BinaryUtil.newBinary(myFhirContext);
-		binary.setContentType(Constants.CT_FHIR_NDJSON);
-		binary.setContent(myOutputStream.toByteArray());
-		DaoMethodOutcome outcome = myBinaryDao.create(binary, new SystemRequestDetails().setRequestPartitionId(RequestPartitionId.defaultPartition()));
-		return outcome.getResource().getIdElement();
-	}
-
 	@SuppressWarnings("unchecked")
 	private IFhirResourceDao<IBaseBinary> getBinaryDao() {
 		return myDaoRegistry.getResourceDao("Binary");
-	}
-
-	@Override
-	public void write(List<? extends List<IBaseResource>> theList) throws Exception {
-
-		int count = 0;
-		for (List<IBaseResource> resourceList : theList) {
-			for (IBaseResource nextFileResource : resourceList) {
-				myParser.encodeResourceToWriter(nextFileResource, myWriter);
-				myWriter.append("\n");
-				count++;
-			}
-		}
-
-		Optional<IIdType> createdId = flushToFiles();
-		if (createdId.isPresent()) {
-			ourLog.info("Created {} resources for bulk export file containing {} resources of type {} ", count, createdId.get().toUnqualifiedVersionless().getValue(), myResourceType);
-		}
 	}
 }
