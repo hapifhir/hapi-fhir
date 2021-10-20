b93f33d4ff02b94b92a2f3fd70478596e300cf56
jmarchionatto <60409882+jmarchionatto@users.noreply.github.com>
2021-10-20 11:05:04 -0400

    When uploading loinc CodeSystem take copyright from loinc.xml input file. (#3092)
    Co-authored-by: juan.marchionatto <juan.marchionatto@smilecdr.com>

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/loinc/LoincDocumentOntologyHandler.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/loinc/LoincDocumentOntologyHandler.java
@@ -43,8 +43,10 @@ public class LoincDocumentOntologyHandler extends BaseLoincHandler implements IZ
 	public static final String DOCUMENT_ONTOLOGY_CODES_VS_NAME = "LOINC Document Ontology Codes";
 	private final Map<String, TermConcept> myCode2Concept;
 
-	public LoincDocumentOntologyHandler(Map<String, TermConcept> theCode2concept, Map<String, CodeSystem.PropertyType> thePropertyNames, List<ValueSet> theValueSets, List<ConceptMap> theConceptMaps, Properties theUploadProperties) {
-		super(theCode2concept, theValueSets, theConceptMaps, theUploadProperties);
+	public LoincDocumentOntologyHandler(Map<String, TermConcept> theCode2concept, Map<String,
+			CodeSystem.PropertyType> thePropertyNames, List<ValueSet> theValueSets, List<ConceptMap> theConceptMaps,
+			Properties theUploadProperties, String theCopyrightStatement) {
+		super(theCode2concept, theValueSets, theConceptMaps, theUploadProperties, theCopyrightStatement);
 		myCode2Concept = theCode2concept;
 	}
 
