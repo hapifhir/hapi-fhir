453901a62fe28c038c5154d23df608c6979704a0
Kevin Dougan <kevin.dougan@smilecdr.com>
2021-10-26 17:16:13 -0400

    3091 - Added a Unit Test called FhirResourceDaoR4SearchSqlTest.testSearchByPractitionerRole_has() to try and reproduce a SQL generation problem, but the Unit Test is not working properly yet.

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4SearchSqlTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4SearchSqlTest.java
@@ -9,7 +9,13 @@ import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.param.UriParam;
 import org.hamcrest.Matchers;
 import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Enumerations;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Practitioner;
+import org.hl7.fhir.r4.model.PractitionerRole;
 import org.hl7.fhir.r4.model.SearchParameter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -127,5 +133,60 @@ public class FhirResourceDaoR4SearchSqlTest extends BaseJpaR4Test {
 
 	}
 
+	@Test
+	public void testSearchByPractitionerRole_has() {
+		SearchParameter searchParameter = new SearchParameter();
+		searchParameter.setId("practitionerrole-role");
+		searchParameter.setStatus(Enumerations.PublicationStatus.ACTIVE);
+		searchParameter.setCode("role");
+		searchParameter.addBase("PractitionerRole");
+		searchParameter.setType(Enumerations.SearchParamType.TOKEN);
+		searchParameter.setExpression("PractitionerRole.code");
+
+		ourLog.info("SearchParam:\n{}", myFhirCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(searchParameter));
+		mySearchParameterDao.update(searchParameter, mySrd);
+		mySearchParamRegistry.forceRefresh();
+
+		int numResources = 1100;
+		for (int i = 1; i <= numResources; i++) {
+			Practitioner p = new Practitioner();
+			p.getIdentifier().add(new Identifier().setSystem("http://hl7.org/fhir/sid/us-npi").setValue(new Integer(i).toString()));
+			IIdType pid = myPractitionerDao.create(p).getId().toUnqualifiedVersionless();
+			PractitionerRole pr = new PractitionerRole();
+			pr.getPractitioner().getIdentifier().setValue(pid.getValueAsString());
+			pr.getCode().add(new CodeableConcept().addCoding(new Coding().setSystem("urn:oid:2.16.840.1.113883.2.4.15.111").setCode("Physician")));
+			IIdType prid = myPractitionerRoleDao.create(pr).getId().toUnqualifiedVersionless();
+		}
+
+		myMemoryCacheService.invalidateAllCaches();
+
+		// Search
+		myCaptureQueriesListener.clear();
+		// http://127.0.0.1:8000/Practitioner
+		// ?_count=750
+		// &_elements=Practitioner.name,Practitioner.identifier,Practitioner.active&_elements:exclude=*.meta
+		// &_has:PractitionerRole:practitioner:role=SC%20Physician,SC%20Physician%201,SC%20Physician%202,SC%20Physician%203
+		// &_lastUpdated=ge2021-10-23T01:00:00
+		SearchParameterMap map = SearchParameterMap.newSynchronous()
+			.add(Constants.PARAM_COUNT, new StringParam("750"))
+			.add(Constants.PARAM_ELEMENTS, new StringParam("Practitioner.name,Practitioner.identifier,Practitioner.active"))
+			.add(Constants.PARAM_ELEMENTS + Constants.PARAM_ELEMENTS_EXCLUDE_MODIFIER, new StringParam("*.meta"))
+			.add("_has:PractitionerRole:practitioner:role", new StringParam("Physician"));
+		IBundleProvider outcome = myPractitionerDao.search(map);
+
+//		assertEquals(3, myCaptureQueriesListener.countSelectQueries());
+		// Query 1 - Find resources: Make sure we search for tag type+system+code always
+		String sql = myCaptureQueriesListener.getSelectQueriesForCurrentThread().get(0).getSql(false, false);
+//		assertEquals("SELECT t0.RES_ID FROM HFJ_RESOURCE t0 LEFT OUTER JOIN HFJ_RES_TAG t1 ON (t0.RES_ID = t1.RES_ID) LEFT OUTER JOIN HFJ_TAG_DEF t2 ON (t1.TAG_ID = t2.TAG_ID) WHERE (((t0.RES_TYPE = ?) AND (t0.RES_DELETED_AT IS NULL)) AND ((t2.TAG_TYPE = ?) AND (t2.TAG_SYSTEM = ?) AND (t2.TAG_CODE = ?)))", sql);
+//		// Query 2 - Load resourece contents
+		sql = myCaptureQueriesListener.getSelectQueriesForCurrentThread().get(1).getSql(false, false);
+//		assertThat(sql, containsString("where resourcese0_.RES_ID in (?)"));
+//		// Query 3 - Load tags and defintions
+		sql = myCaptureQueriesListener.getSelectQueriesForCurrentThread().get(2).getSql(false, false);
+//		assertThat(sql, containsString("from HFJ_RES_TAG resourceta0_ inner join HFJ_TAG_DEF"));
+//
+//		assertThat(toUnqualifiedVersionlessIds(outcome), Matchers.contains(id));
+
+	}
 
 }
