af8d13deb266dce2958932250a8dce9c52af49a1
Kevin Dougan <kevin.dougan@smilecdr.com>
2021-10-28 10:19:19 -0400

    Added non-synchronous search.

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/dstu3/FhirResourceDaoDstu3SearchSqlTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/dstu3/FhirResourceDaoDstu3SearchSqlTest.java
@@ -5,6 +5,7 @@ import ca.uhn.fhir.jpa.util.SqlQuery;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.param.HasParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
+import ca.uhn.fhir.rest.param.TokenOrListParam;
 import ca.uhn.fhir.rest.param.TokenParam;
 import com.ctc.wstx.shaded.msv_core.verifier.jarv.Const;
 import org.hl7.fhir.dstu3.model.Condition;
@@ -27,28 +28,22 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FhirResourceDaoDstu3SearchSqlTest extends BaseJpaDstu3Test {
 	private static final Logger ourLog = LoggerFactory.getLogger(FhirResourceDaoDstu3SearchSqlTest.class);
 
 	@Test
 	public void testSearchCondition_ByPatientAndCategories() {
-		//
-		// TODO KBD Try This:
-		// Start with a fork of the actual branch they are using
-		// Make sure your data actually matches the patterns they have (do they really have a 1:1 ratio of PractitionerRole to Practitioner?)
-		// Make the SearchParameterMap not be synchronous
-		// Then call results.getResources(0,300)  followed by results.getResources(300, 600) etc etc
-		//
-
 		Patient patient = new Patient();
-		//patient.addIdentifier().setSystem("http://mydomain.com/patient-identifier").setValue("PID1");
 		IIdType patientId = myPatientDao.create(patient).getId().toUnqualifiedVersionless();
 
 		int numResources = 3000;
 		for (int i = 1; i <= numResources; i++) {
 			Condition condition = new Condition();
 			condition.setSubject(new Reference(patientId));
-			//condition.getIdentifier().add(new Identifier().setSystem("http://mydomain.com/condition-identifier").setValue(new Integer("CID"+i).toString()));
 			List<CodeableConcept> categories = new ArrayList<>();
 			categories.add(new CodeableConcept()
 				.addCoding(new Coding("acc_condcat_fkc", "ACCEVN", "Access Event")));
@@ -70,19 +65,32 @@ public class FhirResourceDaoDstu3SearchSqlTest extends BaseJpaDstu3Test {
 		// &category=ACCEVN%2CACCMNTRG%2CMIDTX
 		// &patient=Patient%2F123
 		SearchParameterMap map = new SearchParameterMap();
-		map.setLoadSynchronous(true);
+		map.setLoadSynchronous(false);
 		map.setCount(300);
 		map.add("patient", new ReferenceParam(patientId.toString()));
-		//map.add("category", new TokenParam("ACCEVN,ACCMNTRG,MIDTX")); // , = %2C
+		map.add("category", new TokenOrListParam(null, "ACCEVN", "ACCMNTRG", "MIDTX"));
 		IBundleProvider outcome = myConditionDao.search(map);
-		List<IBaseResource> resources = outcome.getResources(0, 300);
-		ourLog.info("Batch 0-300 returned " + resources.size() + " resources.");
-		resources = outcome.getResources(300, 600);
-		ourLog.info("Batch 300-600 returned " + resources.size() + " resources.");
-		resources = outcome.getResources(600, 900);
-		ourLog.info("Batch 600-900 returned " + resources.size() + " resources.");
-		resources = outcome.getResources(900, 1200);
-		ourLog.info("Batch 900-1200 returned " + resources.size() + " resources.");
+		String uuid = outcome.getUuid();
+		ourLog.info("** Search returned UUID: {}", uuid);
+		List<String> ids = toUnqualifiedVersionlessIdValues(outcome, 0, 300, true);
+		//assertEquals(201, myDatabaseBackedPagingProvider.retrieveResultList(null, uuid).size().intValue());
+		ourLog.info("Batch 0-300 returned " + ids.size() + " ids.");
+
+		ids = toUnqualifiedVersionlessIdValues(outcome, 300, 600, false);
+		ourLog.info("Batch 300-600 returned " + ids.size() + " ids.");
+		ids = toUnqualifiedVersionlessIdValues(outcome, 600, 900, false);
+		ourLog.info("Batch 600-900 returned " + ids.size() + " ids.");
+		ids = toUnqualifiedVersionlessIdValues(outcome, 900, 1200, false);
+		ourLog.info("Batch 900-1200 returned " + ids.size() + " ids.");
+
+		//List<IBaseResource> resources = outcome.getResources(0, 300);
+		//ourLog.info("Batch 0-300 returned " + resources.size() + " resources.");
+		//resources = outcome.getResources(300, 600);
+		//ourLog.info("Batch 300-600 returned " + resources.size() + " resources.");
+		//resources = outcome.getResources(600, 900);
+		//ourLog.info("Batch 600-900 returned " + resources.size() + " resources.");
+		//resources = outcome.getResources(900, 1200);
+		//ourLog.info("Batch 900-1200 returned " + resources.size() + " resources.");
 
 //		assertEquals(3, myCaptureQueriesListener.countSelectQueries());
 		for (SqlQuery query : myCaptureQueriesListener.getSelectQueriesForCurrentThread()) {
