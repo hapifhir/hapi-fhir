8d925790d4fe7127060b7dfe494e4569c7df3ed5
Ken Stevens <khstevens@gmail.com>
2021-11-22 10:15:02 -0500

    javadoc

--- hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/api/ISubscriptionMessageKeySvc.java
+++ hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/api/ISubscriptionMessageKeySvc.java
@@ -4,6 +4,9 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 
 import javax.annotation.Nullable;
 
+/**
+ * This is used by "message" type subscriptions to provide a key to the message wrapper before submitting it to the channel
+ */
 public interface ISubscriptionMessageKeySvc {
 	@Nullable
 	String getMessageKeyOrNull(IBaseResource thePayloadResource);
