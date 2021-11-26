19bf87566fa38bad300c28bedc8ce4388708594d
Tadgh <garygrantgraham@gmail.com>
2021-11-26 11:47:35 -0500

    Remove bitwise OR, replace with standard ||

--- hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/dao/MdmLinkDaoSvc.java
+++ hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/dao/MdmLinkDaoSvc.java
@@ -67,8 +67,8 @@ public class MdmLinkDaoSvc {
 		mdmLink.setLinkSource(theLinkSource);
 		mdmLink.setMatchResult(theMatchOutcome.getMatchResultEnum());
 		// Preserve these flags for link updates
-		mdmLink.setEidMatch(theMatchOutcome.isEidMatch() | mdmLink.isEidMatchPresent());
-		mdmLink.setHadToCreateNewGoldenResource(theMatchOutcome.isCreatedNewResource() | mdmLink.getHadToCreateNewGoldenResource());
+		mdmLink.setEidMatch(theMatchOutcome.isEidMatch() || mdmLink.isEidMatchPresent());
+		mdmLink.setHadToCreateNewGoldenResource(theMatchOutcome.isCreatedNewResource() || mdmLink.getHadToCreateNewGoldenResource());
 		mdmLink.setMdmSourceType(myFhirContext.getResourceType(theSourceResource));
 		if (mdmLink.getScore() != null) {
 			mdmLink.setScore(Math.max(theMatchOutcome.score, mdmLink.getScore()));
