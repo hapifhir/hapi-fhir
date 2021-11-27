8a3b03966d3355ef0646c0c5b507a24a76bcdbe5
Ken Stevens <khstevens@gmail.com>
2021-11-27 17:06:20 -0400

    add comment

--- hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/log/Msg.java
+++ hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/log/Msg.java
@@ -6,6 +6,10 @@ import ca.uhn.fhir.i18n.ModuleErrorCodeEnum;
 public final class Msg {
 	private Msg() {}
 
+	/**
+	 * IMPORTANT: Please update the following comment after you add a new code
+	 * Last code value: 26
+	 */
 	public static String code(int theCode) {
 		return HapiErrorCode.code(ModuleErrorCodeEnum.SUBSCRIPTION, theCode);
 	}
