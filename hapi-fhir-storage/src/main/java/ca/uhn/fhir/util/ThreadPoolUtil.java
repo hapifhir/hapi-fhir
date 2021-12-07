3a12de7f99adfac07e73a7c5cf0fe7eb16c3551e
Ken Stevens <khstevens@gmail.com>
2021-12-07 11:00:15 -0500

    fix thread regexp

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/util/ThreadPoolUtil.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/util/ThreadPoolUtil.java
@@ -1,5 +1,25 @@
 package ca.uhn.fhir.util;
 
+/*-
+ * #%L
+ * HAPI FHIR Storage api
+ * %%
+ * Copyright (C) 2014 - 2021 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import ca.uhn.fhir.jpa.search.reindex.BlockPolicy;
 import org.apache.commons.lang3.Validate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -13,8 +33,7 @@ public final class ThreadPoolUtil {
 
 	@Nonnull
 	public static ThreadPoolTaskExecutor newThreadPool(int theCorePoolSize, int theMaxPoolSize, String theThreadNamePrefix) {
-		// FIXME KS: why restrict the name to only lowercase and hyphens? Enabling this causes bunch of CDR tests to fail
-		//Validate.matchesPattern(theThreadNamePrefix, "[a-z\\-]+", "Thread pool prefix name must consist only of lower-case letters and hyphens");
+		Validate.matchesPattern(theThreadNamePrefix, "[a-z1-9\\-]+", "Thread pool prefix name must consist only of lower-case letters and hyphens");
 		Validate.isTrue(theThreadNamePrefix.endsWith("-"), "Thread pool prefix name must end with a hyphen");
 		ThreadPoolTaskExecutor asyncTaskExecutor = new ThreadPoolTaskExecutor();
 		asyncTaskExecutor.setCorePoolSize(theCorePoolSize);
