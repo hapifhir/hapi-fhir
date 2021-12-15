9e20d6238037968111efbe04c203062f0842ea0b
Tadgh <tadgh@cs.toronto.edu>
2021-12-14 22:53:26 -0800

    Issue 3237 delete expunge performance (#3239)
    * Initial test looking for bad deletes
    
    * Add indexes, add migration, add test'
    
    * Fix npe
    
    * Add provider test
    
    * fix up comments, tighten test
    
    * Update hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/delete/job/DeleteExpungeProcessor.java
    
    Co-authored-by: michaelabuckley <michaelabuckley@gmail.com>
    
    * Add changelog
    
    * Remove unnecessary migration
    
    Co-authored-by: michaelabuckley <michaelabuckley@gmail.com>

--- hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ForcedId.java
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ForcedId.java
@@ -56,7 +56,9 @@ import javax.persistence.UniqueConstraint;
 	 * - IDX_FORCEDID_TYPE_RESID
 	 * so don't reuse these names
 	 */
-	@Index(name = "IDX_FORCEID_FID", columnList = "FORCED_ID")
+	@Index(name = "IDX_FORCEID_FID", columnList = "FORCED_ID"),
+	@Index(name = "IDX_FORCEID_RESID", columnList = "RESOURCE_PID"),
+	//TODO GGG potentiall add a type + res_id index here, specifically for deletion?
 })
 public class ForcedId extends BasePartitionable {
 
