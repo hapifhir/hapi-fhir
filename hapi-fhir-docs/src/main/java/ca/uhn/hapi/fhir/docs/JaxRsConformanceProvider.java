fd127e7c67d73e9fbadb8b69852c9ed0b15e78ef
jamesagnew <jamesagnew@gmail.com>
2022-01-18 08:10:59 -0500

    Fix tests

--- hapi-fhir-docs/src/main/java/ca/uhn/hapi/fhir/docs/JaxRsConformanceProvider.java
+++ hapi-fhir-docs/src/main/java/ca/uhn/hapi/fhir/docs/JaxRsConformanceProvider.java
@@ -28,7 +28,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import javax.ws.rs.core.MediaType;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
