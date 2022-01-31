4c33e9aa7efc443e255a37ce8f571c94359c8adc
Michael Buckley <michael.buckley@smilecdr.com>
2022-01-31 18:58:41 -0500

    Failing test

---
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/provider/r4/ResourceProviderR4ElasticTest.java
@@ -0,0 +1,105 @@
+package ca.uhn.fhir.jpa.provider.r4;
+
+import ca.uhn.fhir.jpa.config.TestHibernateSearchAddInConfig;
+import ca.uhn.fhir.jpa.config.TestR4Config;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.test.utilities.docker.RequiresDocker;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.hl7.fhir.instance.model.api.IBaseCoding;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.ValueSet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@RequiresDocker
+@ContextConfiguration(classes = TestHibernateSearchAddInConfig.Elasticsearch.class)
+public class ResourceProviderR4ElasticTest extends BaseResourceProviderR4Test {
+	private static final Logger ourLog = LoggerFactory.getLogger(ResourceProviderR4ElasticTest.class);
+
+	/**
+	 * wipmb TODO-LIST
+	 * - Fix {@link TestR4Config#jpaProperties()}
+	 *
+	 * - Add logging to {@link TestR4Config#jpaProperties()}
+	 * {@link TestHibernateSearchAddInConfig}
+	 * {@link ca.uhn.fhir.jpa.config.TestJPAConfig}
+	 * {@link TestR4Config}
+	 * {@link ca.uhn.fhir.jpa.config.TestR5Config}
+	 */
+
+	// wipmb test the provider here
+	@Test
+	public void testAutocompleteDirectionExisting() throws IOException {
+		// given
+		Coding mean_blood_pressure = new Coding("http://loinc.org", "8478-0", "Mean blood pressure");
+		createObservationWithCode(new Coding("http://loinc.org", "789-8", "Erythrocytes [#/volume] in Blood by Automated count"));
+		createObservationWithCode(mean_blood_pressure);
+		createObservationWithCode(mean_blood_pressure);
+		createObservationWithCode(mean_blood_pressure);
+		createObservationWithCode(mean_blood_pressure);
+
+		// when
+		HttpGet expandQuery = new HttpGet(ourServerBase + "/ValueSet/$expand?contextDirection=existing&context=Observation.code:text&filter=pressure");
+		try (CloseableHttpResponse response = ourHttpClient.execute(expandQuery)) {
+
+			// then
+			assertEquals(Constants.STATUS_HTTP_200_OK, response.getStatusLine().getStatusCode());
+			String text = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+			ValueSet valueSet = myFhirCtx.newXmlParser().parseResource(ValueSet.class, text);
+			ourLog.info(text);
+			assertThat(valueSet, is(not(nullValue())));
+			List<ValueSet.ValueSetExpansionContainsComponent> expansions = valueSet.getExpansion().getContains();
+			assertThat(expansions, hasItem(valueSetExpansionMatching(mean_blood_pressure)));
+			// wipmb what else?
+		}
+
+	}
+
+	public static Matcher<ValueSet.ValueSetExpansionContainsComponent> valueSetExpansionMatching(IBaseCoding theTarget) {
+		return new TypeSafeDiagnosingMatcher<ValueSet.ValueSetExpansionContainsComponent>() {
+			public void describeTo(Description description) {
+				description.appendText("ValueSetExpansionContainsComponent matching ").appendValue(theTarget.getSystem() + "|" + theTarget.getCode());
+			}
+
+			protected boolean matchesSafely(ValueSet.ValueSetExpansionContainsComponent theItem, Description mismatchDescription) {
+				return
+					Objects.equals(theItem.getSystem(), theTarget.getSystem()) &&
+					Objects.equals(theItem.getCode(), theTarget.getCode());
+			}
+		};
+	}
+
+
+	private IIdType createObservationWithCode(Coding c) {
+		// wipmb share with FhirResourceDaoR4SearchWithElasticSearchIT
+		Observation obs1 = new Observation();
+		obs1.getCode().addCoding(c);
+		return myObservationDao.create(obs1, mySrd).getId().toUnqualifiedVersionless();
+	}
+
+
+
+}
