496d3d8a0dd651fa6b300df5cda8f3b941ab6438
jamesagnew <jamesagnew@gmail.com>
2022-02-18 14:16:10 -0500

    Test fix

--- hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/subscription/module/config/TestSubscriptionDstu3Config.java
+++ hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/subscription/module/config/TestSubscriptionDstu3Config.java
@@ -3,8 +3,11 @@ package ca.uhn.fhir.jpa.subscription.module.config;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.model.sched.ISchedulerService;
 import ca.uhn.fhir.jpa.searchparam.registry.ISearchParamProvider;
+import com.google.common.collect.Lists;
+import org.hl7.fhir.dstu3.model.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +16,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Configuration
 @Import(TestSubscriptionConfig.class)
@@ -44,7 +48,18 @@ public class TestSubscriptionDstu3Config {
 
 	@Bean
 	public DaoRegistry daoRegistry() {
-		return new DaoRegistry();
+		DaoRegistry retVal = new DaoRegistry(fhirContext());
+		retVal.setResourceDaos(Lists.newArrayList(
+			subscriptionDao()
+		));
+		return retVal;
+	}
+
+	@Bean
+	public IFhirResourceDao<Subscription> subscriptionDao() {
+		IFhirResourceDao mock = mock(IFhirResourceDao.class);
+		when(mock.getResourceType()).thenReturn(Subscription.class);
+		return mock;
 	}
 
 }
