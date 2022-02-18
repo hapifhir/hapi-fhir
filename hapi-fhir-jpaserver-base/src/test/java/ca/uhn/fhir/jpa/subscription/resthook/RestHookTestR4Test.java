5f6ecc60c9936248a4bf364fa44fa2b717483a02
jamesagnew <jamesagnew@gmail.com>
2022-02-18 09:47:24 -0500

    Test fixes

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/subscription/resthook/RestHookTestR4Test.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/subscription/resthook/RestHookTestR4Test.java
@@ -304,6 +304,7 @@ public class RestHookTestR4Test extends BaseSubscriptionsR4Test {
 		createSubscription(criteria2, payload);
 		waitForActivatedSubscriptionCount(2);
 
+		ourLog.info("Sending an Observation");
 		Observation obs = sendObservation(code, "SNOMED-CT");
 
 		// Should see 1 subscription notification
@@ -313,6 +314,7 @@ public class RestHookTestR4Test extends BaseSubscriptionsR4Test {
 		assertEquals(Constants.CT_FHIR_JSON_NEW, ourRestfulServer.getRequestContentTypes().get(0));
 
 		// Send a meta-add
+		ourLog.info("Sending a meta-add");
 		obs.setId(obs.getIdElement().toUnqualifiedVersionless());
 		myClient.meta().add().onResource(obs.getIdElement()).meta(new Meta().addTag("http://blah", "blah", null)).execute();
 
