b8d7428151cdf7c20f59bcd421141ea766829d9d
juan.marchionatto <juan.marchionatto@smilecdr.com>
2022-02-21 15:07:57 -0500

    Add comment for trick

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/BaseTermReadSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/BaseTermReadSvcImpl.java
@@ -1377,8 +1377,9 @@ public abstract class BaseTermReadSvcImpl implements ITermReadSvc {
 
 		List<Long> parentPids = getCodeParentPids(theSystem, theFilter.getProperty(), theFilter.getValue());
 		if (parentPids.isEmpty()) {
-//			fixme: can't do better?
-			// can't return empty must, because it wil match according to other predicates
+			// At this point we know that we want this search to return nothing, but I couldn't
+			// find any "fail-fast" predicate so have to search for something that won't be found.
+			// Can't return empty must, because it wil match according to other predicates.
 			b.must(f.match().field("myId").matching(-1L));
 			return;
 		}
