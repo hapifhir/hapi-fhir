cea34f14b2e0dc773f04b23cab9f7f7ca3e55612
Thomas Papke <web@thopap.de>
2022-03-24 22:27:15 +0100

    #3490 ApacheProxyAddressStrategy should support Forward header from Rfc7239
    Remove one copy & paste error

--- hapi-fhir-server/src/test/java/ca/uhn/fhir/rest/server/ApacheProxyAddressStrategyTest.java
+++ hapi-fhir-server/src/test/java/ca/uhn/fhir/rest/server/ApacheProxyAddressStrategyTest.java
@@ -108,7 +108,6 @@ public class ApacheProxyAddressStrategyTest {
 
 		request.addHeader("Forwarded", "Forwarded: for=192.0.2.43,"
 				+ " for=198.51.100.17;by=203.0.113.60;proto=http;host=example.com");
-		request.addHeader("X-Forwarded-Port", "-1");
 		String serverBase = addressStrategy.determineServerBase(null, request);
 		assertEquals("http://example.com/imagingstudy/fhir",
 				serverBase);
