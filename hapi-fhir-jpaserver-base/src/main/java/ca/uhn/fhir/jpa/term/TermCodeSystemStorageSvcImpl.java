3e9087aaee1a53c98be6572c4e856e5fb2cb0cdc
olivia-you <olivia.you@smilecdr.com>
2022-04-14 14:45:48 -0400

    apply suggestion

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermCodeSystemStorageSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermCodeSystemStorageSvcImpl.java
@@ -278,7 +278,9 @@ public class TermCodeSystemStorageSvcImpl implements ITermCodeSystemStorageSvc {
 			if (theCodeSystem.getContent() == CodeSystem.CodeSystemContentMode.COMPLETE || theCodeSystem.getContent() == null || theCodeSystem.getContent() == CodeSystem.CodeSystemContentMode.NOTPRESENT) {
 				ourLog.info("CodeSystem {} has a status of {}, going to store concepts in terminology tables", theResourceEntity.getIdDt().getValue(), theCodeSystem.getContentElement().getValueAsString());
 
-				ResourcePersistentId codeSystemResourcePid = new ResourcePersistentId(theCodeSystem.getUserData(RESOURCE_PID_KEY));
+				Long pid = (Long)theCodeSystem.getUserData(RESOURCE_PID_KEY);
+				assert pid != null;
+				ResourcePersistentId codeSystemResourcePid = new ResourcePersistentId(pid);
 
 				/*
 				 * If this is a not-present codesystem and codesystem version already exists, we don't want to
