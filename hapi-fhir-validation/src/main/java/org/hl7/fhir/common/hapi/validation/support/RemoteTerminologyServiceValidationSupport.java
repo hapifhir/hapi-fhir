8a04fc7c7029833f47c769272864ae101a49e598
Sean Mikalson <sean.mikalsonsmilecdr.com@Seans-MacBook-Pro.local>
2022-04-22 08:01:06 -0700

    clean up remote terminology translate methods

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/support/RemoteTerminologyServiceValidationSupport.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/support/RemoteTerminologyServiceValidationSupport.java
@@ -350,50 +350,8 @@ public class RemoteTerminologyServiceValidationSupport extends BaseValidationSup
 	public TranslateConceptResults translateConcept(TranslateCodeRequest theRequest) {
 		IGenericClient client = provideClient();
 		FhirContext fhirContext = client.getFhirContext();
-		IBaseParameters params = ParametersUtil.newInstance(fhirContext);
 
-		if (!StringUtils.isEmpty(theRequest.getConceptMapUrl())) {
-			ParametersUtil.addParameterToParametersUri(fhirContext, params, "url", theRequest.getConceptMapUrl());
-		}
-		if (!StringUtils.isEmpty(theRequest.getConceptMapVersion())) {
-			ParametersUtil.addParameterToParametersString(fhirContext, params, "conceptMapVersion", theRequest.getConceptMapVersion());
-		}
-		if (theRequest.getCodings() != null) {
-			BaseRuntimeElementCompositeDefinition<?> codeableConceptDef = (BaseRuntimeElementCompositeDefinition<?>) Objects.requireNonNull(fhirContext.getElementDefinition("CodeableConcept"));
-			BaseRuntimeChildDefinition codings = codeableConceptDef.getChildByName("coding");
-			BaseRuntimeElementCompositeDefinition<?> codingDef = (BaseRuntimeElementCompositeDefinition<?>) Objects.requireNonNull(fhirContext.getElementDefinition("Coding"));
-			BaseRuntimeChildDefinition codingSystemChild = codingDef.getChildByName("system");
-			BaseRuntimeChildDefinition codingCodeChild = codingDef.getChildByName("code");
-			BaseRuntimeElementDefinition<IPrimitiveType<?>> systemDef = (RuntimePrimitiveDatatypeDefinition) fhirContext.getElementDefinition("uri");
-			BaseRuntimeElementDefinition<IPrimitiveType<?>> codeDef = (RuntimePrimitiveDatatypeDefinition) fhirContext.getElementDefinition("code");
-
-			IBase codeableConcept = codeableConceptDef.newInstance();
-
-			for (IBaseCoding aCoding : theRequest.getCodings()) {
-				IBaseCoding newCoding = (IBaseCoding) codingDef.newInstance();
-
-				IPrimitiveType<?> newSystem = systemDef.newInstance(aCoding.getSystem());
-				codingSystemChild.getMutator().addValue(newCoding, newSystem);
-				IPrimitiveType<?> newCode = codeDef.newInstance(aCoding.getCode());
-				codingCodeChild.getMutator().addValue(newCoding, newCode);
-
-				codings.getMutator().addValue(codeableConcept, newCoding);
-			}
-
-			ParametersUtil.addParameterToParameters(fhirContext, params, "codeableConcept", codeableConcept);
-		}
-		if (!StringUtils.isEmpty(theRequest.getSourceValueSetUrl())) {
-			ParametersUtil.addParameterToParametersUri(fhirContext, params, "source", theRequest.getSourceValueSetUrl());
-		}
-		if (!StringUtils.isEmpty(theRequest.getTargetValueSetUrl())) {
-			ParametersUtil.addParameterToParametersUri(fhirContext, params, "target", theRequest.getTargetValueSetUrl());
-		}
-		if (!StringUtils.isEmpty(theRequest.getTargetSystemUrl())) {
-			ParametersUtil.addParameterToParametersUri(fhirContext, params, "targetsystem", theRequest.getTargetSystemUrl());
-		}
-		if (theRequest.isReverse()) {
-			ParametersUtil.addParameterToParametersBoolean(fhirContext, params, "reverse", theRequest.isReverse());
-		}
+		IBaseParameters params = buildTranslateInputParameters(fhirContext, theRequest);
 
 		IBaseParameters outcome = client
 			.operation()
@@ -402,48 +360,7 @@ public class RemoteTerminologyServiceValidationSupport extends BaseValidationSup
 			.withParameters(params)
 			.execute();
 
-		Optional<String> result = ParametersUtil.getNamedParameterValueAsString(fhirContext, outcome, "result");
-		Optional<String> message = ParametersUtil.getNamedParameterValueAsString(fhirContext, outcome, "message");
-		List<IBase> matches = ParametersUtil.getNamedParameters(fhirContext, outcome, "match");
-
-		TranslateConceptResults retVal = new TranslateConceptResults();
-		if (result.isPresent()) {
-			retVal.setResult(Boolean.parseBoolean(result.get()));
-		}
-		if (message.isPresent()) {
-			retVal.setMessage(message.get());
-		}
-		if (!matches.isEmpty()) {
-
-			List<TranslateConceptResult> resultList = new ArrayList();
-			for (IBase m : matches) {
-				TranslateConceptResult match = new TranslateConceptResult();
-				String equivalence = ParametersUtil.getParameterPartValueAsString(fhirContext, m, "equivalence");
-				Optional<IBase> concept = ParametersUtil.getParameterPartValue(fhirContext, m, "concept");
-				String source = ParametersUtil.getParameterPartValueAsString(fhirContext, m, "source");
-
-				if (StringUtils.isNotBlank(equivalence)) {
-					match.setEquivalence(equivalence);
-				}
-
-				if (concept.isPresent()) {
-					IBaseCoding matchedCoding = (IBaseCoding) concept.get();
-					match.setSystem(matchedCoding.getSystem());
-					match.setCode(matchedCoding.getCode());
-					match.setDisplay(matchedCoding.getDisplay());
-
-					if (StringUtils.isNotBlank(source)) {
-						match.setConceptMapUrl(source);
-					}
-
-					resultList.add(match);
-				}
-			}
-
-			retVal.setResults(resultList);
-		}
-
-		return retVal;
+		return translateOutcomeToResults(fhirContext, outcome);
 	}
 
 	private IGenericClient provideClient() {
@@ -555,6 +472,105 @@ public class RemoteTerminologyServiceValidationSupport extends BaseValidationSup
 	public void addClientInterceptor(@Nonnull Object theClientInterceptor) {
 		Validate.notNull(theClientInterceptor, "theClientInterceptor must not be null");
 		myClientInterceptors.add(theClientInterceptor);
+	}
+
+	private IBaseParameters buildTranslateInputParameters(FhirContext fhirContext, TranslateCodeRequest theRequest) {
+		IBaseParameters params = ParametersUtil.newInstance(fhirContext);
+		if (!StringUtils.isEmpty(theRequest.getConceptMapUrl())) {
+			ParametersUtil.addParameterToParametersUri(fhirContext, params, "url", theRequest.getConceptMapUrl());
+		}
+		if (!StringUtils.isEmpty(theRequest.getConceptMapVersion())) {
+			ParametersUtil.addParameterToParametersString(fhirContext, params, "conceptMapVersion", theRequest.getConceptMapVersion());
+		}
+		if (theRequest.getCodings() != null) {
+			addCodingsToTranslateParameters(fhirContext, theRequest.getCodings(), params);
+		}
+		if (!StringUtils.isEmpty(theRequest.getSourceValueSetUrl())) {
+			ParametersUtil.addParameterToParametersUri(fhirContext, params, "source", theRequest.getSourceValueSetUrl());
+		}
+		if (!StringUtils.isEmpty(theRequest.getTargetValueSetUrl())) {
+			ParametersUtil.addParameterToParametersUri(fhirContext, params, "target", theRequest.getTargetValueSetUrl());
+		}
+		if (!StringUtils.isEmpty(theRequest.getTargetSystemUrl())) {
+			ParametersUtil.addParameterToParametersUri(fhirContext, params, "targetsystem", theRequest.getTargetSystemUrl());
+		}
+		if (theRequest.isReverse()) {
+			ParametersUtil.addParameterToParametersBoolean(fhirContext, params, "reverse", theRequest.isReverse());
+		}
+
+		return params;
+	}
+
+	private void addCodingsToTranslateParameters(FhirContext fhirContext, List<IBaseCoding> theCodings, IBaseParameters theParams) {
+		BaseRuntimeElementCompositeDefinition<?> codeableConceptDef = (BaseRuntimeElementCompositeDefinition<?>) Objects.requireNonNull(fhirContext.getElementDefinition("CodeableConcept"));
+		BaseRuntimeChildDefinition codings = codeableConceptDef.getChildByName("coding");
+		BaseRuntimeElementCompositeDefinition<?> codingDef = (BaseRuntimeElementCompositeDefinition<?>) Objects.requireNonNull(fhirContext.getElementDefinition("Coding"));
+		BaseRuntimeChildDefinition codingSystemChild = codingDef.getChildByName("system");
+		BaseRuntimeChildDefinition codingCodeChild = codingDef.getChildByName("code");
+		BaseRuntimeElementDefinition<IPrimitiveType<?>> systemDef = (RuntimePrimitiveDatatypeDefinition) fhirContext.getElementDefinition("uri");
+		BaseRuntimeElementDefinition<IPrimitiveType<?>> codeDef = (RuntimePrimitiveDatatypeDefinition) fhirContext.getElementDefinition("code");
+
+		IBase codeableConcept = codeableConceptDef.newInstance();
+
+		for (IBaseCoding aCoding : theCodings) {
+			IBaseCoding newCoding = (IBaseCoding) codingDef.newInstance();
+
+			IPrimitiveType<?> newSystem = systemDef.newInstance(aCoding.getSystem());
+			codingSystemChild.getMutator().addValue(newCoding, newSystem);
+			IPrimitiveType<?> newCode = codeDef.newInstance(aCoding.getCode());
+			codingCodeChild.getMutator().addValue(newCoding, newCode);
+
+			codings.getMutator().addValue(codeableConcept, newCoding);
+		}
+
+		ParametersUtil.addParameterToParameters(fhirContext, theParams, "codeableConcept", codeableConcept);
+	}
+
+	private TranslateConceptResults translateOutcomeToResults(FhirContext fhirContext, IBaseParameters outcome) {
+		Optional<String> result = ParametersUtil.getNamedParameterValueAsString(fhirContext, outcome, "result");
+		Optional<String> message = ParametersUtil.getNamedParameterValueAsString(fhirContext, outcome, "message");
+		List<IBase> matches = ParametersUtil.getNamedParameters(fhirContext, outcome, "match");
+
+		TranslateConceptResults retVal = new TranslateConceptResults();
+		if (result.isPresent()) {
+			retVal.setResult(Boolean.parseBoolean(result.get()));
+		}
+		if (message.isPresent()) {
+			retVal.setMessage(message.get());
+		}
+		if (!matches.isEmpty()) {
+			retVal.setResults(matchesToTranslateConceptResults(fhirContext, matches));
+		}
+
+		return retVal;
+	}
+
+	private List<TranslateConceptResult> matchesToTranslateConceptResults(FhirContext fhirContext, List<IBase> theMatches) {
+		List<TranslateConceptResult> resultList = new ArrayList();
+		for (IBase m : theMatches) {
+			TranslateConceptResult match = new TranslateConceptResult();
+			String equivalence = ParametersUtil.getParameterPartValueAsString(fhirContext, m, "equivalence");
+			Optional<IBase> concept = ParametersUtil.getParameterPartValue(fhirContext, m, "concept");
+			String source = ParametersUtil.getParameterPartValueAsString(fhirContext, m, "source");
+
+			if (StringUtils.isNotBlank(equivalence)) {
+				match.setEquivalence(equivalence);
+			}
+
+			if (concept.isPresent()) {
+				IBaseCoding matchedCoding = (IBaseCoding) concept.get();
+				match.setSystem(matchedCoding.getSystem());
+				match.setCode(matchedCoding.getCode());
+				match.setDisplay(matchedCoding.getDisplay());
+
+				if (StringUtils.isNotBlank(source)) {
+					match.setConceptMapUrl(source);
+				}
+
+				resultList.add(match);
+			}
+		}
+		return resultList;
 	}
 
 }
