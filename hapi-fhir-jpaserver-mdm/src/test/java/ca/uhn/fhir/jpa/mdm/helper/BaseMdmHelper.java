0aaff205f811af0ebc7d063030133196159580d2
Tadgh <garygrantgraham@gmail.com>
2022-05-24 07:44:43 -0700

    Move subscription loading

--- hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/helper/BaseMdmHelper.java
+++ hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/helper/BaseMdmHelper.java
@@ -88,7 +88,6 @@ public abstract class BaseMdmHelper implements BeforeEachCallback, AfterEachCall
 		// We need to call this because subscriptions will get deleted in @After cleanup
 		waitForActivatedSubscriptionCount(0);
 		myMdmSubscriptionLoader.daoUpdateMdmSubscriptions();
-		mySubscriptionLoader.syncSubscriptions();
 		waitForActivatedSubscriptionCount(2);
 	}
 
