25a0763adae831a36c808122002edfc972c1eccc
Steven Li <steven@smilecdr.com>
2022-06-01 15:55:00 -0400

    Fixed test to clear DaoConfig

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4HistoryRewriteTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4HistoryRewriteTest.java
@@ -4,62 +4,48 @@ import ca.uhn.fhir.jpa.test.BaseJpaR4Test;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.StringType;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FhirResourceDaoR4HistoryRewriteTest extends BaseJpaR4Test {
-
+	private static final String TEST_SYSTEM_NAME = "testHistoryRewrite";
+	private static final String TEST_FAMILY_NAME = "Johnson";
+	private static final String TEST_GIVEN_NAME = "Dan";
 	private static final Logger ourLog = LoggerFactory.getLogger(FhirResourceDaoR4Test.class);
+
+	@BeforeEach
+	public void setUp() {
+		myDaoConfig.setUpdateWithHistoryRewriteEnabled(true);
+	}
+
+	@AfterEach
+	public void tearDown() {
+		myDaoConfig.setUpdateWithHistoryRewriteEnabled(false);
+	}
 
 	@Test
 	public void testHistoryRewriteNonCurrentVersion() {
-		myDaoConfig.setUpdateWithHistoryRewriteEnabled(true);
-		String methodName = "testHistoryRewrite";
-		String methodNameModified = "testHistoryRewriteDiff";
-		String testFamilyName = "Johnson";
-		String testGivenName = "Dan";
+		String systemNameModified = "testHistoryRewriteDiff";
 		String testFamilyNameModified = "Jackson";
 
 		// setup
-		Patient p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		IIdType id = myPatientDao.create(p, mySrd).getId().toUnqualifiedVersionless();
-		ourLog.info("Created patient, got it: {}", id);
-
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		p.addName().setFamily(testFamilyName);
-		p.setId("Patient/" + id.getIdPart());
-
-		myPatientDao.update(p, mySrd);
-
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		p.addName().setFamily(testFamilyName).setGiven(Arrays.asList(new StringType(testGivenName)));
-		p.setId("Patient/" + id.getIdPart());
-
-		myPatientDao.update(p, mySrd);
-
-		p = myPatientDao.read(id.toVersionless(), mySrd);
-		assertEquals(testFamilyName, p.getName().get(0).getFamily());
-		assertThat(p.getIdElement().toString(), endsWith("/_history/3"));
+		IIdType id = createPatientWithHistory();
 
 		// execute updates
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
+		Patient p = new Patient();
+		p.addIdentifier().setSystem("urn:system").setValue(TEST_SYSTEM_NAME);
 		p.addName().setFamily(testFamilyNameModified);
 		p.setId("Patient/" + id.getIdPart() + "/_history/2");
 
@@ -68,7 +54,7 @@ public class FhirResourceDaoR4HistoryRewriteTest extends BaseJpaR4Test {
 		myPatientDao.update(p, mySrd);
 
 		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodNameModified);
+		p.addIdentifier().setSystem("urn:system").setValue(systemNameModified);
 		p.setId("Patient/" + id.getIdPart() + "/_history/1");
 
 		Patient history1 = myPatientDao.read(id.withVersion("1"));
@@ -81,48 +67,23 @@ public class FhirResourceDaoR4HistoryRewriteTest extends BaseJpaR4Test {
 		assertTrue(Math.abs(h2.getMeta().getLastUpdated().getTime() - new Date().getTime()) < 1000L);
 
 		Patient h1 = myPatientDao.read(id.withVersion("1"), mySrd);
-		assertEquals(methodNameModified, h1.getIdentifier().get(0).getValue());
+		assertEquals(systemNameModified, h1.getIdentifier().get(0).getValue());
 		assertThat(h1.getIdElement().toString(), endsWith("/_history/1"));
 		assertTrue(Math.abs(h1.getMeta().getLastUpdated().getTime() - new Date().getTime()) < 1000L);
 	}
 
 	@Test
 	public void testHistoryRewriteCurrentVersion() {
-		myDaoConfig.setUpdateWithHistoryRewriteEnabled(true);
-		String methodName = "testHistoryRewrite";
-		String testFamilyName = "Johnson";
-		String testGivenName = "Dan";
 		String testFamilyNameModified = "Jackson";
 		String testGivenNameModified = "Randy";
 
 		// setup
-		Patient p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		IIdType id = myPatientDao.create(p, mySrd).getId().toUnqualifiedVersionless();
-		ourLog.info("Created patient, got it: {}", id);
-
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		p.addName().setFamily(testFamilyName);
-		p.setId("Patient/" + id.getIdPart());
-
-		myPatientDao.update(p, mySrd);
-
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		p.addName().setFamily(testFamilyName).setGiven(Arrays.asList(new StringType(testGivenName)));
-		p.setId("Patient/" + id.getIdPart());
-
-		myPatientDao.update(p, mySrd);
-
-		p = myPatientDao.read(id.toVersionless(), mySrd);
-		assertEquals(p.getName().get(0).getFamily(), testFamilyName);
-		assertThat(p.getIdElement().toString(), endsWith("/_history/3"));
+		IIdType id = createPatientWithHistory();
 
 		// execute update
-		p = new Patient();
-		p.addIdentifier().setSystem("urn:system").setValue(methodName);
-		p.addName().setFamily(testFamilyNameModified).setGiven(Arrays.asList(new StringType(testGivenNameModified)));
+		Patient p = new Patient();
+		p.addIdentifier().setSystem("urn:system").setValue(TEST_SYSTEM_NAME);
+		p.addName().setFamily(testFamilyNameModified).setGiven(List.of(new StringType(testGivenNameModified)));
 		p.setId("Patient/" + id.getIdPart() + "/_history/3");
 
 		myPatientDao.update(p, mySrd);
@@ -133,4 +94,33 @@ public class FhirResourceDaoR4HistoryRewriteTest extends BaseJpaR4Test {
 		assertThat(lPatient.getIdElement().toString(), endsWith("/_history/3"));
 		assertTrue(Math.abs(lPatient.getMeta().getLastUpdated().getTime() - new Date().getTime()) < 1000L);
 	}
+
+
+	@NotNull
+	private IIdType createPatientWithHistory() {
+		Patient p = new Patient();
+		p.addIdentifier().setSystem("urn:system").setValue(TEST_SYSTEM_NAME);
+		IIdType id = myPatientDao.create(p, mySrd).getId().toUnqualifiedVersionless();
+		ourLog.info("Created patient, got it: {}", id);
+
+		p = new Patient();
+		p.addIdentifier().setSystem("urn:system").setValue(TEST_SYSTEM_NAME);
+		p.addName().setFamily(TEST_FAMILY_NAME);
+		p.setId("Patient/" + id.getIdPart());
+
+		myPatientDao.update(p, mySrd);
+
+		p = new Patient();
+		p.addIdentifier().setSystem("urn:system").setValue(TEST_SYSTEM_NAME);
+		p.addName().setFamily(TEST_FAMILY_NAME).setGiven(List.of(new StringType(TEST_GIVEN_NAME)));
+		p.setId("Patient/" + id.getIdPart());
+
+		myPatientDao.update(p, mySrd);
+
+		p = myPatientDao.read(id.toVersionless(), mySrd);
+		assertEquals(TEST_FAMILY_NAME, p.getName().get(0).getFamily());
+		assertThat(p.getIdElement().toString(), endsWith("/_history/3"));
+		return id;
+	}
+
 }
