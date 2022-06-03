9d6b380e286a648215682469634bde1c6d626b13
Long Ma <long@smilecdr.com>
2022-06-03 11:03:27 -0600

    added fix for subscription in default partition when default partition's id is null

--- hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/match/matcher/subscriber/SubscriptionMatchingSubscriber.java
+++ hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/subscription/match/matcher/subscriber/SubscriptionMatchingSubscriber.java
@@ -127,7 +127,7 @@ public class SubscriptionMatchingSubscriber implements MessageHandler {
 		for (ActiveSubscription nextActiveSubscription : subscriptions) {
 			// skip if the partitions don't match
 			CanonicalSubscription subscription = nextActiveSubscription.getSubscription();
-			if (subscription != null && subscription.getRequestPartitionId() != null && theMsg.getPartitionId() != null &&
+			if (subscription != null && theMsg.getPartitionId() != null &&
 				theMsg.getPartitionId().hasPartitionIds() && !subscription.getCrossPartitionEnabled() &&
 				!theMsg.getPartitionId().hasPartitionId(subscription.getRequestPartitionId())) {
 				continue;
