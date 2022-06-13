3ca245f20001ac587fc1e80fc238017e95515030
StevenXLi <stevenli_8118@hotmail.com>
2022-06-13 14:04:49 -0400

    3684 subscription for delivering delete events formatted wrong (#3685)
    * Fixed issue with delivering delete events Subscriptions
    
    * Extracted deliverer class name to a constant, now shared across tests and interceptor class
    
    * Added change log and doc changes
    
    * moved the constant to a more appropriate place
    
    Co-authored-by: Steven Li <steven@smilecdr.com>

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/i18n/Msg.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/i18n/Msg.java
@@ -25,7 +25,7 @@ public final class Msg {
 
 	/**
 	 * IMPORTANT: Please update the following comment after you add a new code
-	 * Last code value: 2082
+	 * Last code value: 2086
 	 */
 
 	private Msg() {}
