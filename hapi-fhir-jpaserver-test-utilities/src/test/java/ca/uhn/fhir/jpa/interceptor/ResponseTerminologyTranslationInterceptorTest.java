0d133c4bf274742c3c0fa4ddf80214f2e72d372d
Steven Li <steven@smilecdr.com>
2022-06-20 10:56:11 -0400

    added failing test for Map Response Terminology for bulk export

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/interceptor/ResponseTerminologyTranslationInterceptorTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/interceptor/ResponseTerminologyTranslationInterceptorTest.java
@@ -2,10 +2,19 @@ package ca.uhn.fhir.jpa.interceptor;
 
 import ca.uhn.fhir.interceptor.api.IInterceptorService;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.jpa.batch.config.BatchConstants;
+import ca.uhn.fhir.jpa.bulk.export.api.IBulkDataExportJobSchedulingHelper;
+import ca.uhn.fhir.jpa.bulk.export.api.IBulkDataExportSvc;
+import ca.uhn.fhir.jpa.bulk.export.model.BulkExportJobStatusEnum;
 import ca.uhn.fhir.jpa.provider.r4.BaseResourceProviderR4Test;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.api.server.bulk.BulkDataExportOptions;
 import ca.uhn.fhir.rest.server.interceptor.ResponseTerminologyTranslationInterceptor;
+import ca.uhn.fhir.util.UrlUtil;
+import com.google.common.collect.Sets;
 import org.hamcrest.Matchers;
 import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.Binary;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Observation;
 import org.junit.jupiter.api.AfterEach;
@@ -17,13 +26,16 @@ import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ResponseTerminologyTranslationInterceptorTest extends BaseResourceProviderR4Test {
 
 	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ResponseTerminologyTranslationInterceptorTest.class);
+	public static final String TEST_OBV_FILTER = "Observation?status=amended";
 
 	@Autowired
 	private DaoRegistry myDaoRegistry;
@@ -31,6 +43,10 @@ public class ResponseTerminologyTranslationInterceptorTest extends BaseResourceP
 	private IInterceptorService myInterceptorBroadcaster;
 	@Autowired
 	private ResponseTerminologyTranslationInterceptor myResponseTerminologyTranslationInterceptor;
+	@Autowired
+	private IBulkDataExportSvc myBulkDataExportSvc;
+	@Autowired
+	private IBulkDataExportJobSchedulingHelper myBulkDataExportJobSchedulingHelper;
 
 	@BeforeEach
 	public void beforeEach() {
@@ -124,10 +140,74 @@ public class ResponseTerminologyTranslationInterceptorTest extends BaseResourceP
 		));
 	}
 
+
+	@Test
+	public void testBulkExport_TerminologyTranslation_MappingFound() {
+		// Create some resources to load
+		Observation observation = new Observation();
+		observation.setStatus(Observation.ObservationStatus.AMENDED);
+		observation.getCode()
+			.addCoding(new Coding(CS_URL, "12345", null));
+		myObservationDao.create(observation);
+
+		// set mapping specs
+		myResponseTerminologyTranslationInterceptor.addMappingSpecification(CS_URL, CS_URL_2);
+
+		// Create a bulk job
+		BulkDataExportOptions options = new BulkDataExportOptions();
+		options.setResourceTypes(Sets.newHashSet("Observation"));
+		options.setFilters(Sets.newHashSet(TEST_OBV_FILTER));
+		options.setExportStyle(BulkDataExportOptions.ExportStyle.SYSTEM);
+
+		IBulkDataExportSvc.JobInfo jobDetails = myBulkDataExportSvc.submitJob(options, true, mySrd);
+		assertNotNull(jobDetails.getJobId());
+
+		// Check the status
+		IBulkDataExportSvc.JobInfo status = myBulkDataExportSvc.getJobInfoOrThrowResourceNotFound(jobDetails.getJobId());
+		assertEquals(BulkExportJobStatusEnum.SUBMITTED, status.getStatus());
+		assertEquals("/$export?_outputFormat=application%2Ffhir%2Bndjson&_type=Observation&_typeFilter=" + UrlUtil.escapeUrlParam(TEST_OBV_FILTER), status.getRequest());
+
+		// Run a scheduled pass to build the export
+		myBulkDataExportJobSchedulingHelper.startSubmittedJobs();
+		awaitAllBulkJobCompletions();
+
+		// Fetch the job again
+		status = myBulkDataExportSvc.getJobInfoOrThrowResourceNotFound(jobDetails.getJobId());
+		assertEquals(BulkExportJobStatusEnum.COMPLETE, status.getStatus());
+
+		// Iterate over the files
+		for (IBulkDataExportSvc.FileEntry next : status.getFiles()) {
+			Binary nextBinary = myBinaryDao.read(next.getResourceId());
+			assertEquals(Constants.CT_FHIR_NDJSON, nextBinary.getContentType());
+			String nextContents = new String(nextBinary.getContent(), Constants.CHARSET_UTF8);
+			ourLog.info("Next contents for type {}:\n{}", next.getResourceType(), nextContents);
+			if ("Observation".equals(next.getResourceType())) {
+				assertThat(nextContents, containsString(
+					"{\"system\":\"http://example.com/my_code_system\",\"code\":\"12345\"}"
+				));
+				assertThat(nextContents, containsString(
+					"{\"system\":\"http://example.com/my_code_system2\",\"code\":\"34567\",\"display\":\"Target Code 34567\"}"
+				));
+			} else {
+				fail(next.getResourceType());
+			}
+		}
+
+		assertEquals(1, status.getFiles().size());
+	}
+
 	@Nonnull
 	private List<String> toCodeStrings(Observation observation) {
 		return observation.getCode().getCoding().stream().map(t -> "[system=" + t.getSystem() + ", code=" + t.getCode() + ", display=" + t.getDisplay() + "]").collect(Collectors.toList());
 	}
 
-
+	private void awaitAllBulkJobCompletions() {
+		myBatchJobHelper.awaitAllBulkJobCompletions(
+			BatchConstants.BULK_EXPORT_JOB_NAME,
+			BatchConstants.PATIENT_BULK_EXPORT_JOB_NAME,
+			BatchConstants.GROUP_BULK_EXPORT_JOB_NAME,
+			BatchConstants.DELETE_EXPUNGE_JOB_NAME,
+			BatchConstants.MDM_CLEAR_JOB_NAME
+		);
+	}
 }
