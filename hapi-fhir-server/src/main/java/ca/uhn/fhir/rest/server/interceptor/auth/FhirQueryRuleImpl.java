c5f1605abbc7f701cf7ad3b92390f1f3acb7ecb8
Michael Buckley <michael.buckley@smilecdr.com>
2022-06-24 10:30:50 -0400

    notes

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/auth/FhirQueryRuleImpl.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/auth/FhirQueryRuleImpl.java
@@ -39,6 +39,7 @@ public class FhirQueryRuleImpl extends RuleImplOp {
 	@Override
 	protected AuthorizationInterceptor.Verdict applyRuleLogic(RestOperationTypeEnum theOperation, RequestDetails theRequestDetails, IBaseResource theInputResource, IIdType theInputResourceId, IBaseResource theOutputResource, Set<AuthorizationFlagsEnum> theFlags, FhirContext theFhirContext, RuleTarget theRuleTarget, IRuleApplier theRuleApplier) {
 		ourLog.trace("applyRuleLogic {} {}", theOperation, theRuleTarget);
+		// wipjv hack alert - theOutputResource == null means we're in the pre-show pointcut.
 		if (theOutputResource == null) {
 			return super.applyRuleLogic(theOperation, theRequestDetails, theInputResource, theInputResourceId, theOutputResource, theFlags, theFhirContext, theRuleTarget, theRuleApplier);
 		}
