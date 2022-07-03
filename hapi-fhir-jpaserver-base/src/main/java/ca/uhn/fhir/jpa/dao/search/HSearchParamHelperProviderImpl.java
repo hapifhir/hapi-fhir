0cd78ad40e7494bfcd308d84f5812e00df894f1a
juan.marchionatto <juan.marchionatto@smilecdr.com>
2022-07-03 16:25:31 -0400

    Cache providers y Resource type and param name

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/search/HSearchParamHelperProviderImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/search/HSearchParamHelperProviderImpl.java
@@ -6,19 +6,42 @@ import ca.uhn.fhir.rest.api.RestSearchParameterTypeEnum;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.util.ISearchParamRegistry;
 import ca.uhn.fhir.rest.server.util.ResourceSearchParams;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HSearchParamHelperProviderImpl implements IHSearchParamHelperProvider {
 
 	@Autowired
 	private ISearchParamRegistry mySearchParamRegistry;
 
+	private final Map<String, Map<String, HSearchParamHelper<?>>> providerCache = new ConcurrentHashMap<>();
 
-//	fixme jm: cache this
+
 	@Override
 	public HSearchParamHelper<?> provideHelper(String theResourceTypeName, String theParamName) {
+		HSearchParamHelper<?> cachedProvider = getCached(theResourceTypeName, theParamName);
+		if (cachedProvider != null) { return cachedProvider; }
+
+		HSearchParamHelper<?> helper = findHelper(theResourceTypeName, theParamName);
+
+		cache(theResourceTypeName, theParamName, helper);
+		return helper;
+	}
+
+
+	private void cache(String theResourceTypeName, String theParamName, HSearchParamHelper<?> theHelper) {
+		providerCache .computeIfAbsent(theResourceTypeName, k -> new HashMap<>()) .put(theParamName, theHelper);   // IfAbsent(theResourceTypeName,  new HashSet<<String, HSearchParamHelper<?>>())
+	}
+
+
+	@NotNull
+	private HSearchParamHelper<?> findHelper(String theResourceTypeName, String theParamName) {
 		Optional<RestSearchParameterTypeEnum> paramTypeOpt = getParamType(theResourceTypeName, theParamName);
 
 		if (paramTypeOpt.isEmpty()) {
@@ -34,8 +57,12 @@ public class HSearchParamHelperProviderImpl implements IHSearchParamHelperProvid
 			throw new InternalErrorException(Msg.code(0) +
 				"HSearchParamHelper.myTypeHelperMap doesn't contain an entry for " + paramTypeOpt.get());
 		}
-
 		return helper;
+	}
+
+
+	private HSearchParamHelper<?> getCached(String theResourceTypeName, String theParamName) {
+		return providerCache.getOrDefault(theResourceTypeName, Collections.emptyMap()).get(theParamName);
 	}
 
 
