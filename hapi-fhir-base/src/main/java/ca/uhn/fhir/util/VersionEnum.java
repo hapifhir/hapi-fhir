57ee93251af6a7114776430e9003911111612272
Tadgh <garygrantgraham@gmail.com>
2022-07-12 11:08:46 -0700

    Mergeback 2022 07 08 (#3771)
    * fix handling of common search parameters
    
    * Revert "fix handling of common search parameters"
    
    This reverts commit 89c45eebdc31c3dd9533570d9c709ecc7ed5a7a6.
    
    * Fix implementation, add test (#3378)
    
    * Fix implementation, add test
    
    * Tighten test
    
    * Rip out dead modules
    
    * Add changelog
    
    * Jr 20220210 handle common search params in contained searches (#3377)
    
    * fix handling of common search parameters
    
    * add support for reference search parameters with multiple paths
    
    * Issue 3357
    
    * Version bump
    
    * Fixed null pointer exception for re-loading subscription on cdr restart and when there's no partition id in the request, and added tests
    
    * added changelogs for this fix
    
    * Fix broken changelog file
    
    * Can't specify specific resource type permissions for bulk export (#3376)
    
    * deny user from exporting without perms
    
    * add unit tests
    
    * add changelog
    
    Co-authored-by: olivia-you <olivia.you@smilecdr.com>
    
    * Make migration donothing as it was added in error
    
    * Remove ehcache
    
    * Add version.yaml:
    
    * Add to sources/javadocs for dist
    
    * Fix typo
    
    * fix up pom
    
    * wip test removing checkstyle plugin from deployable pom
    
    * remove test pom changes, instead just dont deploy to sonatype
    
    * typo
    
    * Version bump
    
    * Add implementation, testing, and changelog
    
    * Add backport info, make aware of 6_0_0
    
    * add jira ref
    
    * Fix test
    
    * Add versionenum
    
    * Bump dep for spring, add changelog, add versionenum, add changelog folder
    
    * Actually do the version bump
    
    * 3654 postgres lobs (#3656)
    
    * Implementation, test, changelog
    
    * Modify changelog
    
    * Fix changelog
    
    * Add upgrade.md file
    
    * Modify to yaml changes
    
    * Update hapi-fhir-docs/src/main/resources/ca/uhn/hapi/fhir/changelog/5_7_3/3654-lob-problems.yaml
    
    Co-authored-by: Ken Stevens <khstevens@gmail.com>
    
    * tidy
    
    * more work
    
    Co-authored-by: Ken Stevens <khstevens@gmail.com>
    
    * Fix bad yaml indent
    
    * Version bump
    
    * Back out the reindex changes introduced in 5.7.3
    
    * update changelog
    
    * version bump
    
    * added fix for subscription in default partition when default partition's id is null
    
    * added changelog
    
    * Add version info and changelog folder
    
    * adding release pipeline yml file fo azure
    
    * Fix parameter count limitation on MSSQL/Oracle large transactions (#3075)
    
    * Fix parameter count limitation on MSSQL/Oracle large transactions
    
    * Changelog fix
    
    * Move changelog
    
    * Force a change to trigger CI
    
    * Test refactoring
    
    * Backport oracle fix
    
    * Fix backport
    
    * upping version to 5.7.5 for release
    
    * add versionenum
    
    * Updating version to: 5.7.6 post release.
    
    * Add query-chunk-aware code
    
    * Add new folder
    
    * Fix up versionenum
    
    * Remove imports
    
    Co-authored-by: Jason Roberts <jason.roberts@smilecdr.com>
    Co-authored-by: JasonRoberts-smile <85363818+JasonRoberts-smile@users.noreply.github.com>
    Co-authored-by: Mark Iantorno <markiantorno@gmail.com>
    Co-authored-by: Steven Li <steven@smilecdr.com>
    Co-authored-by: Olivia You <46392181+oliviayou@users.noreply.github.com>
    Co-authored-by: olivia-you <olivia.you@smilecdr.com>
    Co-authored-by: Ken Stevens <khstevens@gmail.com>
    Co-authored-by: Long Ma <long@smilecdr.com>
    Co-authored-by: James Agnew <jamesagnew@gmail.com>

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
@@ -89,10 +89,14 @@ public enum VersionEnum {
 	V5_7_0,
 	V5_7_1,
 	V5_7_2,
+	V5_7_3,
+	V5_7_4,
+	V5_7_5,
+	V5_7_6,
 	V6_0_0,
 	V6_0_1,
 	V6_0_2,
-	V6_1_0
+	V6_1_0,
 	;
 
 	public static VersionEnum latestVersion() {
