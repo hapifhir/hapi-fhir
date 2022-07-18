70556a57cadd3a4d62cb182149e5f98ab45d7d8c
Ken Stevens <ken@smilecdr.com>
2022-07-18 18:09:54 -0400

    jpa is failing atm

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/JpaConfig.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/JpaConfig.java
@@ -500,8 +500,8 @@ public class JpaConfig {
 
 	@Bean(name = RepositoryValidatingRuleBuilder.REPOSITORY_VALIDATING_RULE_BUILDER)
 	@Scope("prototype")
-	public RepositoryValidatingRuleBuilder repositoryValidatingRuleBuilder() {
-		return new RepositoryValidatingRuleBuilder();
+	public RepositoryValidatingRuleBuilder repositoryValidatingRuleBuilder(IValidationSupport theValidationSupport) {
+		return new RepositoryValidatingRuleBuilder(theValidationSupport);
 	}
 
 	@Bean
