d138de68c98d939a866a7d032a9360131d033393
dotasek <david.otasek@smilecdr.com>
2022-08-08 15:23:01 -0400

    Add necessary package load to test

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ParserWithValidationR4Test.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ParserWithValidationR4Test.java
@@ -4,19 +4,24 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.ResultSeverityEnum;
 import ca.uhn.fhir.validation.ValidationResult;
+import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 import org.hl7.fhir.r4.model.MedicationRequest;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static ca.uhn.fhir.util.ClasspathUtil.loadResource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserWithValidationR4Test {
 	private static final FhirContext ourCtx = FhirContext.forR4();
 
 	@Test
-	public void testActivityDefinitionElementsOrder() {
+	public void testActivityDefinitionElementsOrder() throws IOException {
 		ourCtx.setValidationSupport(getValidationSupport());
 		MedicationRequest med_req = ourCtx.newJsonParser().parseResource(MedicationRequest.class, loadResource("/r4/amz/medication-request-amz.json"));
 
@@ -26,10 +31,17 @@ public class ParserWithValidationR4Test {
 
 		validator.registerValidatorModule(instanceValidator);
 		ValidationResult validationResult = validator.validateWithResult(med_req);
-		validationResult.getMessages().forEach(System.out::println);
+		assertEquals(0, validationResult.getMessages().stream().filter(message -> message.getSeverity() == ResultSeverityEnum.ERROR));
+
 	}
 
-	private IValidationSupport getValidationSupport() {
-		return new ValidationSupportChain(new DefaultProfileValidationSupport(ourCtx));
+	private IValidationSupport getValidationSupport() throws IOException {
+		NpmPackageValidationSupport npmPackageSupport = new NpmPackageValidationSupport(ourCtx);
+		npmPackageSupport.loadPackageFromClasspath("classpath:package/hl7.fhir.xver-extensions-0.0.11.tgz");
+
+		return new ValidationSupportChain(
+			new DefaultProfileValidationSupport(ourCtx),
+			npmPackageSupport
+		);
 	}
 }
