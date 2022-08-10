8c97f10925fff4684f41aa05c15a13fa472dec1f
dotasek <david.otasek@smilecdr.com>
2022-08-10 14:38:28 -0400

    Rename Tests + InvalidExtensionValue test

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/XverExtensionsValidationTest.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/XverExtensionsValidationTest.java
@@ -5,41 +5,61 @@ import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.ResultSeverityEnum;
+import ca.uhn.fhir.validation.SingleValidationMessage;
 import ca.uhn.fhir.validation.ValidationResult;
 import org.hl7.fhir.common.hapi.validation.support.CachingValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
+import org.hl7.fhir.r4.model.IntegerType;
 import org.hl7.fhir.r4.model.MedicationRequest;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 import static ca.uhn.fhir.util.ClasspathUtil.loadResource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class XverValidationTest {
+public class XverExtensionsValidationTest {
 	private static final FhirContext ourCtx = FhirContext.forR4();
 
 	@Test
-	public void validateWithXverExtensionsAndCachingValidationSupport() throws IOException {
+	public void validation_XverExtensionsAndCachingValidationSupport_ReturnsNoErrors() throws IOException {
 		ourCtx.setValidationSupport(new CachingValidationSupport(getValidationSupport()));
 		MedicationRequest med_req;
 		med_req = getMedicationRequest();
 		FhirValidator validator = getFhirValidator();
 		ValidationResult validationResult = validator.validateWithResult(med_req);
-		assertEquals(0, validationResult.getMessages().stream().filter(message -> message.getSeverity() == ResultSeverityEnum.ERROR).count());
+		assertEquals(0, validationResult.getMessages().stream().filter(errorMessagePredicate()).count());
 	}
 
 
 	@Test
-	public void validateWithXverExtensions() throws IOException {
+	public void validation_XverExtensions_ReturnsNoErrors() throws IOException {
 		ourCtx.setValidationSupport(getValidationSupport());
 		MedicationRequest med_req = getMedicationRequest();
 		FhirValidator validator = getFhirValidator();
 		ValidationResult validationResult = validator.validateWithResult(med_req);
-		assertEquals(0, validationResult.getMessages().stream().filter(message -> message.getSeverity() == ResultSeverityEnum.ERROR).count());
+		assertEquals(0, validationResult.getMessages().stream().filter(errorMessagePredicate()).count());
+	}
+
+	@Test
+	public void validation_InvalidExtensionValue_ReturnsError() throws IOException {
+		ourCtx.setValidationSupport(getValidationSupport());
+		MedicationRequest med_req = getMedicationRequest();
+		med_req.getMeta().getExtension().get(0).setValue(new IntegerType().setValue(123));
+		FhirValidator validator = getFhirValidator();
+		ValidationResult validationResult = validator.validateWithResult(med_req);
+		assertEquals(1, validationResult.getMessages().stream().filter(errorMessagePredicate()).count());
+		SingleValidationMessage errorMessage = validationResult.getMessages().stream().filter(errorMessagePredicate()).findFirst().get();
+		assertEquals("Extension_EXT_Type", errorMessage.getMessageId());
+	}
+
+	@NotNull
+	private static Predicate<SingleValidationMessage> errorMessagePredicate() {
+		return message -> message.getSeverity() == ResultSeverityEnum.ERROR;
 	}
 
 	@NotNull
