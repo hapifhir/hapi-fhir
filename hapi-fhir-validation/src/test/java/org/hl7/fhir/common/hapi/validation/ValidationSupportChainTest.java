578d47242f2f77da6acd6b98bdda74758f2c6f54
dotasek <david.otasek@smilecdr.com>
2022-08-11 10:01:40 -0400

    Test ValidationSupportChain

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/common/hapi/validation/ValidationSupportChainTest.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/common/hapi/validation/ValidationSupportChainTest.java
@@ -2,16 +2,20 @@ package org.hl7.fhir.common.hapi.validation;
 
 import ca.uhn.fhir.context.ConfigurationException;
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.i18n.Msg;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ValidationSupportChainTest {
+
 
 	@Test
 	public void testVersionCheck() {
@@ -37,5 +41,41 @@ public class ValidationSupportChainTest {
 		}
 	}
 
+	@Test
+	public void fetchBinary_normally_returnsExpectedBinaries() {
 
+		final byte[] EXPECTED_BINARY_CONTENT_1 = "dummyBinaryContent1".getBytes();
+		final byte[] EXPECTED_BINARY_CONTENT_2 = "dummyBinaryContent2".getBytes();
+		final String EXPECTED_BINARY_KEY_1 = "dummyBinaryKey1";
+		final String EXPECTED_BINARY_KEY_2 = "dummyBinaryKey2";
+
+		IValidationSupport validationSupport1 = createMockValidationSupportWithSingleBinary(EXPECTED_BINARY_KEY_1, EXPECTED_BINARY_CONTENT_1);
+		IValidationSupport validationSupport2 = createMockValidationSupportWithSingleBinary(EXPECTED_BINARY_KEY_2, EXPECTED_BINARY_CONTENT_2);
+
+		ValidationSupportChain validationSupportChain = new ValidationSupportChain(validationSupport1, validationSupport2);
+
+		final byte[] actualBinaryContent1 = validationSupportChain.fetchBinary(EXPECTED_BINARY_KEY_1 );
+		final byte[] actualBinaryContent2 = validationSupportChain.fetchBinary(EXPECTED_BINARY_KEY_2 );
+
+		assertArrayEquals(EXPECTED_BINARY_CONTENT_1, actualBinaryContent1);
+		assertArrayEquals(EXPECTED_BINARY_CONTENT_2, actualBinaryContent2);
+		assertNull(validationSupportChain.fetchBinary("nonExistentKey"));
+	}
+
+
+	private static IValidationSupport createMockValidationSupport() {
+		IValidationSupport validationSupport;
+		validationSupport = mock(IValidationSupport.class);
+		FhirContext mockContext = mock(FhirContext.class);
+		when(mockContext.getVersion()).thenReturn(FhirVersionEnum.R4.getVersionImplementation());
+		when(validationSupport.getFhirContext()).thenReturn(mockContext);
+		return validationSupport;
+	}
+
+
+	private static IValidationSupport createMockValidationSupportWithSingleBinary(String expected_binary_key, byte[] expected_binary_content) {
+		IValidationSupport validationSupport1  = createMockValidationSupport();
+		when(validationSupport1.fetchBinary(expected_binary_key)).thenReturn(expected_binary_content);
+		return validationSupport1;
+	}
 }
