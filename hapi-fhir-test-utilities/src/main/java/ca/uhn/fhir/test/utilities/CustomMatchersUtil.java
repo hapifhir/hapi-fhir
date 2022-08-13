7b84400cb1ad1962282d90ca6c618f09088ba445
Justin_Dar <justin.dar@smilecdr.com>
2022-08-12 18:04:45 -0700

    add javadocs to new util class

--- hapi-fhir-test-utilities/src/main/java/ca/uhn/fhir/test/utilities/CustomMatchersUtil.java
+++ hapi-fhir-test-utilities/src/main/java/ca/uhn/fhir/test/utilities/CustomMatchersUtil.java
@@ -7,6 +7,12 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.not;
 
 public class CustomMatchersUtil {
+
+	/**
+	 * Asserts that none of the items in theShouldNotContain are in theActual
+	 * @param theActual the actual results
+	 * @param theShouldNotContain the items that should not be in theActual
+	 */
 	public static <T> void assertDoesNotContainAllOf(List<T> theActual, List<T> theShouldNotContain) {
 		for (T item : theShouldNotContain) {
 			assertThat(theActual, not(hasItem(item)));
