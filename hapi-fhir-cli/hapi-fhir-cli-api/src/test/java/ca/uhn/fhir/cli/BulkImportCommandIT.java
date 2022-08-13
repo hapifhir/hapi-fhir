526718329d6941dee72e9ab454c9b831e62db301
JasonRoberts-smile <85363818+JasonRoberts-smile@users.noreply.github.com>
2022-08-13 07:35:38 -0400

    3915 do not collapse rules with filters (#3916)
    * do not attempt to merge compartment permissions with filters
    
    * changelog
    
    * Rename to IT for concurrency problems
    
    Co-authored-by: Tadgh <garygrantgraham@gmail.com>

--- hapi-fhir-cli/hapi-fhir-cli-api/src/test/java/ca/uhn/fhir/cli/BulkImportCommandIT.java
+++ hapi-fhir-cli/hapi-fhir-cli-api/src/test/java/ca/uhn/fhir/cli/BulkImportCommandIT.java
@@ -50,9 +50,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class BulkImportCommandTest {
+public class BulkImportCommandIT {
 
-	private static final Logger ourLog = LoggerFactory.getLogger(BulkImportCommandTest.class);
+	private static final Logger ourLog = LoggerFactory.getLogger(BulkImportCommandIT.class);
 
 	static {
 		System.setProperty("test", "true");
