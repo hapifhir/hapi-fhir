6935484e6ef6c66e1e9587fcdfa343905c81892d
Justin_Dar <justin.dar@smilecdr.com>
2022-08-22 23:19:39 -0700

    typo

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/api/dao/PatientEverythingParameters.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/api/dao/PatientEverythingParameters.java
@@ -16,7 +16,7 @@ public final class PatientEverythingParameters {
 	@Description(shortDefinition="Only return resources which were last updated as specified by the given range")
 	private DateRangeParam myLastUpdated;
 
-	@Description(shortDefinition="The order in which to sort the rsults by")
+	@Description(shortDefinition="The order in which to sort the results by")
 	private SortSpec mySort;
 
 	@Description(shortDefinition="Filter the resources to return only resources matching the given _content filter (note that this filter is applied only to results which link to the given patient, not to the patient itself or to supporting resources linked to by the matched resources)")
