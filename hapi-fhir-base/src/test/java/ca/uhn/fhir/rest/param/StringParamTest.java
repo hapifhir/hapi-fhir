2da0610234fdb7fddaff101de151adf6b535527e
nathaniel.doef <nathaniel.doef@smilecdr.com>
2022-08-29 17:39:11 -0400

    - add failing test

--- hapi-fhir-base/src/test/java/ca/uhn/fhir/rest/param/StringParamTest.java
+++ hapi-fhir-base/src/test/java/ca/uhn/fhir/rest/param/StringParamTest.java
@@ -2,9 +2,19 @@ package ca.uhn.fhir.rest.param;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import ca.uhn.fhir.context.FhirContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class StringParamTest {
+
+	@Mock
+	private FhirContext myContext;
 
 	@Test
 	public void testEquals() {
@@ -14,6 +24,29 @@ public class StringParamTest {
 		assertFalse(input.equals(null));
 		assertFalse(input.equals(""));
 		assertFalse(input.equals(new StringParam("foo", false)));
+	}
+
+	@Test
+	public void doSetValueAsQueryToken_withCustomSearchParameterAndNicknameQualifier_enablesNicknameExpansion(){
+		String customSearchParamName = "firstName";
+		StringParam stringParam = new StringParam();
+		stringParam.doSetValueAsQueryToken(myContext, customSearchParamName, ":nickname", "John");
+		assertNicknameQualifierSearchParameterIsValid(stringParam, "John");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"name", "given"})
+	public void doSetValueAsQueryToken_withPredefinedSearchParametersAndNicknameQualifier_enablesNicknameExpansion(String theSearchParameterName){
+		StringParam stringParam = new StringParam();
+		stringParam.doSetValueAsQueryToken(myContext, theSearchParameterName, ":nickname", "John");
+		assertNicknameQualifierSearchParameterIsValid(stringParam, "John");
+	}
+
+	private void assertNicknameQualifierSearchParameterIsValid(StringParam theStringParam, String theExpectedValue){
+		assertTrue(theStringParam.isNicknameExpand());
+		assertFalse(theStringParam.isExact());
+		assertFalse(theStringParam.isContains());
+		assertEquals(theExpectedValue, theStringParam.getValue());
 	}
 	
 }
