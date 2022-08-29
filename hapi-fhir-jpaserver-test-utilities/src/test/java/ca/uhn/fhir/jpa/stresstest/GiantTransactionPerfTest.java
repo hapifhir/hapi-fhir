bfa4d39835f21d6cff410724f3ae9faf96c51c26
Tadgh <garygrantgraham@gmail.com>
2022-08-29 12:34:12 -0700

    Undo bad fix

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/stresstest/GiantTransactionPerfTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/stresstest/GiantTransactionPerfTest.java
@@ -501,11 +501,6 @@ public class GiantTransactionPerfTest {
 		}
 
 		@Override
-		public ResourceHistoryTable getReferenceById(Long theLong) {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
 		public <S extends ResourceHistoryTable> Optional<S> findOne(Example<S> example) {
 			return Optional.empty();
 		}
