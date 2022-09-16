69a2f8c215c41e2b271b7f9f9ce854112b85ccce
Steven Li <steven@smilecdr.com>
2022-09-16 10:59:07 -0400

    refactored methods, added new changes to changelog

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/export/svc/JpaBulkExportProcessor.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/bulk/export/svc/JpaBulkExportProcessor.java
@@ -59,6 +59,7 @@ import org.hl7.fhir.instance.model.api.IBaseExtension;
 import org.hl7.fhir.instance.model.api.IBaseReference;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -147,25 +148,7 @@ public class JpaBulkExportProcessor implements IBulkExportProcessor {
 
 			ISearchBuilder searchBuilder = getSearchBuilderForResourceType(theParams.getResourceType());
 
-			if (!resourceType.equalsIgnoreCase("Patient")) {
-				if (theParams.getPatientId() != null) {
-					ReferenceOrListParam referenceOrListParam = new ReferenceOrListParam();
-					for (String patientId : theParams.getPatientId()) {
-						referenceOrListParam.addOr(new ReferenceParam(patientId));
-					}
-					map.add(patientSearchParam, referenceOrListParam);
-				} else {
-					map.add(patientSearchParam, new ReferenceParam().setMissing(false));
-				}
-			} else {
-				if (theParams.getPatientId() != null) {
-					ReferenceOrListParam referenceOrListParam = new ReferenceOrListParam();
-					for (String patientId : theParams.getPatientId()) {
-						referenceOrListParam.addOr(new ReferenceParam(patientId));
-					}
-					map.add(PARAM_ID, referenceOrListParam);
-				}
-			}
+			additionSearchParameterForPatientExport(theParams, resourceType, patientSearchParam, map);
 
 			SearchRuntimeDetails searchRuntime = new SearchRuntimeDetails(null, jobId);
 			IResultIterator resultIterator = searchBuilder.createQuery(map, searchRuntime, null, RequestPartitionId.allPartitions());
@@ -174,6 +157,31 @@ public class JpaBulkExportProcessor implements IBulkExportProcessor {
 			}
 		}
 		return pids;
+	}
+
+	private static void additionSearchParameterForPatientExport(ExportPIDIteratorParameters theParams, String resourceType, String patientSearchParam, SearchParameterMap map) {
+		if (!resourceType.equalsIgnoreCase("Patient")) {
+			if (theParams.getPatientId() != null) {
+				ReferenceOrListParam referenceOrListParam = getReferenceOrListParam(theParams);
+				map.add(patientSearchParam, referenceOrListParam);
+			} else {
+				map.add(patientSearchParam, new ReferenceParam().setMissing(false));
+			}
+		} else {
+			if (theParams.getPatientId() != null) {
+				ReferenceOrListParam referenceOrListParam = getReferenceOrListParam(theParams);
+				map.add(PARAM_ID, referenceOrListParam);
+			}
+		}
+	}
+
+	@NotNull
+	private static ReferenceOrListParam getReferenceOrListParam(ExportPIDIteratorParameters theParams) {
+		ReferenceOrListParam referenceOrListParam = new ReferenceOrListParam();
+		for (String patientId : theParams.getPatientId()) {
+			referenceOrListParam.addOr(new ReferenceParam(patientId));
+		}
+		return referenceOrListParam;
 	}
 
 	private Set<ResourcePersistentId> getPidsForSystemStyleExport(ExportPIDIteratorParameters theParams, String theJobId, RuntimeResourceDefinition theDef) {
