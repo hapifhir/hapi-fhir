b77d8ca77ef67ea1b2e0d8aeaf806500141af6de
Ken Stevens <ken@smilecdr.com>
2022-09-18 23:05:26 -0400

    log

--- hapi-fhir-sql-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/MigrationResult.java
+++ hapi-fhir-sql-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/MigrationResult.java
@@ -12,10 +12,10 @@ public class MigrationResult {
 	public final List<BaseTask> failedTasks = new ArrayList<>();
 
 	public String summary() {
-		return String.format("Completed executing %s migration tasks: %s succeeded, %s failed.  %s changes were applied to the database.",
+		return String.format("Completed executing %s migration tasks: %s succeeded, %s failed.  %s SQL statements were executed.",
 			succeededTasks.size() + failedTasks.size(),
 			succeededTasks.size(),
 			failedTasks.size(),
-			changes);
+			executedStatements.size());
 	}
 }
