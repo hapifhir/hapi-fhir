61cee01c400b90d5e5a65e8f0bc33c26189961fd
Ken Stevens <ken@smilecdr.com>
2022-09-20 14:04:11 -0400

    javadoc

--- hapi-fhir-sql-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/HapiMigrationStorageSvc.java
+++ hapi-fhir-sql-migrate/src/main/java/ca/uhn/fhir/jpa/migrate/HapiMigrationStorageSvc.java
@@ -15,16 +15,30 @@ public class HapiMigrationStorageSvc {
 		myHapiMigrationDao = theHapiMigrationDao;
 	}
 
+	/**
+	 * Returns a list of migration tasks that have not yet been successfully run against the database
+	 * @param theTaskList the full list of tasks for this release
+	 * @return a list of tasks that have not yet been successfully run against the database
+	 */
+
 	public MigrationTaskList diff(MigrationTaskList theTaskList) {
 		Set<MigrationVersion> appliedMigrationVersions = fetchAppliedMigrationVersions();
 
 		return theTaskList.diff(appliedMigrationVersions);
 	}
 
+	/**
+	 *
+	 * @return a list of migration versions that have been successfully run against the database
+	 */
 	Set<MigrationVersion> fetchAppliedMigrationVersions() {
 		return myHapiMigrationDao.fetchSuccessfulMigrationVersions();
 	}
 
+	/**
+	 *
+	 * @return the most recent version that was run against the database (used for logging purposes)
+	 */
 	public String getLatestAppliedVersion() {
 		return fetchAppliedMigrationVersions().stream()
 			.sorted()
@@ -33,12 +47,19 @@ public class HapiMigrationStorageSvc {
 			.orElse(UNKNOWN_VERSION);
 	}
 
+	/**
+	 * Save a migration task to the database
+	 */
 	public void saveTask(BaseTask theBaseTask, Integer theMillis, boolean theSuccess) {
 		HapiMigrationEntity entity = HapiMigrationEntity.fromBaseTask(theBaseTask);
 		entity.setExecutionTime(theMillis);
 		entity.setSuccess(theSuccess);
 		myHapiMigrationDao.save(entity);
 	}
+
+	/**
+	 * Create the migration table if it does not already exist
+	 */
 
 	public void createMigrationTableIfRequired() {
 		myHapiMigrationDao.createMigrationTableIfRequired();
