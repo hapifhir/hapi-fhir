b8682e0a108eae3cc464a9aa8601520703cecb89
Tadgh <garygrantgraham@gmail.com>
2022-09-27 16:40:37 -0700

    change return type

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/dao/ThreadPoolFactory.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/dao/ThreadPoolFactory.java
@@ -1,15 +1,14 @@
 package ca.uhn.fhir.jpa.dao;
 
 import ca.uhn.fhir.util.ThreadPoolUtil;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.core.task.AsyncTaskExecutor;
 
 /**
  * Basic bean which will create a Task Executor
  */
 public class ThreadPoolFactory {
 
-
-	public ThreadPoolTaskExecutor newThreadPool(Integer theBundleBatchPoolSize, Integer theBundleBatchMaxPoolSize, String theThreadPrefix) {
+	public AsyncTaskExecutor newThreadPool(Integer theBundleBatchPoolSize, Integer theBundleBatchMaxPoolSize, String theThreadPrefix) {
 		return ThreadPoolUtil.newThreadPool(theBundleBatchPoolSize, theBundleBatchMaxPoolSize, theThreadPrefix);
 	}
 }
