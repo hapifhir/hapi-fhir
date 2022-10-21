b24c39dcc5e302c8f9b58831998846d9db7227b7
leif stawnyczy <leifstawnyczy@leifs-mbp.home>
2022-10-21 14:40:35 -0400

    blah

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/r4/IConsentExtensionProvider.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/r4/IConsentExtensionProvider.java
@@ -8,6 +8,13 @@ import java.util.Collections;
 
 public interface IConsentExtensionProvider {
 
+	/**
+	 * Takes a Consent resource and returns a collection of Extensions that will
+	 * be added to the base resource.
+	 *
+	 * @param theConsentResource - the consent resource
+	 * @return - a collection of resources (or an empty collection if none).
+	 */
 	default Collection<IBaseExtension> getConsentExtension(IBaseResource theConsentResource) {
 		return Collections.emptyList();
 	};
