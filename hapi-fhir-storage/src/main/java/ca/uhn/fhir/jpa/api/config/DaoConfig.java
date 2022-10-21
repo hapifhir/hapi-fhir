929767a09b5eee7db4dc8c6275d9818103e406db
michaelabuckley <michaelabuckley@gmail.com>
2022-10-21 16:45:30 -0400

    Change to a minimal first pre-fetch to avoid latency on the async path. (#4174)
    * Change to a minimal first pre-fetch to avoid latency on the async path.
    
    The "async" path is now synchronous, and was causing latency for first page by loading 500 results.
    We avoid pre-fetch on the first page, and only pre-fetch after users fetch the second page.
    
    * Fix a nasty bug continuing a pre-fetched search.
    
    * Update hapi-fhir-docs/src/main/resources/ca/uhn/hapi/fhir/changelog/6_2_0/3664-shorten-first-prefetch.yaml
    
    * Patch up tests that assumed 500 pre-fetch first step

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/api/config/DaoConfig.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/api/config/DaoConfig.java
@@ -183,7 +183,11 @@ public class DaoConfig {
 	private int myExpungeThreadCount;
 	private Set<String> myBundleTypesAllowedForStorage;
 	private boolean myValidateSearchParameterExpressionsOnSave = true;
-	private List<Integer> mySearchPreFetchThresholds = Arrays.asList(500, 2000, -1);
+
+	// start with a tiny number so our first page always loads quickly.
+	// If they fetch the second page, fetch more.
+	// Use prime sizes to avoid empty next links.
+	private List<Integer> mySearchPreFetchThresholds = Arrays.asList(13, 503, 2003, -1);
 	private List<WarmCacheEntry> myWarmCacheEntries = new ArrayList<>();
 	private boolean myDisableHashBasedSearches;
 	private boolean myEnableInMemorySubscriptionMatching = true;
