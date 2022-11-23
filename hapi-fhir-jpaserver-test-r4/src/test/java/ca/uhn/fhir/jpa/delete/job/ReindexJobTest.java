300db42b5265f20f455a9b5f3dc6211eba66982d
Nathan Doef <n.doef@protonmail.com>
2022-11-22 20:40:04 -0500

    Update ReindexJobTest.java
    adding comment to trigger ci

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/delete/job/ReindexJobTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/delete/job/ReindexJobTest.java
@@ -30,6 +30,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// TODO adding comment to trigger CI - 1
 public class ReindexJobTest extends BaseJpaR4Test {
 
 	@Autowired
