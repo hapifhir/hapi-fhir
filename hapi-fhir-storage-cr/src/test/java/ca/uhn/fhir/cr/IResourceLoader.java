b21a07875b7978e3b73292660b0de0165574b70c
Chalma Maadaadi <chalma@alphora.com>
2022-12-22 15:01:48 -0500

    addressing code review comments.

--- hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/IResourceLoader.java
+++ hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/IResourceLoader.java
@@ -9,9 +9,17 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 
 /**
  * This is a utility interface that allows a class that has a DaoRegistry to load Bundles and read Resources.
- * This is used primarily to set up integration tests for clinical reasoning operations since they often require big bundles of content, such as Libraries, ValueSets, Measures, and so on.
+ * This is used primarily to set up integration tests for clinical reasoning operations since they often
+ * require big bundles of content, such as Libraries, ValueSets, Measures, and so on.
  */
 public interface IResourceLoader extends IDaoRegistryUser {
+	/**
+	 * Method to load bundles
+	 * @param theType, resource type
+	 * @param theLocation, location of the resource
+	 * @return of type bundle
+	 * @param <T>
+	 */
 	default <T extends IBaseBundle> T loadBundle(Class<T> theType, String theLocation) {
 		var bundle = readResource(theType, theLocation);
 		getDaoRegistry().getSystemDao().transaction(new SystemRequestDetails(), bundle);
@@ -19,10 +27,24 @@ public interface IResourceLoader extends IDaoRegistryUser {
 		return bundle;
 	}
 
+	/**
+	 * Method to read resource
+	 * @param theType, resource type
+	 * @param theLocation, location of the resource
+	 * @return of type resource
+	 * @param <T>
+	 */
 	default <T extends IBaseResource> T readResource(Class<T> theType, String theLocation) {
 		return ClasspathUtil.loadResource(getFhirContext(), theType, theLocation);
 	}
 
+	/**
+	 * Method to load resource
+	 * @param theType, resource type
+	 * @param theLocation, location of the resource
+	 * @return of type resource
+	 * @param <T>
+	 */
 	default <T extends IBaseResource> T loadResource(Class<T> theType, String theLocation, RequestDetails theRequestDetails) {
 		var resource = readResource(theType, theLocation);
 		getDaoRegistry().getResourceDao(theType).update(resource, theRequestDetails);
