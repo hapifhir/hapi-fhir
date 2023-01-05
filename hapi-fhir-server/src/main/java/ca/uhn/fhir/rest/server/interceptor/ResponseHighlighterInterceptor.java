d29ae9c83917836bdf86a475d8f80d739047752e
Luke deGruchy <luke.degruchy@smilecdr.com>
2023-01-05 17:01:34 -0500

    Check for a null request resource in the ResponseHighlighterInterceptor when handling the response.

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/ResponseHighlighterInterceptor.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/ResponseHighlighterInterceptor.java
@@ -430,6 +430,11 @@ public class ResponseHighlighterInterceptor {
 
 
 	private boolean handleOutgoingResponse(RequestDetails theRequestDetails, ResponseDetails theResponseObject, HttpServletRequest theServletRequest, HttpServletResponse theServletResponse, String theGraphqlResponse, IBaseResource theResourceResponse) {
+    if (theRequestDetails.getResource() == null) {
+      // TODO:  unit test this
+      // this will happen during, for example, a bulk export polling request
+      return true;
+    }
 		/*
 		 * Request for _raw
 		 */
