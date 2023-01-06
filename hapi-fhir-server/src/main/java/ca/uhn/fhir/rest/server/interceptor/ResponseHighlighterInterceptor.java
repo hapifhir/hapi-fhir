a43cee0ef8226c84151f84feb5180b8253e79a47
Luke deGruchy <luke.degruchy@smilecdr.com>
2023-01-06 10:40:48 -0500

    Check for theGraphqlResponse being null also, as otherwise this breaks a unit test.

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/ResponseHighlighterInterceptor.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/ResponseHighlighterInterceptor.java
@@ -430,11 +430,11 @@ public class ResponseHighlighterInterceptor {
 
 
 	private boolean handleOutgoingResponse(RequestDetails theRequestDetails, ResponseDetails theResponseObject, HttpServletRequest theServletRequest, HttpServletResponse theServletResponse, String theGraphqlResponse, IBaseResource theResourceResponse) {
-	if (theResourceResponse == null) {
-      // TODO:  unit test this
-      // this will happen during, for example, a bulk export polling request
-      return true;
-    }
+		if (theResourceResponse == null && theGraphqlResponse == null) {
+			// TODO:  unit test this
+			// this will happen during, for example, a bulk export polling request
+			return true;
+		}
 		/*
 		 * Request for _raw
 		 */
