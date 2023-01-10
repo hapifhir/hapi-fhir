8c48a6fb91729dbb4ff9c243167bebdc6366f92a
Justin McKelvy <60718638+Capt-Mac@users.noreply.github.com>
2023-01-10 19:49:57 +0000

    Clinical reasoning module (#4347)
    * Add new Clinical Reasoning module
    
    * Add config to get test up and running
    
    * Removed FhirClient from tests
    
    * First passing test
    
    * Remove unused utilities
    
    * Build updates
    
    * exclude xxp3 and substitute with unbundled version
    
    * Refactor to use provider factory and loader pattern
    
    * First passing dstu3 test
    
    * CR r4 test config and immunization tests
    
    * Add base test classes
    
    * Exclude transitive dependency xmlpull
    
    * More dependency cleanup
    
    * Refactor to remove JPA dependencies, rename module to hapi-fhir-storage-cr
    
    * Fixes for duplicate xpp3 classes
    
    * Remove more references to jpa
    
    * Add mock FHIR server for terminology endpoint tests
    
    * fix cql file for making providerr4 tests pass
    
    * fix pom
    
    * fix generic
    
    * Adding r4 test cases for measure operations.
    
    * Fix endpoint tests
    
    * javadoc comment, temporarily comment out test results for practitioner sync
    
    * javadoc typo fix
    
    * adding java docs for cr module WIP
    
    * Updates to clinical reasoning documentation
    
    * More documentation updates
    
    * docs now compile
    
    * fix test
    
    * DqmR4IT now successfully starts up, but the test does not pass
    
    * Fix additionalData test
    
    * Make measure service bean overridable
    
    * adding clinical reasoning javadoc information
    
    * Rename interfaces to start with I
    
    * More styleguide updates
    
    * Reformat all code in the storage-cr module
    
    * More coding convention updates
    
    * Remove bunches of unused code, reogranize so that fhir version in package name is consistent
    
    * Clean up some static array initializations
    
    * add practitioner patient list
    
    * refactor product-line constant
    
    * test dependency update
    
    * add change log for cr module
    
    * Add hapi-validation to the pom
    
    * Exclude dependencies that are causing duplicates
    
    * More dependencies and exclusions
    
    * Make some base MeasureService components protected
    
    * Update to latest cql compiler release
    
    * fix measure service for null subject & practitioner and open up practitioner test
    
    * Rev to release versions
    
    * Add remaining Dstu3 tests
    
    * update hedis bundle from policyHolder to beneficiary
    
    * update snapshot
    
    * Delete old CQL module
    
    * Post-master-merge recovery
    
    * Fix Java 17 feature usage issue
    
    * update changelog with more information about new functionality
    
    * Remove support chain wrapper usage
    
    * Add javadocs to IResourceLoader
    
    * added the property versioning for antlr jar.
    
    * addressing code review comments.
    
    * addressing code review comments.
    
    * Fix typos in changelog
    
    * Remove azure pipeline for cql module, add it for cr module
    
    * Address exception comments, add Msg.code calls to exceptions
    
    * Fix self-assignment bug
    
    * Add storage-cr module to bom
    
    * addressing coding comments.
    
    * Addressed the variable naming convention to camelCase.
    
    * Snapshot version bump
    
    Co-authored-by: Brenin Rhodes <brenin@alphora.com>
    Co-authored-by: Jonathan Percival <jonathan.i.percival@gmail.com>
    Co-authored-by: justin.mckelvy <justin.mckelvy@smilecdr.com>
    Co-authored-by: Ken Stevens <ken@smilecdr.com>
    Co-authored-by: Chalma Maadaadi <chalma@alphora.com>
    Co-authored-by: mdnazmulkarim <nazmul.karim@smilecdr.com>
    Co-authored-by: Tadgh <garygrantgraham@gmail.com>

---
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/common/HapiLibrarySourceProvider.java
@@ -0,0 +1,85 @@
+package ca.uhn.fhir.cr.common;
+
+/*-
+ * #%L
+ * HAPI FHIR - Clinical Reasoning
+ * %%
+ * Copyright (C) 2014 - 2022 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import org.cqframework.cql.cql2elm.LibraryContentType;
+import org.cqframework.cql.cql2elm.LibrarySourceProvider;
+import org.hl7.elm.r1.VersionedIdentifier;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.opencds.cqf.cql.evaluator.fhir.util.Libraries;
+import org.opencds.cqf.cql.evaluator.fhir.util.Versions;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * This class provides an implementation of the cql-translator's LibrarySourceProvider
+ * interface which is used for loading
+ * library resources during CQL evaluation.
+ */
+public class HapiLibrarySourceProvider
+	implements LibrarySourceProvider, IDaoRegistryUser {
+	protected final DaoRegistry myDaoRegistry;
+	protected final RequestDetails myRequestDetails;
+
+	public HapiLibrarySourceProvider(DaoRegistry theDaoRegistry) {
+		this(theDaoRegistry, null);
+	}
+
+	public HapiLibrarySourceProvider(DaoRegistry theDaoRegistry, RequestDetails theRequestDetails) {
+		this.myDaoRegistry = theDaoRegistry;
+		this.myRequestDetails = theRequestDetails;
+	}
+
+	@Override
+	public DaoRegistry getDaoRegistry() {
+		return this.myDaoRegistry;
+	}
+
+	@Override
+	public InputStream getLibraryContent(VersionedIdentifier theLibraryIdentifier,
+													 LibraryContentType theLibraryContentType) {
+		String name = theLibraryIdentifier.getId();
+		String version = theLibraryIdentifier.getVersion();
+		List<IBaseResource> libraries = search(getClass("Library"), Searches.byName(name), myRequestDetails)
+			.getAllResources();
+		IBaseResource library = Versions.selectByVersion(libraries, version,
+			Libraries::getVersion);
+
+		if (library == null) {
+			return null;
+		}
+		byte[] content = Libraries.getContent(library, theLibraryContentType.mimeType());
+		if (content == null) {
+			return null;
+		}
+
+		return new ByteArrayInputStream(content);
+	}
+
+	@Override
+	public InputStream getLibrarySource(VersionedIdentifier libraryIdentifier) {
+		return this.getLibraryContent(libraryIdentifier, LibraryContentType.CQL);
+	}
+}
