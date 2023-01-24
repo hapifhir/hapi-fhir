f57c7f78eda9f89002a9921e8a9b74dbc1524086
Luke deGruchy <luke.degruchy@smilecdr.com>
2023-01-24 09:24:38 -0500

    Add code reuse to ValidationSupportConfigUtil and increase test coverage for CachingValidationSupportTest.

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/util/ValidationSupportConfigUtil.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/util/ValidationSupportConfigUtil.java
@@ -27,11 +27,7 @@ public final class ValidationSupportConfigUtil {
 	private ValidationSupportConfigUtil() {}
 
 	public static CachingValidationSupport newCachingValidationSupport(JpaValidationSupportChain theJpaValidationSupportChain) {
-		// Short timeout for code translation because TermConceptMappingSvcImpl has its own caching
-		CachingValidationSupport.CacheTimeouts cacheTimeouts = CachingValidationSupport.CacheTimeouts.defaultValues()
-			.setTranslateCodeMillis(1000);
-
-		return new CachingValidationSupport(theJpaValidationSupportChain, cacheTimeouts);
+		return newCachingValidationSupport(theJpaValidationSupportChain, false);
 	}
 
 	public static CachingValidationSupport newCachingValidationSupport(JpaValidationSupportChain theJpaValidationSupportChain, boolean theIsEnabledValidationForCodingsLogicalAnd) {
