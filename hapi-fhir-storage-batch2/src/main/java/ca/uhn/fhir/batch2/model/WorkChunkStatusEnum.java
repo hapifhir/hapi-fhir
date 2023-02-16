7694bd5affc45cc240fa5a8941868fd5b280eb8f
Michael Buckley <michaelabuckley@gmail.com>
2023-02-15 22:41:56 -0500

    Simpler

--- hapi-fhir-storage-batch2/src/main/java/ca/uhn/fhir/batch2/model/WorkChunkStatusEnum.java
+++ hapi-fhir-storage-batch2/src/main/java/ca/uhn/fhir/batch2/model/WorkChunkStatusEnum.java
@@ -4,11 +4,6 @@ public enum WorkChunkStatusEnum {
 	QUEUED, IN_PROGRESS, ERRORED, FAILED, COMPLETED;
 
 	public boolean isIncomplete() {
-		switch (this) {
-			case COMPLETED:
-				return false;
-			default:
-				return true;
-		}
+		return (this != WorkChunkStatusEnum.COMPLETED);
 	}
 }
