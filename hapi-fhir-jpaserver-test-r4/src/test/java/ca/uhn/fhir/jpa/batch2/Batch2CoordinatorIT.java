19c0065b52217e8eef5d3000073bdb01fbb861a2
Luke deGruchy <luke.degruchy@smilecdr.com>
2023-02-24 19:37:32 -0500

    Disable intermittently failing tests.

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/batch2/Batch2CoordinatorIT.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/batch2/Batch2CoordinatorIT.java
@@ -33,6 +33,7 @@ import ca.uhn.test.concurrency.PointcutLatch;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -255,6 +256,7 @@ public class Batch2CoordinatorIT extends BaseJpaR4Test {
 
 	@ParameterizedTest
 	@ValueSource(booleans = { true, false })
+	@Disabled("failing intermittently for latest rel_6_4 was not fulfilled within 2 minutes")
 	public void testJobDefinitionWithReductionStepIT(boolean theDelayReductionStepBool) throws InterruptedException {
 		// setup
 		String jobId = new Exception().getStackTrace()[0].getMethodName() + "_" + theDelayReductionStepBool;
