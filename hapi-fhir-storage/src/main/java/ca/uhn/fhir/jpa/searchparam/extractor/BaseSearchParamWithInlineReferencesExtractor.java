ada467f2215a5bdfd18e173bce1cade1ba5a1a83
Steven Li <steven@smilecdr.com>
2023-03-03 11:54:28 -0500

    moved error message to properties file, made base class abstract

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/searchparam/extractor/BaseSearchParamWithInlineReferencesExtractor.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/searchparam/extractor/BaseSearchParamWithInlineReferencesExtractor.java
@@ -1,5 +1,25 @@
 package ca.uhn.fhir.jpa.searchparam.extractor;
 
+/*-
+ * #%L
+ * HAPI FHIR Storage api
+ * %%
+ * Copyright (C) 2014 - 2023 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.RuntimeResourceDefinition;
 import ca.uhn.fhir.i18n.Msg;
@@ -29,7 +49,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-public class BaseSearchParamWithInlineReferencesExtractor<T extends IResourcePersistentId> implements ISearchParamWithInlineReferencesExtractor {
+public abstract class BaseSearchParamWithInlineReferencesExtractor<T extends IResourcePersistentId> implements ISearchParamWithInlineReferencesExtractor {
 	private static final Logger ourLog = LoggerFactory.getLogger(BaseSearchParamWithInlineReferencesExtractor.class);
 
 	@Autowired
@@ -63,7 +83,8 @@ public class BaseSearchParamWithInlineReferencesExtractor<T extends IResourcePer
 			int qmIndex = nextIdText.indexOf('?');
 			if (qmIndex != -1) {
 				if (!myStorageSettings.isAllowInlineMatchUrlReferences()) {
-					throw new InvalidRequestException(Msg.code(2282) + "Inline match URLs are not supported on this server. Can not process reference: " + UrlUtil.sanitizeUrlPart(nextRef.getReferenceElement().getValueAsString()));
+					String msg = myFhirContext.getLocalizer().getMessage(BaseStorageDao.class, "inlineMatchNotSupported", UrlUtil.sanitizeUrlPart(nextRef.getReferenceElement().getValueAsString()));
+					throw new InvalidRequestException(Msg.code(2282) + msg);
 				}
 				for (int i = qmIndex - 1; i >= 0; i--) {
 					if (nextIdText.charAt(i) == '/') {
