0d99a9bcd4c1a3c4613b9639fe6dbccec867a5f8
Rosie Elphick <rosalie.elphick@smilecdr.com>
2023-03-22 09:16:37 -0400

    add test for keymap

--- hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/r4/SearchConverterTest.java
+++ hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/r4/SearchConverterTest.java
@@ -1,11 +1,8 @@
 package ca.uhn.fhir.cr.r4;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.cr.repo.SearchConverter;
-import ca.uhn.fhir.model.api.IQueryParameterType;
-import ca.uhn.fhir.rest.param.UriParam;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,16 +10,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.cr.repo.SearchConverter;
+import ca.uhn.fhir.model.api.IQueryParameterType;
+import ca.uhn.fhir.rest.param.UriParam;
 
 class SearchConverterTest {
 	private SearchConverter myFixture;
+
 	@BeforeEach
 	public void setupFixture() {
 		myFixture = new SearchConverter();
 	}
+
 	@Test
 	void isSearchParameterShouldReturnTrue() {
 		boolean result = myFixture.isSearchParameter("_elements");
@@ -36,30 +39,6 @@ class SearchConverterTest {
 	}
 
 	@Test
-	void convertToSearchParameterMapShouldConvertWithOneEntry() {
-		Map<String, List<IQueryParameterType>> paramMap = new HashMap<>();
-		List<IQueryParameterType> paramList = withUriParam(1);
-		paramMap.put("theKey", paramList);
-		myFixture.convertToSearchParameterMap(paramMap);
-		final String expected = "?theKey=0";
-		final String result = myFixture.searchParameterMap.toNormalizedQueryString(withFhirContext());
-		assertEquals(expected, result);
-	}
-
-	@Test
-	void convertToSearchParameterMapShouldConvertWithMultipleEntries() {
-		Map<String, List<IQueryParameterType>> paramMap = new HashMap<>();
-		List<IQueryParameterType> firstParamList = withUriParam(1);
-		List<IQueryParameterType> secondParamList = withUriParam(2);
-		paramMap.put("theFirstKey", firstParamList);
-		paramMap.put("theSecondKey", secondParamList);
-		myFixture.convertToSearchParameterMap(paramMap);
-		final String expected = "?theFirstKey=0&theSecondKey=0&theSecondKey=1";
-		final String result = myFixture.searchParameterMap.toNormalizedQueryString(withFhirContext());
-		assertEquals(expected, result);
-	}
-
-	@Test
 	void separateParameterTypesShouldSeparateSearchAndResultParams() {
 		myFixture.separateParameterTypes(withParamList());
 		assertEquals(3, myFixture.separatedSearchParameters.size());
@@ -69,19 +48,20 @@ class SearchConverterTest {
 	@Test
 	void convertToStringMapShouldConvert() {
 		Map<String, String[]> expected = withParamListAsStrings();
-		myFixture.convertToStringMap(withParamList(), withFhirContext());
-		Map<String, String[]> result = myFixture.resultParameters;
+		Map<String, String[]> result =
+				myFixture.convertToStringMap(withParamList(), withFhirContext());
 		assertEquals(result.keySet(), expected.keySet());
-		assertTrue(result.entrySet().stream().allMatch(e -> Arrays.equals(e.getValue(), expected.get(e.getKey()))));
+		assertTrue(result.entrySet().stream()
+				.allMatch(e -> Arrays.equals(e.getValue(), expected.get(e.getKey()))));
 	}
 
 	Map<String, List<IQueryParameterType>> withParamList() {
 		Map<String, List<IQueryParameterType>> paramList = new HashMap<>();
-		paramList.put("_id", withUriParam(1));
-		paramList.put("_elements", withUriParam(2));
-		paramList.put("_lastUpdated", withUriParam(1));
-		paramList.put("_total", withUriParam(1));
-		paramList.put("_count", withUriParam(3));
+		paramList.put("_id", withParam(1));
+		paramList.put("_elements", withParam(2));
+		paramList.put("_lastUpdated", withParam(1));
+		paramList.put("_total", withParam(1));
+		paramList.put("_count", withParam(3));
 		return paramList;
 	}
 
@@ -95,7 +75,7 @@ class SearchConverterTest {
 		return paramList;
 	}
 
-	List<IQueryParameterType> withUriParam(int theNumberOfParams) {
+	List<IQueryParameterType> withParam(int theNumberOfParams) {
 		List<IQueryParameterType> paramList = new ArrayList<>();
 		for (int i = 0; i < theNumberOfParams; i++) {
 			paramList.add(new UriParam(Integer.toString(i)));
@@ -111,5 +91,7 @@ class SearchConverterTest {
 		return paramList;
 	}
 
-	FhirContext withFhirContext() {return new FhirContext();}
+	FhirContext withFhirContext() {
+		return new FhirContext();
+	}
 }
