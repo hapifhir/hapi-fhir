670dd74f02033cd56fb8bd80d94451f30a481a53
Tadgh <garygrantgraham@gmail.com>
2023-04-25 17:17:17 -0700

    Cherry pick some pom fixes

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/batch2/JpaJobPersistenceImplTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/batch2/JpaJobPersistenceImplTest.java
@@ -1,5 +1,4 @@
 package ca.uhn.fhir.jpa.batch2;
-
 import ca.uhn.fhir.batch2.api.IJobPersistence;
 import ca.uhn.fhir.batch2.api.JobOperationResultJson;
 import ca.uhn.fhir.batch2.coordinator.BatchWorkChunk;
