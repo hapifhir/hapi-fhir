f4cb8b87ca93d61880455979e28e8ba5b822ac26
Tadgh <garygrantgraham@gmail.com>
2023-05-07 10:17:47 -0700

    Remove superfluous migration

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
@@ -100,15 +100,7 @@ public class HapiFhirJpaMigrationTasks extends BaseMigrationTasks<VersionEnum> {
 
 	protected void init680() {
 		Builder version = forVersion(VersionEnum.V6_8_0);
-
-      // HAPI-FHIR #4801 - Add New Index On HFJ_RESOURCE
-    Builder.BuilderWithTableName resourceTable = version.onTable("HFJ_RESOURCE");
-    resourceTable
-      .addIndex("20230502.1", "IDX_RES_RESID_UPDATED")
-      .unique(false)
-      .online(true)
-      .withColumns("RES_ID", "RES_UPDATED", "PARTITION_ID");
-
+		
 		Builder.BuilderWithTableName tagDefTable = version.onTable("HFJ_TAG_DEF");
 		tagDefTable.dropIndex("20230505.1", "IDX_TAGDEF_TYPESYSCODEVERUS");
 
