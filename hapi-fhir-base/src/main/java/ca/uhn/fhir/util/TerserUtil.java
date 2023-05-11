679b809bf331e378e00edbe09ac7619b28e18060
Nicolai Gjøderum <43570675+nigtrifork@users.noreply.github.com>
2023-05-11 05:09:43 +0200

    Fix: Replace basic field with empty value in TerserUtil (#4842)
    * add test proving bug
    
    * fix: handle empty fromValue in TerserUtil.replaceField
    
    * add comment to TerserUtilTest.testReplaceFieldByEmptyValue

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/TerserUtil.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/TerserUtil.java
@@ -441,7 +441,10 @@ public final class TerserUtil {
 	private static void replaceField(FhirTerser theTerser, IBaseResource theFrom, IBaseResource theTo, BaseRuntimeChildDefinition childDefinition) {
 		List<IBase> fromValues = childDefinition.getAccessor().getValues(theFrom);
 		List<IBase> toValues = childDefinition.getAccessor().getValues(theTo);
-		if (fromValues != toValues) {
+
+		if (fromValues.isEmpty() && !toValues.isEmpty()) {
+			childDefinition.getMutator().setValue(theTo, null);
+		} else if (fromValues != toValues) {
 			clear(toValues);
 
 			mergeFields(theTerser, theTo, childDefinition, fromValues, toValues);
@@ -528,7 +531,6 @@ public final class TerserUtil {
 	/**
 	 * Creates a new element taking into consideration elements with choice that are not directly retrievable by element
 	 * name
-	 *
 	 *
 	 * @param theFhirTerser
 	 * @param theChildDefinition  Child to create a new instance for
