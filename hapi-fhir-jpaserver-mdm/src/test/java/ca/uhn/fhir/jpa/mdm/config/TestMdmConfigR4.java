bf808cc471b2b0ad6a366e628ef700b8394d93a1
peartree <etienne.poirier@smilecdr.com>
2023-06-12 10:36:32 -0400

    Adding import dependency to class TestSubscriptionSubmitterConfig to allow submission of messages with the SynchronousSubscriptionMatcherInterceptor.

--- hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/config/TestMdmConfigR4.java
+++ hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/config/TestMdmConfigR4.java
@@ -2,11 +2,11 @@ package ca.uhn.fhir.jpa.mdm.config;
 
 import ca.uhn.fhir.jpa.mdm.helper.MdmHelperR4;
 import ca.uhn.fhir.jpa.subscription.channel.config.SubscriptionChannelConfig;
-import ca.uhn.fhir.jpa.subscription.submit.config.SubscriptionSubmitterConfig;
+import ca.uhn.fhir.jpa.test.config.TestSubscriptionSubmitterConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({SubscriptionSubmitterConfig.class, SubscriptionChannelConfig.class})
+@Import({TestSubscriptionSubmitterConfig.class, SubscriptionChannelConfig.class})
 public class TestMdmConfigR4 extends BaseTestMdmConfig {
 	@Bean
 	MdmHelperR4 mdmHelperR4() {
