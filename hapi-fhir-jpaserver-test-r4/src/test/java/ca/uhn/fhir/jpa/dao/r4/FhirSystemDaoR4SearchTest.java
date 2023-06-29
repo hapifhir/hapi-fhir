97f9cbb019d7583252b529fa4e656587fd359bb4
tyner <tyner.guo@smilecdr.com>
2023-06-29 15:24:53 -0400

    combined redundant tests

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirSystemDaoR4SearchTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirSystemDaoR4SearchTest.java
@@ -3,59 +3,22 @@ package ca.uhn.fhir.jpa.dao.r4;
 import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.jpa.model.config.PartitionSettings;
 import ca.uhn.fhir.jpa.model.entity.ResourceIndexedSearchParamToken;
-import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
-import ca.uhn.fhir.model.dstu2.resource.Observation;
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
-import ca.uhn.fhir.rest.param.TokenParam;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.eclipse.jetty.util.StringUtil;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Organization;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FhirSystemDaoR4SearchTest extends BaseJpaR4SystemTest {
 
 	@Test
-	public void testSearchParameter_WithIdentifierValueUnder200Chars_ShouldSuccess() {
+	public void testSearchParameter_WithIdentifierValueUnderMaxLength_ShouldSuccess() {
 		// setup
+		final String identifierSystem = "http://www.acme.org.au/units";
 		final String identifierValue = "identifierValue";
-		final Organization organization = new Organization();
-		final Identifier identifier = new Identifier();
-		identifier.setSystem("http://www.acme.org.au/units");
-		identifier.setValue(identifierValue);
-		organization.addIdentifier(identifier);
-
-		myOrganizationDao.create(organization, mySrd);
-
-		final List<ResourceIndexedSearchParamToken> allSearchTokens = myResourceIndexedSearchParamTokenDao.findAll();
-		assertTrue(allSearchTokens.stream().anyMatch(token -> identifierValue.equals(token.getValue())));
-
-		final SearchParameterMap searchParameterMap = SearchParameterMap.newSynchronous(Observation.SP_IDENTIFIER, new TokenParam(identifierValue));
-
-		final IBundleProvider bundle = myOrganizationDao.search(searchParameterMap, mySrd);
-		final List<IBaseResource> allResources = bundle.getAllResources();
-
-		assertEquals(1, allResources.size());
-
-		final IBaseResource resource = allResources.get(0);
-		assertTrue(resource instanceof Organization);
-
-		final List<Identifier> identifiersFromResults = ((Organization) resource).getIdentifier();
-		assertEquals(1, identifiersFromResults.size());
-		assertEquals(identifierValue, identifiersFromResults.get(0).getValue());
-	}
-
-	@Test
-	public void testSearchParameter_WithIdentifierValueUnder200Chars_ShouldHashBeforeTruncate() {
-		// setup
-		final String identifierValue = RandomStringUtils.randomAlphanumeric(ResourceIndexedSearchParamToken.MAX_LENGTH + 100);
-		final Identifier identifier = new Identifier().setSystem("http://www.acme.org.au/units").setValue(identifierValue);
+		final Identifier identifier = new Identifier().setSystem(identifierSystem).setValue(identifierValue);
 		final Organization organization = new Organization().addIdentifier(identifier);
 
 		// execute
@@ -63,58 +26,51 @@ public class FhirSystemDaoR4SearchTest extends BaseJpaR4SystemTest {
 
 		// verify token value
 		final ResourceIndexedSearchParamToken resultSearchTokens = myResourceIndexedSearchParamTokenDao.findAll().get(0);
+		final String tokenSystem = resultSearchTokens.getSystem();
 		final String tokenValue = resultSearchTokens.getValue();
-		final String expectedValue = StringUtil.truncate(identifierValue, ResourceIndexedSearchParamToken.MAX_LENGTH);
-		assertEquals(expectedValue, tokenValue);
+
+		assertEquals(identifierSystem, tokenSystem);
+		assertEquals(identifierValue, tokenValue);
 
 		// verify hash
-		final Long tokensHashValue = resultSearchTokens.getHashValue();
+		final long tokensHashSystem = resultSearchTokens.getHashSystem();
+		final long expectedHashSystem = ResourceIndexedSearchParamToken.calculateHashValue(new PartitionSettings(), RequestPartitionId.defaultPartition(), "Organization", "identifier", identifierSystem);
+		final long tokensHashValue = resultSearchTokens.getHashValue();
 		final long expectedHashValue = ResourceIndexedSearchParamToken.calculateHashValue(new PartitionSettings(), RequestPartitionId.defaultPartition(), "Organization", "identifier", identifierValue);
+
+		assertEquals(expectedHashSystem, tokensHashSystem);
 		assertEquals(expectedHashValue, tokensHashValue);
-
-		// verify resource
-		final SearchParameterMap searchParameterMap = SearchParameterMap.newSynchronous(Observation.SP_IDENTIFIER, new TokenParam(identifierValue));
-		final List<IBaseResource> allResources = myOrganizationDao.search(searchParameterMap, mySrd).getAllResources();
-		final IBaseResource resource = allResources.get(0);
-		assertTrue(resource instanceof Organization);
-
-		final List<Identifier> identifiersFromResults = ((Organization) resource).getIdentifier();
-		assertEquals(1, allResources.size());
-		assertEquals(1, identifiersFromResults.size());
-		assertEquals(identifierValue, identifiersFromResults.get(0).getValue());
 	}
 
 	@Test
-	public void testSearchParameter_WithIdentifierSystemUnder200Chars_ShouldHashBeforeTruncate() {
+	public void testSearchParameter_WithIdentifierSystemAndValueOverMaxLength_ShouldHashBeforeTruncate() {
 		// setup
-		final String identifierSystem = RandomStringUtils.randomAlphanumeric(ResourceIndexedSearchParamToken.MAX_LENGTH + 100);;
-		final Identifier identifier = new Identifier().setSystem(identifierSystem).setValue("1234");
+		final String identifierSystem = RandomStringUtils.randomAlphanumeric(ResourceIndexedSearchParamToken.MAX_LENGTH + 100);
+		final String identifierValue = RandomStringUtils.randomAlphanumeric(ResourceIndexedSearchParamToken.MAX_LENGTH + 100);
+		final Identifier identifier = new Identifier().setSystem(identifierSystem).setValue(identifierValue);
 		final Organization organization = new Organization().addIdentifier(identifier);
 
 		// execute
 		myOrganizationDao.create(organization, mySrd);
 
-		// verify token system
+		// verify token value
 		final ResourceIndexedSearchParamToken resultSearchTokens = myResourceIndexedSearchParamTokenDao.findAll().get(0);
 		final String tokenSystem = resultSearchTokens.getSystem();
+		final String tokenValue = resultSearchTokens.getValue();
 		final String expectedSystem = StringUtil.truncate(identifierSystem, ResourceIndexedSearchParamToken.MAX_LENGTH);
+		final String expectedValue = StringUtil.truncate(identifierValue, ResourceIndexedSearchParamToken.MAX_LENGTH);
+
 		assertEquals(expectedSystem, tokenSystem);
+		assertEquals(expectedValue, tokenValue);
 
 		// verify hash
-		final Long tokensHashSystem = resultSearchTokens.getHashSystem();
+		final long tokensHashSystem = resultSearchTokens.getHashSystem();
 		final long expectedHashSystem = ResourceIndexedSearchParamToken.calculateHashValue(new PartitionSettings(), RequestPartitionId.defaultPartition(), "Organization", "identifier", identifierSystem);
+		final long tokensHashValue = resultSearchTokens.getHashValue();
+		final long expectedHashValue = ResourceIndexedSearchParamToken.calculateHashValue(new PartitionSettings(), RequestPartitionId.defaultPartition(), "Organization", "identifier", identifierValue);
+
 		assertEquals(expectedHashSystem, tokensHashSystem);
-
-		// verify resource
-		final SearchParameterMap searchParameterMap = SearchParameterMap.newSynchronous(Observation.SP_IDENTIFIER, new TokenParam("1234"));
-		final List<IBaseResource> allResources = myOrganizationDao.search(searchParameterMap, mySrd).getAllResources();
-		final IBaseResource resource = allResources.get(0);
-		assertTrue(resource instanceof Organization);
-
-		final List<Identifier> identifiersFromResults = ((Organization) resource).getIdentifier();
-		assertEquals(1, allResources.size());
-		assertEquals(1, identifiersFromResults.size());
-		assertEquals(identifierSystem, identifiersFromResults.get(0).getSystem());
+		assertEquals(expectedHashValue, tokensHashValue);
 	}
 	
 	/*//@formatter:off
