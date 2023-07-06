3b4a90f27434a262c20f20f969617c55da0d7379
Tadgh <garygrantgraham@gmail.com>
2023-07-05 19:50:28 -0700

    Autoformat

--- hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/repo/RequestDetailsCloner.java
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/repo/RequestDetailsCloner.java
@@ -19,16 +19,14 @@
  */
 package ca.uhn.fhir.cr.repo;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.hl7.fhir.instance.model.api.IBaseParameters;
-import org.hl7.fhir.instance.model.api.IIdType;
-
-
 import ca.uhn.fhir.rest.api.RequestTypeEnum;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
+import org.hl7.fhir.instance.model.api.IBaseParameters;
+import org.hl7.fhir.instance.model.api.IIdType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class produces partial clones of RequestDetails, the intent being to reuse the context of a
