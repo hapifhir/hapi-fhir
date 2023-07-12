5577a3559d9a9b88b8ecea6f14d55b842498a5e5
justindar <justin.dar@smilecdr.com>
2023-07-12 09:34:25 -0700

    remove unnecessary bean

--- hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/helper/MdmHelperConfig.java
+++ hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/helper/MdmHelperConfig.java
@@ -3,7 +3,6 @@ package ca.uhn.fhir.jpa.mdm.helper;
 import ca.uhn.fhir.mdm.api.IMdmSettings;
 import ca.uhn.fhir.mdm.rules.config.MdmRuleValidator;
 import ca.uhn.fhir.mdm.rules.config.MdmSettings;
-import ca.uhn.fhir.mdm.svc.MdmLinkExpandSvc;
 import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,10 +38,5 @@ public class MdmHelperConfig {
 			.setScriptText(json)
 			.setPreventEidUpdates(myPreventEidUpdates)
 			.setPreventMultipleEids(myPreventMultipleEids);
-	}
-
-	@Bean
-	MdmLinkExpandSvc mdmLinkExpandSvc() {
-		return new MdmLinkExpandSvc();
 	}
 }
