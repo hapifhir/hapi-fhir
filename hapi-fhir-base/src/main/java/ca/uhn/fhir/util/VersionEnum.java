1877a3d86abec64985fe768f691f09efdb91c218
James Agnew <jamesagnew@gmail.com>
2023-07-24 08:01:23 -0400

    Version bump HAPI

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
@@ -118,7 +118,8 @@ public enum VersionEnum {
 	V6_5_0,
 	V6_6_0,
 	V6_7_0,
-	V6_8_0;
+	V6_8_0,
+	V6_9_0;
 
 	public static VersionEnum latestVersion() {
 		VersionEnum[] values = VersionEnum.values();
