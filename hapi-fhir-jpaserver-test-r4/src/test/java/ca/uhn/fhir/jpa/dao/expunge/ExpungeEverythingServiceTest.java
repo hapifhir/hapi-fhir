55c3dbaa0b5af4aa8b08a0660dfddc2e7a1fc8c8
Ken Stevens <khstevens@gmail.com>
2023-08-17 17:11:41 -0400

    add test

---
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/expunge/ExpungeEverythingServiceTest.java
@@ -0,0 +1,104 @@
+package ca.uhn.fhir.jpa.dao.expunge;
+
+import ca.uhn.fhir.batch2.model.JobInstance;
+import ca.uhn.fhir.batch2.model.StatusEnum;
+import ca.uhn.fhir.jpa.api.config.JpaStorageSettings;
+import ca.uhn.fhir.jpa.api.model.DeleteMethodOutcome;
+import ca.uhn.fhir.jpa.entity.PartitionEntity;
+import ca.uhn.fhir.jpa.model.util.JpaConstants;
+import ca.uhn.fhir.jpa.partition.IPartitionLookupSvc;
+import ca.uhn.fhir.jpa.partition.PartitionLookupSvcImpl;
+import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
+import ca.uhn.fhir.jpa.test.BaseJpaR4Test;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import ca.uhn.fhir.util.BundleBuilder;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+class ExpungeEverythingServiceTest extends BaseJpaR4Test {
+	@Autowired
+	private ExpungeEverythingService myExpungeEverythingService;
+	@Autowired
+	private IPartitionLookupSvc myPartitionLookupSvc;
+
+	@Override
+	@BeforeEach
+	public void before() throws Exception {
+		super.before();
+		myStorageSettings.setAllowMultipleDelete(true);
+		myStorageSettings.setExpungeEnabled(true);
+		myStorageSettings.setDeleteExpungeEnabled(true);
+		myStorageSettings.setInternalSynchronousSearchSize(new JpaStorageSettings().getInternalSynchronousSearchSize());
+	}
+
+	@AfterEach
+	public void after() {
+		JpaStorageSettings defaultStorageSettings = new JpaStorageSettings();
+		myStorageSettings.setAllowMultipleDelete(defaultStorageSettings.isAllowMultipleDelete());
+		myStorageSettings.setExpungeEnabled(defaultStorageSettings.isExpungeEnabled());
+		myStorageSettings.setDeleteExpungeEnabled(defaultStorageSettings.isDeleteExpungeEnabled());
+		myStorageSettings.setExpungeBatchSize(defaultStorageSettings.getExpungeBatchSize());
+	}
+
+	@Test
+	public void testExpungeEverythingRefreshesPartitionCache() {
+		// Setup
+
+		IIdType p1 = createPatient(withActiveTrue());
+		IIdType o1 = createObservation(withSubject(p1));
+		IIdType o1b = createObservation(withReference("hasMember", o1));
+		IIdType o1c = createObservation(withReference("hasMember", o1b));
+
+		PartitionEntity partition = new PartitionEntity();
+		partition.setId(123);
+		partition.setName("PART");
+		myPartitionLookupSvc.createPartition(partition, mySrd);
+
+		// validate precondition
+		assertEquals(1, myPatientDao.search(SearchParameterMap.newSynchronous()).size());
+		assertEquals(3, myObservationDao.search(SearchParameterMap.newSynchronous()).size());
+		assertEquals("PART", myPartitionLookupSvc.getPartitionById(123).getName());
+		assertEquals(123, myPartitionLookupSvc.getPartitionByName("PART").getId());
+
+		// execute
+		myExpungeEverythingService.expungeEverything(mySrd);
+
+		// Validate
+		assertDoesntExist(p1);
+		assertDoesntExist(o1);
+		assertDoesntExist(o1b);
+		assertDoesntExist(o1c);
+
+		assertThat(myPartitionLookupSvc.listPartitions(), hasSize(0));
+		try {
+			myPartitionLookupSvc.getPartitionById(123);
+			fail();
+		} catch (ResourceNotFoundException e) {
+			assertEquals("No partition exists with ID 123", e.getMessage());
+		}
+		try {
+			myPartitionLookupSvc.getPartitionByName("PART");
+			fail();
+		} catch (ResourceNotFoundException e) {
+			assertEquals("Partition name \"PART\" is not valid", e.getMessage());
+		}
+	}
+
+}
