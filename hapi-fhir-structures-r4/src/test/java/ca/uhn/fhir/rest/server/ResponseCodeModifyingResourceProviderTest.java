25bb96aeb364fce8df872a4bf7d8ea8875c4959f
nathaniel.doef <nathaniel.doef@smilecdr.com>
2023-08-31 11:19:49 -0400

    remove TestResponseCodeModifyingPatientProvider

--- hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/ResponseCodeModifyingResourceProviderTest.java
+++ hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/rest/server/ResponseCodeModifyingResourceProviderTest.java
@@ -1,19 +1,28 @@
 package ca.uhn.fhir.rest.server;
 
 import ca.uhn.fhir.context.FhirVersionEnum;
-import ca.uhn.fhir.provider.TestResponseCodeModifyingPatientProvider;
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.annotation.Update;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.test.utilities.server.RestfulServerExtension;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static ca.uhn.fhir.provider.TestResponseCodeModifyingPatientProvider.CUSTOM_RESPONSE_CODE;
+import javax.servlet.ServletRequest;
+
+import static ca.uhn.fhir.rest.api.Constants.STATUS_HTTP_202_ACCEPTED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResponseCodeModifyingResourceProviderTest {
+
+	public static final int CUSTOM_RESPONSE_CODE = STATUS_HTTP_202_ACCEPTED;
 
 	@RegisterExtension
 	private RestfulServerExtension myServer = new RestfulServerExtension(FhirVersionEnum.R4)
@@ -41,6 +50,28 @@ public class ResponseCodeModifyingResourceProviderTest {
 		myPatient.setId("1");
 		MethodOutcome outcome = myClient.update().resource(myPatient).execute();
 		assertEquals(CUSTOM_RESPONSE_CODE, outcome.getResponseStatusCode());
+	}
+
+	class TestResponseCodeModifyingPatientProvider implements IResourceProvider {
+
+		@Create()
+		public MethodOutcome createPatient(@ResourceParam Patient thePatient, ServletRequest theServletRequest) {
+			MethodOutcome methodOutcome = new MethodOutcome();
+			methodOutcome.setResponseStatusCode(CUSTOM_RESPONSE_CODE);
+			return methodOutcome;
+		}
+
+		@Update()
+		public MethodOutcome updatePatient(@IdParam IdType theId, @ResourceParam Patient thePatient) {
+			MethodOutcome methodOutcome = new MethodOutcome();
+			methodOutcome.setResponseStatusCode(CUSTOM_RESPONSE_CODE);
+			return methodOutcome;
+		}
+
+		@Override
+		public Class<? extends IBaseResource> getResourceType() {
+			return Patient.class;
+		}
 	}
 
 }
