9a37013ab3cd1498c1e73b50bbc3ca644cc70836
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2023-11-10 15:01:53 -0500

    (#5442) PR feedback

--- hapi-fhir-storage/src/test/java/ca/uhn/fhir/jpa/validation/ValidatorResourceFetcherTest.java
+++ hapi-fhir-storage/src/test/java/ca/uhn/fhir/jpa/validation/ValidatorResourceFetcherTest.java
@@ -1,39 +1,41 @@
 package ca.uhn.fhir.jpa.validation;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
+import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
+import ca.uhn.fhir.rest.server.SimpleBundleProvider;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import ca.uhn.fhir.test.BaseTest;
+import ca.uhn.fhir.util.ClasspathUtil;
+import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
+import org.hl7.fhir.common.hapi.validation.validator.VersionSpecificWorkerContextWrapper;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r5.elementmodel.Element;
+import org.hl7.fhir.r5.utils.XVerExtensionManager;
+import org.hl7.fhir.validation.instance.InstanceValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.util.Arrays;
-import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
-import ca.uhn.fhir.rest.api.server.RequestDetails;
-import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
-import ca.uhn.fhir.rest.server.SimpleBundleProvider;
-import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
-import ca.uhn.fhir.test.BaseTest;
-import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
-import org.hl7.fhir.exceptions.FHIRException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.hl7.fhir.r5.elementmodel.Element;
-import org.hl7.fhir.r5.utils.XVerExtensionManager;
-import org.hl7.fhir.validation.instance.InstanceValidator;
-import org.hl7.fhir.common.hapi.validation.validator.VersionSpecificWorkerContextWrapper;
-import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 
 
 public class ValidatorResourceFetcherTest extends BaseTest {
-  private static FhirContext ourCtx = FhirContext.forR4();
-	private static DefaultProfileValidationSupport myDefaultValidationSupport = new DefaultProfileValidationSupport(ourCtx);
+  private static final FhirContext ourCtx = FhirContext.forR4();
+	private static final DefaultProfileValidationSupport myDefaultValidationSupport = new DefaultProfileValidationSupport(ourCtx);
 	private static ValidatorResourceFetcher fetcher;
 	private static DaoRegistry mockDaoRegistry;
-	private static IFhirResourceDao mockResourceDao;
+	private static IFhirResourceDao<IBaseResource> mockResourceDao;
 
+	@SuppressWarnings("unchecked")
 	@BeforeEach
 	public void before() {
     mockDaoRegistry = mock(DaoRegistry.class);
@@ -44,17 +46,12 @@ public class ValidatorResourceFetcherTest extends BaseTest {
   @Test
 	public void checkFetchByUrl() {
     // setup mocks
-    String resource;
-    try {
-      resource = loadResource("/q_jon_with_url_version.json");
-    } catch (IOException e) {
-      throw new FHIRException("Couldn't read");
-    }
+    String resource = ClasspathUtil.loadResource("/q_jon_with_url_version.json");
     doReturn(mockResourceDao).when(mockDaoRegistry).getResourceDao("Questionnaire");
     doThrow(new ResourceNotFoundException("Not Found")).when(mockResourceDao).read(any(),any());
-    doReturn(new SimpleBundleProvider(Arrays.asList(
-      ourCtx.newJsonParser().parseResource(resource)
-    ))).when(mockResourceDao).search(any(),any());
+    doReturn(new SimpleBundleProvider(List.of(
+		ourCtx.newJsonParser().parseResource(resource)
+	))).when(mockResourceDao).search(any(),any());
     VersionSpecificWorkerContextWrapper wrappedWorkerContext = VersionSpecificWorkerContextWrapper.newVersionSpecificWorkerContextWrapper(myDefaultValidationSupport);
     InstanceValidator v = new InstanceValidator(
       wrappedWorkerContext,
