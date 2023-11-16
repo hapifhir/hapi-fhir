ee41fa7d39f5b9ee6f644ed8bc98ffd0a10267b7
Justin McKelvy <60718638+Capt-Mac@users.noreply.github.com>
2023-11-16 16:01:02 -0700

    update unit tests and config for new cql and terminology properties

--- hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/config/RepositoryConfig.java
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/config/RepositoryConfig.java
@@ -19,13 +19,10 @@
  */
 package ca.uhn.fhir.cr.config;
 
-import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.cr.common.IRepositoryFactory;
-import ca.uhn.fhir.cr.common.PreExpandedValidationSupportLoader;
 import ca.uhn.fhir.cr.repo.HapiFhirRepository;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.rest.server.RestfulServer;
-import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,11 +33,5 @@ public class RepositoryConfig {
 	@Bean
 	IRepositoryFactory repositoryFactory(DaoRegistry theDaoRegistry, RestfulServer theRestfulServer) {
 		return rd -> new HapiFhirRepository(theDaoRegistry, rd, theRestfulServer);
-	}
-
-	@Bean
-	public PreExpandedValidationSupportLoader preExpandedValidationSupportLoader(
-			ValidationSupportChain theSupportChain, FhirContext theFhirContext) {
-		return new PreExpandedValidationSupportLoader(theSupportChain, theFhirContext);
 	}
 }
