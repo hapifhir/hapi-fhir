6e683405a1840cbfc4d0006f4ab81719e3f83c88
TipzCM <leif.stawnyczy@gmail.com>
2023-11-28 16:28:17 -0500

    5237 fixing empty last page if even results (#5506)
    paging will not return empty pages if no results left

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/method/ResponseBundleBuilder.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/method/ResponseBundleBuilder.java
@@ -105,8 +105,11 @@ public class ResponseBundleBuilder {
 			pageSize = pagingCalculatePageSize(requestedPage, server.getPagingProvider());
 
 			Integer size = bundleProvider.size();
-			numToReturn =
-					(size == null) ? pageSize : Math.min(pageSize, size.intValue() - theResponseBundleRequest.offset);
+			if (size == null) {
+				numToReturn = pageSize;
+			} else {
+				numToReturn = Math.min(pageSize, size.intValue() - theResponseBundleRequest.offset);
+			}
 
 			resourceList =
 					pagingBuildResourceList(theResponseBundleRequest, bundleProvider, numToReturn, responsePageBuilder);
@@ -252,6 +255,7 @@ public class ResponseBundleBuilder {
 				RestfulServerUtils.prettyPrintResponse(server, theResponseBundleRequest.requestDetails),
 				theResponseBundleRequest.bundleType);
 
+		// set self link
 		retval.setSelf(theResponseBundleRequest.linkSelf);
 
 		// determine if we are using offset / uncached pages
