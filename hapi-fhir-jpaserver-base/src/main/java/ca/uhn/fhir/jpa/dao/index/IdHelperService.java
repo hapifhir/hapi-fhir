da9be29047abd9a7781868a3c2602f192b2c57e5
kateryna-mironova <107507153+kateryna-mironova@users.noreply.github.com>
2023-12-06 16:26:52 -0500

    Fixed search for resources for $everything operation with partitions and MDM (#5504)
    * Fixed search for resources for $everything operation with partitions and client id mode = ANY
    
    * fixed formatting
    
    * changelog file renamed for tests
    
    * code review fixes: moved generics mocks to class fields, changed size > 0 to empty check
    
    * removed unused fields
    
    * moved fields back to methods, changed generic anys
    
    * changed imports from javax to jakarta
    
    * code review fixes

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/index/IdHelperService.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/index/IdHelperService.java
@@ -210,7 +210,9 @@ public class IdHelperService implements IIdHelperService<JpaPid> {
 		Validate.isTrue(!theIds.isEmpty(), "theIds must not be empty");
 
 		Map<String, JpaPid> retVals = new HashMap<>();
-
+		RequestPartitionId partitionId = myPartitionSettings.isAllowUnqualifiedCrossPartitionReference()
+				? RequestPartitionId.allPartitions()
+				: theRequestPartitionId;
 		for (String id : theIds) {
 			JpaPid retVal;
 			if (!idRequiresForcedId(id)) {
@@ -221,18 +223,17 @@ public class IdHelperService implements IIdHelperService<JpaPid> {
 				// is a forced id
 				// we must resolve!
 				if (myStorageSettings.isDeleteEnabled()) {
-					retVal = resolveResourceIdentity(theRequestPartitionId, theResourceType, id, theExcludeDeleted)
+					retVal = resolveResourceIdentity(partitionId, theResourceType, id, theExcludeDeleted)
 							.getPersistentId();
 					retVals.put(id, retVal);
 				} else {
 					// fetch from cache... adding to cache if not available
-					String key = toForcedIdToPidKey(theRequestPartitionId, theResourceType, id);
+					String key = toForcedIdToPidKey(partitionId, theResourceType, id);
 					retVal = myMemoryCacheService.getThenPutAfterCommit(
 							MemoryCacheService.CacheEnum.FORCED_ID_TO_PID, key, t -> {
 								List<IIdType> ids = Collections.singletonList(new IdType(theResourceType, id));
 								// fetches from cache using a function that checks cache first...
-								List<JpaPid> resolvedIds =
-										resolveResourcePersistentIdsWithCache(theRequestPartitionId, ids);
+								List<JpaPid> resolvedIds = resolveResourcePersistentIdsWithCache(partitionId, ids);
 								if (resolvedIds.isEmpty()) {
 									throw new ResourceNotFoundException(Msg.code(1100) + ids.get(0));
 								}
