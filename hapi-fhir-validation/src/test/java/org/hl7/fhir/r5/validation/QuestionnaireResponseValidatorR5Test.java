42f7fb2320eea9cfcb7d5080daec77b387893d0b
dotasek <david.otasek@smilecdr.com>
2023-12-19 09:31:29 -0500

    Fix QuestionnaireResponseValidatorR5Test

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/r5/validation/QuestionnaireResponseValidatorR5Test.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r5/validation/QuestionnaireResponseValidatorR5Test.java
@@ -328,10 +328,12 @@ public class QuestionnaireResponseValidatorR5Test {
 	public void testMissingRequiredQuestion() {
 
 		Questionnaire q = new Questionnaire();
+		q.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		q.addItem().setLinkId("link0").setRequired(true).setType(QuestionnaireItemType.STRING);
 		q.addItem().setLinkId("link1").setRequired(true).setType(QuestionnaireItemType.STRING);
 
 		QuestionnaireResponse qa = new QuestionnaireResponse();
+		qa.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		qa.setStatus(QuestionnaireResponseStatus.COMPLETED);
 		qa.getQuestionnaireElement().setValue("http://example.com/Questionnaire/q1");
 		qa.addItem().setLinkId("link1").addAnswer().setValue(new StringType("FOO"));
@@ -623,9 +625,11 @@ public class QuestionnaireResponseValidatorR5Test {
 	@Test
 	public void testUnexpectedAnswer() {
 		Questionnaire q = new Questionnaire();
+		q.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		q.addItem().setLinkId("link0").setRequired(false).setType(QuestionnaireItemType.BOOLEAN);
 
 		QuestionnaireResponse qa = new QuestionnaireResponse();
+		qa.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		qa.setStatus(QuestionnaireResponseStatus.COMPLETED);
 		qa.getQuestionnaireElement().setValue("http://example.com/Questionnaire/q1");
 		qa.addItem().setLinkId("link1").addAnswer().setValue(new StringType("FOO"));
@@ -641,9 +645,11 @@ public class QuestionnaireResponseValidatorR5Test {
 	@Test
 	public void testUnexpectedGroup() {
 		Questionnaire q = new Questionnaire();
+		q.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		q.addItem().setLinkId("link0").setRequired(false).setType(QuestionnaireItemType.BOOLEAN);
 
 		QuestionnaireResponse qa = new QuestionnaireResponse();
+		qa.getText().setDiv(new XhtmlNode().setValue("<div>AA</div>")).setStatus(org.hl7.fhir.r5.model.Narrative.NarrativeStatus.GENERATED);
 		qa.setStatus(QuestionnaireResponseStatus.COMPLETED);
 		qa.getQuestionnaireElement().setValue("http://example.com/Questionnaire/q1");
 		qa.addItem().setLinkId("link1").addItem().setLinkId("link2");
