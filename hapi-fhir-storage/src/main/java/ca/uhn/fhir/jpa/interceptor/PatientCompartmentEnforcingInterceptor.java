836b755ad3776b14cdfc6fe95e7e3a2b714bd33a
jmarchionatto <60409882+jmarchionatto@users.noreply.github.com>
2023-12-21 12:57:58 -0500

    5558 disallow resource updates crossing patient compartment (#5562)
    * Improve readability
    
    * Fix warnings
    
    * Add interceptor, test doc and changelog. Also, extract common used function to utility class and add tests.
    
    * spotless
    
    * Fix changelog link
    
    * Fix changelog fragment warnings
    
    * Fix changelog link
    
    * Fix documentation link
    
    * Implement review comments
    
    * Implement review comments
    
    * Keep old exception code for tests
    
    * Revert refactoring which lost difference between NonCompartmentMemberTypeResponse and NonCompartmentMemberInstanceResponse
    
    * spotless
    
    * Remove version restriction
    
    * Differentiate exceptions thrown by different code
    
    ---------
    
    Co-authored-by: juan.marchionatto <juan.marchionatto@smilecdr.com>

---
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/interceptor/PatientCompartmentEnforcingInterceptor.java
@@ -0,0 +1,65 @@
+package ca.uhn.fhir.jpa.interceptor;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.i18n.Msg;
+import ca.uhn.fhir.interceptor.api.Hook;
+import ca.uhn.fhir.interceptor.api.Interceptor;
+import ca.uhn.fhir.interceptor.api.Pointcut;
+import ca.uhn.fhir.jpa.searchparam.extractor.ISearchParamExtractor;
+import ca.uhn.fhir.jpa.util.ResourceCompartmentUtil;
+import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
+import ca.uhn.fhir.util.StopWatch;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+/**
+ * This interceptor can be used to block resource updates which would make resource patient compartment change.
+ * <p/>
+ * This could be used when the JPA server has partitioning enabled, and Tenant Identification Strategy is PATIENT_ID.
+ */
+@Interceptor
+public class PatientCompartmentEnforcingInterceptor {
+	private static final Logger ourLog = LoggerFactory.getLogger(PatientCompartmentEnforcingInterceptor.class);
+
+	private final FhirContext myFhirContext;
+	private final ISearchParamExtractor mySearchParamExtractor;
+
+	public PatientCompartmentEnforcingInterceptor(
+			FhirContext theFhirContext, ISearchParamExtractor theSearchParamExtractor) {
+		myFhirContext = theFhirContext;
+		mySearchParamExtractor = theSearchParamExtractor;
+	}
+
+	/**
+	 * Blocks resource updates which would make the resource change Patient Compartment.
+	 * @param theOldResource the original resource state
+	 * @param theResource the updated resource state
+	 */
+	@Hook(Pointcut.STORAGE_PRESTORAGE_RESOURCE_UPDATED)
+	public void storagePreStorageResourceUpdated(IBaseResource theOldResource, IBaseResource theResource) {
+
+		ourLog.info("Interceptor STORAGE_PRESTORAGE_RESOURCE_UPDATED - started");
+		StopWatch stopWatch = new StopWatch();
+		try {
+			String patientCompartmentOld = ResourceCompartmentUtil.getPatientCompartmentIdentity(
+							theOldResource, myFhirContext, mySearchParamExtractor)
+					.orElse(EMPTY);
+			String patientCompartmentCurrent = ResourceCompartmentUtil.getPatientCompartmentIdentity(
+							theResource, myFhirContext, mySearchParamExtractor)
+					.orElse(EMPTY);
+
+			if (!StringUtils.equals(patientCompartmentOld, patientCompartmentCurrent)) {
+				// Avoid disclosing compartments in message, which could have security implications
+				throw new InternalErrorException(
+						Msg.code(2476) + "Resource compartment changed. Was a referenced Patient changed?");
+			}
+
+		} finally {
+			ourLog.info("Interceptor STORAGE_PRESTORAGE_RESOURCE_UPDATED - ended, execution took {}", stopWatch);
+		}
+	}
+}
