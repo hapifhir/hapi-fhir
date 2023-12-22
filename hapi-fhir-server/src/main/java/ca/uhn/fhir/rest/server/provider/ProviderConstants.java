1ad8c1d55466f6e6af7f7781171bcdbead9e28a1
Justin McKelvy <60718638+Capt-Mac@users.noreply.github.com>
2023-12-22 14:25:49 -0700

    spotless edit

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ProviderConstants.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ProviderConstants.java
@@ -109,6 +109,7 @@ public class ProviderConstants {
 	 * Clinical Reasoning Operations
 	 */
 	public static final String CR_OPERATION_EVALUATE_MEASURE = "$evaluate-measure";
+
 	public static final String CR_OPERATION_CARE_GAPS = "$care-gaps";
 	public static final String CR_OPERATION_SUBMIT_DATA = "$submit-data";
 	public static final String CR_OPERATION_EVALUATE = "$evaluate";
