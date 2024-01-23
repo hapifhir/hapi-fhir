4cad26a7d7808e98d1257e03a246b6f7d5e174ee
peartree <etienne.poirier@smilecdr.com>
2024-01-23 14:49:15 -0500

    Addressing comments from code review.

--- hapi-fhir-serviceloaders/hapi-fhir-caching-caffeine/src/main/java/ca/uhn/fhir/sl/cache/caffeine/CacheProvider.java
+++ hapi-fhir-serviceloaders/hapi-fhir-caching-caffeine/src/main/java/ca/uhn/fhir/sl/cache/caffeine/CacheProvider.java
@@ -44,6 +44,8 @@ public class CacheProvider<K, V> implements ca.uhn.fhir.sl.cache.CacheProvider<K
 	public Cache<K, V> create(long timeoutMillis, long maximumSize) {
 		return new CacheDelegator<K, V>(Caffeine.newBuilder()
 				.expireAfterWrite(timeoutMillis, TimeUnit.MILLISECONDS)
+				// Caffeine locks the whole array when growing the hash table.
+				// Set initial capacity to max to avoid this.  All our caches are <1M entries.
 				.initialCapacity((int) maximumSize)
 				.maximumSize(maximumSize)
 				.build());
@@ -52,6 +54,8 @@ public class CacheProvider<K, V> implements ca.uhn.fhir.sl.cache.CacheProvider<K
 	public LoadingCache<K, V> create(long timeoutMillis, long maximumSize, CacheLoader<K, V> loading) {
 		return new LoadingCacheDelegator<K, V>(Caffeine.newBuilder()
 				.expireAfterWrite(timeoutMillis, TimeUnit.MILLISECONDS)
+				// Caffeine locks the whole array when growing the hash table.
+				// Set initial capacity to max to avoid this.  All our caches are <1M entries.
 				.initialCapacity((int) maximumSize)
 				.maximumSize(maximumSize)
 				.build(loading::load));
