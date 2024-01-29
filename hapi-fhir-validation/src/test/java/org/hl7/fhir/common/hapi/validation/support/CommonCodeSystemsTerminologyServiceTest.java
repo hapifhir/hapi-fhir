c7fd01519508edcaed03fb730b04c697b9fd10c2
Martha Mitran <martha.mitran@smilecdr.com>
2024-01-29 13:18:40 -0800

    Fix expansion for `ValueSet` with no concepts based on CodeSystem `urn:ietf:bcp:13` (#5638)
    * When fetching the mimetype code system, return empty CodeSystem with NOTSUPPORTED content. Update expansion logic to handle this case. Add some test cases.
    
    * Minor change to fix test
    
    * Rename changelog file
    
    * Remove TODOs as they're replaced by reported issue
    
    * Revert accidental change added with TODO removal

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/common/hapi/validation/support/CommonCodeSystemsTerminologyServiceTest.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/common/hapi/validation/support/CommonCodeSystemsTerminologyServiceTest.java
@@ -3,16 +3,24 @@ package org.hl7.fhir.common.hapi.validation.support;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.ConceptValidationOptions;
 import ca.uhn.fhir.context.support.IValidationSupport;
-import ca.uhn.fhir.context.support.ValidationSupportContext;
 import ca.uhn.fhir.context.support.LookupCodeRequest;
+import ca.uhn.fhir.context.support.ValidationSupportContext;
 import ca.uhn.fhir.fhirpath.BaseValidationTestWithInlineMocks;
 import ca.uhn.fhir.i18n.Msg;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.api.EncodingEnum;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.CodeSystem.CodeSystemContentMode;
 import org.hl7.fhir.r4.model.ValueSet;
+import org.hl7.fhir.r5.model.Enumerations;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService.MIMETYPES_CODESYSTEM_URL;
+import static org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService.MIMETYPES_VALUESET_URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -134,6 +142,7 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 	@Test
 	public void testLanguages_CommonLanguagesVs_OnlyLanguage_NoRegion() {
 		IValidationSupport.LookupCodeResult nl = mySvc.lookupCode(newSupport(), new LookupCodeRequest("urn:ietf:bcp:47", "nl"));
+		assertNotNull(nl);
 		assertTrue(nl.isFound());
 		assertEquals("Dutch", nl.getCodeDisplay());
 	}
@@ -141,6 +150,7 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 	@Test
 	public void testLanguages_CommonLanguagesVs_LanguageAndRegion() {
 		IValidationSupport.LookupCodeResult nl = mySvc.lookupCode(newSupport(), new LookupCodeRequest("urn:ietf:bcp:47", "nl-NL"));
+		assertNotNull(nl);
 		assertTrue(nl.isFound());
 		assertEquals("Dutch Netherlands", nl.getCodeDisplay());
 	}
@@ -190,6 +200,7 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 		CodeSystem cs = (CodeSystem) mySvc.fetchCodeSystem(CommonCodeSystemsTerminologyService.COUNTRIES_CODESYSTEM_URL);
 		assert cs != null;
 		assertEquals(498, cs.getConcept().size());
+		assertEquals(CodeSystemContentMode.COMPLETE, cs.getContent());
 	}
 
 	@Test
@@ -198,6 +209,7 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 		org.hl7.fhir.dstu3.model.CodeSystem cs = (org.hl7.fhir.dstu3.model.CodeSystem) svc.fetchCodeSystem(CommonCodeSystemsTerminologyService.COUNTRIES_CODESYSTEM_URL);
 		assert cs != null;
 		assertEquals(498, cs.getConcept().size());
+		assertEquals(org.hl7.fhir.dstu3.model.CodeSystem.CodeSystemContentMode.COMPLETE, cs.getContent());
 	}
 
 	@Test
@@ -206,6 +218,7 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 		org.hl7.fhir.r5.model.CodeSystem cs = (org.hl7.fhir.r5.model.CodeSystem) svc.fetchCodeSystem(CommonCodeSystemsTerminologyService.COUNTRIES_CODESYSTEM_URL);
 		assert cs != null;
 		assertEquals(498, cs.getConcept().size());
+		assertEquals(Enumerations.CodeSystemContentMode.COMPLETE, cs.getContent());
 	}
 
 	@Test
@@ -216,10 +229,11 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 	}
 
 	@Test
-	public void testFetchCodeSystemBuiltIn_Iso_R4() {
+	public void testFetchCodeSystemBuiltIn_Iso4217_R4() {
 		CodeSystem cs = (CodeSystem) mySvc.fetchCodeSystem(CommonCodeSystemsTerminologyService.CURRENCIES_CODESYSTEM_URL);
 		assert cs != null;
 		assertEquals(182, cs.getConcept().size());
+		assertEquals(CodeSystemContentMode.COMPLETE, cs.getContent());
 	}
 
 	@Test
@@ -237,6 +251,120 @@ public class CommonCodeSystemsTerminologyServiceTest extends BaseValidationTestW
 		} catch (IllegalArgumentException e) {
 			assertEquals(Msg.code(696) + "Can not handle version: DSTU3", e.getMessage());
 		}
+	}
+
+	@Test
+	public void testFetchCodeSystem_withMimeType_returnsOk() {
+		CodeSystem cs = (CodeSystem) mySvc.fetchCodeSystem(MIMETYPES_CODESYSTEM_URL);
+		assertNotNull(cs);
+		assertTrue(cs.getConcept().isEmpty());
+		assertEquals(CodeSystemContentMode.NOTPRESENT, cs.getContent());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { EncodingEnum.JSON_PLAIN_STRING, Constants.CT_FHIR_JSON_NEW, Constants.CT_FHIR_JSON })
+	public void testValidateCode_withMimetypesValueSetWithStandardCode_returnsValid(String code) {
+		// test
+		IValidationSupport.CodeValidationResult result = mySvc.validateCode(newSupport(), newOptions(), MIMETYPES_CODESYSTEM_URL, code, null, MIMETYPES_VALUESET_URL);
+
+		// verify
+		assertNotNull(result);
+		assertEquals(code, result.getCode());
+		assertTrue(result.isOk());
+		assertNull(result.getSeverity());
+		assertNull(result.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { EncodingEnum.JSON_PLAIN_STRING, Constants.CT_FHIR_JSON_NEW, Constants.CT_FHIR_JSON })
+	public void testValidateCode_withMimetypesValueSetWithInferSystemWithStandardCode_returnsValid(String code) {
+		// test
+		IValidationSupport.CodeValidationResult result = mySvc.validateCode(newSupport(), newOptions().setInferSystem(true), null, code, null, MIMETYPES_VALUESET_URL);
+
+		// verify
+		assertNotNull(result);
+		assertEquals(code, result.getCode());
+		assertTrue(result.isOk());
+		assertNull(result.getSeverity());
+		assertNull(result.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { EncodingEnum.JSON_PLAIN_STRING, Constants.CT_FHIR_JSON_NEW, Constants.CT_FHIR_JSON })
+	public void testValidateCode_withMimetypesWithStandardCode_returnsValid(String code) {
+		// test
+		IValidationSupport.CodeValidationResult result = mySvc.validateCode(newSupport(), newOptions(), MIMETYPES_CODESYSTEM_URL, code, null, null);
+
+		// verify
+		assertNotNull(result);
+		assertEquals(code, result.getCode());
+		assertTrue(result.isOk());
+		assertNull(result.getSeverity());
+		assertNull(result.getMessage());
+	}
+
+	@Test
+	public void testValidateCode_withMimetypeValueSetWithArbitraryCode_returnsValid() {
+		// setup
+		final String code = "someCode";
+		final String display = "displayValue";
+
+		// test
+		IValidationSupport.CodeValidationResult result = mySvc.validateCode(newSupport(), newOptions(), MIMETYPES_CODESYSTEM_URL, code, display, MIMETYPES_VALUESET_URL);
+
+		// verify
+		assertNotNull(result);
+		assertEquals(code, result.getCode());
+		assertTrue(result.isOk());
+		assertEquals(display, result.getDisplay());
+	}
+
+	@Test
+	public void testValidateCode_withMimetypesWithArbitraryCode_returnsValid() {
+		// setup
+		final String code = "someCode";
+		final String display = "displayValue";
+
+		// test
+		IValidationSupport.CodeValidationResult result = mySvc.validateCode(newSupport(), newOptions(), MIMETYPES_CODESYSTEM_URL, code, display, null);
+
+		// verify
+		assertNotNull(result);
+		assertEquals(code, result.getCode());
+		assertTrue(result.isOk());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { EncodingEnum.JSON_PLAIN_STRING, Constants.FORMAT_TURTLE, Constants.CT_FHIR_JSON_NEW, Constants.CT_FHIR_JSON })
+	public void testLookupCode_withMimetypesWithStandardCode_returnFound(String code) {
+		// setup
+		final String system = MIMETYPES_CODESYSTEM_URL;
+
+		// test
+		IValidationSupport.LookupCodeResult result = mySvc.lookupCode(newSupport(), new LookupCodeRequest(system, code));
+
+		// verify
+		assertNotNull(result);
+		assertEquals(system, result.getSearchedForSystem());
+		assertEquals(code, result.getSearchedForCode());
+		assertTrue(result.isFound());
+	}
+
+	@Test
+	public void testLookupCode_withMimetypesWithArbitraryCode_returnsFound() {
+		// setup
+		final String system = MIMETYPES_CODESYSTEM_URL;
+		final String code = "someCode";
+
+		// test
+		IValidationSupport.LookupCodeResult result = mySvc.lookupCode(newSupport(), new LookupCodeRequest(system, code));
+
+		// verify
+		assertNotNull(result);
+		assertEquals(system, result.getSearchedForSystem());
+		assertEquals(code, result.getSearchedForCode());
+		assertTrue(result.isFound());
+		assertNull(result.getCodeDisplay());
 	}
 
 	private ValidationSupportContext newSupport() {
