c30608a55cef0922ebc6375104184121a4e72c7c
Tadgh <garygrantgraham@gmail.com>
2024-02-27 12:50:01 -0500

    Rel 6 4 mb update (#5741)
    * Add backport and folder
    
    * Add missing changelog
    
    * Cherry p icking
    
    * Update upgrade.md
    
    * added new version version.yaml
    
    * Provide default update time to batch2 entities (#4630)
    
    * Data migration - default value for batch2 UPDATE_TIME
    
    * fixed changelog, added 6.4.3 changelog folder
    
    * Spotless
    
    ---------
    
    Co-authored-by: Kevin Dougan SmileCDR <72025369+KevinDougan-SmileCDR@users.noreply.github.com>
    Co-authored-by: longma1 <long@smilecdr.com>
    Co-authored-by: michaelabuckley <michaelabuckley@gmail.com>

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
@@ -221,15 +221,6 @@ public class HapiFhirJpaMigrationTasks extends BaseMigrationTasks<VersionEnum> {
 	protected void init680() {
 		Builder version = forVersion(VersionEnum.V6_8_0);
 
-		// HAPI-FHIR #4801 - Add New Index On HFJ_RESOURCE
-		Builder.BuilderWithTableName resourceTable = version.onTable("HFJ_RESOURCE");
-
-		resourceTable
-				.addIndex("20230502.1", "IDX_RES_RESID_UPDATED")
-				.unique(false)
-				.online(true)
-				.withColumns("RES_ID", "RES_UPDATED", "PARTITION_ID");
-
 		Builder.BuilderWithTableName tagDefTable = version.onTable("HFJ_TAG_DEF");
 		tagDefTable.dropIndex("20230505.1", "IDX_TAGDEF_TYPESYSCODEVERUS");
 
@@ -731,6 +722,48 @@ public class HapiFhirJpaMigrationTasks extends BaseMigrationTasks<VersionEnum> {
 				.addColumn("20230110.2", "UPDATE_TIME")
 				.nullable()
 				.type(ColumnTypeEnum.DATE_TIMESTAMP);
+
+		Map<DriverTypeEnum, String> updateBatch2JobInstance = new HashMap<>();
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.H2_EMBEDDED,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.MARIADB_10_1,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.MYSQL_5_7,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.ORACLE_12C,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.POSTGRES_9_4,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2JobInstance.put(
+				DriverTypeEnum.MSSQL_2012,
+				"update BT2_JOB_INSTANCE set UPDATE_TIME = coalesce(end_time, start_time, create_time, CONVERT(DATETIME,'2023-01-01 00:00:00')) where UPDATE_TIME is null");
+		version.executeRawSql("20230397.1", updateBatch2JobInstance);
+
+		Map<DriverTypeEnum, String> updateBatch2WorkChunk = new HashMap<>();
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.H2_EMBEDDED,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.MARIADB_10_1,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.MYSQL_5_7,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.ORACLE_12C,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.POSTGRES_9_4,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, TIMESTAMP '2023-01-01 00:00:00') where UPDATE_TIME is null");
+		updateBatch2WorkChunk.put(
+				DriverTypeEnum.MSSQL_2012,
+				"update bt2_work_chunk set UPDATE_TIME = coalesce(end_time, start_time, create_time, CONVERT(DATETIME,'2023-01-01 00:00:00')) where UPDATE_TIME is null");
+		version.executeRawSql("20230397.2", updateBatch2WorkChunk);
 	}
 
 	private void init610() {
