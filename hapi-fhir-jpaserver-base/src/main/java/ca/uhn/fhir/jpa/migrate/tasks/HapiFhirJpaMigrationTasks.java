41d1133ab0bc985e46f2ea370d22df7a7717f4a7
Tadgh <garygrantgraham@gmail.com>
2024-02-28 10:41:50 -0800

    spotless

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/migrate/tasks/HapiFhirJpaMigrationTasks.java
@@ -224,10 +224,10 @@ public class HapiFhirJpaMigrationTasks extends BaseMigrationTasks<VersionEnum> {
 		Builder.BuilderWithTableName resourceTable = version.onTable("HFJ_RESOURCE");
 
 		resourceTable
-			.addIndex("20230502.1", "IDX_RES_RESID_UPDATED")
-			.unique(false)
-			.online(true)
-			.withColumns("RES_ID", "RES_UPDATED", "PARTITION_ID");
+				.addIndex("20230502.1", "IDX_RES_RESID_UPDATED")
+				.unique(false)
+				.online(true)
+				.withColumns("RES_ID", "RES_UPDATED", "PARTITION_ID");
 
 		Builder.BuilderWithTableName tagDefTable = version.onTable("HFJ_TAG_DEF");
 		tagDefTable.dropIndex("20230505.1", "IDX_TAGDEF_TYPESYSCODEVERUS");
