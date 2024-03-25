fcc44fc2b80d88777f60fac58b613b15f68ad7e0
Lila Mikalson <lila.mikalson@smilecdr.com>
2024-03-25 07:54:05 -0700

    Add _language param to providers and tests

---
+++ hapi-fhir-jpaserver-test-r4b/src/test/java/ca/uhn/fhir/jpa/provider/r4b/ResourceProviderLanguageParamR4BTest.java
@@ -0,0 +1,77 @@
+package ca.uhn.fhir.jpa.provider.r4b;
+
+import ca.uhn.fhir.i18n.Msg;
+import ca.uhn.fhir.jpa.api.config.JpaStorageSettings;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4b.model.Bundle;
+import org.hl7.fhir.r4b.model.Patient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ResourceProviderLanguageParamR4BTest extends BaseResourceProviderR4BTest {
+
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ResourceProviderLanguageParamR4BTest.class);
+
+	@BeforeEach
+	@Override
+	public void before() throws Exception {
+		super.before();
+		myStorageSettings.setLanguageSearchParameterEnabled(new JpaStorageSettings().isLanguageSearchParameterEnabled());
+		mySearchParamRegistry.forceRefresh();
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void testSearchWithLanguageParamEnabled() {
+		myStorageSettings.setLanguageSearchParameterEnabled(true);
+		mySearchParamRegistry.forceRefresh();
+
+		Patient pat = new Patient();
+		pat.setLanguage("en");
+		IIdType patId = myPatientDao.create(pat, mySrd).getId().toUnqualifiedVersionless();
+
+		Patient pat2 = new Patient();
+		pat.setLanguage("fr");
+		IIdType patId2 = myPatientDao.create(pat2, mySrd).getId().toUnqualifiedVersionless();
+
+		List<String> foundResources;
+		Bundle result;
+
+		result = myClient
+			.search()
+			.forResource(Patient.class)
+			.where(new TokenClientParam(Constants.PARAM_LANGUAGE).exactly().code("en"))
+			.returnBundle(Bundle.class)
+			.execute();
+
+		foundResources = toUnqualifiedVersionlessIdValues(result);
+		assertThat(foundResources, contains(patId.getValue()));
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void testSearchWithLanguageParamDisabled() {
+		myStorageSettings.setLanguageSearchParameterEnabled(new JpaStorageSettings().isLanguageSearchParameterEnabled());
+		mySearchParamRegistry.forceRefresh();
+
+		InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> {
+			myClient
+				.search()
+				.forResource(Patient.class)
+				.where(new TokenClientParam(Constants.PARAM_LANGUAGE).exactly().code("en"))
+				.returnBundle(Bundle.class)
+				.execute();
+		});
+		assertThat(exception.getMessage(), containsString(Msg.code(1223)));
+	}
+}
