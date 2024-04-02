9834899eb17328ead863dd80e3c19add64aaacba
nathaniel.doef <nathaniel.doef@smilecdr.com>
2024-04-02 17:48:06 -0400

    change mock test to jpa test

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/partition/RequestPartitionHelperSvcTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/partition/RequestPartitionHelperSvcTest.java
@@ -1,51 +1,54 @@
 package ca.uhn.fhir.jpa.partition;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.interceptor.api.IInterceptorBroadcaster;
 import ca.uhn.fhir.interceptor.model.RequestPartitionId;
+import ca.uhn.fhir.jpa.dao.data.IPartitionDao;
 import ca.uhn.fhir.jpa.entity.PartitionEntity;
 import ca.uhn.fhir.jpa.model.config.PartitionSettings;
+import ca.uhn.fhir.jpa.test.BaseJpaR4Test;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Patient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class RequestPartitionHelperSvcTest {
-	static final Integer PARTITION_ID = 2401;
-	static final String PARTITION_NAME = "JIMMY";
-	static final PartitionEntity ourPartitionEntity = new PartitionEntity().setName(PARTITION_NAME);
+class RequestPartitionHelperSvcTest extends BaseJpaR4Test {
 
-	@Mock
+	static final String PARTITION_NAME = "SOME-PARTITION";
+	static final int PARTITION_ID = 1;
+
+	@Autowired
+	IPartitionDao myPartitionDao;
+	@Autowired
 	PartitionSettings myPartitionSettings;
-	@Mock
-	IPartitionLookupSvc myPartitionLookupSvc;
-	@Mock
-	FhirContext myFhirContext;
-	@Mock
-	IInterceptorBroadcaster myInterceptorBroadcaster;
+	@Autowired
+	RequestPartitionHelperSvc mySvc;
 
-	@InjectMocks
-	RequestPartitionHelperSvc mySvc = new RequestPartitionHelperSvc();
+	Patient myPatient;
+
+	@BeforeEach
+	public void before(){
+		myPartitionDao.deleteAll();
+		myPartitionSettings.setPartitioningEnabled(true);
+
+		myPatient = new Patient();
+		myPatient.setId(new IdType("Patient", "123", "1"));
+	}
 
 	@Test
 	public void determineReadPartitionForSystemRequest() {
 		// setup
+		PartitionEntity partitionEntity = createPartition();
 		SystemRequestDetails srd = new SystemRequestDetails();
-		RequestPartitionId requestPartitionId = RequestPartitionId.fromPartitionId(PARTITION_ID);
-		srd.setRequestPartitionId(requestPartitionId);
-		when(myPartitionSettings.isPartitioningEnabled()).thenReturn(true);
-		when(myPartitionLookupSvc.getPartitionById(PARTITION_ID)).thenReturn(ourPartitionEntity);
+		srd.setRequestPartitionId(RequestPartitionId.fromPartitionId(partitionEntity.getId()));
 
 		// execute
-		RequestPartitionId result = mySvc.determineReadPartitionForRequestForRead(srd, "Patient", new IdType("Patient/123"));
+		RequestPartitionId result = mySvc.determineReadPartitionForRequestForRead(srd, myPatient.fhirType(), myPatient.getIdElement());
 
 		// verify
 		assertEquals(PARTITION_ID, result.getFirstPartitionIdOrNull());
@@ -55,20 +58,22 @@ class RequestPartitionHelperSvcTest {
 	@Test
 	public void determineCreatePartitionForSystemRequest() {
 		// setup
+		PartitionEntity partitionEntity = createPartition();
 		SystemRequestDetails srd = new SystemRequestDetails();
-		RequestPartitionId requestPartitionId = RequestPartitionId.fromPartitionId(PARTITION_ID);
-		srd.setRequestPartitionId(requestPartitionId);
-		when(myPartitionSettings.isPartitioningEnabled()).thenReturn(true);
-		when(myPartitionLookupSvc.getPartitionById(PARTITION_ID)).thenReturn(ourPartitionEntity);
-		Patient resource = new Patient();
-		when(myFhirContext.getResourceType(resource)).thenReturn("Patient");
+		srd.setRequestPartitionId(RequestPartitionId.fromPartitionId(partitionEntity.getId()));
 
 		// execute
-		RequestPartitionId result = mySvc.determineCreatePartitionForRequest(srd, resource, "Patient");
+		Patient patient = new Patient();
+		RequestPartitionId result = mySvc.determineCreatePartitionForRequest(srd, patient, patient.fhirType());
 
 		// verify
 		assertEquals(PARTITION_ID, result.getFirstPartitionIdOrNull());
 		assertEquals(PARTITION_NAME, result.getFirstPartitionNameOrNull());
 	}
 
+	private PartitionEntity createPartition() {
+		PartitionEntity partitionEntity = new PartitionEntity().setId(PARTITION_ID).setName(PARTITION_NAME);
+		partitionEntity = myPartitionDao.save(partitionEntity);
+		return partitionEntity;
+	}
 }
