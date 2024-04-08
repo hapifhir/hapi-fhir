ad39f43af426819e5becf0bf8f5a8e817f2a6008
Brenin Rhodes <brenin@alphora.com>
2024-04-08 14:01:11 -0600

    Add tests

--- hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/dstu3/PlanDefinitionOperationsProviderTest.java
+++ hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/dstu3/PlanDefinitionOperationsProviderTest.java
@@ -5,9 +5,11 @@ import org.hl7.fhir.dstu3.model.BooleanType;
 import org.hl7.fhir.dstu3.model.CarePlan;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.PlanDefinition;
+import org.hl7.fhir.dstu3.model.RequestGroup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PlanDefinitionOperationsProviderTest extends BaseCrDstu3TestServer {
@@ -29,5 +31,11 @@ public class PlanDefinitionOperationsProviderTest extends BaseCrDstu3TestServer 
 			requestDetails);
 
 		assertNotNull(result);
+		assertEquals(patientId, result.getSubject().getReference());
+		assertEquals(1, result.getContained().size());
+		var requestGroup = (RequestGroup) result.getContained().get(0);
+		assertEquals(1, requestGroup.getAction().size());
+		var action = requestGroup.getAction().get(0);
+		assertEquals("Hello World!", action.getTitle());
 	}
 }
