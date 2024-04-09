4dd20b400075192248a93bf491c9ca2982842193
Luke deGruchy <luke.degruchy@smilecdr.com>
2024-04-09 09:15:20 -0400

    Resolve static analysis warnings.

--- hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/subscription/module/subscriber/SubscriptionMatchingSubscriberTest.java
+++ hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/subscription/module/subscriber/SubscriptionMatchingSubscriberTest.java
@@ -40,6 +40,7 @@ import java.util.Optional;
 
 import static ca.uhn.fhir.jpa.subscription.match.matcher.subscriber.SubscriptionCriteriaParser.TypeEnum.STARTYPE_EXPRESSION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
@@ -572,6 +573,7 @@ public class SubscriptionMatchingSubscriberTest extends BaseBlockingQueueSubscri
 			mySubscriptionResourceMatched.awaitExpected();
 			ourObservationListener.awaitExpected();
 		} catch (InterruptedException exception) {
+			fail();
 			Thread.currentThread().interrupt();
 		}
 	}
