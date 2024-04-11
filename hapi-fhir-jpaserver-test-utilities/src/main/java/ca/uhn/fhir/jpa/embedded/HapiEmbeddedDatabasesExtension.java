2fc5d846a3a03fe5e660ee95b8c23c2c44133293
peartree <etienne.poirier@smilecdr.com>
2024-04-11 15:00:06 -0400

    fixing test failure - 3

--- hapi-fhir-jpaserver-test-utilities/src/main/java/ca/uhn/fhir/jpa/embedded/HapiEmbeddedDatabasesExtension.java
+++ hapi-fhir-jpaserver-test-utilities/src/main/java/ca/uhn/fhir/jpa/embedded/HapiEmbeddedDatabasesExtension.java
@@ -55,9 +55,9 @@ public class HapiEmbeddedDatabasesExtension implements AfterAllCallback {
 		if (DockerRequiredCondition.isDockerAvailable()) {
 			myEmbeddedDatabases.add(new H2EmbeddedDatabase());
 			myEmbeddedDatabases.add(new PostgresEmbeddedDatabase());
-			myEmbeddedDatabases.add(new MsSqlEmbeddedDatabase());
+			//			myEmbeddedDatabases.add(new MsSqlEmbeddedDatabase());
 			if (canUseOracle()) {
-				myEmbeddedDatabases.add(new OracleEmbeddedDatabase());
+				//				myEmbeddedDatabases.add(new OracleEmbeddedDatabase());
 			} else {
 				String message =
 						"Cannot add OracleEmbeddedDatabase. If you are using a Mac you must configure the TestContainers API to run using Colima (https://www.testcontainers.org/supported_docker_environment#using-colima)";
