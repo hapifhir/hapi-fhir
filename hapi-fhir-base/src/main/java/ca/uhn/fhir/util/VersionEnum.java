6653a1e1b2d2ef0470d6086190bc656879cfb456
Tadgh <garygrantgraham@gmail.com>
2024-04-21 13:23:34 -0700

    Versions added

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
@@ -153,7 +153,10 @@ public enum VersionEnum {
 	V7_0_2,
 
 	V7_1_0,
-	V7_2_0;
+	V7_2_0,
+
+	V7_3_0,
+	V7_4_0;
 
 	public static VersionEnum latestVersion() {
 		VersionEnum[] values = VersionEnum.values();
