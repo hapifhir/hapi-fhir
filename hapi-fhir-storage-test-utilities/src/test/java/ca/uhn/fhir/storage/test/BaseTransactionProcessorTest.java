21ae1569539d69697130d2f598b2af4403886290
Ken Stevens <khstevens@gmail.com>
2024-05-03 10:53:47 -0400

    fixing compile issues

--- hapi-fhir-storage-test-utilities/src/test/java/ca/uhn/fhir/storage/test/BaseTransactionProcessorTest.java
+++ hapi-fhir-storage-test-utilities/src/test/java/ca/uhn/fhir/storage/test/BaseTransactionProcessorTest.java
@@ -27,6 +27,7 @@ import org.hl7.fhir.r4.model.IdType;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BaseTransactionProcessorTest {
 
