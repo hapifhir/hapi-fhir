51d941ed76b8b3fab76cfd5327a603880f3b716c
Ken Stevens <khstevens@gmail.com>
2024-06-04 10:06:31 -0400

    fix test

--- hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/narrative/DefaultThymeleafNarrativeGeneratorR4Test.java
+++ hapi-fhir-structures-r4/src/test/java/ca/uhn/fhir/narrative/DefaultThymeleafNarrativeGeneratorR4Test.java
@@ -146,7 +146,7 @@ public class DefaultThymeleafNarrativeGeneratorR4Test {
 		String output = value.getText().getDiv().getValueAsString();
 
 		ourLog.info(output);
-		assertThat(output).contains("<div class=\"hapiHeaderText\"> Untitled Diagnostic Report </div>");
+		assertThat(output).contains("<div class=\"hapiHeaderText\"> Some &amp; Diagnostic Report </div>");
 
 	}
 
@@ -210,7 +210,7 @@ public class DefaultThymeleafNarrativeGeneratorR4Test {
 		String output = value.getText().getDiv().getValueAsString();
 
 		ourLog.info(output);
-		assertThat(output).contains("<div class=\"hapiHeaderText\"> Some &amp; Diagnostic Report </div>");
+		assertThat(output).contains("<div class=\"hapiHeaderText\"> Untitled Diagnostic Report </div>");
 
 	}
 
