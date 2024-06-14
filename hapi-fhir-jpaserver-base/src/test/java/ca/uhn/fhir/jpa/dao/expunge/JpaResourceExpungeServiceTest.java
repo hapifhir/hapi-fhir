c429977e43c015a3dad94ba99f919e3a02315ecb
Tadgh <garygrantgraham@gmail.com>
2024-06-14 08:34:39 -0700

    bump checkstyle deps

--- hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/expunge/JpaResourceExpungeServiceTest.java
+++ hapi-fhir-jpaserver-base/src/test/java/ca/uhn/fhir/jpa/dao/expunge/JpaResourceExpungeServiceTest.java
@@ -51,7 +51,7 @@ public class JpaResourceExpungeServiceTest {
 	public void testExpungeDoesNotDeleteAllSearchParams() {
 		when(myResourceTableDao.findById(any())).thenReturn(Optional.of(resourceTable));
 		when(resourceTable.getIdDt()).thenReturn(new IdDt());
-
+		//FIXME GGG
 		myService.expungeCurrentVersionOfResource(myRequestDetails, 1L, new AtomicInteger(1));
 
 		verify(myService, never()).deleteAllSearchParams(any());
