87fab18123f76db2d43866fe8630d0c762f0b5b2
Tadgh <garygrantgraham@gmail.com>
2024-06-15 18:15:55 -0700

    6007 scheduler id problems with multiple instances (#6012)
    * Modify cluster instance name
    
    * spotless
    
    * Add test for scheduler name
    
    * Changelog
    
    * Remove atomic int for autogenerating IDs
    
    * Remove atomic int for autogenerating IDs
    
    * Fix test

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/sched/BaseHapiSchedulerTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/sched/BaseHapiSchedulerTest.java
@@ -3,7 +3,9 @@ package ca.uhn.fhir.jpa.sched;
 import ca.uhn.fhir.i18n.Msg;
 import org.junit.jupiter.api.Test;
 import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -19,6 +21,30 @@ public class BaseHapiSchedulerTest {
 		} catch (SchedulerException e) {
 			assertEquals(Msg.code(1633) + "java.lang.NullPointerException: No instance name supplied", e.getMessage());
 		}
+	}
+
+	@Test
+	public void testSchedulersShareTheSameServiceName() throws SchedulerException {
+
+		String instanceName = "local-scheduler";
+		String instanceID = "NON_CLUSTERED";
+
+		BaseHapiScheduler firstScheduler = new BaseHapiScheduler("hello", new AutowiringSpringBeanJobFactory()) {
+		};
+		firstScheduler.setInstanceName(instanceName);
+
+		BaseHapiScheduler secondScheduler = new BaseHapiScheduler("hello", new AutowiringSpringBeanJobFactory()) {
+		};
+		secondScheduler.setInstanceName(instanceName);
+
+		firstScheduler.init();
+		secondScheduler.init();
+
+		assertThat(firstScheduler.getPropertiesForUnitTest()).containsEntry(StdSchedulerFactory.PROP_SCHED_INSTANCE_NAME, instanceName);
+		assertThat(secondScheduler.getPropertiesForUnitTest()).containsEntry(StdSchedulerFactory.PROP_SCHED_INSTANCE_NAME, instanceName);
+
+		assertThat(firstScheduler.getPropertiesForUnitTest()).containsEntry(StdSchedulerFactory.PROP_SCHED_INSTANCE_ID, instanceID);
+		assertThat(secondScheduler.getPropertiesForUnitTest()).containsEntry(StdSchedulerFactory.PROP_SCHED_INSTANCE_ID, instanceID);
 	}
 
 
