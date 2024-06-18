5a6dc2cb0e8868fe6a39249d00bef5bec1a24564
aditya_dave <aditya@smiledigitalhealth.com>
2024-06-18 11:35:30 -0400

    spotless

--- hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/json/ICdsHooksExtension.java
+++ hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/json/ICdsHooksExtension.java
@@ -3,5 +3,4 @@ package ca.uhn.hapi.fhir.cdshooks.api.json;
 import ca.uhn.fhir.model.api.IModelJson;
 
 // FIXME AD javadoc
-public interface ICdsHooksExtension extends IModelJson {
-}
+public interface ICdsHooksExtension extends IModelJson {}
