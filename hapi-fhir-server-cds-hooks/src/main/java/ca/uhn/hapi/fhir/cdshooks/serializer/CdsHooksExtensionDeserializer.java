3a2c19da73a587ca711db38105ba26bec67ff7f3
ad1306 <adave.dev@gmail.com>
2024-06-25 14:31:22 -0400

    get hook in deserializer

--- hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/serializer/CdsHooksExtensionDeserializer.java
+++ hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/serializer/CdsHooksExtensionDeserializer.java
@@ -1,15 +1,14 @@
 package ca.uhn.hapi.fhir.cdshooks.serializer;
 
 import ca.uhn.hapi.fhir.cdshooks.api.json.CdsHooksExtension;
+import ca.uhn.hapi.fhir.cdshooks.api.json.CdsServicesJson;
 import ca.uhn.hapi.fhir.cdshooks.svc.CdsServiceRegistryImpl;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.io.IOException;
 
@@ -17,7 +16,6 @@ public class CdsHooksExtensionDeserializer extends StdDeserializer<CdsHooksExten
 
 	private final CdsServiceRegistryImpl myCdsServiceRegistry;
 	private final ObjectMapper myObjectMapper;
-
 	public CdsHooksExtensionDeserializer(CdsServiceRegistryImpl theCdsServiceRegistry, ObjectMapper theObjectMapper) {
 		super(CdsHooksExtension.class);
 		myCdsServiceRegistry = theCdsServiceRegistry;
@@ -27,17 +25,9 @@ public class CdsHooksExtensionDeserializer extends StdDeserializer<CdsHooksExten
 	@Override
 	public CdsHooksExtension deserialize(JsonParser theJsonParser, DeserializationContext theDeserializationContext)
 			throws IOException {
-		//		CdsServiceJson cdsServiceJson = myCdsServiceRegistry.getCdsService();
-		//		cdsServiceJson.
-		//		new ObjectMapper().readValue(theJsonParser.getCodec().readTree(theJsonParser).toString(),
-		// MyRequestExtension.class)
-		JsonNode rootNode = theJsonParser.getCodec().readTree(theJsonParser);
-		/**
-		 * getParent(hook)
-		 */
-		HttpServletRequest request =
-				((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		final String serviceId = request.getRequestURI().replace("/cds-services/", "");
+		JsonNode extensionNode = theJsonParser.getCodec().readTree(theJsonParser);
+		TreeNode hookNode = theJsonParser.getCodec().readTree(theJsonParser).get("hook");
+		CdsServicesJson cdsServicesJson = myCdsServiceRegistry.getCdsServicesJson();
 		return new CdsHooksExtension();
 	}
 }
