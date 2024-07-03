453e9eb70b6625b233541d5e93d64ae2e0af8c5e
ad1306 <adave.dev@gmail.com>
2024-07-03 11:38:30 -0400

    enable tests for CdsHooksContextBooterTest

--- hapi-fhir-server-cds-hooks/src/test/java/ca/uhn/hapi/fhir/cdshooks/svc/CdsHooksContextBooterTest.java
+++ hapi-fhir-server-cds-hooks/src/test/java/ca/uhn/hapi/fhir/cdshooks/svc/CdsHooksContextBooterTest.java
@@ -1,6 +1,13 @@
 package ca.uhn.hapi.fhir.cdshooks.svc;
 
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
+import ca.uhn.hapi.fhir.cdshooks.api.json.CdsHooksExtension;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CdsHooksContextBooterTest {
 
@@ -10,37 +17,40 @@ class CdsHooksContextBooterTest {
 	void setUp() {
 		myFixture = new CdsHooksContextBooter();
 	}
-// TODO enable them @Adi
-//	@Test
-//	void validateJsonReturnsNullWhenInputIsEmptyString() {
-//		// execute
-//		final JsonNode actual = myFixture.serializeExtensions("");
-//		// validate
-//		assertThat(actual).isNull();
-//	}
-//
-//	@Test
-//	void validateJsonThrowsExceptionWhenInputIsInvalid() {
-//		// setup
-//		final String expected = "HAPI-2378: Invalid JSON: Unrecognized token 'abc': was expecting (JSON String, Number, Array, Object or token 'null', 'true' or 'false')\n" +
-//			" at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION` disabled); line: 1, column: 4]";
-//		// execute & validate
-//		assertThatThrownBy(
-//			() -> myFixture.serializeExtensions("abc"))
-//			.isInstanceOf(UnprocessableEntityException.class)
-//			.hasMessage(expected);
-//	}
-//
-//	@Test
-//	void validateJsonReturnsInputWhenInputIsValidJsonString() {
-//		// setup
-//		final String input = "{\n      \"com.example.timestamp\": \"2017-11-27T22:13:25Z\",\n      \"myextension-practitionerspecialty\" : \"gastroenterology\"\n   }";
-//		// execute
-//		final JsonNode actual = myFixture.serializeExtensions(input);
-//		// validate
-//		assertThat(actual.get("com.example.timestamp").asText()).isEqualTo("2017-11-27T22:13:25Z");
-//		assertThat(actual.get("myextension-practitionerspecialty").asText()).isEqualTo("gastroenterology");
-//	}
+	@Test
+	void serializeExtensionsReturnsNullWhenInputIsEmptyString() {
+		// execute
+		final CdsHooksExtension actual = myFixture.serializeExtensions("", CustomExtension.class);
+		// validate
+		assertThat(actual).isNull();
+	}
 
+	@Test
+	void serializeExtensionsThrowsExceptionWhenInputIsInvalid() {
+		// setup
+		final String expected = "HAPI-2378: Invalid JSON: Unrecognized token 'abc': was expecting (JSON String, Number, Array, Object or token 'null', 'true' or 'false')\n" +
+			" at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION` disabled); line: 1, column: 4]";
+		// execute & validate
+		assertThatThrownBy(
+			() -> myFixture.serializeExtensions("abc", CustomExtension.class))
+			.isInstanceOf(UnprocessableEntityException.class)
+			.hasMessage(expected);
+	}
+
+	@Test
+	void serializeExtensionsReturnsInputWhenInputIsValidJsonString() {
+		// setup
+		final String input = "{\n\"example-property\": \"some-value\" }";
+		// execute
+		final CustomExtension actual = (CustomExtension) myFixture.serializeExtensions(input, CustomExtension.class);
+		// validate
+		assertThat(actual.myExampleProperty).isEqualTo("some-value");
+	}
+
+
+	private static class CustomExtension extends CdsHooksExtension {
+		@JsonProperty("example-property")
+		String myExampleProperty;
+	}
 
 }
