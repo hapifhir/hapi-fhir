9ed37402c06a3c2087439d40c6c064711570ed1b
Aditya Dave <adave.dev@gmail.com>
2024-07-08 10:34:07 -0400

    Update hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/json/CdsHooksExtension.java
    Co-authored-by: Ken Stevens <khstevens@gmail.com>

--- hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/json/CdsHooksExtension.java
+++ hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/json/CdsHooksExtension.java
@@ -3,7 +3,7 @@ package ca.uhn.hapi.fhir.cdshooks.api.json;
 import ca.uhn.fhir.model.api.IModelJson;
 
 /**
- * This class is empty on purpose as CDS hooks need to support arbitrary extensions.
+ * Users can define CDS Hooks extensions by extending this class.
  * Implementors can extend this class for defining their custom extensions.
  */
 public class CdsHooksExtension implements IModelJson {}
