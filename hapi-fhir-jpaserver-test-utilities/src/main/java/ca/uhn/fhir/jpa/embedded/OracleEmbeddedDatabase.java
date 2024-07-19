a3203fd2046812ed858e5c32dfd812716ddf2c19
Michael Buckley <michaelabuckley@gmail.com>
2024-07-19 09:29:40 -0400

    Fix stray hack testing

--- hapi-fhir-jpaserver-test-utilities/src/main/java/ca/uhn/fhir/jpa/embedded/OracleEmbeddedDatabase.java
+++ hapi-fhir-jpaserver-test-utilities/src/main/java/ca/uhn/fhir/jpa/embedded/OracleEmbeddedDatabase.java
@@ -40,14 +40,7 @@ public class OracleEmbeddedDatabase extends JpaEmbeddedDatabase {
 	private final OracleContainer myContainer;
 
 	public OracleEmbeddedDatabase() {
-		// myContainer = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart").withPrivilegedMode(true);
-		DockerImageName armImage = DockerImageName.parse("smile/oracle/database:19.3.0-ee")
-				.asCompatibleSubstituteFor("gvenzl/oracle-xe:21-slim-faststart");
-		myContainer = new OracleContainer(armImage)
-				.withPrivilegedMode(true)
-				.withDatabaseName("ORCLPDB1")
-				.withUsername("cdr")
-				.withPassword("SmileCDR");
+		myContainer = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart").withPrivilegedMode(true);
 
 		myContainer.start();
 		super.initialize(
