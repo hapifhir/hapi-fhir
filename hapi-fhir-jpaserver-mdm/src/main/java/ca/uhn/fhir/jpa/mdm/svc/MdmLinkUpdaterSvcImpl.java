5449162acbaf7b2ca1b75c99576148bc161613c3
Jake Gillberg <jgillberg@galileo.io>
2024-09-16 15:06:19 -0500

    add side-effect link updates to link event

--- hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/svc/MdmLinkUpdaterSvcImpl.java
+++ hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/svc/MdmLinkUpdaterSvcImpl.java
@@ -188,6 +188,12 @@ public class MdmLinkUpdaterSvcImpl implements IMdmLinkUpdaterSvc {
 			// pointcut for MDM_POST_UPDATE_LINK
 			MdmLinkEvent event = new MdmLinkEvent();
 			event.addMdmLink(myModelConverter.toJson(mdmLink));
+
+			// add any link updates from side effects
+			mdmContext.getMdmLinks().stream().forEach(link -> {
+				event.addMdmLink(myModelConverter.toJson(link));
+			});
+
 			HookParams hookParams = new HookParams();
 			hookParams.add(RequestDetails.class, theParams.getRequestDetails()).add(MdmLinkEvent.class, event);
 			myInterceptorBroadcaster.callHooks(Pointcut.MDM_POST_UPDATE_LINK, hookParams);
