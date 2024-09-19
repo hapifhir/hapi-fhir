fb93c3d601272a146904e438c11af93f32e70f1e
Luke deGruchy <luke.degruchy@smilecdr.com>
2024-09-19 13:59:58 -0400

    Expose hapi-fhir-storage-cr config to downstream modules for testing (#6272)
    * Ensure IRepositoryFactory returns a Repository, and not a HapiFhirRepository.
    
    * Rename test so that it will run from Maven and the pipeline since it didn't before.  Fix small bug that made the test error out.
    
    * Move TestCrConfig and TestCrR4Config to main source folder.  Extract TestHapiFhirCrPartitionConfig as separate test config to be used only in the test source folder.  Add Maven compile dependency to hapi-fhir-jpaserver-base in order to ensure that moved Config classes will compile.
    
    * Add possible code change to RequestDetailsClone to clone partition info.
    
    * Introduce separate factory interface for Repository to preserve backward compatibility.
    
    * Remove IRepositoryFactoryForInterface.
    
    * Spotless
    
    * Add javadoc.
    
    * Get rid of TODO.
    
    * Restore RepositoryConfig to master.
    
    * Move test config to different packages.
    
    * Leverage use of new RepositoryFactoryForRepositoryInterface for CrR4Config and RepositoryFactoryForRepositoryInterface.
    
    * Ensure CrR4Config and RepositoryConfig make use of new RepositoryFactoryForRepositoryInterface.
    
    * Add copyright header
    
    * Add @FunctionalInterface to factory.

--- hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/config/test/TestCrConfig.java
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/config/test/TestCrConfig.java
@@ -1,4 +1,23 @@
-package ca.uhn.fhir.cr;
+/*-
+ * #%L
+ * HAPI FHIR - Clinical Reasoning
+ * %%
+ * Copyright (C) 2014 - 2024 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package ca.uhn.fhir.cr.config.test;
 
 import ca.uhn.fhir.batch2.jobs.reindex.ReindexProvider;
 import ca.uhn.fhir.context.FhirContext;
@@ -47,6 +66,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
+/**
+ * Common hapi-fhir clinical reasoning config shared with downstream modules.
+ */
 @Configuration
 @Import({SubscriptionSubmitterConfig.class, SubscriptionChannelConfig.class})
 public class TestCrConfig {
@@ -77,7 +99,9 @@ public class TestCrConfig {
 	}
 	@Bean
 	public TestCqlProperties testCqlProperties(){
-		return new TestCqlProperties();}
+		return new TestCqlProperties();
+	}
+
 	@Bean
 	public JpaStorageSettings storageSettings() {
 		JpaStorageSettings storageSettings = new JpaStorageSettings();
@@ -87,12 +111,6 @@ public class TestCrConfig {
 		storageSettings.setResourceClientIdStrategy(JpaStorageSettings.ClientIdStrategyEnum.ANY);
 		return storageSettings;
 	}
-
-	@Bean
-	public PartitionHelper partitionHelper() {
-		return new PartitionHelper();
-	}
-
 	@Bean
 	public ModelManager modelManager(Map<ModelIdentifier, Model> theGlobalModelCache) {
 		return new ModelManager(theGlobalModelCache);
