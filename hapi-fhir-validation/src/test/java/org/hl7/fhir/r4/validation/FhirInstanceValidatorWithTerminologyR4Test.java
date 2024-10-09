76d2564a146dedb77d479934773d23f4af99f230
Martha <marthamitran@gmail.com>
2024-10-09 16:37:39 -0700

    Add support for response parameter issues for validate-code operation. Implement workaround for missing error for invalid codes by adding an issue when it is absent.

---
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/FhirInstanceValidatorWithTerminologyR4Test.java
@@ -0,0 +1,9 @@
+package org.hl7.fhir.r4.validation;
+
+import ca.uhn.fhir.context.support.IValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.CachingValidationSupport;
+
+public class FhirInstanceValidatorWithTerminologyR4Test {
+	private CachingValidationSupport myValidationSupport;
+	private IValidationSupport myMockSupport;
+}
