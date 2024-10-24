ac22baae7abf4e5d0ce606291436e9822f6566c0
James Agnew <jamesagnew@gmail.com>
2024-10-24 15:56:49 -0400

    Fix test

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/reindex/ReindexTaskTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/reindex/ReindexTaskTest.java
@@ -12,6 +12,8 @@ import ca.uhn.fhir.jpa.api.config.JpaStorageSettings;
 import ca.uhn.fhir.jpa.api.dao.ReindexParameters;
 import ca.uhn.fhir.jpa.api.model.DaoMethodOutcome;
 import ca.uhn.fhir.jpa.batch.models.Batch2JobStartResponse;
+import ca.uhn.fhir.jpa.model.dao.JpaPid;
+import ca.uhn.fhir.jpa.model.entity.ResourceHistoryProvenanceEntity;
 import ca.uhn.fhir.jpa.model.entity.ResourceHistoryTable;
 import ca.uhn.fhir.jpa.model.entity.ResourceIndexedComboStringUnique;
 import ca.uhn.fhir.jpa.model.entity.ResourceIndexedComboTokenNonUnique;
@@ -22,7 +24,6 @@ import ca.uhn.fhir.jpa.test.PatientReindexTestHelper;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.ResourceGoneException;
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.Query;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
@@ -67,8 +68,10 @@ public class ReindexTaskTest extends BaseJpaR4Test {
 	@AfterEach
 	public void after() {
 		myInterceptorRegistry.unregisterAllAnonymousInterceptors();
-		myStorageSettings.setStoreMetaSourceInformation(new JpaStorageSettings().getStoreMetaSourceInformation());
-		myStorageSettings.setPreserveRequestIdInResourceBody(new JpaStorageSettings().isPreserveRequestIdInResourceBody());
+		JpaStorageSettings defaults = new JpaStorageSettings();
+		myStorageSettings.setStoreMetaSourceInformation(defaults.getStoreMetaSourceInformation());
+		myStorageSettings.setPreserveRequestIdInResourceBody(defaults.isPreserveRequestIdInResourceBody());
+		myStorageSettings.setAccessMetaSourceInformationFromProvenanceTable(defaults.isAccessMetaSourceInformationFromProvenanceTable());
 	}
 
 	@Test
@@ -184,24 +187,34 @@ public class ReindexTaskTest extends BaseJpaR4Test {
 	public void testOptimizeStorage_AllVersions_CopyProvenanceEntityData() {
 		// Setup
 		myStorageSettings.setStoreMetaSourceInformation(JpaStorageSettings.StoreMetaSourceInformationEnum.SOURCE_URI_AND_REQUEST_ID);
+		myStorageSettings.setAccessMetaSourceInformationFromProvenanceTable(true);
 		myStorageSettings.setPreserveRequestIdInResourceBody(true);
 
 		for (int i = 0; i < 10; i++) {
 			Patient p = new Patient();
 			p.setId("PATIENT" + i);
-			p.getMeta().setSource("http://foo#bar");
 			p.addIdentifier().setValue(String.valueOf(i));
 			myPatientDao.update(p, mySrd);
 
-			p.addIdentifier().setSystem("http://blah");
+			p.setActive(true);
 			myPatientDao.update(p, mySrd);
 		}
 
 		runInTransaction(()->{
+			List<ResourceHistoryTable> versions = myResourceHistoryTableDao.findAll();
+			for (var version : versions) {
+				ResourceHistoryProvenanceEntity provenance = new ResourceHistoryProvenanceEntity();
+				provenance.setResourceTable(version.getResourceTable());
+				provenance.setResourceHistoryTable(version);
+				provenance.setSourceUri("http://foo");
+				provenance.setRequestId("bar");
+				myResourceHistoryProvenanceDao.save(provenance);
+			}
+		});
+
+		runInTransaction(()->{
 			assertEquals(20, myResourceHistoryTableDao.count());
 			assertEquals(20, myResourceHistoryProvenanceDao.count());
-			Query query = myEntityManager.createQuery("UPDATE " + ResourceHistoryTable.class.getSimpleName() + " p SET p.mySourceUri = NULL, p.myRequestId = NULL");
-			assertEquals(20, query.executeUpdate());
 		});
 
 		runInTransaction(()-> {
@@ -228,6 +241,7 @@ public class ReindexTaskTest extends BaseJpaR4Test {
 
 		// validate
 		runInTransaction(()-> {
+			assertEquals(0, myResourceHistoryProvenanceDao.count());
 			for (var next : myResourceHistoryProvenanceDao.findAll()) {
 				assertEquals("bar", next.getRequestId());
 				assertEquals("http://foo", next.getSourceUri());
@@ -338,7 +352,7 @@ public class ReindexTaskTest extends BaseJpaR4Test {
 		IIdType obsId = myReindexTestHelper.createObservationWithAlleleExtension(Observation.ObservationStatus.FINAL);
 
 		runInTransaction(() -> {
-			int entriesInSpIndexTokenTable = myResourceIndexedSearchParamTokenDao.countForResourceId(obsId.getIdPartAsLong());
+			int entriesInSpIndexTokenTable = myResourceIndexedSearchParamTokenDao.countForResourceId(JpaPid.fromId(obsId.getIdPartAsLong()));
 			assertEquals(1, entriesInSpIndexTokenTable);
 
 			// simulate resource deletion
@@ -363,7 +377,7 @@ public class ReindexTaskTest extends BaseJpaR4Test {
 
 		// then
 		runInTransaction(() -> {
-			int entriesInSpIndexTokenTablePostReindexing = myResourceIndexedSearchParamTokenDao.countForResourceId(obsId.getIdPartAsLong());
+			int entriesInSpIndexTokenTablePostReindexing = myResourceIndexedSearchParamTokenDao.countForResourceId(JpaPid.fromId(obsId.getIdPartAsLong()));
 			assertEquals(0, entriesInSpIndexTokenTablePostReindexing);
 		});
 	}
