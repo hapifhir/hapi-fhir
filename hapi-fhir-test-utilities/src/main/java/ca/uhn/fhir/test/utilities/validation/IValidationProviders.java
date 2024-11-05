fca3ea444593175b5187525ec33cbb56142d1383
Martha Mitran <martha.mitran@smiledigitalhealth.com>
2024-11-05 09:13:44 -0800

    Fixes for the translation of parameter issues as part of the output for $validate-code operation. (#6423)
    * Move the validation providers to the test utilities package such that they can be reused.
    
    * Update IValidationSupport.CodeValidationIssue structure such that it meets the FHIR specification. Update RemoteTerminologyServiceValidationSupport and VersionSpecificWorkerContextWrapper translation for issues.
    
    * New tests for issue translation. Move test class to a different package so that we can add another test class.
    
    * Some simplification in the issue API to simplify building of issues.
    
    * Fix compilation errors.
    
    * Update providers to allow multiple responses and add  support for the fetchCodeSystem call through method find.
    
    * Setup the first test for resource validation with remote terminology providers.
    
    * Fix NullPointerException
    
    * avoid calling validateCode for CodeSystem where system is null
    
    * Keep old public API methods (and class name) in IValidationSupport and mark them as deprecated to avoid breaking dependencies.
    
    * Revert local change to debug for duplicate errors.
    
    * Add more test cases for resource validation. Throw exception to signal missing test setup to make it obvious.
    
    * Simplify test setup.
    
    * Add some more javadoc
    
    * Add javadoc for the new test class
    
    * Add more tests
    
    * Address code review comments in IValidationSupport.
    
    * Add changelog

---
+++ hapi-fhir-test-utilities/src/main/java/ca/uhn/fhir/test/utilities/validation/IValidationProviders.java
@@ -0,0 +1,104 @@
+package ca.uhn.fhir.test.utilities.validation;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.rest.annotation.RequiredParam;
+import ca.uhn.fhir.rest.annotation.Search;
+import ca.uhn.fhir.rest.param.UriParam;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.util.ClasspathUtil;
+import org.hl7.fhir.instance.model.api.IBaseParameters;
+import org.hl7.fhir.instance.model.api.IDomainResource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface IValidationProviders {
+	String CODE_SYSTEM = "http://code.system/url";
+	String CODE_SYSTEM_VERSION = "1.0.0";
+	String CODE_SYSTEM_NAME = "Test Code System";
+	String CODE = "CODE";
+	String VALUE_SET_URL = "http://value.set/url";
+	String DISPLAY = "Explanation for code TestCode.";
+	String LANGUAGE = "en";
+	String ERROR_MESSAGE = "This is an error message";
+
+	interface IMyValidationProvider extends IResourceProvider {
+		void addException(String theOperation, String theUrl, String theCode, Exception theException);
+		<P extends IBaseParameters> void addTerminologyResponse(String theOperation, String theUrl, String theCode, P theReturnParams);
+		IBaseParameters addTerminologyResponse(String theOperation, String theUrl, String theCode, FhirContext theFhirContext, String theTerminologyResponseFile);
+	}
+
+	abstract class MyValidationProvider<T extends IDomainResource> implements IMyValidationProvider {
+		private final Map<String, Exception> myExceptionMap = new HashMap<>();
+		private boolean myShouldThrowExceptionForResourceNotFound = true;
+		private final Map<String, IBaseParameters> myTerminologyResponseMap = new HashMap<>();
+		private final Map<String, T> myTerminologyResourceMap = new HashMap<>();
+
+		static String getInputKey(String theOperation, String theUrl, String theCode) {
+			return theOperation + "-" + theUrl + "#" + theCode;
+		}
+
+		public void setShouldThrowExceptionForResourceNotFound(boolean theShouldThrowExceptionForResourceNotFound) {
+			myShouldThrowExceptionForResourceNotFound = theShouldThrowExceptionForResourceNotFound;
+		}
+
+		public void addException(String theOperation, String theUrl, String theCode, Exception theException) {
+			String inputKey = getInputKey(theOperation, theUrl, theCode);
+			myExceptionMap.put(inputKey, theException);
+		}
+
+		abstract Class<? extends IBaseParameters> getParameterType();
+
+		@Override
+		public <P extends IBaseParameters> void addTerminologyResponse(String theOperation, String theUrl, String theCode, P theReturnParams) {
+			myTerminologyResponseMap.put(getInputKey(theOperation, theUrl, theCode), theReturnParams);
+		}
+
+		public IBaseParameters addTerminologyResponse(String theOperation, String theUrl, String theCode, FhirContext theFhirContext, String theTerminologyResponseFile) {
+			IBaseParameters responseParams = ClasspathUtil.loadResource(theFhirContext, getParameterType(), theTerminologyResponseFile);
+			addTerminologyResponse(theOperation, theUrl, theCode, responseParams);
+			return responseParams;
+		}
+
+		protected void addTerminologyResource(String theUrl, T theResource) {
+			myTerminologyResourceMap.put(theUrl, theResource);
+		}
+
+		public abstract T addTerminologyResource(String theUrl);
+
+		protected IBaseParameters getTerminologyResponse(String theOperation, String theUrl, String theCode) throws Exception {
+			String inputKey = getInputKey(theOperation, theUrl, theCode);
+			if (myExceptionMap.containsKey(inputKey)) {
+				throw myExceptionMap.get(inputKey);
+			}
+			IBaseParameters params = myTerminologyResponseMap.get(inputKey);
+			if (params == null) {
+				throw new IllegalStateException("Test setup incomplete. Missing return params for " + inputKey);
+			}
+			return params;
+		}
+
+		protected T getTerminologyResource(UriParam theUrlParam) {
+			if (theUrlParam.isEmpty()) {
+				throw new IllegalStateException("CodeSystem url should not be null.");
+			}
+			String urlValue = theUrlParam.getValue();
+			if (!myTerminologyResourceMap.containsKey(urlValue) && myShouldThrowExceptionForResourceNotFound) {
+				throw new IllegalStateException("Test setup incomplete. CodeSystem not found " + urlValue);
+			}
+			return myTerminologyResourceMap.get(urlValue);
+		}
+
+		@Search
+		public List<T> find(@RequiredParam(name = "url") UriParam theUrlParam) {
+			T resource = getTerminologyResource(theUrlParam);
+			return resource != null ? List.of(resource) : List.of();
+		}
+	}
+
+	interface IMyLookupCodeProvider extends IResourceProvider {
+		void setLookupCodeResult(IValidationSupport.LookupCodeResult theLookupCodeResult);
+	}
+}
