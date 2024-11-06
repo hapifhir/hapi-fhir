c3b09754550a3391e94d9e537fe9f95bf813fcab
Michael Buckley <michaelabuckley@gmail.com>
2024-11-06 17:29:10 -0500

    Android jdk 8 limitation for hapi-base.

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/adapters/AdapterManager.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/adapters/AdapterManager.java
@@ -5,6 +5,7 @@ import jakarta.annotation.Nonnull;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class AdapterManager implements IAdapterManager {
 	public static final AdapterManager INSTANCE = new AdapterManager();
@@ -20,7 +21,15 @@ public class AdapterManager implements IAdapterManager {
 		// todo this can be sped up with a cache of type->Factory.
 		return myAdapterFactories.stream()
 				.filter(nextFactory -> nextFactory.getAdapters().stream().anyMatch(theTargetType::isAssignableFrom))
-				.flatMap(nextFactory -> nextFactory.getAdapter(theObject, theTargetType).stream())
+				.flatMap(nextFactory -> {
+					var adapter = nextFactory.getAdapter(theObject, theTargetType);
+					// can't use Optional.stream() because of our Android target is API level 26/JDK 8.
+					if (adapter.isPresent()) {
+						return Stream.of(adapter.get());
+					} else {
+						return Stream.empty();
+					}
+				})
 				.findFirst();
 	}
 
