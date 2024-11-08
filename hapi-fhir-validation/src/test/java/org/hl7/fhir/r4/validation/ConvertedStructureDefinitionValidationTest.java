2af096ae2e0c2ced4a324d383560fae6e47ce53b
James Agnew <jamesagnew@gmail.com>
2024-11-08 15:06:20 -0500

    Add verification

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ConvertedStructureDefinitionValidationTest.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ConvertedStructureDefinitionValidationTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * See #6424
  */
@@ -26,8 +28,9 @@ public class ConvertedStructureDefinitionValidationTest extends BaseTest {
 	private final FhirContext myCtx = FhirContext.forR4Cached();
 
 	@Test
-	public void test() throws Exception {
+	public void testInvalidateCacheThenValidate() throws Exception {
 
+		// Create a validation support that loads the patient-citizenship extension
 		PrePopulatedValidationSupport val = new PrePopulatedValidationSupport(myCtx);
 		val.addStructureDefinition(loadResource(myCtx, StructureDefinition.class, "r4/extension-patient-citizenship.json"));
 
@@ -39,6 +42,7 @@ public class ConvertedStructureDefinitionValidationTest extends BaseTest {
 		FhirValidator validator = myCtx.newValidator();
 		validator.registerValidatorModule(instanceValidator);
 
+		// Create a patient with an invalid citizenship extension
 		Patient patient = new Patient();
 		patient.setActive(true);
 
@@ -46,13 +50,20 @@ public class ConvertedStructureDefinitionValidationTest extends BaseTest {
 		citizenship.setUrl("http://hl7.org/fhir/StructureDefinition/patient-citizenship");
 		citizenship.addExtension("period", new StringType("NOT A PERIOD"));
 
+		// Validate once
 		ValidationResult outcome = validator.validateWithResult(patient);
 		ourLog.info(myCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(outcome.toOperationOutcome()));
+		assertEquals("The Profile 'http://hl7.org/fhir/StructureDefinition/patient-citizenship|5.1.0' definition allows for the type Period but found type string", outcome.getMessages().get(0).getMessage());
+		assertEquals(1, outcome.getMessages().size());
 
+		// Invalidate the caches
 		instanceValidator.invalidateCaches();
 
+		// Validate a second time
 		outcome = validator.validateWithResult(patient);
 		ourLog.info(myCtx.newJsonParser().setPrettyPrint(true).encodeResourceToString(outcome.toOperationOutcome()));
+		assertEquals("The Profile 'http://hl7.org/fhir/StructureDefinition/patient-citizenship|5.1.0' definition allows for the type Period but found type string", outcome.getMessages().get(0).getMessage());
+		assertEquals(1, outcome.getMessages().size());
 
 	}
 
