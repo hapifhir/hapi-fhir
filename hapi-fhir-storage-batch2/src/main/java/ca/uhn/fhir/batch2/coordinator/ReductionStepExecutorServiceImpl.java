b1349aaaa2ffa1945ecc2610c1bc289695e7da45
Emre Dincturk <emre.dincturk@smilecdr.com>
2024-11-13 14:49:57 -0500

    disabled heartbeat for try if it will break anything

--- hapi-fhir-storage-batch2/src/main/java/ca/uhn/fhir/batch2/coordinator/ReductionStepExecutorServiceImpl.java
+++ hapi-fhir-storage-batch2/src/main/java/ca/uhn/fhir/batch2/coordinator/ReductionStepExecutorServiceImpl.java
@@ -103,11 +103,11 @@ public class ReductionStepExecutorServiceImpl implements IReductionStepExecutorS
 
 	@EventListener(ContextRefreshedEvent.class)
 	public void start() {
-		if (myHeartbeatTimer == null) {
+/*		if (myHeartbeatTimer == null) {
 			myHeartbeatTimer = new Timer("batch2-reducer-heartbeat");
 			myHeartbeatTimer.schedule(
 					new HeartbeatTimerTask(), DateUtils.MILLIS_PER_MINUTE, DateUtils.MILLIS_PER_MINUTE);
-		}
+		}*/
 	}
 
 	private void runHeartbeat() {
