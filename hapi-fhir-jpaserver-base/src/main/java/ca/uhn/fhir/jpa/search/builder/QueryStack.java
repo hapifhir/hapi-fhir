2bfb51b32fd459de9d474272f844a11f1a11d269
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-11-22 09:52:21 -0500

    remove warnings

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/search/builder/QueryStack.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/search/builder/QueryStack.java
@@ -210,7 +210,7 @@ public class QueryStack {
 				CoordsPredicateBuilder coordsBuilder = (CoordsPredicateBuilder) builder;
 
 				List<List<IQueryParameterType>> params = theParams.get(theParamName);
-				if (params.size() > 0 && params.get(0).size() > 0) {
+				if (!params.isEmpty() && !params.get(0).isEmpty()) {
 					IQueryParameterType param = params.get(0).get(0);
 					ParsedLocationParam location = ParsedLocationParam.from(theParams, param);
 					double latitudeValue = location.getLatitudeValue();
