f9d92354db19ad25f953323fc70e5e77177cface
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-11-22 10:40:30 -0500

    cleanup imports

--- hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/r4/HapiFhirRepositoryR4Test.java
+++ hapi-fhir-storage-cr/src/test/java/ca/uhn/fhir/cr/r4/HapiFhirRepositoryR4Test.java
@@ -7,10 +7,8 @@ import ca.uhn.fhir.rest.param.InternalCodingDt;
 import ca.uhn.fhir.rest.param.NumberParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import ca.uhn.fhir.rest.param.TokenParam;
-import ca.uhn.fhir.rest.param.UriParam;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.IdType;
@@ -18,7 +16,6 @@ import org.hl7.fhir.r4.model.Immunization;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.opencds.cqf.fhir.utility.search.Searches;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -64,7 +61,7 @@ public class HapiFhirRepositoryR4Test extends BaseCrR4TestServer {
 		var repository = new HapiFhirRepository(myDaoRegistry, setupRequestDetails(), myRestfulServer);
 		Map<String, List<IQueryParameterType>> map = new HashMap<>();
 		map.put("_profile", Collections.singletonList(new ReferenceParam("test")));
-		assertDoesNotThrow(() -> repository.search(Bundle.class, Observation.class, map));
+		assertDoesNotThrow(() -> repository.search(Bundle.class, Encounter.class, map));
 	}
 
 
