11f968dccf0f1bf0e63ef2cc511e95d3400f6227
mangala.ekanayake <Mangala.Ekanayake@cambio.lk>
2024-11-28 13:43:43 +0530

    [6463] Fix test failures

--- hapi-fhir-jpaserver-test-r5/src/test/java/ca/uhn/fhir/jpa/provider/r5/ResourceProviderR5Test.java
+++ hapi-fhir-jpaserver-test-r5/src/test/java/ca/uhn/fhir/jpa/provider/r5/ResourceProviderR5Test.java
@@ -284,8 +284,7 @@ public class ResourceProviderR5Test extends BaseResourceProviderR5Test {
 						    "lastUpdated": "2024-03-27T19:20:25.200+00:00",
 						    "source": "#384dd6bccaeafa6c"
 						  },
-						  "url": "https://health.gov.on.ca/idms/fhir/SearchParameter/MedicinalProductDefinition-SearchableString",
-						  "version": "1.0.0",
+						  "url": "https://health.gov.on.ca/idms/fhir/SearchParameter/MedicinalProductDefinition-SearchableString",						  
 						  "name": "MedicinalProductDefinitionSearchableString",
 						  "status": "active",
 						  "publisher": "MOH-IDMS",
