057da7f6a8f0944477e6c6c693ecf695eacb2da1
mangala.ekanayake <Mangala.Ekanayake@cambio.lk>
2024-12-02 11:12:04 +0530

    [6463] merge conflicts

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/JpaPersistedResourceValidationSupportTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/JpaPersistedResourceValidationSupportTest.java
@@ -26,16 +26,10 @@ import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
-import ca.uhn.fhir.jpa.term.api.ITermReadSvc;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.param.UriParam;
-import ca.uhn.fhir.sl.cache.Cache;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.CodeSystem;
 import org.hl7.fhir.r4.model.StructureDefinition;
-import org.hl7.fhir.r4.model.ValueSet;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -45,20 +39,11 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import java.util.function.Function;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.hl7.fhir.common.hapi.validation.support.ValidationConstants.LOINC_LOW;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -66,67 +51,6 @@ import static org.mockito.Mockito.when;
 class JpaPersistedResourceValidationSupportTest {
 
 	private FhirContext theFhirContext = FhirContext.forR4();
-
-	@Mock private ITermReadSvc myTermReadSvc;
-	@Mock private DaoRegistry myDaoRegistry;
-	@Mock private Cache<String, IBaseResource> myLoadCache;
-	@Mock private IFhirResourceDao<ValueSet> myValueSetResourceDao;
-
-	@InjectMocks
-	private IValidationSupport testedClass =
-		new JpaPersistedResourceValidationSupport(theFhirContext);
-
-	private Class<? extends IBaseResource> myCodeSystemType = CodeSystem.class;
-	private Class<? extends IBaseResource> myValueSetType = ValueSet.class;
-
-
-	@BeforeEach
-	public void setup() {
-		ReflectionTestUtils.setField(testedClass, "myValueSetType", myValueSetType);
-	}
-
-
-	@Nested
-	public class FetchCodeSystemTests {
-
-		@Test
-		void fetchCodeSystemMustUseForcedId() {
-			testedClass.fetchCodeSystem("string-containing-loinc");
-
-			verify(myTermReadSvc, times(1)).readCodeSystemByForcedId(LOINC_LOW);
-			verify(myLoadCache, never()).get(anyString(), isA(Function.class));
-		}
-
-
-		@Test
-		void fetchCodeSystemMustNotUseForcedId() {
-			testedClass.fetchCodeSystem("string-not-containing-l-o-i-n-c");
-
-			verify(myTermReadSvc, never()).readCodeSystemByForcedId(LOINC_LOW);
-			verify(myLoadCache, times(1)).get(anyString(), isA(Function.class));
-		}
-
-	}
-
-
-	@Nested
-	public class FetchValueSetTests {
-
-		@Test
-		void fetchValueSetMustUseForcedId() {
-			final String valueSetId = "string-containing-loinc";
-			assertNull(testedClass.fetchValueSet(valueSetId));
-		}
-
-
-		@Test
-		void fetchValueSetMustNotUseForcedId() {
-			testedClass.fetchValueSet("string-not-containing-l-o-i-n-c");
-
-			verify(myLoadCache, times(1)).get(anyString(), isA(Function.class));
-		}
-
-	}
 
 	@Nested
 	class FetchStructureDefinitionTests {
@@ -139,6 +63,7 @@ class JpaPersistedResourceValidationSupportTest {
 
 		@Captor
 		ArgumentCaptor<SearchParameterMap> searchParameterMapCaptor;
+
 		@Test
 		@DisplayName("fetch StructureDefinition by version less url")
 		void fetchStructureDefinitionForUrl() {
