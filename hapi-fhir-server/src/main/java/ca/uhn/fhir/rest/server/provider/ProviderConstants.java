9bfdbea1ef250320ec2c0db30f644c70d10e5bbc
jmarchionatto <60409882+jmarchionatto@users.noreply.github.com>
2024-12-04 14:27:27 -0500

    Add replace-references operation (#6526)
    * Rehoming operation
    
    * Rename operation
    
    * Recover state before moving function to smile
    Batch job artifacts are very preliminary
    
    * Rename rehome to replace_references
    
    * Rename rehome to replace_references
    
    * spotless
    
    * Add message code
    
    * Fix op name
    
    * Implement plain provider
    
    * Add msg code
    
    * Fix parameter types fhir version compatibility
    
    * spotless
    
    * Deduplicate code
    
    * Update hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/JpaSystemProvider.java
    
    Implement suggestion
    
    Co-authored-by: Ken Stevens <khstevens@gmail.com>
    
    * Implement review request: move constant
    
    * Change operation interface to use parameters for both source and target reference ids
    
    * spotless
    
    * Add revision requested todo
    
    * spotless
    
    ---------
    
    Co-authored-by: juan.marchionatto <juan.marchionatto@smilecdr.com>
    Co-authored-by: Ken Stevens <khstevens@gmail.com>

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ProviderConstants.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/provider/ProviderConstants.java
@@ -239,8 +239,24 @@ public class ProviderConstants {
 	 * Operation name for the "$export-poll-status" operation
 	 */
 	public static final String OPERATION_EXPORT_POLL_STATUS = "$export-poll-status";
+
 	/**
 	 * Operation name for the "$export" operation
 	 */
 	public static final String OPERATION_EXPORT = "$export";
+
+	/**
+	 * Operation name for the "$replace-references" operation
+	 */
+	public static final String OPERATION_REPLACE_REFERENCES = "$replace-references";
+
+	/**
+	 * Parameter for source reference of the "$replace-references" operation
+	 */
+	public static final String PARAM_SOURCE_REFERENCE_ID = "sourceReferenceId";
+
+	/**
+	 * Parameter for target reference of the "$replace-references" operation
+	 */
+	public static final String PARAM_TARGET_REFERENCE_ID = "targetReferenceId";
 }
