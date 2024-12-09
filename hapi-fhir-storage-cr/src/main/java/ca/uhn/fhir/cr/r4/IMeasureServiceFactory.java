68e8dc69b7eb5249262f5a175bf407afea700bb4
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2024-12-09 13:02:00 -0500

    Have IMeasureServiceFactory return R4MeasureEvaluatorSingle.

--- hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/r4/IMeasureServiceFactory.java
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/r4/IMeasureServiceFactory.java
@@ -20,9 +20,10 @@
 package ca.uhn.fhir.cr.r4;
 
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import org.opencds.cqf.fhir.cr.measure.r4.R4MeasureEvaluatorSingle;
 import org.opencds.cqf.fhir.cr.measure.r4.R4MeasureService;
 
 @FunctionalInterface
 public interface IMeasureServiceFactory {
-	R4MeasureService create(RequestDetails theRequestDetails);
+	R4MeasureEvaluatorSingle create(RequestDetails theRequestDetails);
 }
