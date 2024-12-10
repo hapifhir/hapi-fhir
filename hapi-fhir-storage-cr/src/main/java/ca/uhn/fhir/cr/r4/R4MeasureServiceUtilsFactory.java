ed50cb6b1d9e3bef49eabbaca6e15d82c7aaab3e
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2024-12-10 09:26:31 -0500

    Add javadoc.

--- hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/r4/R4MeasureServiceUtilsFactory.java
+++ hapi-fhir-storage-cr/src/main/java/ca/uhn/fhir/cr/r4/R4MeasureServiceUtilsFactory.java
@@ -22,7 +22,9 @@ package ca.uhn.fhir.cr.r4;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import org.opencds.cqf.fhir.cr.measure.r4.utils.R4MeasureServiceUtils;
 
-// LUKETODO: javadoc
+/**
+ * Factory to create an {@link R4MeasureServiceUtils} from a {@link RequestDetails}
+ */
 @FunctionalInterface
 public interface R4MeasureServiceUtilsFactory {
 	R4MeasureServiceUtils create(RequestDetails requestDetails);
