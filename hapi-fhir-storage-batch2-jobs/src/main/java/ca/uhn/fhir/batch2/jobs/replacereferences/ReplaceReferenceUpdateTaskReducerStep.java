40b8d75795460efd8ba89b16283f144c2eb3486c
Ken Stevens <ken@smiledigitalhealth.com>
2024-12-20 12:04:46 -0500

    review feedback

--- hapi-fhir-storage-batch2-jobs/src/main/java/ca/uhn/fhir/batch2/jobs/replacereferences/ReplaceReferenceUpdateTaskReducerStep.java
+++ hapi-fhir-storage-batch2-jobs/src/main/java/ca/uhn/fhir/batch2/jobs/replacereferences/ReplaceReferenceUpdateTaskReducerStep.java
@@ -28,6 +28,7 @@ import ca.uhn.fhir.batch2.api.StepExecutionDetails;
 import ca.uhn.fhir.batch2.model.ChunkOutcome;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 import jakarta.annotation.Nonnull;
 import org.hl7.fhir.r4.model.Bundle;
@@ -44,11 +45,13 @@ public class ReplaceReferenceUpdateTaskReducerStep<PT extends ReplaceReferencesJ
 
 	protected final FhirContext myFhirContext;
 	protected final DaoRegistry myDaoRegistry;
+	private final IFhirResourceDao<Task> myTaskDao;
 
 	private List<Bundle> myPatchOutputBundles = new ArrayList<>();
 
 	public ReplaceReferenceUpdateTaskReducerStep(DaoRegistry theDaoRegistry) {
 		myDaoRegistry = theDaoRegistry;
+		myTaskDao = myDaoRegistry.getResourceDao(Task.class);
 		myFhirContext = theDaoRegistry.getFhirContext();
 	}
 
@@ -69,35 +72,39 @@ public class ReplaceReferenceUpdateTaskReducerStep<PT extends ReplaceReferencesJ
 			@Nonnull IJobDataSink<ReplaceReferenceResultsJson> theDataSink)
 			throws JobExecutionFailedException {
 
-		ReplaceReferencesJobParameters params = theStepExecutionDetails.getParameters();
-		SystemRequestDetails requestDetails = SystemRequestDetails.forRequestPartitionId(params.getPartitionId());
-		Task task =
-				myDaoRegistry.getResourceDao(Task.class).read(params.getTaskId().asIdDt(), requestDetails);
+		try {
+			ReplaceReferencesJobParameters params = theStepExecutionDetails.getParameters();
+			SystemRequestDetails requestDetails = SystemRequestDetails.forRequestPartitionId(params.getPartitionId());
+			Task task =
+				myTaskDao.read(params.getTaskId().asIdDt(), requestDetails);
 
-		task.setStatus(Task.TaskStatus.COMPLETED);
-		// TODO KHS this Task will probably be too large for large jobs. Revisit this model once we support Provenance
-		// resources.
-		myPatchOutputBundles.forEach(outputBundle -> {
-			Task.TaskOutputComponent output = task.addOutput();
-			Coding coding = output.getType().getCodingFirstRep();
-			coding.setSystem(RESOURCE_TYPES_SYSTEM);
-			coding.setCode("Bundle");
-			Reference outputBundleReference =
+			task.setStatus(Task.TaskStatus.COMPLETED);
+			// TODO KHS this Task will probably be too large for large jobs. Revisit this model once we support Provenance
+			// resources.
+			myPatchOutputBundles.forEach(outputBundle -> {
+				Task.TaskOutputComponent output = task.addOutput();
+				Coding coding = output.getType().getCodingFirstRep();
+				coding.setSystem(RESOURCE_TYPES_SYSTEM);
+				coding.setCode("Bundle");
+				Reference outputBundleReference =
 					new Reference("#" + outputBundle.getIdElement().getIdPart());
-			output.setValue(outputBundleReference);
-			task.addContained(outputBundle);
-		});
+				output.setValue(outputBundleReference);
+				task.addContained(outputBundle);
+			});
 
-		myDaoRegistry.getResourceDao(Task.class).update(task, requestDetails);
+			myTaskDao.update(task, requestDetails);
 
-		ReplaceReferenceResultsJson result = new ReplaceReferenceResultsJson();
-		result.setTaskId(params.getTaskId());
-		theDataSink.accept(result);
+			ReplaceReferenceResultsJson result = new ReplaceReferenceResultsJson();
+			result.setTaskId(params.getTaskId());
+			theDataSink.accept(result);
 
-		// Reusing the same reducer for all jobs feels confusing and dangerous to me. We need to fix this.
-		// See https://github.com/hapifhir/hapi-fhir/pull/6551
-		myPatchOutputBundles.clear();
-
-		return new RunOutcome(myPatchOutputBundles.size());
+			return new RunOutcome(myPatchOutputBundles.size());
+		} finally {
+			// Reusing the same reducer for all jobs feels confusing and dangerous to me. We need to fix this.
+			// See https://github.com/hapifhir/hapi-fhir/pull/6551
+			// TODO KHS add new methods to the api called init() and cleanup() that are called by the api so we can move
+			// this finally block out
+			myPatchOutputBundles.clear();
+		}
 	}
 }
