97660a3b8f466eff3f25e6182a609d6c90351e58
leif stawnyczy <leifstawnyczy@leifs-mbp.home>
2025-01-09 17:13:27 -0500

    failing test for invalid profile during validation

--- hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ParserWithValidationR4Test.java
+++ hapi-fhir-validation/src/test/java/org/hl7/fhir/r4/validation/ParserWithValidationR4Test.java
@@ -4,14 +4,21 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.context.support.IValidationSupport;
 import ca.uhn.fhir.fhirpath.BaseValidationTestWithInlineMocks;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.ResultSeverityEnum;
+import ca.uhn.fhir.validation.ValidationOptions;
 import ca.uhn.fhir.validation.ValidationResult;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 import org.hl7.fhir.r4.model.MedicationRequest;
+import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserWithValidationR4Test extends BaseValidationTestWithInlineMocks {
 	private static final FhirContext ourCtx = FhirContext.forR4();
@@ -29,6 +36,44 @@ public class ParserWithValidationR4Test extends BaseValidationTestWithInlineMock
 		ValidationResult validationResult = validator.validateWithResult(med_req);
 		validationResult.getMessages().forEach(System.out::println);
 	}
+
+	// https://www.hl7.org/fhir/r4/resource-operation-validate.html
+	@Test
+	public void validate_withUnknownProfile_shouldFail() {
+		// setup
+		IParser parser = ourCtx.newJsonParser();
+		Patient patient;
+		{
+			String patientStr = """
+    			{
+    				"resourceType": "Patient",
+    				"name": [{
+    					"family": "Hirasawa",
+    					"given": [ "yui" ]
+    				}]
+    			}
+				""";
+			patient = parser.parseResource(Patient.class, patientStr);
+		}
+
+		// test
+		ValidationOptions options = new ValidationOptions();
+		options.addProfile("http://tempuri.org/does-not-exist");
+
+		final FhirInstanceValidator instanceValidator = new FhirInstanceValidator(ourCtx);
+		FhirValidator validator = ourCtx.newValidator();
+
+		// test
+		validator.registerValidatorModule(instanceValidator);
+		ValidationResult validationResult = validator.validateWithResult(patient, options);
+
+		// verify
+		assertFalse(validationResult.isSuccessful());
+		assertTrue(validationResult.getMessages().stream()
+			.anyMatch(r -> r.getSeverity() == ResultSeverityEnum.ERROR));
+		// TODO - add validation for the specific error to be thrown
+	}
+
 
 	private IValidationSupport getValidationSupport() {
 		return new ValidationSupportChain(new DefaultProfileValidationSupport(ourCtx));
