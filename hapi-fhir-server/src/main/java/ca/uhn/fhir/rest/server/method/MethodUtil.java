b823a1efa85bb9c1f128c32d9ab0ce849c9220a2
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-01-26 16:34:57 -0500

    Spotless.

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/method/MethodUtil.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/method/MethodUtil.java
@@ -173,10 +173,9 @@ public class MethodUtil {
 						final Operation op = theMethod.getAnnotation(Operation.class);
 
 						if (op == null) {
-							final String error = String.format("%s@OperationParam or @EmbeddedOperationParams detected on method: [%s] that is not annotated with @Operation: %s",
-								Msg.code(404),
-								theMethod.getName(),
-								theMethod.toGenericString());
+							final String error = String.format(
+									"%s@OperationParam or @EmbeddedOperationParams detected on method: [%s] that is not annotated with @Operation: %s",
+									Msg.code(404), theMethod.getName(), theMethod.toGenericString());
 							throw new ConfigurationException(error);
 						}
 
