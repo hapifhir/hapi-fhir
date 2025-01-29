040220130d12d7495027cdb34a162cfa25c2191d
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-01-29 13:20:29 -0500

    Add javadoc to Header annotation and introduce a new Headers annotation.

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Headers.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Headers.java
@@ -3,11 +3,13 @@ package ca.uhn.fhir.rest.annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+import com.google.common.collect.Multimap;
+
 /**
  * This annotation may be placed on a operation provider method parameter or embedded parameter class to indicate that
- * it should be populated with the value of an REST header.
+ * it should be populated with a {@link Multimap} containing all headers names and values in the request.
  */
 @Target(value = {ElementType.PARAMETER})
-public @interface Header {
+public @interface Headers {
 	String value();
 }
