30cf4b41b081888566168584215152586ca258f3
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-01-29 14:45:08 -0500

    Clarify javadoc.

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Header.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Header.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Target;
 
 /**
  * This annotation may be placed on a operation provider method parameter or embedded parameter class to indicate that
- * it should be populated with the value of an REST header.
+ * it should be populated with the value of an REST header, which this is a single or multiple values.
  */
 @Target(value = {ElementType.PARAMETER})
 public @interface Header {
