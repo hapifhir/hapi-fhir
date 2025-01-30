e4bd979ef64a524488c40a2ce18124fb6a3d1b0d
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-01-30 10:59:46 -0500

    Add retention runtime to the header annotation.

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Header.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/rest/annotation/Header.java
@@ -20,12 +20,15 @@
 package ca.uhn.fhir.rest.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * This annotation may be placed on a operation provider method parameter or embedded parameter class to indicate that
  * it should be populated with the value of an REST header, which this is a single or multiple values.
  */
+@Retention(RetentionPolicy.RUNTIME)
 @Target(value = {ElementType.PARAMETER})
 public @interface Header {
 	String value();
