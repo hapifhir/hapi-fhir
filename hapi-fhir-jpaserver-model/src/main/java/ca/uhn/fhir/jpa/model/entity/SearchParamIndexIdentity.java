91240e05b67513e7fd57c6d5c180c78bc88b2fd7
volodymyr <volodymyr.korzh@smilecdr.com>
2025-02-13 17:07:11 -0700

    Normalize sp_name and res_type columns from hfj_spidx_* tables - SearchParameterIndexIdentity added

---
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/SearchParamIndexIdentity.java
@@ -0,0 +1,54 @@
+/*-
+ * #%L
+ * HAPI FHIR JPA Model
+ * %%
+ * Copyright (C) 2014 - 2025 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package ca.uhn.fhir.jpa.model.entity;
+
+import ca.uhn.fhir.rest.api.Constants;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+
+import static ca.uhn.fhir.jpa.model.entity.BaseResourceIndexedSearchParam.MAX_SP_NAME;
+
+@Entity
+@Table(name = "HFJ_SPIDX_IDENTITY")
+public class SearchParamIndexIdentity {
+
+	@Id
+	@SequenceGenerator(name = "SEQ_SPIDX_IDENTITY", sequenceName = "SEQ_SPIDX_IDENTITY")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_SPIDX_IDENTITY")
+	@Column(name = "SP_IDENTITY_ID")
+	private Integer mySpIdentityId;
+
+	@Column(name = "HASH_IDENTITY")
+	private Long myHashIdentity;
+
+	@FullTextField
+	@Column(name = "RES_TYPE", length = Constants.MAX_RESOURCE_NAME_LENGTH)
+	private String myResourceType;
+
+	@FullTextField
+	@Column(name = "SP_NAME", length = MAX_SP_NAME)
+	private String myParamName;
+}
