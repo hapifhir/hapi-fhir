b3b34ae77a06203aac992de2795762ce629dcdb6
James Agnew <jamesagnew@gmail.com>
2025-03-06 14:34:56 -0800

    Call presearch pointcut for conditional URL evaluation

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/search/builder/StorageInterceptorHooksFacade.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/search/builder/StorageInterceptorHooksFacade.java
@@ -23,7 +23,6 @@ import ca.uhn.fhir.interceptor.api.HookParams;
 import ca.uhn.fhir.interceptor.api.IInterceptorBroadcaster;
 import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.interceptor.model.RequestPartitionId;
-import ca.uhn.fhir.jpa.entity.Search;
 import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
@@ -45,19 +44,19 @@ public class StorageInterceptorHooksFacade {
 	 *
 	 * @param theRequestDetails
 	 * @param theParams
-	 * @param search
+	 * @param theSearch
 	 * @param theRequestPartitionId
 	 */
 	public void callStoragePresearchRegistered(
 			RequestDetails theRequestDetails,
 			SearchParameterMap theParams,
-			Search search,
+			ICachedSearchDetails theSearch,
 			RequestPartitionId theRequestPartitionId) {
 		IInterceptorBroadcaster compositeBroadcaster =
 				CompositeInterceptorBroadcaster.newCompositeBroadcaster(myInterceptorBroadcaster, theRequestDetails);
 		if (compositeBroadcaster.hasHooks(Pointcut.STORAGE_PRESEARCH_REGISTERED)) {
 			HookParams params = new HookParams()
-					.add(ICachedSearchDetails.class, search)
+					.add(ICachedSearchDetails.class, theSearch)
 					.add(RequestDetails.class, theRequestDetails)
 					.addIfMatchesType(ServletRequestDetails.class, theRequestDetails)
 					.add(SearchParameterMap.class, theParams)
