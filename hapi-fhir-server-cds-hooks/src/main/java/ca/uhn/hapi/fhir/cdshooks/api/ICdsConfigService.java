71b302be5322481f9ab5492fb1a1c39781fcd319
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-03-06 15:12:04 -0500

    Experiment:  hapi-fhir-cds-hooks without CR.

--- hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/ICdsConfigService.java
+++ hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/api/ICdsConfigService.java
@@ -20,17 +20,18 @@
 package ca.uhn.hapi.fhir.cdshooks.api;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.cr.common.IRepositoryFactory;
+import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 import ca.uhn.fhir.rest.api.server.SystemRestfulResponse;
 import ca.uhn.fhir.rest.server.RestfulServer;
-import ca.uhn.hapi.fhir.cdshooks.svc.cr.CdsCrSettings;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
-import org.opencds.cqf.fhir.utility.Ids;
+import org.hl7.fhir.instance.model.api.IIdType;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public interface ICdsConfigService {
 	@Nonnull
@@ -39,16 +40,8 @@ public interface ICdsConfigService {
 	@Nonnull
 	ObjectMapper getObjectMapper();
 
-	@Nonnull
-	CdsCrSettings getCdsCrSettings();
-
 	@Nullable
 	default DaoRegistry getDaoRegistry() {
-		return null;
-	}
-
-	@Nullable
-	default IRepositoryFactory getRepositoryFactory() {
 		return null;
 	}
 
@@ -61,7 +54,41 @@ public interface ICdsConfigService {
 		SystemRequestDetails rd = new SystemRequestDetails();
 		rd.setServer(getRestfulServer());
 		rd.setResponse(new SystemRestfulResponse(rd));
-		rd.setId(Ids.newId(theFhirContext.getVersion().getVersion(), theResourceType, theId));
+		rd.setId(newId(theFhirContext.getVersion().getVersion(), theResourceType, theId));
 		return rd;
+	}
+
+	private static <IdType extends IIdType> IdType newId(
+		FhirVersionEnum fhirVersionEnum, String resourceType, String idPart) {
+		checkNotNull(fhirVersionEnum);
+		checkNotNull(resourceType);
+		checkNotNull(idPart);
+
+		return newId(fhirVersionEnum, resourceType + "/" + idPart);
+	}
+
+	private static <IdType extends IIdType> IdType newId(FhirVersionEnum fhirVersionEnum, String id) {
+		checkNotNull(fhirVersionEnum);
+		checkNotNull(id);
+
+		switch (fhirVersionEnum) {
+			case DSTU2:
+				return (IdType) new ca.uhn.fhir.model.primitive.IdDt(id);
+			case DSTU2_1:
+				return (IdType) new org.hl7.fhir.dstu2016may.model.IdType(id);
+			case DSTU2_HL7ORG:
+				return (IdType) new org.hl7.fhir.dstu2.model.IdType(id);
+			case DSTU3:
+				return (IdType) new org.hl7.fhir.dstu3.model.IdType(id);
+			case R4:
+				return (IdType) new org.hl7.fhir.r4.model.IdType(id);
+			case R4B:
+				return (IdType) new org.hl7.fhir.r4b.model.IdType(id);
+			case R5:
+				return (IdType) new org.hl7.fhir.r5.model.IdType(id);
+			default:
+				throw new IllegalArgumentException(String.format(
+					"newId does not support FHIR version %s", fhirVersionEnum.getFhirVersionString()));
+		}
 	}
 }
