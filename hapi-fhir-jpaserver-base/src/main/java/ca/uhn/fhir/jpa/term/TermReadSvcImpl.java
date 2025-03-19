0f145487ece41b97b8af89aa0a611914e6b2bcc4
peartree <etienne.poirier@smilecdr.com>
2025-03-19 10:29:53 -0400

    spotless

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermReadSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermReadSvcImpl.java
@@ -1205,7 +1205,8 @@ public class TermReadSvcImpl implements ITermReadSvc, IHasScheduledJobs {
 								// the valueSet concept.display property was populated and may be different from the
 								// codeSystem.concept.display.  Since 'concept' is a managed object, we need to tell the
 								// entityManager to ignore any changes to it before assigning the custom display value.
-								// If we don't, the modification will propagate to persistence during the flush() operation
+								// If we don't, the modification will propagate to persistence during the flush()
+								// operation
 								// effectively corrupting the codeSystem.
 								myEntityManager.detach(concept);
 								concept.setDisplay(theIncludeConcept.getDisplay());
