eeea56f11ca58394d8ab14d9ee0eb7bf5580f169
jdar <justin.dar@smiledigitalhealth.com>
2025-03-25 16:41:22 -0700

    remove unused import

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/FhirDefaultPolicyAdvisor.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/FhirDefaultPolicyAdvisor.java
@@ -1,7 +1,5 @@
 package org.hl7.fhir.common.hapi.validation.validator;
 
-import java.util.Arrays;
-
 import org.hl7.fhir.r5.elementmodel.Element;
 import org.hl7.fhir.r5.model.ElementDefinition;
 import org.hl7.fhir.r5.model.StructureDefinition;
