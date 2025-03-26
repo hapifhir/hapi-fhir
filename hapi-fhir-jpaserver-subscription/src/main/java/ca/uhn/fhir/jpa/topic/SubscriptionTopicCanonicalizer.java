0fc22fa87d2364758f8c398ca8479bfdfe55e928
Ken Stevens <ken@smiledigitalhealth.com>
2025-03-26 16:43:21 -0400

    test passes

--- hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicCanonicalizer.java
+++ hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicCanonicalizer.java
@@ -21,17 +21,33 @@ package ca.uhn.fhir.jpa.topic;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.i18n.Msg;
+import ca.uhn.fhir.subscription.SubscriptionConstants;
 import org.hl7.fhir.convertors.factory.VersionConvertorFactory_43_50;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Basic;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r5.model.DateTimeType;
+import org.hl7.fhir.r5.model.Enumerations.PublicationStatus;
 import org.hl7.fhir.r5.model.SubscriptionTopic;
+import org.hl7.fhir.r5.model.SubscriptionTopic.InteractionTrigger;
+import org.hl7.fhir.r5.model.SubscriptionTopic.SubscriptionTopicCanFilterByComponent;
+import org.hl7.fhir.r5.model.SubscriptionTopic.SubscriptionTopicNotificationShapeComponent;
+import org.hl7.fhir.r5.model.SubscriptionTopic.SubscriptionTopicResourceTriggerComponent;
 
 public final class SubscriptionTopicCanonicalizer {
 	private static final FhirContext ourFhirContextR5 = FhirContext.forR5();
-
+	
 	private SubscriptionTopicCanonicalizer() {}
 
 	public static SubscriptionTopic canonicalizeTopic(FhirContext theFhirContext, IBaseResource theSubscriptionTopic) {
 		switch (theFhirContext.getVersion().getVersion()) {
+			case R4:
+				if (theSubscriptionTopic instanceof Basic) {
+					return canonicalizeR4BasicTopic((Basic) theSubscriptionTopic);
+				}
+				throw new UnsupportedOperationException(
+						Msg.code(2337) + "Unsupported R4 resource type for subscription topic: " + 
+						theSubscriptionTopic.getClass().getSimpleName());
 			case R4B:
 				return (SubscriptionTopic) VersionConvertorFactory_43_50.convertResource(
 						(org.hl7.fhir.r4b.model.SubscriptionTopic) theSubscriptionTopic);
@@ -42,5 +58,106 @@ public final class SubscriptionTopicCanonicalizer {
 						Msg.code(2337) + "Subscription topics are not supported in FHIR version "
 								+ theFhirContext.getVersion().getVersion());
 		}
+	}
+	
+	private static SubscriptionTopic canonicalizeR4BasicTopic(Basic theBasicTopic) {
+		SubscriptionTopic retVal = new SubscriptionTopic();
+		
+		// Basic properties
+		retVal.setId(theBasicTopic.getIdElement().getIdPart());
+		
+		// Extract regular extensions
+		for (Extension extension : theBasicTopic.getExtension()) {
+			String url = extension.getUrl();
+			
+			if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_URL.equals(url)) {
+				retVal.setUrl(extension.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_VERSION.equals(url)) {
+				retVal.setVersion(extension.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_NAME.equals(url)) {
+				retVal.setName(extension.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_TITLE.equals(url)) {
+				retVal.setTitle(extension.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_DATE.equals(url)) {
+				retVal.setDateElement(new DateTimeType(extension.getValue().primitiveValue()));
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_DESCRIPTION.equals(url)) {
+				retVal.setDescription(extension.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_RESOURCE_TRIGGER.equals(url)) {
+				processResourceTrigger(extension, retVal);
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_CAN_FILTER_BY.equals(url)) {
+				processCanFilterBy(extension, retVal);
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_NOTIFICATION_SHAPE.equals(url)) {
+				processNotificationShape(extension, retVal);
+			}
+		}
+		
+		// Extract modifier extensions (e.g., status)
+		for (Extension extension : theBasicTopic.getModifierExtension()) {
+			String url = extension.getUrl();
+			
+			if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_TOPIC_STATUS.equals(url)) {
+				String statusCode = extension.getValue().primitiveValue();
+				retVal.setStatus(PublicationStatus.fromCode(statusCode));
+			}
+		}
+		
+		return retVal;
+	}
+	
+	private static void processResourceTrigger(Extension theExtension, SubscriptionTopic theTopic) {
+		SubscriptionTopicResourceTriggerComponent trigger = new SubscriptionTopicResourceTriggerComponent();
+		
+		for (Extension ext : theExtension.getExtension()) {
+			String url = ext.getUrl();
+			
+			if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_DESCRIPTION.equals(url)) {
+				trigger.setDescription(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_RESOURCE.equals(url)) {
+				trigger.setResource(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_SUPPORTED_INTERACTION.equals(url)) {
+				trigger.addSupportedInteraction(
+						InteractionTrigger.fromCode(ext.getValue().primitiveValue()));
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_FHIRPATH_CRITERIA.equals(url)) {
+				trigger.setFhirPathCriteria(ext.getValue().primitiveValue());
+			}
+		}
+		
+		theTopic.addResourceTrigger(trigger);
+	}
+	
+	private static void processCanFilterBy(Extension theExtension, SubscriptionTopic theTopic) {
+		SubscriptionTopicCanFilterByComponent filterBy = new SubscriptionTopicCanFilterByComponent();
+		
+		for (Extension ext : theExtension.getExtension()) {
+			String url = ext.getUrl();
+			
+			if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_DESCRIPTION.equals(url)) {
+				filterBy.setDescription(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_RESOURCE.equals(url)) {
+				filterBy.setResource(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_FILTER_PARAMETER.equals(url)) {
+				filterBy.setFilterParameter(ext.getValue().primitiveValue());
+			}
+		}
+		
+		theTopic.addCanFilterBy(filterBy);
+	}
+	
+	private static void processNotificationShape(Extension theExtension, SubscriptionTopic theTopic) {
+		SubscriptionTopicNotificationShapeComponent shape = new SubscriptionTopicNotificationShapeComponent();
+		
+		for (Extension ext : theExtension.getExtension()) {
+			String url = ext.getUrl();
+			
+			if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_RESOURCE.equals(url)) {
+				shape.setResource(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_INCLUDE.equals(url)) {
+				shape.addInclude(ext.getValue().primitiveValue());
+			} else if (SubscriptionConstants.SUBSCRIPTION_TOPIC_R4_EXT_REVINCLUDE.equals(url)) {
+				shape.addRevInclude(ext.getValue().primitiveValue());
+			}
+		}
+		
+		theTopic.addNotificationShape(shape);
 	}
 }
--- hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicCanonicalizerTest.java
+++ hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicCanonicalizerTest.java
@@ -2,8 +2,6 @@ package ca.uhn.fhir.jpa.topic;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.util.ClasspathUtil;
-import ca.uhn.fhir.util.ResourceUtil;
-import ca.uhn.fhir.util.StreamUtil;
 import org.hl7.fhir.r5.model.Enumerations.PublicationStatus;
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +24,43 @@ class SubscriptionTopicCanonicalizerTest {
 		FhirContext fhirContext = FhirContext.forR4Cached();
 		org.hl7.fhir.r4.model.Basic topic = fhirContext.newJsonParser().parseResource(org.hl7.fhir.r4.model.Basic.class, basicResourceString);
 		org.hl7.fhir.r5.model.SubscriptionTopic canonicalized = SubscriptionTopicCanonicalizer.canonicalizeTopic(fhirContext, topic);
+		
+		// Basic properties
 		assertEquals("r4-encounter-complete", canonicalized.getId());
 		assertEquals(PublicationStatus.DRAFT, canonicalized.getStatus());
+		assertEquals("http://hl7.org/fhir/uv/subscriptions-backport/SubscriptionTopic/r4-encounter-complete", canonicalized.getUrl());
+		assertEquals("1.2.0", canonicalized.getVersion());
+		assertEquals("R4 example of a basic-converted subscription topic for completed encounters.", canonicalized.getName());
+		assertEquals("Backported SubscriptionTopic: R4 Encounter Complete", canonicalized.getTitle());
+		assertEquals("2019-01-01", canonicalized.getDateElement().getValueAsString());
+		assertEquals("R4 example of a subscription topic for completed encounters.", canonicalized.getDescription());
+		
+		// Resource Trigger
+		assertEquals(1, canonicalized.getResourceTrigger().size());
+		assertEquals("Triggered when an encounter is completed.", canonicalized.getResourceTrigger().get(0).getDescription());
+		assertEquals("http://hl7.org/fhir/StructureDefinition/Encounter", canonicalized.getResourceTrigger().get(0).getResource());
+		assertEquals(2, canonicalized.getResourceTrigger().get(0).getSupportedInteraction().size());
+		assertEquals("create", canonicalized.getResourceTrigger().get(0).getSupportedInteraction().get(0).getValue().toCode());
+		assertEquals("update", canonicalized.getResourceTrigger().get(0).getSupportedInteraction().get(1).getValue().toCode());
+		assertEquals("(%previous.id.empty() or (%previous.status != 'finished')) and (%current.status = 'finished')", 
+			canonicalized.getResourceTrigger().get(0).getFhirPathCriteria());
+		
+		// Can Filter By
+		assertEquals(2, canonicalized.getCanFilterBy().size());
+		assertEquals("Filter based on the subject of an encounter.", canonicalized.getCanFilterBy().get(0).getDescription());
+		assertEquals("Encounter", canonicalized.getCanFilterBy().get(0).getResource());
+		assertEquals("subject", canonicalized.getCanFilterBy().get(0).getFilterParameter());
+		
+		assertEquals("Filter based on the group membership of the subject of an encounter.", canonicalized.getCanFilterBy().get(1).getDescription());
+		assertEquals("Encounter", canonicalized.getCanFilterBy().get(1).getResource());
+		assertEquals("_in", canonicalized.getCanFilterBy().get(1).getFilterParameter());
+		
+		// Notification Shape
+		assertEquals(1, canonicalized.getNotificationShape().size());
+		assertEquals("Encounter", canonicalized.getNotificationShape().get(0).getResource());
+		assertEquals(7, canonicalized.getNotificationShape().get(0).getInclude().size());
+		assertEquals("Encounter:patient&iterate=Patient.link", canonicalized.getNotificationShape().get(0).getInclude().get(0).getValue());
+		assertEquals(1, canonicalized.getNotificationShape().get(0).getRevInclude().size());
+		assertEquals("Encounter:subject", canonicalized.getNotificationShape().get(0).getRevInclude().get(0).getValue());
 	}
 }
