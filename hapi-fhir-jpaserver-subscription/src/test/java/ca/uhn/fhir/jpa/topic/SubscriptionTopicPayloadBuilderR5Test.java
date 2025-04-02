556d016422ff17448da26816cff0632acc249483
Ken Stevens <khstevens@gmail.com>
2025-04-02 11:15:48 -0400

    r4 topic subscription (#6829)
    * begin with failing test
    
    * test passes
    
    * start roughing out R4 SubscriptionTopic registry
    
    * start building topic test
    
    * start building topic test. add r4 builder to help
    
    * basic scenario appears to be working now
    
    * include and revinclude failing test
    
    * include and revinclude support added
    
    * fix test regression
    
    * clean up ISubscriptionTopicLoader
    
    * sonar
    
    * sonar
    
    * review cleanup
    
    * review cleanup
    
    * spotless
    
    * fix api
    
    * fix api
    
    * fix test
    
    * consolidate code duplication
    
    * spotless
    
    * spotless
    
    * need Basic searchparams for R4 SubscriptionTopic registry
    
    * review feedback
    
    * review feedback
    
    * review feedback
    
    * Update hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicPayloadBuilder.java
    
    Co-authored-by: James Agnew <jamesagnew@gmail.com>
    
    * Update hapi-fhir-jpaserver-subscription/src/main/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicPayloadBuilder.java
    
    Co-authored-by: James Agnew <jamesagnew@gmail.com>
    
    * review feedback
    
    * spotless
    
    * bump version
    
    ---------
    
    Co-authored-by: Ken Stevens <ken@smiledigitalhealth.com>
    Co-authored-by: James Agnew <jamesagnew@gmail.com>

--- hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicPayloadBuilderR5Test.java
+++ hapi-fhir-jpaserver-subscription/src/test/java/ca/uhn/fhir/jpa/topic/SubscriptionTopicPayloadBuilderR5Test.java
@@ -34,7 +34,7 @@ class SubscriptionTopicPayloadBuilderR5Test {
 
     @BeforeEach
     void before() {
-        myStPayloadBuilder = new SubscriptionTopicPayloadBuilder(ourFhirContext);
+        myStPayloadBuilder = new SubscriptionTopicPayloadBuilder(ourFhirContext, null, null, null);
         myEncounter = new Encounter();
         myEncounter.setId("Encounter/1");
         myCanonicalSubscription = new CanonicalSubscription();
