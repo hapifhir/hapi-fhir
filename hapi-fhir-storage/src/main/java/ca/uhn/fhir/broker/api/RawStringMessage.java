9e01444735cfe4eda8c80294b3add1603a9642d0
Ken Stevens <ken@smiledigitalhealth.com>
2025-04-28 14:58:33 -0400

    rename RawMessage to RawStringMessage

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/broker/api/RawStringMessage.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/broker/api/RawStringMessage.java
@@ -29,12 +29,12 @@ import java.util.Map;
  * is available as a string. There are no constraints on the contents of the String. It could be an HL7v2 message,
  * JSON, XML, CSV, etc.
  */
-public class RawMessage extends GenericMessage<String> implements IMessage<String> {
-	public RawMessage(String thePayload) {
+public class RawStringMessage extends GenericMessage<String> implements IMessage<String> {
+	public RawStringMessage(String thePayload) {
 		super(thePayload);
 	}
 
-	public RawMessage(String thePayload, Map<String, Object> theHeaders) {
+	public RawStringMessage(String thePayload, Map<String, Object> theHeaders) {
 		super(thePayload, theHeaders);
 	}
 }
