7801a3167f87b6becccc1ede2db4e55d53e9a76b
Tadgh <garygrantgraham@gmail.com>
2025-05-12 12:00:55 -0700

    Cleanup on the imports

--- hapi-fhir-jpa/src/main/java/ca/uhn/fhir/jpa/model/dialect/HapiFhirSQLServerDialect.java
+++ hapi-fhir-jpa/src/main/java/ca/uhn/fhir/jpa/model/dialect/HapiFhirSQLServerDialect.java
@@ -20,7 +20,6 @@
 package ca.uhn.fhir.jpa.model.dialect;
 
 import ca.uhn.fhir.jpa.migrate.DriverTypeEnum;
-import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
 
