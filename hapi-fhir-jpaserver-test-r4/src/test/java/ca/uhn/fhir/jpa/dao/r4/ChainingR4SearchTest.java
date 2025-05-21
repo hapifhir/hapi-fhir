fb9db6f21588c146f9fd9fd8a2fea49677d19612
Abayomi Adebowale <abayomi.adebowale@smiledigitalhealth.com>
2025-05-21 10:05:37 -0400

    edit

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/ChainingR4SearchTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/ChainingR4SearchTest.java
@@ -16,6 +16,7 @@ import org.hl7.fhir.r4.model.Device;
 import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.Group;
 import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Location;
 import org.hl7.fhir.r4.model.MessageHeader;
 import org.hl7.fhir.r4.model.Meta;
@@ -139,31 +140,19 @@ public class ChainingR4SearchTest extends BaseJpaR4Test {
 		Meta meta = new Meta();
 		meta.addTag("http://bluecrossnc.com/fhir/lob", "test", "the display");
 		patient.setMeta(meta);
-
-		//Given: A group with a tag
-		Group group = new Group();
-		Meta metaForGroup = new Meta();
-		metaForGroup.addTag("http://deez/nuts/fhir", "mynutz", "the display");
-		group.setActual(true);
-		group.setMeta(meta);
-
 		IIdType idType = myPatientDao.create(patient, mySrd).getId();
+
 		//Given: An encounter that references the patient
 		Encounter encounter =  buildResource("Encounter", withReference("subject", idType));
-
-		IIdType groupIdType =  myGroupDao.create(group, mySrd).getId();
-		//Given: An encounter that references the group
-		Encounter secondEncounter = buildResource("Encounter", withReference("subject", groupIdType));
-
 		myEncounterDao.create(encounter, mySrd);
-		myEncounterDao.create(secondEncounter, mySrd);
-		String url = "/Encounter?subject._tag=http://bluecrossnc.com/fhir/lob|test,http://deez/nuts/fhir|mynutz";
+		String url = "/Encounter?subject._tag=http://bluecrossnc.com/fhir/lob|test";
 
 		// execute
 		List<IBaseResource> encounters = myTestDaoSearch.searchForResources(url);
 
 		//Verify: Chained search with _tag returns results
-		assertEquals(2, encounters.size());
+		assertEquals(1, encounters.size());
+
 	}
 
 	@Test
