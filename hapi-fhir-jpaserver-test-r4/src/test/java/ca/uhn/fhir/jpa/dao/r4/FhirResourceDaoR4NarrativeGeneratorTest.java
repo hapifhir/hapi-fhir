bdadb6b83814bb3f3522b6b3b48d73860387adf2
yalingpei <yaling.pei@smiledigitalhealth.com>
2025-05-29 18:51:34 -0700

    [7003] update unit test

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4NarrativeGeneratorTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4NarrativeGeneratorTest.java
@@ -38,8 +38,8 @@ public class FhirResourceDaoR4NarrativeGeneratorTest extends BaseJpaR4Test {
 
 		// execute
 		IBaseResource resource = myPatientDao.create(patient).getResource();
-		String output = myFhirContext.newXmlParser().setPrettyPrint(true).encodeResourceToString(resource);
-		ourLog.info("Output:\n{}", output);
+		String output = myFhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(resource);
+		ourLog.debug("Output:\n{}", output);
 
 		// verify
 		assertThat(output).doesNotContain("<text>");
@@ -49,58 +49,23 @@ public class FhirResourceDaoR4NarrativeGeneratorTest extends BaseJpaR4Test {
 	public void testNarrativeGenerator_whenConfiguredWithFhirContext_resourceCreatedContainsNarrative() {
 		// setup
 		String expectedNarrative = """
-			   <text>
-			      <status value="generated"/>
-			      <div xmlns="http://www.w3.org/1999/xhtml">
-			         <div class="hapiHeaderText">May Flower\s
-			            <b>CHARM </b>
-			         </div>
-			         <table class="hapiPropertyTable">
-			            <tbody>
-			               <tr>
-			                  <td>Identifier</td>
-			                  <td>121212</td>
-			               </tr>
-			               <tr>
-			                  <td>Address</td>
-			                  <td>
-			                     <span>2002 Test Ave. </span>
-			                     <br/>
-			                     <span>Unit 1 </span>
-			                     <br/>
-			                     <span>Sunny Town </span>
-			                     <span>BC </span>
-			                     <span>Canada </span>
-			                  </td>
-			               </tr>
-			               <tr>
-			                  <td>Date of birth</td>
-			                  <td>
-			                     <span>30 April 1990</span>
-			                  </td>
-			               </tr>
-			            </tbody>
-			         </table>
-			      </div>
-			   </text>
-			""";
-
+			"text": {
+			    "status": "generated",
+			    "div": "<div xmlns=\\"http://www.w3.org/1999/xhtml\\"><div class=\\"hapiHeaderText\\">May Flower <b>CHARM </b></div><table class=\\"hapiPropertyTable\\"><tbody><tr><td>Date of birth</td><td><span>30 April 1990</span></td></tr></tbody></table></div>"
+			  }""";
 		CustomThymeleafNarrativeGenerator generator = new CustomThymeleafNarrativeGenerator("classpath:ca/uhn/fhir/narrative/narratives.properties");
 		myFhirContext.setNarrativeGenerator(generator);
 
 		Patient patient = new Patient();
-		patient.addIdentifier().setSystem("urn:test").setValue("121212");
 		patient.addName().setFamily("Charm").addGiven("May").addGiven("Flower");
-		patient.addAddress()
-			.addLine("2002 Test Ave.").addLine("Unit 1")
-			.setCity("Sunny Town").setState("BC").setCountry("Canada");
 		patient.setBirthDate(fromLocalDate(LocalDate.of(1990, Month.APRIL, 30)));
 
 		// execute
 		IBaseResource resource = myPatientDao.create(patient).getResource();
-		String output = myFhirContext.newXmlParser().setPrettyPrint(true).encodeResourceToString(resource);
+		String output = myFhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(resource);
 		ourLog.debug(output);
 
+		// verify
 		assertThat(output).contains(expectedNarrative);
 	}
 }
