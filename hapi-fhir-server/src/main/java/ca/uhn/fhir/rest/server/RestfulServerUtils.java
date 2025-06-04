d538fe1329ad3503e502e2aaedafb38df6923596
James Agnew <james@Jamess-MacBook-Pro.local>
2025-06-04 14:58:17 +0200

    Spotless formatting

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/RestfulServerUtils.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/RestfulServerUtils.java
@@ -78,7 +78,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
