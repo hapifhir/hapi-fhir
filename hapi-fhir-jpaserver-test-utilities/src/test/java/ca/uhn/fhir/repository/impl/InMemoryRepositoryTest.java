daa231d12d8cad883e5121fdef0e348b67ce6765
Michael Buckley <michaelabuckley@gmail.com>
2025-07-07 16:22:52 -0400

    InMemory test

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/repository/impl/InMemoryRepositoryTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/repository/impl/InMemoryRepositoryTest.java
@@ -1,6 +1,7 @@
-package ca.uhn.fhir.repository;
+package ca.uhn.fhir.repository.impl;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.repository.IRepositoryTest;
 
 public class InMemoryRepositoryTest implements IRepositoryTest {
 	FhirContext myFhirContext = FhirContext.forR4();
