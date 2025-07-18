eeedb8ec5ee500f12e3a5b7c38f0a4e32185f5d1
Michael Buckley <michaelabuckley@gmail.com>
2025-07-18 16:23:50 -0400

    Document limitations and add some transaction processing tests.

--- hapi-fhir-repositories/src/main/java/ca/uhn/fhir/repository/impl/NaiveRepositoryTransactionProcessor.java
+++ hapi-fhir-repositories/src/main/java/ca/uhn/fhir/repository/impl/NaiveRepositoryTransactionProcessor.java
@@ -6,6 +6,7 @@ import ca.uhn.fhir.repository.IRepository;
 import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.server.exceptions.NotImplementedOperationException;
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import ca.uhn.fhir.util.BundleBuilder;
 import ca.uhn.fhir.util.BundleUtil;
 import ca.uhn.fhir.util.bundle.BundleEntryParts;
@@ -14,7 +15,11 @@ import jakarta.annotation.Nonnull;
 import org.apache.commons.lang3.Validate;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseParameters;
+import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.instance.model.api.IPrimitiveType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 import java.util.List;
@@ -24,8 +29,16 @@ import static java.util.Objects.requireNonNull;
 
 /**
  * Implements a naive transaction processor for repositories in terms of crud primitives.
+ * Limitations:
+ * <ul>
+ *     <li>Does not support GET requests.</li>
+ *     <li>Does not support extended operations.</li>
+ *     <li>Does not support conditional update or create.</li>
+ * </ul>
  */
 public class NaiveRepositoryTransactionProcessor {
+	static final Logger ourLog = LoggerFactory.getLogger(NaiveRepositoryTransactionProcessor.class);
+
 	// SOMEDAY implement GET, PATCH, and other methods as needed.
 	// SOMEDAY order entries
 	// SOMEDAY distinguish batch and transaction processing.  Not really a problem yet since we can't fail any of our
@@ -59,9 +72,13 @@ public class NaiveRepositoryTransactionProcessor {
 		// SOMEDAY: sort the entries by method and dependency order to match the spec
 		// SOMEDAY: validate there aren't any unsupported entries before we start to avoid partial-evaluation
 		List<BundleEntryParts> entries = BundleUtil.toListOfEntries(myRepository.fhirContext(), theTransactionBundle);
+		if (entries.isEmpty()) {
+			ourLog.warn("Processing empty bundle, returning empty response");
+		}
 		for (BundleEntryParts e : entries) {
 			BundleResponseEntryParts responseEntry =
 					switch (e.getMethod()) {
+						case PATCH -> processPatch(e, now);
 						case POST -> processPost(e, now);
 						case PUT -> processPut(e, now);
 						case DELETE -> processDelete(e, now);
@@ -74,10 +91,47 @@ public class NaiveRepositoryTransactionProcessor {
 		return bundleBuilder.getBundleTyped();
 	}
 
+	private BundleResponseEntryParts processPatch(
+			BundleEntryParts theBundleEntryParts, IPrimitiveType<Date> theInstant) {
+		MethodOutcome methodOutcome = myRepository.patch(
+				getIdFromUrl(theBundleEntryParts), (IBaseParameters) theBundleEntryParts.getResource());
+		String location = null;
+		if (methodOutcome.getResponseStatusCode() == Constants.STATUS_HTTP_201_CREATED) {
+			location = methodOutcome.getId().getValue();
+		}
+
+		return new BundleResponseEntryParts(
+				theBundleEntryParts.getFullUrl(),
+				methodOutcome.getResource(),
+				statusCodeToStatusLine(methodOutcome.getResponseStatusCode()),
+				location,
+				null,
+				theInstant,
+				methodOutcome.getOperationOutcome());
+	}
+
+	private IIdType getIdFromUrl(BundleEntryParts theBundleEntryParts) {
+		validateNoLogicalUrl(theBundleEntryParts);
+		return myRepository.fhirContext().getVersion().newIdType(theBundleEntryParts.getUrl());
+	}
+
+	@Nonnull
+	private static void validateNoLogicalUrl(BundleEntryParts theBundleEntryParts) {
+		String url = theBundleEntryParts.getUrl();
+		Validate.notNull(url, "request url must not be null: entry %s", theBundleEntryParts.getFullUrl());
+		if (url.contains("?")) {
+			throw new UnprocessableEntityException("Conditional urls are not supported");
+		}
+	}
+
 	@Nonnull
 	protected BundleResponseEntryParts processPost(
 			BundleEntryParts theBundleEntryParts, IPrimitiveType<Date> theInstant) {
 		// we assume POST is always "create", not an operation invocation
+		if (theBundleEntryParts.getConditionalUrl() != null) {
+			throw new UnprocessableEntityException("Conditional create urls are not supported");
+		}
+
 		var responseOutcome = myRepository.create(theBundleEntryParts.getResource());
 		var location = responseOutcome.getId().getValue();
 
@@ -94,6 +148,7 @@ public class NaiveRepositoryTransactionProcessor {
 	@Nonnull
 	protected BundleResponseEntryParts processPut(
 			BundleEntryParts theBundleEntryParts, IPrimitiveType<Date> theInstant) {
+		validateNoLogicalUrl(theBundleEntryParts);
 		MethodOutcome methodOutcome = myRepository.update(theBundleEntryParts.getResource());
 		String location = null;
 		if (methodOutcome.getResponseStatusCode() == Constants.STATUS_HTTP_201_CREATED) {
