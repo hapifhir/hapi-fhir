35cc4d0ffccbf29a3862d234f317249a232b975c
Michael Buckley <michaelabuckley@gmail.com>
2025-07-22 14:45:11 -0500

    Add a repository loader for :exp-kalm-filesystem: repositories (#7150)

---
+++ hapi-fhir-repositories/src/main/java/ca/uhn/fhir/repository/impl/kalm/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Loader support for the IgRepository from the Clinical Reasoning group.
+ * This is a placeholder until this can move to cqf-fhir-utility or the code is up-streamed here to hapi-fhir-repositories.
+ */
+package ca.uhn.fhir.repository.impl.kalm;
