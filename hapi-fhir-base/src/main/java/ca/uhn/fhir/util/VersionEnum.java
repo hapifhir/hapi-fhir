00320595fc7941f70e1887d0c03ecd562e6fbb8c
jdar8 <69840459+jdar8@users.noreply.github.com>
2025-07-24 00:18:09 -0700

    Bump to 8_5_0 (#7151)
    * version bump, enum value
    
    * changelog folder
    
    ---------
    
    Co-authored-by: jdar <justin.dar@smiledigitalhealth.com>

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
@@ -180,7 +180,9 @@ public enum VersionEnum {
 	V8_2_1,
 	V8_2_2,
 	V8_3_0,
-	V8_4_0;
+	V8_4_0,
+	V8_5_0,
+	V8_6_0;
 
 	public static VersionEnum latestVersion() {
 		VersionEnum[] values = VersionEnum.values();
