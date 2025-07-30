d0ad8a721ba5b7c015cf120d4f6036f561d4f4f6
Emre Dincturk <emre.dincturk@smilecdr.com>
2025-07-30 13:03:22 -0400

    addressed code review comments

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/replacereferences/ReplaceReferencesPatchBundleSvc.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/replacereferences/ReplaceReferencesPatchBundleSvc.java
@@ -28,6 +28,7 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.util.BundleBuilder;
 import ca.uhn.fhir.util.ResourceReferenceInfo;
 import jakarta.annotation.Nonnull;
+import org.hl7.fhir.instance.model.api.IBaseReference;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Bundle;
@@ -112,14 +113,13 @@ public class ReplaceReferencesPatchBundleSvc {
 
 	private static boolean matches(ResourceReferenceInfo refInfo, IIdType theSourceId) {
 
-		Reference ref = (Reference) refInfo.getResourceReference();
-		if (!ref.hasReferenceElement()) {
+		IBaseReference iBaseRef = refInfo.getResourceReference();
+		if (iBaseRef == null || (iBaseRef instanceof Reference ref && !ref.hasReferenceElement())) {
 			// the reference doesn't have a reference element, it is probably just a logical reference (using an
-			// identifier).
-			// so this is not a match.
+			// identifier). so this is not a match.
 			return false;
 		}
-		return ref.getReferenceElement().toUnqualified().getValueAsString().equals(theSourceId.getValueAsString());
+		return iBaseRef.getReferenceElement().toUnqualified().getValueAsString().equals(theSourceId.getValueAsString());
 	}
 
 	private String getFhirPathForPatch(IBaseResource theReferencingResource, ResourceReferenceInfo theRefInfo) {
