5d4b47d12c0dd9dc183cd7bbf8e024875753e8a1
James Agnew <jamesagnew@gmail.com>
2025-09-12 08:23:37 -0400

    fix(7248): IdHelperService.resolveResourceIdentities fails with PreconditionFailedException on duplicate id inputs (#7254)
    * fix(7248): IdHelperService.resolveResourceIdentities fails with PreconditionFailedException on duplicate id inputs
    
    * Use hashset instead of arraylist with search
    
    * Test fix
    
    ---------
    
    Co-authored-by: Tue Toft Carr Nørgård <ttn@trifork.com>

--- hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/registry/ReadOnlySearchParamCache.java
+++ hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/registry/ReadOnlySearchParamCache.java
@@ -80,6 +80,7 @@ public class ReadOnlySearchParamCache {
 		return myResourceNameToSpNameToSp.size();
 	}
 
+	@Nullable
 	public RuntimeSearchParam getByUrl(String theUrl) {
 		return myUrlToParam.get(theUrl);
 	}
