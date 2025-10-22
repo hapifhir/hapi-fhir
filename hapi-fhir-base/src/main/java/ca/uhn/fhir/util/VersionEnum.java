51a631ff60f141f0109ee172dd0f98c8dd891dd5
Gary Graham <gary@tadgh.io>
2025-10-22 08:08:38 -0700

    Bump for modern versions

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/util/VersionEnum.java
@@ -178,7 +178,9 @@ public enum VersionEnum {
 	V8_1_0,
 	V8_2_0,
 	V8_2_1,
-	V8_2_2;
+	V8_2_2,
+	V8_4_0,
+	V8_6_0;
 
 	public static VersionEnum latestVersion() {
 		VersionEnum[] values = VersionEnum.values();
