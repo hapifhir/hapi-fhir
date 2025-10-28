2c7934fc895b5379e73a7854eff65b7f0bd2f596
yalingpei <yaling.pei@smiledigitalhealth.com>
2025-10-28 09:41:54 -0700

    [7332] add tests and changelog

--- hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/provider/MdmProviderCrossPartitionR4Test.java
+++ hapi-fhir-jpaserver-mdm/src/test/java/ca/uhn/fhir/jpa/mdm/provider/MdmProviderCrossPartitionR4Test.java
@@ -1,15 +1,23 @@
 package ca.uhn.fhir.jpa.mdm.provider;
 
 import ca.uhn.fhir.interceptor.model.RequestPartitionId;
+import ca.uhn.fhir.jpa.entity.MdmLink;
 import ca.uhn.fhir.jpa.entity.PartitionEntity;
 import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
 import ca.uhn.fhir.mdm.api.IMdmSettings;
+import ca.uhn.fhir.mdm.model.MdmTransactionContext;
 import ca.uhn.fhir.mdm.util.MdmResourceUtil;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
+import ca.uhn.fhir.rest.server.interceptor.partition.RequestTenantPartitionInterceptor;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.UnsignedIntType;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.DecimalType;
+import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.StringType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -87,5 +95,66 @@ public class MdmProviderCrossPartitionR4Test extends BaseProviderR4Test{
 		assertEquals(searchResult.getAllResources().size(), 1);
 
 		assertTrue(MdmResourceUtil.isGoldenRecord(searchResult.getAllResources().get(0)));
+	}
+
+	@Test
+	void testQueryMdmLink_goldenResourceInSamePartition() {
+		myMdmSettings.setSearchAllPartitionForMatch(false);
+		myMdmSettings.setGoldenResourcePartitionName(StringUtils.EMPTY);
+
+		Patient jane = createPatientOnPartition(buildJanePatient(), RequestPartitionId.fromPartitionId(1));
+		MdmTransactionContext mdmContext =
+			myMdmMatchLinkSvc.updateMdmLinksForMdmSource(jane, createContextForCreate("Patient"));
+
+		MdmLink mdmLink = (MdmLink) mdmContext.getMdmLinks().get(0);
+		assertThat(mdmLink.getSourcePersistenceId().getPartitionId()).isEqualTo(1);
+		assertThat(mdmLink.getGoldenResourcePersistenceId().getPartitionId()).isEqualTo(1);
+
+		RequestTenantPartitionInterceptor interceptor = new RequestTenantPartitionInterceptor();
+		interceptor.setPartitionSettings(myPartitionSettings);
+		myInterceptorRegistry.registerInterceptor(interceptor);
+		myRequestDetails.setTenantId(PARTITION_1);
+
+		Parameters result = (Parameters) myMdmProvider.queryLinks(null, null, null, null, new UnsignedIntType(0),
+			new UnsignedIntType(10), new StringType(), myRequestDetails, new StringType("Patient"));
+
+		assertThat(result.getParameter()).hasSize(3);
+		assertThat(result.getParameter().get(0).getName()).isEqualTo("self");
+		assertThat(result.getParameter().get(1).getName()).isEqualTo("total");
+		assertThat(((DecimalType)(result.getParameter().get(1).getValue())).getValueAsInteger()).isEqualTo(1);
+		assertThat(result.getParameter().get(2).getName()).isEqualTo("link");
+		assertThat(result.getParameter().get(2).getPart()).hasSize(9);
+
+		myInterceptorRegistry.unregisterInterceptor(interceptor);
+	}
+
+	@Test
+	void testQueryMdmLink_goldenResourceInDifferentPartitions() {
+		myMdmSettings.setSearchAllPartitionForMatch(true);
+		myMdmSettings.setGoldenResourcePartitionName(PARTITION_GOLDEN_RESOURCE);
+
+		Patient jane = createPatientOnPartition(buildJanePatient(), RequestPartitionId.fromPartitionId(1));
+		MdmTransactionContext mdmContext =
+			myMdmMatchLinkSvc.updateMdmLinksForMdmSource(jane, createContextForCreate("Patient"));
+
+		MdmLink mdmLink = (MdmLink) mdmContext.getMdmLinks().get(0);
+		assertThat(mdmLink.getSourcePersistenceId().getPartitionId()).isEqualTo(1);
+		assertThat(mdmLink.getGoldenResourcePersistenceId().getPartitionId()).isEqualTo(3);
+
+		RequestTenantPartitionInterceptor interceptor = new RequestTenantPartitionInterceptor();
+		interceptor.setPartitionSettings(myPartitionSettings);
+		myInterceptorRegistry.registerInterceptor(interceptor);
+		myRequestDetails.setTenantId(PARTITION_1);
+
+		Parameters result = (Parameters) myMdmProvider.queryLinks(null, null, null, null, new UnsignedIntType(0),
+			new UnsignedIntType(10), new StringType(), myRequestDetails, new StringType("Patient"));
+		assertThat(result.getParameter()).hasSize(3);
+		assertThat(result.getParameter().get(0).getName()).isEqualTo("self");
+		assertThat(result.getParameter().get(1).getName()).isEqualTo("total");
+		assertThat(((DecimalType)(result.getParameter().get(1).getValue())).getValueAsInteger()).isEqualTo(1);
+		assertThat(result.getParameter().get(2).getName()).isEqualTo("link");
+		assertThat(result.getParameter().get(2).getPart()).hasSize(9);
+
+		myInterceptorRegistry.unregisterInterceptor(interceptor);
 	}
 }
