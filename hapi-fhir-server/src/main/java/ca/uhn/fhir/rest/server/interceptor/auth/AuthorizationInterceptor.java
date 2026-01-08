ac4b1591b9466087aff0a6e6b48e3be41be84055
Gary Graham <gary@tadgh.io>
2026-01-08 12:42:41 -0800

    wip

--- hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/auth/AuthorizationInterceptor.java
+++ hapi-fhir-server/src/main/java/ca/uhn/fhir/rest/server/interceptor/auth/AuthorizationInterceptor.java
@@ -427,7 +427,7 @@ public class AuthorizationInterceptor implements IRuleApplier {
 	@Hook(Pointcut.STORAGE_PRESHOW_RESOURCES)
 	public void hookPreShow(
 			RequestDetails theRequestDetails, IPreResourceShowDetails theDetails, Pointcut thePointcut) {
-		for (IBaseResource resource: theDetails.getAllResources()) {
+		for (IBaseResource resource : theDetails.getAllResources()) {
 			checkOutgoingResourceAndFailIfDeny(theRequestDetails, resource, thePointcut);
 		}
 	}
