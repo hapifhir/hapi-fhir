1e68a7cff9268a663c7bb289ade39bf9f49b2036
Ádám Z. Kövér <adamzkover@gmail.com>
2026-01-15 14:19:50 +0100

    Add title and usageRequirements fields to the CDS Hooks discovery resopne only if they are not empty

--- hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/svc/CdsHooksContextBooter.java
+++ hapi-fhir-server-cds-hooks/src/main/java/ca/uhn/hapi/fhir/cdshooks/svc/CdsHooksContextBooter.java
@@ -85,8 +85,12 @@ public class CdsHooksContextBooter {
 				cdsServiceJson.setId(annotation.value());
 				cdsServiceJson.setHook(annotation.hook());
 				cdsServiceJson.setDescription(annotation.description());
-				cdsServiceJson.setUsageRequirements(annotation.usageRequirements());
-				cdsServiceJson.setTitle(annotation.title());
+				if (StringUtils.isNotEmpty(annotation.usageRequirements())) {
+					cdsServiceJson.setUsageRequirements(annotation.usageRequirements());
+				}
+				if (StringUtils.isNotEmpty(annotation.title())) {
+					cdsServiceJson.setTitle(annotation.title());
+				}
 				cdsServiceJson.setExtension(serializeExtensions(annotation.extension(), annotation.extensionClass()));
 				cdsServiceJson.setExtensionClass(annotation.extensionClass());
 				for (CdsServicePrefetch prefetch : annotation.prefetch()) {
