dfd0c4555fffda60e455479999157504c89372ae
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2026-02-04 15:33:38 -0500

    Add comment to not remove this change and tweak warning log.

--- hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/extractor/SearchParamExtractorService.java
+++ hapi-fhir-jpaserver-searchparam/src/main/java/ca/uhn/fhir/jpa/searchparam/extractor/SearchParamExtractorService.java
@@ -851,9 +851,10 @@ public class SearchParamExtractorService implements ISearchParamExtractorSvc {
 			boolean hasMatchingResourceId = false;
 			Optional<String> idPartOpt = targetResourceIdMap.get(resourceLink.getTargetResourcePk());
 
+			// DON'T REMOVE THIS CHECK:  In some circumstances, clinical-reasoning code will trigger a null value here:
 			if (idPartOpt == null) {
 				ourLog.warn(
-						"Cannot find id part in the target resource ID Map: {}", resourceLink.getTargetResourcePk());
+						"Cannot find id: {} in the target resource ID Map", resourceLink.getTargetResourcePk());
 				idPartOpt = Optional.empty();
 			}
 
