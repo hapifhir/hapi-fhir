4a2bb5acc260c93ad21910cf37e9efd33b8b4764
jmarchionatto <juan.r.marchio@gmail.com>
2026-03-03 15:35:41 -0500

    Fix deprecation since rel

--- hapi-fhir-server-mdm/src/main/java/ca/uhn/fhir/mdm/rules/json/MdmMatcherJson.java
+++ hapi-fhir-server-mdm/src/main/java/ca/uhn/fhir/mdm/rules/json/MdmMatcherJson.java
@@ -49,7 +49,7 @@ public class MdmMatcherJson implements IModelJson {
 	 * Convenience overload for backward compatibility with code that passes a {@link MatchTypeEnum}.
 	 * @deprecated Use {@link #setAlgorithm(String)} instead.
 	 */
-	@Deprecated(since = "8.10", forRemoval = true)
+	@Deprecated(since = "8_10", forRemoval = true)
 	public MdmMatcherJson setAlgorithm(MatchTypeEnum theAlgorithm) {
 		myAlgorithm = theAlgorithm.name();
 		return this;
