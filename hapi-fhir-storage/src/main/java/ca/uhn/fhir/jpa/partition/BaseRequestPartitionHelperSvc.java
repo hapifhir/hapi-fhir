f43b681cdcf2dc3d54ed4129c869a9d22a014ae4
nathaniel.doef <nathaniel.doef@smilecdr.com>
2026-03-04 15:10:14 -0500

    add better fix

--- hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/partition/BaseRequestPartitionHelperSvc.java
+++ hapi-fhir-storage/src/main/java/ca/uhn/fhir/jpa/partition/BaseRequestPartitionHelperSvc.java
@@ -272,7 +272,7 @@ public abstract class BaseRequestPartitionHelperSvc implements IRequestPartition
 		if (theRequest.getRequestPartitionId() != null) {
 			return theRequest.getRequestPartitionId();
 		}
-		if (StringUtils.isNotBlank(theRequest.getTenantId())) {
+		if (theRequest.getTenantId() != null) {
 			// TODO: JA2 we should not be inferring the partition name from the tenant name
 			return RequestPartitionId.fromPartitionName(theRequest.getTenantId());
 		} else {
@@ -353,7 +353,7 @@ public abstract class BaseRequestPartitionHelperSvc implements IRequestPartition
 	}
 
 	private boolean systemRequestHasExplicitPartition(@Nonnull SystemRequestDetails theRequest) {
-		return theRequest.getRequestPartitionId() != null || StringUtils.isNotBlank(theRequest.getTenantId());
+		return theRequest.getRequestPartitionId() != null || theRequest.getTenantId() != null;
 	}
 
 	@Nonnull
@@ -384,12 +384,6 @@ public abstract class BaseRequestPartitionHelperSvc implements IRequestPartition
 			} else if (retVal.hasPartitionIds()) {
 				retVal = validateAndNormalizePartitionIds(retVal);
 			}
-		}
-
-		// In unnamed partition mode, partition names can't be resolved to IDs.
-		// Fall back to default partition rather than passing an unresolvable partition.
-		if (myPartitionSettings.isUnnamedPartitionMode() && retVal.hasPartitionNames() && !retVal.hasPartitionIds()) {
-			retVal = myPartitionSettings.getDefaultRequestPartitionId();
 		}
 
 		// Note: It's still possible that the partition only has a date but no name/id
