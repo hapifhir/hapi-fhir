70f2a0e03e861552425d1adf47ecb960ccc2d289
Martha <marthamitran@gmail.com>
2026-03-24 15:19:10 -0700

    Remove MDM change and keep that separate pull request. Adjust changelog.

--- hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/config/MdmConsumerConfig.java
+++ hapi-fhir-jpaserver-mdm/src/main/java/ca/uhn/fhir/jpa/mdm/config/MdmConsumerConfig.java
@@ -98,7 +98,7 @@ public class MdmConsumerConfig {
 	}
 
 	@Bean
-	public MdmMessageListener mdmMessageHandler() {
+	MdmMessageListener mdmMessageHandler() {
 		return new MdmMessageListener();
 	}
 
