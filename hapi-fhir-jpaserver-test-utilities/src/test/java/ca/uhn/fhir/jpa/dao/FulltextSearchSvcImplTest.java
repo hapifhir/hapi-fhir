70841c6bba8d050c9992e684d045f0299fc1b440
Jorge Pinilla <jorge.pinilla@smiledigitalhealth.com>
2026-04-02 18:52:24 -0400

    updating unit tests

--- hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/dao/FulltextSearchSvcImplTest.java
+++ hapi-fhir-jpaserver-test-utilities/src/test/java/ca/uhn/fhir/jpa/dao/FulltextSearchSvcImplTest.java
@@ -7,27 +7,20 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 import ca.uhn.fhir.rest.param.StringParam;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
-import ca.uhn.fhir.rest.server.util.ISearchParamRegistry;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestHSearchAddInConfig.DefaultLuceneHeap.class})
 class FulltextSearchSvcImplTest extends BaseJpaR4Test {
-
-	@Autowired
-	private ISearchParamRegistry mySearchParamRegistry;
 
 	@Autowired
 	private IFulltextSearchSvc myFulltextSearchSvc;
