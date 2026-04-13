06603d9cc8c7b038abea5b19d6c7603ea6b94577
Michael Buckley <michaelabuckley@gmail.com>
2026-04-13 17:01:33 -0400

    sketch out DialectOverride

--- hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamTokenCommon.java
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceIndexedSearchParamTokenCommon.java
@@ -19,15 +19,21 @@
  */
 package ca.uhn.fhir.jpa.model.entity;
 
+import ca.uhn.fhir.jpa.model.dialect.HapiFhirOracleDialect;
+import ca.uhn.fhir.jpa.model.dialect.HapiFhirPostgresDialect;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.DialectOverride;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.SQLInsert;
 
 import java.io.Serializable;
 
 @Entity
+@Immutable
 @Table(
 		name = ResourceIndexedSearchParamTokenCommon.HFJ_SPIDX2_TOKEN_COMMON,
 		indexes = {
@@ -35,24 +41,44 @@ import java.io.Serializable;
 			@Index(name = "IDX_SP_TOKEN_COMMON_VALUE", columnList = "HASH_VALUE,HASH_SYS_AND_VALUE"),
 			@Index(name = "IDX_SP_TOKEN_IDENTITY", columnList = "HASH_IDENTITY,HASH_SYS_AND_VALUE")
 		})
+@DialectOverride.SQLInserts({
+	@DialectOverride.SQLInsert(
+		dialect = HapiFhirPostgresDialect.class,
+		override = @SQLInsert(sql = """
+						insert into HFJ_SPIDX2_TOKEN_COMMON (HASH_SYS_AND_VALUE, SYSTEM_ID, SP_VALUE, HASH_IDENTITY, HASH_VALUE) 
+						values(?,?,?,?,?) 
+						on conflict do nothing
+						""")),
+	@DialectOverride.SQLInsert(
+		dialect = HapiFhirOracleDialect.class,
+		override = @SQLInsert(sql = """
+						MERGE INTO HFJ_SPIDX2_TOKEN_COMMON target
+						USING (SELECT ? AS HASH_SYS_AND_VALUE, ? AS SYSTEM_ID, ? AS SP_VALUE, ? AS HASH_IDENTITY, ? AS HASH_VALUE FROM DUAL) src
+						ON (target.HASH_SYS_AND_VALUE = src.HASH_SYS_AND_VALUE)
+						WHEN NOT MATCHED THEN
+						  INSERT (HASH_SYS_AND_VALUE, SYSTEM_ID, SP_VALUE, HASH_IDENTITY, HASH_VALUE)
+						  VALUES (src.HASH_SYS_AND_VALUE, src.SYSTEM_ID, src.SP_VALUE, src.HASH_IDENTITY, src.HASH_VALUE)
+						""")),
+
+})
 public class ResourceIndexedSearchParamTokenCommon implements Serializable {
 	public static final String HFJ_SPIDX2_TOKEN_COMMON = "HFJ_SPIDX2_TOKEN_COMMON";
 	public static final int MAX_LENGTH = 200;
 
 	@Id
-	@Column(name = "HASH_SYS_AND_VALUE", nullable = false)
+	@Column(name = "HASH_SYS_AND_VALUE", nullable = false, updatable = false)
 	private long myHashSystemAndValue;
 
-	@Column(name = "SYSTEM_ID", nullable = false)
+	@Column(name = "SYSTEM_ID", nullable = false, updatable = false)
 	private long mySystemId;
 
-	@Column(name = "SP_VALUE", length = MAX_LENGTH, nullable = true)
+	@Column(name = "SP_VALUE", length = MAX_LENGTH, nullable = false, updatable = false)
 	private String myValue;
 
-	@Column(name = "HASH_IDENTITY", nullable = false)
+	@Column(name = "HASH_IDENTITY", nullable = false, updatable = false)
 	private long myHashIdentity;
 
-	@Column(name = "HASH_VALUE", nullable = false)
+	@Column(name = "HASH_VALUE", nullable = false, updatable = false)
 	private long myHashValue;
 
 	public ResourceIndexedSearchParamTokenCommon() {}
