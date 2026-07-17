efd88ebfa33abcd8bfd112b74fefbdf99fd82be9
James Agnew <jamesagnew@gmail.com>
2026-07-17 17:01:54 -0400

    Add proper error codes

--- hapi-fhir-base/src/main/java/ca/uhn/fhir/parser/json/jackson/JacksonWriter.java
+++ hapi-fhir-base/src/main/java/ca/uhn/fhir/parser/json/jackson/JacksonWriter.java
@@ -128,27 +128,27 @@ public class JacksonWriter extends BaseJsonLikeWriter {
 
 			@Override
 			public ArrayTreeNode createArrayNode() {
-				throw new UnsupportedOperationException(Msg.code(9981) + "Tree-node creation is not supported by "
+				throw new UnsupportedOperationException(Msg.code(2997) + "Tree-node creation is not supported by "
 						+ JacksonWriter.class.getSimpleName());
 			}
 
 			@Override
 			public ObjectTreeNode createObjectNode() {
-				throw new UnsupportedOperationException(Msg.code(9982) + "Tree-node creation is not supported by "
+				throw new UnsupportedOperationException(Msg.code(2998) + "Tree-node creation is not supported by "
 						+ JacksonWriter.class.getSimpleName());
 			}
 
 			@Override
 			public void writeValue(JsonGenerator gen, Object value) {
 				throw new UnsupportedOperationException(
-						Msg.code(9983) + JacksonWriter.class.getSimpleName()
+						Msg.code(2999) + JacksonWriter.class.getSimpleName()
 								+ " writes values via type-specific JsonGenerator methods and does not support writePOJO(..)/writeValue(..)");
 			}
 
 			@Override
 			public void writeTree(JsonGenerator gen, TreeNode tree) {
 				throw new UnsupportedOperationException(
-						Msg.code(9984) + "Tree writing is not supported by " + JacksonWriter.class.getSimpleName());
+						Msg.code(3000) + "Tree writing is not supported by " + JacksonWriter.class.getSimpleName());
 			}
 		};
 
