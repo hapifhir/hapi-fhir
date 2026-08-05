f01cdc437ffe88cfd2456596ea5e578ec369dad5
James Agnew <jamesagnew@gmail.com>
2026-08-05 19:08:34 -0400

    Make migrator check optional

--- hapi-fhir-cli/hapi-fhir-cli-api/src/main/java/ca/uhn/fhir/cli/BaseFlywayMigrateDatabaseCommand.java
+++ hapi-fhir-cli/hapi-fhir-cli-api/src/main/java/ca/uhn/fhir/cli/BaseFlywayMigrateDatabaseCommand.java
@@ -149,6 +149,7 @@ public abstract class BaseFlywayMigrateDatabaseCommand<T extends Enum> extends B
 			HapiMigrator migrator =
 					new HapiMigrator(myMigrationTableName, connectionProperties.getDataSource(), driverType);
 
+			migrator.setEnableBaselineCheck(true);
 			migrator.createMigrationTableIfRequired();
 			migrator.setDryRun(dryRun);
 			migrator.setRunHeavyweightSkippableTasks(runHeavyweight);
