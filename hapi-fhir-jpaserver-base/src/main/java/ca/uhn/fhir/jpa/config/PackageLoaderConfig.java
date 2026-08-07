455c76c9e829c5b2438ec0d58f664913cbcc950f
leifstawnyczy <leif.stawnyczy@smiledigitalhealth.com>
2026-08-07 14:51:42 -0400

    adding whitelist support on packageloadersvc

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/PackageLoaderConfig.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/PackageLoaderConfig.java
@@ -24,6 +24,7 @@ import ca.uhn.fhir.jpa.packages.loader.IPackageUrlAllowListProvider;
 import ca.uhn.fhir.jpa.packages.loader.PackageLoaderSettings;
 import ca.uhn.fhir.jpa.packages.loader.PackageLoaderSvc;
 import ca.uhn.fhir.jpa.packages.loader.PackageResourceParsingSvc;
+import ca.uhn.fhir.jpa.packages.loader.PackageUrlAllowList;
 import org.hl7.fhir.utilities.npm.PackageServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,10 +36,10 @@ public class PackageLoaderConfig {
 
 	@Bean
 	public PackageLoaderSettings loaderSettings(Optional<IPackageUrlAllowListProvider> theProvider) {
-		return theProvider
-				.map(t -> PackageLoaderSettings.restricted(
+		return new PackageLoaderSettings(theProvider
+				.map(t -> PackageUrlAllowList.of(
 						theProvider.get().getRemotePrefixes(), theProvider.get().getLocalPrefixes()))
-				.orElse(PackageLoaderSettings.unrestricted());
+				.orElse(PackageUrlAllowList.allowAll()));
 	}
 
 	@Bean
@@ -46,6 +47,8 @@ public class PackageLoaderConfig {
 		PackageLoaderSvc.initSettings(theLoaderSettings);
 		PackageLoaderSvc svc = new PackageLoaderSvc(theLoaderSettings);
 		svc.getPackageServers().clear();
+		// these servers are both https and remote
+		// so no further config is needed here
 		svc.getPackageServers().add(PackageServer.primaryServer());
 		svc.getPackageServers().add(PackageServer.secondaryServer());
 		return svc;
--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/packages/loader/PackageLoaderSettings.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/packages/loader/PackageLoaderSettings.java
@@ -1,45 +1,14 @@
 package ca.uhn.fhir.jpa.packages.loader;
 
-import com.google.common.collect.ImmutableList;
-import jakarta.annotation.Nonnull;
-
-import java.util.List;
-
 public class PackageLoaderSettings {
 
-	// allow all
-	public static final String WILDCARD = "*";
+	private final PackageUrlAllowList myPackageUrlAllowList;
 
-	public static PackageLoaderSettings restricted(
-			@Nonnull List<String> theRemotePrefixes, @Nonnull List<String> theLocalPrefixes) {
-		return new PackageLoaderSettings(theRemotePrefixes, theLocalPrefixes);
+	public PackageLoaderSettings(PackageUrlAllowList theAllowList) {
+		myPackageUrlAllowList = theAllowList;
 	}
 
-	public static PackageLoaderSettings unrestricted() {
-		return new PackageLoaderSettings(List.of(WILDCARD), List.of(WILDCARD));
-	}
-
-	/**
-	 * A list of url (prefixes) that are allowed
-	 * for packageloader.
-	 */
-	private final List<String> myRemotePrefixes;
-
-	/**
-	 * A list of prefixes allowed (for file/classpath: 'urls')
-	 */
-	private final List<String> myLocalPrefixes;
-
-	private PackageLoaderSettings(List<String> theRemotePrefixes, List<String> theLocalPrefixes) {
-		myRemotePrefixes = ImmutableList.copyOf(theRemotePrefixes);
-		myLocalPrefixes = ImmutableList.copyOf(theLocalPrefixes);
-	}
-
-	public List<String> getRemotePrefixes() {
-		return myRemotePrefixes;
-	}
-
-	public List<String> getLocalPrefixes() {
-		return myLocalPrefixes;
+	public PackageUrlAllowList getPackageUrlAllowList() {
+		return myPackageUrlAllowList;
 	}
 }
