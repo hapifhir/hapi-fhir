d837f584ce1f5f8d39fb0632a6446bcb1ccd1f14
leifstawnyczy <leif.stawnyczy@smiledigitalhealth.com>
2026-08-13 08:46:13 -0400

    adding copyright stuffs

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/packages/loader/PackageUrlAllowList.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/packages/loader/PackageUrlAllowList.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * HAPI FHIR JPA Server
+ * %%
+ * Copyright (C) 2014 - 2026 Smile CDR, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package ca.uhn.fhir.jpa.packages.loader;
 
 import ca.uhn.fhir.jpa.model.PackageUrlScheme;
@@ -188,8 +207,7 @@ public class PackageUrlAllowList {
 
 		String prefix = Strings.CS.removeEnd(theAllowed.getPath(), "/");
 		String candidate = Strings.CS.removeEnd(StringUtils.defaultIfEmpty(theCandidate.getPath(), "/"), "/");
-
-		return prefix.equals(candidate) || candidate.startsWith(prefix);
+		return prefix.equals(candidate) || candidate.startsWith(prefix + "/");
 	}
 
 	private URI parseHttpUri(String theUri) {
